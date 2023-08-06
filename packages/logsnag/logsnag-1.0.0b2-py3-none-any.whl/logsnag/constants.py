f779d3d934edc6142f1c9835e5b5adf97e490be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:34 +0000

    Add package logsnag/1.0.0b2/logsnag-1.0.0b2-py3-none-any.whl

---
+++ packages/logsnag/logsnag-1.0.0b2-py3-none-any.whl/logsnag/constants.py
@@ -0,0 +1,10 @@
+"""Constant Values"""
+
+# LogSnag's API Endpoint
+LOGSNAG_BASE_ENDPOINT = 'https://api.logsnag.com'
+
+
+class ENDPOINTS:
+    LOG = LOGSNAG_BASE_ENDPOINT + '/v1/log'
+    INSIGHT = LOGSNAG_BASE_ENDPOINT + '/v1/insight'
+    IDENTIFY = LOGSNAG_BASE_ENDPOINT + '/v1/identify'
