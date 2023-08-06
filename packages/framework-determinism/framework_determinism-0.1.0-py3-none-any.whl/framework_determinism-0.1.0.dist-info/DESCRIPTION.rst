b87f162eb503752670b53ea71a57ef0b1aaa211f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:10 +0000

    Add package framework-determinism/0.1.0/framework_determinism-0.1.0-py3-none-any.whl

---
+++ packages/framework-determinism/framework_determinism-0.1.0-py3-none-any.whl/framework_determinism-0.1.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,9 @@
+
+The `framework-determinism` PyPI distribution has been deprecated and the
+`fwd9m` package in this distribution contains no code.
+
+Please install the latest version of the
+[framework-reproducibility](https://pypi.org/project/framework-reproducibility/)
+PyPI distribution.
+
+
---
+++ packages/framework-determinism/framework_determinism-0.1.0-py3-none-any.whl/fwd9m/__init__.py
@@ -0,0 +1,33 @@
+# Copyright 2023 NVIDIA. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+distribution_name = "framework-determinism"
+
+version = "0.1.0"
+
+long_description = """
+The `framework-determinism` PyPI distribution has been deprecated and the
+`fwd9m` package in this distribution contains no code.
+
+Please install the latest version of the
+[framework-reproducibility](https://pypi.org/project/framework-reproducibility/)
+PyPI distribution.
+"""
+
+print(long_description)
