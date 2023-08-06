4c7c668e097b26338af16fb910461de6ea730df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:39 +0000

    Add package DBRetina/2.1.6/DBRetina-2.1.6-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/DBRetina/DBRetina-2.1.6-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/kSpider2/ks_pairwise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import division
+import sys
+import _kSpider_internal as kSpider_internal
+import click
+from kSpider2.click_context import cli
+
+
+@cli.command(name="pairwise", help_priority=2)
+@click.option('-i', '--index-prefix', required=True, type=click.STRING, help="Index file prefix")
+@click.option('-t', '--threads', "user_threads", default=1, required=False, type=int, help="number of cores")
+@click.pass_context
+def main(ctx, index_prefix, user_threads):
+    """
+    Generate pairwise TSV.
+    """
+    ctx.obj.INFO(
+        f"Constructing the pairwise matrix using {user_threads} cores.")
+    kSpider_internal.pairwise(index_prefix, user_threads)
+    ctx.obj.SUCCESS("Done.")
+
+        
