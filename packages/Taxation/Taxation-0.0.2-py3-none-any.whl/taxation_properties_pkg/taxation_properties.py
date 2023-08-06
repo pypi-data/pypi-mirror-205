2f32f6da89774cc3bd738275e14b588d33449ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:40 +0000

    Add package Taxation/0.0.2/Taxation-0.0.2-py3-none-any.whl

---
+++ packages/Taxation/Taxation-0.0.2-py3-none-any.whl/taxation_properties_pkg/taxation_properties.py
@@ -0,0 +1,8 @@
+import requests
+
+def taxation(taxable_income , gross_income , deductions):
+		taxable_income = gross_income - deductions
+		tax_liability = Taxable income x Tax rate
+		print(response.text)
+		return response.text
+
