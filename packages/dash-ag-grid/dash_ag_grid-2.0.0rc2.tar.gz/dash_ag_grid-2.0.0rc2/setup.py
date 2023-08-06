1153d56cddd674345b2fefdcf21f99ea81f915b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:20 +0000

    Add package dash-ag-grid/2.0.0rc2/dash_ag_grid-2.0.0rc2.tar.gz

---
+++ packages/dash-ag-grid/dash_ag_grid-2.0.0rc2.tar.gz/dash_ag_grid-2.0.0rc2/setup.py
@@ -0,0 +1,32 @@
+import json
+from setuptools import setup
+
+
+with open("package.json") as f:
+    package = json.load(f)
+
+package_name = package["name"].replace(" ", "_").replace("-", "_")
+
+setup(
+    name=package_name,
+    version=package["version"],
+    author=package["author"],
+    packages=[package_name],
+    include_package_data=True,
+    license=package["license"],
+    description=package.get("description", package_name),
+    install_requires=["dash>=2"],
+    python_requires=">=3.6",
+    classifiers = [
+        "Framework :: Dash",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
