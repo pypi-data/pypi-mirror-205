0186e2a0f61057a4107a789649af9a313a2d2e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:24 +0000

    Add package HelloKaistRaiLabPractice/0.1.0/HelloKaistRaiLabPractice-0.1.0.tar.gz

---
+++ packages/HelloKaistRaiLabPractice/HelloKaistRaiLabPractice-0.1.0.tar.gz/HelloKaistRaiLabPractice-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="HelloKaistRaiLabPractice",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Choongin",
+    author_email="choongin.lee@kaist.ac.kr",
+    description="A simple greeting package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/greeting",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
