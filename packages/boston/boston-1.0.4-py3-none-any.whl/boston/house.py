0d99e9a5a1fce95dee01fda967f9df60c008a7ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:09 +0000

    Add package boston/1.0.4/boston-1.0.4-py3-none-any.whl

---
+++ packages/boston/boston-1.0.4-py3-none-any.whl/boston/house.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from urllib import request
+
+def load_boston():
+  filename = "resources/boston_house_prices.csv"
+
+  class __Boston:
+    def __init__(self):
+      df = pd.read_csv(filename, header=1)
+      self.data = df.drop('MEDV', axis=1).values
+      self.target = df['MEDV'].values
+      self.feature_names = df.columns.values.tolist()
+  return __Boston()
