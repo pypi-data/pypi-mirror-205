3e9bfd1b7d1c36509ee1bc1dca95e06e5d2225be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:40 +0000

    Add package pollstats/0.4/pollstats-0.4.tar.gz

---
+++ packages/pollstats/pollstats-0.4.tar.gz/pollstats-0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pollstats",
+    version="0.4",
+    packages=find_packages(),
+    install_requires=[
+        # List your package's dependencies here
+
+    ],
+    author="Vijayanand",
+    author_email="vijayanand563@gmail.com",
+    description="A short description of your package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Vijayanand-debug/pollstats",
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
