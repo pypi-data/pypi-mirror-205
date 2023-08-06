39d76047096608e700256bb1828c77dffc9a9e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:12 +0000

    Add package risk-mgmt/0.4.0/risk_mgmt-0.4.0.tar.gz

---
+++ packages/risk-mgmt/risk_mgmt-0.4.0.tar.gz/risk_mgmt-0.4.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='risk_mgmt',
+    version='0.4.0',
+    description='using for quatitative risk management',
+    author='Qihang Ma',
+    packages=['risk_mgmt'],
+    install_requires=['pandas','numpy'],
+)
