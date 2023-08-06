7c95afdd94addc10572b09058cd4c5af6b196832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:22 +0000

    Add package questdb-connect/0.0.25/questdb-connect-0.0.25.tar.gz

---
+++ packages/questdb-connect/questdb-connect-0.0.25.tar.gz/questdb-connect-0.0.25/src/questdb_connect/__init__.py
@@ -0,0 +1,50 @@
+#
+#     ___                  _   ____  ____
+#    / _ \ _   _  ___  ___| |_|  _ \| __ )
+#   | | | | | | |/ _ \/ __| __| | | |  _ \
+#   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+#    \__\_\\__,_|\___||___/\__|____/|____/
+#
+#  Copyright (c) 2014-2019 Appsicle
+#  Copyright (c) 2019-2023 QuestDB
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+import psycopg2
+
+# ===== DBAPI =====
+
+# https://peps.python.org/pep-0249/
+
+apilevel = '2.0'
+threadsafety = 2
+paramstyle = 'pyformat'
+
+
+class Error(Exception):
+    pass
+
+
+def connect(**kwargs):
+    host = kwargs.get('host') or '127.0.0.1'
+    port = kwargs.get('port') or 8812
+    user = kwargs.get('username') or 'admin'
+    passwd = kwargs.get('password') or 'quest'
+    database = kwargs.get('database') or 'main'
+    return psycopg2.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=passwd,
+        database=database
+    )
