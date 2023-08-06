8db5a9888cd99177c371c96df2ad601722b81b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:53 +0000

    Add package txx3tr-asef-qe11/1.0/txx3tr_asef_qe11-1.0.tar.gz

---
+++ packages/txx3tr-asef-qe11/txx3tr_asef_qe11-1.0.tar.gz/txx3tr_asef_qe11-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='txx3tr_asef_qe11',
+      version='1.0',
+      description='test',
+      packages=['xx3tr_asef_qe11'],
+      author_email='testdd12d@mail.ru',
+      zip_safe=False)
