040a14fed863ee7e6e75afde4b865796ee611fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:58 +0000

    Add package figures-090807/0.1/figures_090807-0.1.tar.gz

---
+++ packages/figures-090807/figures_090807-0.1.tar.gz/figures_090807-0.1/figures_090807/__init__.py
@@ -0,0 +1,8 @@
+from .circle import circle_perimeter, circle_area
+from .triangle import triangle_perimeter, triangle_area
+from .square import square_perimeter, square_area
+
+__all__ = ['circle_perimeter', 'circle_area',
+           'triangle_perimeter', 'triangle_area',
+           'square_perimeter', 'square_area'
+          ]
---
+++ packages/figures-090807/figures_090807-0.1.tar.gz/figures_090807-0.1/setup.py
@@ -0,0 +1,8 @@
+﻿from setuptools import setup
+
+setup(name='figures_090807',
+      version='0.1',
+      description='Areas and Perimeters',
+      packages=['figures_090807'],
+      author_email='xoptka16@mail.ru',
+      zip_safe=False)
