688411fb0bb6b60994ef389bac98c518c97263bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:56 +0000

    Add package airportcountries/1.0.0/airportcountries-1.0.0.tar.gz

---
+++ packages/airportcountries/airportcountries-1.0.0.tar.gz/airportcountries-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="airportcountries",
+    version="1.0.0",
+    description="A library for storing airport country codes",
+    packages=find_packages(),
+    install_requires=[],
+    author="Chaitenya Kumar",
+    author_email="x21174211@student.ncirl.ie",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
