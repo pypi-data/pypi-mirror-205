e1fb60998918ffbc509d1ccc2ef9d26320d68033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:07 +0000

    Add package catnapmodule/0.0/catnapmodule-0.0.tar.gz

---
+++ packages/catnapmodule/catnapmodule-0.0.tar.gz/catnapmodule-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='catnapmodule',
+      version='0.0',
+      description='just testing Pypi upload',
+      packages=find_packages(),
+      author_email='mohulhack@gmail.com',
+      zip_safe=False)
