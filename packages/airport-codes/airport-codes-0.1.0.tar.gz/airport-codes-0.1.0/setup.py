f341b88c89da0953c569e2d32d00ac0e12fad9a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:44 +0000

    Add package airport-codes/0.1.0/airport-codes-0.1.0.tar.gz

---
+++ packages/airport-codes/airport-codes-0.1.0.tar.gz/airport-codes-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='airport-codes',
+    version='0.1.0',
+    description='A library for searching airport codes',
+    author='Your Name',
+    author_email='your.email@example.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
