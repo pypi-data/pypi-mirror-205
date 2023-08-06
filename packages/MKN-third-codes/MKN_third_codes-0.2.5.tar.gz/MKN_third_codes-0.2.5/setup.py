f53b05f88cd7e048e59bdfa41065d1a7d764fb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:22 +0000

    Add package MKN-third-codes/0.2.5/MKN_third_codes-0.2.5.tar.gz

---
+++ packages/MKN-third-codes/MKN_third_codes-0.2.5.tar.gz/MKN_third_codes-0.2.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='MKN_third_codes',
+    version='0.2.5',
+    description='Python library with standart solutions for probability tasks',
+    author='Dolgun Ivan',
+    author_email='vanadolgun@gmail.com',
+    packages=['MKN_third_codes/'],
+    license='MIT'
+)
