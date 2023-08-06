e371265c9c36fb739f3e68da460b9cb3e121413e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:49 +0000

    Add package exc-handler/1.0.1/exc_handler-1.0.1.tar.gz

---
+++ packages/exc-handler/exc_handler-1.0.1.tar.gz/exc_handler-1.0.1/exc_handler/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+doc = """
+This module handles exception handling for Python applications. It catches exceptions thrown by the application and prints out a stack trace with color-coded syntax for better readability.\n
+It also displays the date and time the exception occurred, the exception name and value.
+
+Functions:
+    - exc_handler(): handles exception thrown by the application
+
+Dependencies:
+    - datetime: for getting the date and time of the exception
+    - sys: for setting the excepthook
+    - traceback: for extracting the traceback info
+    - types: for the TracebackType
+    - termcolor: for color-coding the stack trace
+
+Usage:
+    - Just import this module
+"""
+
+setup(
+    name="exc_handler",
+    version="1.0.1",
+    description=doc,
+    author="iineolineii",
+    packages=["exc_handler"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+	]
+)
