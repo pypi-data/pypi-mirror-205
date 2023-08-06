c2a9478fc2e5a54d6c20232c9b0bea1919063322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:59 +0000

    Add package gqylpy-exception/2.0.2/gqylpy_exception-2.0.2.tar.gz

---
+++ packages/gqylpy-exception/gqylpy_exception-2.0.2.tar.gz/gqylpy_exception-2.0.2/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+import gqylpy_exception as g
+
+with open(g.__file__, encoding='utf8') as f:
+    for line in f:
+        if line.startswith('@version: ', 4):
+            version = line.split()[-1]
+            break
+    author, email = f.readline().split(maxsplit=1)[-1].rstrip().split()
+    source = f.readline().split()[-1]
+
+setuptools.setup(
+    name=g.__name__,
+    version=version,
+    author=author,
+    author_email=email,
+    license='Apache 2.0',
+    url='http://gqylpy.com',
+    project_urls={'Source': source},
+    description='在执行 raise 语句的同时创建异常类，无需事先定义异常类，方便快捷。',
+    long_description=open('README.md', encoding='utf8').read(),
+    long_description_content_type='text/markdown',
+    packages=[g.__name__],
+    python_requires='>=3.6, <4',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing :: Indexing',
+        'Topic :: Utilities',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ]
+)
