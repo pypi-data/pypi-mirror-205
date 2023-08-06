c454da2d2c9c00e49380ad0c5e8ca514e47f6d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:57 +0000

    Add package aqueduct-ml/0.2.12/aqueduct-ml-0.2.12.tar.gz

---
+++ packages/aqueduct-ml/aqueduct-ml-0.2.12.tar.gz/aqueduct-ml-0.2.12/setup.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+install_requires = open("requirements.txt").read().strip().split("\n")
+
+readme_path = Path(os.environ["PWD"], "../../README.md")
+long_description = open(readme_path).read()
+
+setup(
+    name="aqueduct-ml",
+    version="0.2.12",
+    install_requires=install_requires,
+    scripts=["bin/aqueduct"],
+    packages=find_packages(),
+    description="The control center for ML in the cloud",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.aqueducthq.com/",
+    license="Apache License 2.0",
+    author="Aqueduct, Inc.",
+    author_email="hello@aqueducthq.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.7",
+)
