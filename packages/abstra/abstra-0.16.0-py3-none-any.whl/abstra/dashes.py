8b984b350627103a0714ec69a2dfec1ccafe5df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:05 +0000

    Add package abstra/0.16.0/abstra-0.16.0-py3-none-any.whl

---
+++ packages/abstra/abstra-0.16.0-py3-none-any.whl/abstra/dashes.py
@@ -0,0 +1,18 @@
+# This is an interface only file
+# Actual functions are defined on runtime
+
+
+class AuthResponse:
+    email: str
+
+
+def get_user() -> AuthResponse:
+    pass
+
+
+def redirect(url: str, params={}):
+    pass
+
+
+def get_query_params() -> dict:
+    pass
