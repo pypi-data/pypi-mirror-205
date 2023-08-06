1ae3dc4a186d843cf03c9d898661dca2e1ff6bdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:16 +0000

    Add package fastapi-sql/0.1.3/fastapi_sql-0.1.3.tar.gz

---
+++ packages/fastapi-sql/fastapi_sql-0.1.3.tar.gz/fastapi_sql-0.1.3/fastapi_sql/migrate.py
@@ -0,0 +1,32 @@
+from alembic.config import main
+from typing import Any
+
+class Migration:
+    def __init__(self, init_options: 'dict[str,Any]' = {}):
+        argv = ['init']
+        argv.extend(init_options)
+        main(argv)
+        
+    def migrate(self, options: 'dict[str,Any]' = {}):
+        argv = ['migrate']
+        argv.extend(options)
+        main(argv)
+        
+    def revision(self, options: 'dict[str,Any]' = {}, revision_name:str = 'Commit'):
+        argv = ['revision', revision_name]
+        argv.extend(options)
+        main(argv)
+        
+    def upgrade(self, options: 'dict[str,Any]' = {}, branch:str = 'Head'):
+        self.revision()
+        argv = ['upgrade', branch]
+        argv.extend(options)
+        main(argv)
+        
+    def current(self):
+        main(['current'])
+        
+    def history(self, options: 'dict[str,Any]' = {}):
+        argv = ['history']
+        argv.extend(options)
+        main(argv)
