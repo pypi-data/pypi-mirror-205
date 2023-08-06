f22a38641870c978f21886211f9db95d180f08c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:51 +0000

    Add package molab/0.4.4/molab-0.4.4.tar.gz

---
+++ packages/molab/molab-0.4.4.tar.gz/molab-0.4.4/molab/__init__.py
@@ -0,0 +1,6 @@
+### molab is a python package for setting up and configuring Morpheus training labs ###
+
+from .deploy import *
+from .destroy import *
+from .configure import *
+from .courses import *
