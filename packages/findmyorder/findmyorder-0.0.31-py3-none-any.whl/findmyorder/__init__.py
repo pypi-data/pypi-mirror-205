4d2b747bb4c640cca886cbfb53ffdd1510cc92ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:10 +0000

    Add package findmyorder/0.0.31/findmyorder-0.0.31-py3-none-any.whl

---
+++ packages/findmyorder/findmyorder-0.0.31-py3-none-any.whl/findmyorder/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.0.31"
+
+from .main import findmyorder 
+# from .main import search
+# from .main import identify
+from .config import settings
+# __all__ = ['findmyorder', 'search','identify','settings']
