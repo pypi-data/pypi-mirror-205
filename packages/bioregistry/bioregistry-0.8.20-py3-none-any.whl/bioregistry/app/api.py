5ddba95c03a507d0be92d84839846b39fac0c4c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:00 +0000

    Add package bioregistry/0.8.20/bioregistry-0.8.20-py3-none-any.whl

---
+++ packages/bioregistry/bioregistry-0.8.20-py3-none-any.whl/bioregistry/app/api.py
@@ -0,0 +1,579 @@
+# -*- coding: utf-8 -*-
+
+"""API blueprint and routes."""
+
+from typing import cast
+
+from flask import Blueprint, abort, jsonify, request
+
+from .proxies import manager
+from .utils import (
+    _autocomplete,
+    _normalize_prefix_or_404,
+    _search,
+    serialize,
+    serialize_model,
+)
+from .. import Collection, Registry
+from ..export.rdf_export import (
+    collection_to_rdf_str,
+    metaresource_to_rdf_str,
+    resource_to_rdf_str,
+)
+from ..schema import sanitize_mapping
+from ..schema_utils import (
+    read_collections_contributions,
+    read_prefix_contacts,
+    read_prefix_contributions,
+    read_prefix_reviews,
+    read_registry_contributions,
+)
+from ..utils import curie_to_str
+
+__all__ = [
+    "api_blueprint",
+]
+
+api_blueprint = Blueprint("metaregistry_api", __name__, url_prefix="/api")
+
+
+@api_blueprint.route("/registry")
+def resources():
+    """Get all resources.
+
+    ---
+    tags:
+    - resource
+    parameters:
+    - name: format
+      description: The file type
+      in: query
+      required: false
+      default: json
+      schema:
+        type: string
+        enum: [json, yaml]
+    """  # noqa:DAR101,DAR201
+    return serialize(sanitize_mapping(manager.registry))
+
+
+@api_blueprint.route("/registry/<prefix>")
+def resource(prefix: str):
+    """Get a resource.
+
+    ---
+    tags:
+    - resource
+    parameters:
+    - name: prefix
+      in: path
+      description: The prefix for the entry
+      required: true
+      type: string
+      example: doid
+    - name: format
+      description: The file type
+      in: query
+      required: false
+      default: json
+      schema:
+        type: string
+        enum: [json, yaml, turtle, jsonld]
+    """  # noqa:DAR101,DAR201
+    prefix = _normalize_prefix_or_404(prefix)
+    if not isinstance(prefix, str):
+        return jsonify(query=prefix, message="Invalid prefix"), 404
+    resource = manager.get_resource(prefix)
+    assert resource is not None
+    # TODO rasterize?
+    return serialize_model(resource, resource_to_rdf_str)
+
+
+@api_blueprint.route("/metaregistry/<metaprefix>/resolve/<metaidentifier>")
+def resource_from_metaregistry(metaprefix: str, metaidentifier: str):
+    """Get a resource by an external prefix.
+
+    ---
+    tags:
+    - resource
+    parameters:
+    - name: metaprefix
+      in: path
+      description: The meteprefix for a registry
+      required: true
+      type: string
+      example: obofoundry
+    - name: metaidentifier
+      in: path
+      description: The prefix instide for a registry
+      required: true
+      type: string
+      example: GO
+    - name: format
+      description: The file type
+      in: query
+      required: false
+      default: json
+      schema:
+        type: string
+        enum: [json, yaml, turtle, jsonld]
+
+    """  # noqa:DAR101,DAR201
+    if metaprefix not in manager.metaregistry:
+        return abort(404, f"invalid metaprefix: {metaprefix}")
+    prefix = manager.lookup_from(metaprefix, metaidentifier, normalize=True)
+    if not prefix:
+        return abort(404, f"invalid metaidentifier: {metaidentifier}")
+    resource = manager.get_resource(prefix)
+    assert resource is not None
+    resource = manager.rasterized_resource(resource)
+    return serialize_model(resource, resource_to_rdf_str)
+
+
+@api_blueprint.route("/metaregistry/<metaprefix>/mappings.json")
+def bioregistry_to_external_mapping(metaprefix: str):
+    """Get mappings from the Bioregistry to an external registry.
+
+    ---
+    parameters:
+    - name: metaprefix
+      in: path
+      description: The target metaprefix (e.g., bioportal)
+      required: true
+      type: string
+    """  # noqa:DAR101,DAR201
+    if metaprefix not in manager.metaregistry:
+        return abort(404, f"invalid metaprefix: {metaprefix}")
+    return jsonify(manager.get_registry_map(metaprefix))
+
+
+@api_blueprint.route("/metaregistry/<metaprefix>/registry_subset.json")
+def get_external_registry_slim(metaprefix: str):
+    """Get a slim, rasterized version of the Bioregistry for records in the external registry.
+
+    ---
+    parameters:
+    - name: metaprefix
+      in: path
+      description: The target metaprefix (e.g., bioportal)
+      required: true
+      type: string
+    """  # noqa:DAR101,DAR201
+    if metaprefix not in manager.metaregistry:
+        return abort(404, f"invalid metaprefix: {metaprefix}")
+    return sanitize_mapping(
+        {
+            prefix: manager.rasterized_resource(resource_)
+            for prefix, resource_ in manager.registry.items()
+            if metaprefix in resource_.get_mappings()
+        }
+    )
+
+
+@api_blueprint.route("/metaregistry")
+def metaresources():
+    """Get all metaresources.
+
+    ---
+    tags:
+    - metaresource
+    parameters:
+    - name: format
+      description: The file type
+      in: query
+      required: false
+      default: json
+      schema:
+        type: string
+        enum: [json, yaml]
+    """  # noqa:DAR101,DAR201
+    return serialize(sanitize_mapping(manager.metaregistry))
+
+
+@api_blueprint.route("/metaregistry/<metaprefix>")
+def metaresource(metaprefix: str):
+    """Get a metaresource.
+
+    ---
+    tags:
+    - metaresource
+    parameters:
+    - name: prefix
+      in: path
+      description: The prefix for the metaresource
+      required: true
+      type: string
+      example: doid
+    - name: format
+      description: The file type
+      in: query
+      required: false
+      default: json
+      schema:
+        type: string
+        enum: [json, yaml, turtle, jsonld]
+    """  # noqa:DAR101,DAR201
+    data = manager.metaregistry.get(metaprefix)
+    if data is None:
+        abort(404, f"Invalid metaprefix: {metaprefix}")
+    return serialize_model(cast(Registry, data), metaresource_to_rdf_str)
+
+
+@api_blueprint.route("/collections")
+def collections():
+    """Get all collections.
+
+    ---
+    tags:
+    - collection
+    parameters:
+    - name: format
+      description: The file type
+      in: query
+      required: false
+      default: json
+      schema:
+        type: string
+        enum: [json, yaml]
+    """  # noqa:DAR101,DAR201
+    return serialize(sanitize_mapping(manager.collections))
+
+
+@api_blueprint.route("/collection/<identifier>")
+def collection(identifier: str):
+    """Get a collection.
+
+    ---
+    tags:
+    - collection
+    parameters:
+    - name: prefix
+      in: path
+      description: The identifier of the collection
+      required: true
+      type: string
+      example: 0000001
+    - name: format
+      description: The file type
+      in: query
+      required: false
+      default: json
+      schema:
+        type: string
+        enum: [json, yaml, context, turtle, jsonld]
+    """  # noqa:DAR101,DAR201
+    data = manager.collections.get(identifier)
+    if not data:
+        abort(404, f"Invalid collection: {identifier}")
+    # ("context", "application/ld+json", Collection.as_context_jsonld_str),
+    return serialize_model(cast(Collection, data), collection_to_rdf_str)
+
+
+@api_blueprint.route("/collection/<identifier>.context.jsonld")
+def collection_context(identifier: str):
+    """Get a collection as a JSON-LD context.
+
+    ---
+    tags:
+    - collection
+    parameters:
+    - name: prefix
+      in: path
+      description: The identifier of the collection
+      required: true
+      type: string
+      example: 0000001
+    - name: format
+      description: The file type
+      in: query
+      required: false
+      default: json
+      schema:
+        type: string
+        enum: [json, yaml, context, turtle, jsonld]
+    """  # noqa:DAR101,DAR201
+    data = manager.collections.get(identifier)
+    if data is None:
+        abort(404, f"Invalid collection: {identifier}")
+    return jsonify(cast(Collection, data).as_context_jsonld())
+
+
+@api_blueprint.route("/contexts")
+def contexts():
+    """Get all contexts.
+
+    ---
+    tags:
+    - context
+    parameters:
+    - name: format
+      description: The file type
+      in: query
+      required: false
+      default: json
+      schema:
+        type: string
+        enum: [json, yaml]
+    """  # noqa:DAR101,DAR201
+    return serialize(sanitize_mapping(manager.contexts))
+
+
+@api_blueprint.route("/context/<identifier>")
+def context(identifier: str):
+    """Get a context.
+
+    ---
+    tags:
+    - context
+    parameters:
+    - name: identifier
+      in: path
+      description: The identifier of the context
+      required: true
+      type: string
+      example: obo
+    - name: format
+      description: The file type
+      in: query
+      required: false
+      default: json
+      schema:
+        type: string
+        enum: [json, yaml]
+    """  # noqa:DAR101,DAR201
+    data = manager.contexts.get(identifier)
+    if not data:
+        abort(404, f"Invalid context: {identifier}")
+    return serialize(data)
+
+
+@api_blueprint.route("/reference/<prefix>:<identifier>")
+def reference(prefix: str, identifier: str):
+    """Look up information on the reference.
+
+    ---
+    tags:
+    - reference
+    parameters:
+    - name: prefix
+      in: path
+      description: The prefix for the entry
+      required: true
+      type: string
+      example: efo
+    - name: identifier
+      in: path
+      description: The identifier for the entry
+      required: true
+      type: string
+      example: 0000311
+    - name: format
+      description: The file type
+      in: query
+      required: false
+      default: json
+      schema:
+        type: string
+        enum: [json, yaml]
+    """  # noqa:DAR101,DAR201
+    prefix = _normalize_prefix_or_404(prefix)
+    if not manager.is_standardizable_identifier(prefix, identifier):
+        return abort(
+            404,
+            f"invalid identifier: {curie_to_str(prefix, identifier)} for pattern {manager.get_pattern(prefix)}",
+        )
+    providers = manager.get_providers(prefix, identifier)
+    if not providers:
+        return abort(404, f"no providers available for {curie_to_str(prefix, identifier)}")
+
+    return serialize(
+        dict(
+            query=dict(prefix=prefix, identifier=identifier),
+            providers=providers,
+        )
+    )
+
+
+@api_blueprint.route("/contributors")
+def contributors():
+    """Get all contributors.
+
+    ---
+    tags:
+    - contributor
+    parameters:
+    - name: format
+      description: The file type
+      in: query
+      required: false
+      default: json
+      schema:
+        type: string
+        enum: [json, yaml]
+    """  # noqa:DAR101,DAR201
+    return serialize(sanitize_mapping(manager.read_contributors()))
+
+
+@api_blueprint.route("/contributor/<orcid>")
+def contributor(orcid: str):
+    """Get a contributor.
+
+    ---
+    tags:
+    - contributor
+    parameters:
+    - name: orcid
+      in: path
+      description: The ORCID identifier of the contributor
+      required: true
+      type: string
+      example: 0000-0002-8424-0604
+    - name: format
+      description: The file type
+      in: query
+      required: false
+      default: json
+      schema:
+        type: string
+        enum: [json, yaml]
+    """  # noqa:DAR101,DAR201
+    author = manager.read_contributors().get(orcid)
+    if author is None:
+        return abort(404, f"No contributor with orcid:{orcid}")
+
+    return serialize(
+        {
+            **author.dict(),
+            "prefix_contributions": sorted(
+                read_prefix_contributions(manager.registry).get(orcid, [])
+            ),
+            "prefix_reviews": sorted(read_prefix_reviews(manager.registry).get(orcid, [])),
+            "prefix_contacts": sorted(read_prefix_contacts(manager.registry).get(orcid, [])),
+            "registries": sorted(read_registry_contributions(manager.metaregistry).get(orcid, [])),
+            "collections": sorted(
+                read_collections_contributions(manager.collections).get(orcid, [])
+            ),
+        }
+    )
+
+
+@api_blueprint.route("/search")
+def search():
+    """Search for a prefix.
+
+    ---
+    parameters:
+    - name: q
+      in: query
+      description: The prefix for the entry
+      required: true
+      type: string
+    """  # noqa:DAR101,DAR201
+    q = request.args.get("q")
+    if q is None:
+        abort(400)
+    return jsonify(_search(manager, q))
+
+
+@api_blueprint.route("/autocomplete")
+def autocomplete():
+    """Complete a resolution query.
+
+    ---
+    parameters:
+    - name: q
+      in: query
+      description: The prefix for the entry
+      required: true
+      type: string
+    """  # noqa:DAR101,DAR201
+    q = request.args.get("q")
+    if q is None:
+        abort(400)
+    return jsonify(_autocomplete(manager, q))
+
+
+@api_blueprint.route("/context.jsonld")
+def generate_context_json_ld():
+    """Generate an *ad-hoc* context JSON-LD file from the given parameters.
+
+    You can either give prefixes as a comma-separated list like:
+
+    https://bioregistry.io/api/context.jsonld?prefix=go,doid,oa
+
+    or you can use multiple entries for "prefix" like:
+
+    https://bioregistry.io/api/context.jsonld?prefix=go&prefix=doid&prefix=oa
+    ---
+    parameters:
+    - name: prefix
+      in: query
+      description: The prefix for the entry. Can be given multiple.
+      required: true
+      type: string
+    """  # noqa:DAR101,DAR201
+    prefix_map = {}
+    for arg in request.args.getlist("prefix", type=str):
+        for prefix in arg.split(","):
+            prefix = manager.normalize_prefix(prefix.strip())
+            if prefix is None:
+                continue
+            uri_prefix = manager.get_uri_prefix(prefix)
+            if uri_prefix is None:
+                continue
+            prefix_map[prefix] = uri_prefix
+
+    return jsonify(
+        {
+            "@context": prefix_map,
+        }
+    )
+
+
+@api_blueprint.route("/external/mapping/<source>/<target>")
+def mapping(source: str, target: str):
+    """Get mappings between two external prefixes.
+
+    ---
+    parameters:
+    - name: source
+      in: path
+      description: The source metaprefix (e.g., obofoundry)
+      required: true
+      type: string
+    - name: target
+      in: path
+      description: The target metaprefix (e.g., bioportal)
+      required: true
+      type: string
+    """  # noqa:DAR101,DAR201
+    if source not in manager.metaregistry:
+        return {"bad source prefix": source}, 400
+    if target not in manager.metaregistry:
+        return {"bad target prefix": target}, 400
+    rv = {}
+    source_only = set()
+    target_only = set()
+    for resource in manager.registry.values():
+        mappings = resource.get_mappings()
+        mp1_prefix = mappings.get(source)
+        mp2_prefix = mappings.get(target)
+        if mp1_prefix and mp2_prefix:
+            rv[mp1_prefix] = mp2_prefix
+        elif mp1_prefix and not mp2_prefix:
+            source_only.add(mp1_prefix)
+        elif not mp1_prefix and mp2_prefix:
+            target_only.add(mp2_prefix)
+
+    return jsonify(
+        meta=dict(
+            len_overlap=len(rv),
+            source=source,
+            target=target,
+            len_source_only=len(source_only),
+            len_target_only=len(target_only),
+            source_only=sorted(source_only),
+            target_only=sorted(target_only),
+        ),
+        mappings=rv,
+    )
---
+++ packages/bioregistry/bioregistry-0.8.20-py3-none-any.whl/bioregistry/app/impl.py
@@ -0,0 +1,212 @@
+"""App builder interface."""
+
+import json
+from pathlib import Path
+from textwrap import dedent
+from typing import TYPE_CHECKING, Any, Mapping, Optional, Union
+
+from curies.mapping_service import MappingServiceGraph, MappingServiceSPARQLProcessor
+from fastapi import APIRouter, FastAPI
+from flasgger import Swagger
+from flask import Flask
+from flask_bootstrap import Bootstrap4
+from rdflib_endpoint import SparqlRouter
+from starlette.middleware.wsgi import WSGIMiddleware
+
+from bioregistry import curie_to_str, resource_manager, version
+
+from .api import api_blueprint
+from .constants import BIOSCHEMAS
+from .ui import ui_blueprint
+
+if TYPE_CHECKING:
+    import bioregistry
+
+__all__ = [
+    "get_app",
+]
+
+TITLE_DEFAULT = "Bioregistry"
+FOOTER_DEFAULT = dedent(
+    """\
+    Developed with ❤️ by the <a href="https://indralab.github.io">INDRA Lab</a> in the
+    <a href="https://hits.harvard.edu">Harvard Program in Therapeutic Science (HiTS)</a>.<br/>
+    Funded by the DARPA Young Faculty Award W911NF2010255 (PI: Benjamin M. Gyori).<br/>
+    Point of contact: <a href="https://github.com/cthoyt">@cthoyt</a>.
+    (<a href="https://github.com/biopragmatics/bioregistry">Source code</a>)
+"""
+)
+HEADER_DEFAULT = dedent(
+    """\
+    <p class="lead">
+        The Bioregistry is an open source, community curated registry, meta-registry, and compact
+        identifier resolver. Here's what that means:
+    </p>
+    <dl class="row">
+        <dt class="col-lg-2 text-right text-nowrap">Registry</dt>
+        <dd class="col-lg-10">
+            A collection of prefixes and metadata for ontologies, controlled vocabularies, and other semantic
+            spaces. Some other well-known registries are the <a href="http://www.obofoundry.org/">OBO Foundry</a>,
+            <a href="https://identifiers.org">Identifiers.org</a>, and the
+            the <a href="https://www.ebi.ac.uk/ols/index">OLS</a>.
+        </dd>
+        <dt class="col-lg-2 text-right text-nowrap">Metaregistry</dt>
+        <dd class="col-lg-10">
+            A collection of metadata about registries and mappings between their constituent prefixes. For
+            example, <a href="https://www.ebi.ac.uk/chebi">ChEBI</a> appears in all of the example registries
+            from above. So far, the Bioregistry is the <i>only</i> metaregistry.
+        </dd>
+        <dt class="col-lg-2 text-right text-nowrap">Resolver</dt>
+        <dd class="col-lg-10">
+            A tool for mapping compact URIs (CURIEs) of the form <code>prefix:identifier</code> to HTML and
+            structured content providers. Some other well-known resolvers are
+            <a href="https://identifiers.org">Identifiers.org</a> and <a href="https://n2t.net/">Name-To-Thing</a>.
+        </dd>
+        <dt class="col-lg-2 text-right text-nowrap">Open Source</dt>
+        <dd class="col-lg-10">
+            Anyone can <a href="https://github.com/biopragmatics/bioregistry/issues/new/choose">suggest
+            improvements</a> or make pull requests to update the underlying database, which is stored in
+            <a href="https://github.com/biopragmatics/bioregistry/blob/main/src/bioregistry/data/bioregistry.json">
+                JSON</a> on GitHub where the community can engage in an open review process.
+        </dd>
+        <dt class="col-lg-2 text-right text-nowrap">Community</dt>
+        <dd class="col-lg-10">
+            Governed by public, well-defined
+            <a href="https://github.com/biopragmatics/bioregistry/blob/main/docs/CONTRIBUTING.md">contribution
+                guidelines</a>,
+            <a href="https://github.com/biopragmatics/bioregistry/blob/main/docs/CODE_OF_CONDUCT.md">code of
+            conduct</a>, and
+            <a href="https://github.com/biopragmatics/bioregistry/blob/main/docs/GOVERNANCE.md">project
+                governance</a> to promote the project's inclusivity and longevity.
+        </dd>
+    </dl>
+"""
+)
+RESOURCES_SUBHEADER_DEFAULT = dedent(
+    """\
+    <p style="margin-bottom: 0">
+        Anyone can <a href="https://github.com/biopragmatics/bioregistry/issues/new/choose">suggest
+        improvements</a>, <a href="https://github.com/biopragmatics/bioregistry/issues/new?labels=\
+            New%2CPrefix&template=new-prefix.yml&title=Add+prefix+%5BX%5D">request a new prefix</a>,
+             or make pull requests to update the underlying database, which is stored in
+        <a href="https://github.com/biopragmatics/bioregistry/blob/main/src/bioregistry/data/bioregistry.json">
+            JSON</a> on GitHub where the community can engage in an open review process.
+    </p>
+    """
+)
+
+
+def get_app(
+    manager: Optional["bioregistry.Manager"] = None,
+    config: Union[None, str, Path, Mapping[str, Any]] = None,
+    first_party: bool = True,
+    return_flask: bool = False,
+):
+    """Prepare the flask application.
+
+    :param manager: A pre-configured manager. If none given, uses the default manager.
+    :param config: Additional configuration to be passed to the flask application. See below.
+    :param first_party: Set to true if deploying the "canonical" bioregistry instance
+    :param return_flask: Set to true to get internal flask app
+    :returns: An instantiated WSGI application
+    :raises ValueError: if there's an issue with the configuration's integrity
+    """
+    app = Flask(__name__)
+    if isinstance(config, (str, Path)):
+        with open(config) as file:
+            app.config.update(json.load(file))
+    elif config is not None:
+        app.config.update(config)
+    app.config.setdefault("METAREGISTRY_TITLE", "Bioregistry")
+    app.config.setdefault("METAREGISTRY_FOOTER", FOOTER_DEFAULT)
+    app.config.setdefault("METAREGISTRY_HEADER", HEADER_DEFAULT)
+    app.config.setdefault("METAREGISTRY_RESOURCES_SUBHEADER", RESOURCES_SUBHEADER_DEFAULT)
+    app.config.setdefault("METAREGISTRY_VERSION", version.get_version())
+    app.config.setdefault("METAREGISTRY_EXAMPLE_PREFIX", "chebi")
+    app.config.setdefault("METAREGISTRY_EXAMPLE_IDENTIFIER", "138488")
+    app.config.setdefault("METAREGISTRY_FIRST_PARTY", first_party)
+
+    app.manager = manager or resource_manager.manager
+
+    if app.config.get("METAREGISTRY_FIRST_PARTY"):
+        app.config.setdefault("METAREGISTRY_BIOSCHEMAS", BIOSCHEMAS)
+
+    example_prefix = app.config["METAREGISTRY_EXAMPLE_PREFIX"]
+    resource = app.manager.registry.get(example_prefix)
+    if resource is None:
+        raise ValueError(
+            f"{example_prefix} is not available as a prefix. Set a different METAREGISTRY_EXAMPLE_PREFIX"
+        )
+    if resource.get_example() is None:
+        raise ValueError("Must use an example prefix with an example identifier")
+    if resource.get_uri_format() is None:
+        raise ValueError("Must use an example prefix with a URI format")
+
+    Swagger.DEFAULT_CONFIG.update(
+        {
+            "info": {
+                "title": app.config["METAREGISTRY_TITLE"],
+                "description": "A service for resolving CURIEs",
+                "contact": {
+                    "responsibleDeveloper": "Charles Tapley Hoyt",
+                    "email": "cthoyt@gmail.com",
+                },
+                "version": "1.0",
+                "license": {
+                    "name": "Code available under the MIT License",
+                    "url": "https://github.com/biopragmatics/bioregistry/blob/main/LICENSE",
+                },
+            },
+            "host": app.manager.base_url,
+            "tags": [
+                {
+                    "name": "collections",
+                    "externalDocs": {
+                        "url": f"{app.manager.base_url}/collection/",
+                    },
+                },
+            ],
+        }
+    )
+
+    Swagger(app)
+    Bootstrap4(app)
+
+    app.register_blueprint(api_blueprint)
+    app.register_blueprint(ui_blueprint)
+
+    # Make manager available in all jinja templates
+    app.jinja_env.globals.update(manager=app.manager, curie_to_str=curie_to_str)
+
+    fast_api = FastAPI()
+    fast_api.include_router(_get_sparql_router(app))
+    fast_api.mount("/", WSGIMiddleware(app))
+    if return_flask:
+        return fast_api, app
+    return fast_api
+
+
+example_query = """\
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+
+SELECT ?s ?o WHERE {
+    VALUES ?s { <http://purl.obolibrary.org/obo/CHEBI_1> }
+    ?s owl:sameAs ?o
+}
+""".rstrip()
+
+
+def _get_sparql_router(app) -> APIRouter:
+    sparql_graph = MappingServiceGraph(converter=app.manager.converter)
+    sparql_processor = MappingServiceSPARQLProcessor(graph=sparql_graph)
+    sparql_router = SparqlRouter(
+        path="/sparql",
+        title=f"{app.config['METAREGISTRY_TITLE']} SPARQL Service",
+        description="An identifier mapping service",
+        version=version.get_version(),
+        example_query=example_query,
+        graph=sparql_graph,
+        processor=sparql_processor,
+        public_url=f"{app.manager.base_url}/sparql",
+    )
+    return sparql_router
