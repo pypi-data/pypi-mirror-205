4cb8e45db1aa9459cd1326687507be181e554753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:42 +0000

    Add package stirpy/1.1.5/stirpy-1.1.5.tar.gz

---
+++ packages/stirpy/stirpy-1.1.5.tar.gz/stirpy-1.1.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("stripy.py", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='stirpy',
+    version='1.1.5',
+    author='KhalidWalidAghrini',
+    author_email='kwaaghrini@gmail.com',
+    description='A Powerful scientific strings-processing library called stipy',
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'nltk',
+        'afinn'
+    ],
+)
