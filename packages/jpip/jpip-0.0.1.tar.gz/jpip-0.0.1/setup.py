1aad734a40618f7fb22cd8a91ea3e699f9b94f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:45 +0000

    Add package jpip/0.0.1/jpip-0.0.1.tar.gz

---
+++ packages/jpip/jpip-0.0.1.tar.gz/jpip-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'basic hello package'
+LONG_DESCRIPTION = 'basic hello pacakge'
+
+# Setting up
+setup(
+    name="jpip",
+    version=VERSION,
+    author="Junichi Shimizu",
+    author_email="jun.shimi51@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=[''],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
