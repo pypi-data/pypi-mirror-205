bb5c93783f4c103abd1e5e5be4d72481a408de80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:18 +0000

    Add package tof/23.4.1/tof-23.4.1.tar.gz

---
+++ packages/tof/tof-23.4.1.tar.gz/tof-23.4.1/src/tof/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+
+# flake8: noqa F401
+
+from .chopper import Chopper
+from .detector import Detector
+from .model import Model
+from .pulse import Pulse
