ed611db882597993fbfc05de7350a83c6ce8f2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:33 +0000

    Add package chatllm/2023.4.26.12.10.47/ChatLLM-2023.4.26.12.10.47-py3-none-any.whl

---
+++ packages/chatllm/ChatLLM-2023.4.26.12.10.47-py3-none-any.whl/chatllm/applications/chatpdf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : ChatPDF
+# @Time         : 2023/4/21 11:44
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+from meutils.pipe import *
+from meutils.office_automation.pdf import extract_text, pdf2text
+
+from chatllm.utils import textsplitter
+from chatllm.applications.chatann import ChatANN
+
+
+class ChatPDF(ChatANN):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def create_ann_index(self, filename):
+        bytes_array = Path(filename).read_bytes()
+        texts = extract_text(stream=bytes_array)
+        texts = textsplitter(texts)
+        return super().create_ann_index(texts)
+
+
+if __name__ == '__main__':
+    filename = '../../data/财报.pdf'
+    bytes_array = Path(filename).read_bytes()
+    texts = extract_text(stream=bytes_array)
+    texts = textsplitter(texts)
+    print(texts)
