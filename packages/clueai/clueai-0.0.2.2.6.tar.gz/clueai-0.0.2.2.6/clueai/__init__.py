e8bd2524fb17a2624c2c71c46388dc3be1dc3bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:56 +0000

    Add package clueai/0.0.2.2.6/clueai-0.0.2.2.6.tar.gz

---
+++ packages/clueai/clueai-0.0.2.2.6.tar.gz/clueai-0.0.2.2.6/clueai/__init__.py
@@ -0,0 +1,13 @@
+from .client import Client
+from .error import ClueaiError
+
+MODELFUN_API_URL = 'https://www.modelfun.cn/modelfun/api/'
+TEXT_TO_IMAGE_URL= "https://clueai.cn/clueai/text2image"
+CLUEAI_API_URL = "https://clueai.cn/clueai"
+MODELFUN_VERSION = '2022-08-08'
+GENERATE_URL = 'serving_api'
+CLASSIFY_URL = 'serving_api'
+EXTRACT_URL = 'extract'
+CHECK_API_KEY_URL = 'check_api_key'
+TOKENIZE_URL = 'tokenize'
+TIMEOUT = 200
