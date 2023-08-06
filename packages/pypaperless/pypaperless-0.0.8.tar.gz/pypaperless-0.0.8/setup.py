3eaf6aacdb35fbc90e6b6a11a3d86e66b8c48543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:53 +0000

    Add package pypaperless/0.0.8/pypaperless-0.0.8.tar.gz

---
+++ packages/pypaperless/pypaperless-0.0.8.tar.gz/pypaperless-0.0.8/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pypaperless",
+    version="0.0.8",
+    author="tbsch",
+    author_email="info@tbsch.de",
+    description="A small API wrapper for paperless-ngx dms.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/grdn1337/paperless-api",
+    project_urls={
+        "Bug Tracker": "https://github.com/grdn1337/paperless-api/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["pypaperless"],
+    python_requires=">=3.6",
+)
