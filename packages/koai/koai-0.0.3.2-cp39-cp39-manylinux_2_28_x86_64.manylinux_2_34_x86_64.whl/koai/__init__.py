e1221eb111a28822b0c1b51a03282e19520044f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:45 +0000

    Add package koai/0.0.3.2/koai-0.0.3.2-cp39-cp39-manylinux_2_28_x86_64.manylinux_2_34_x86_64.whl

---
+++ packages/koai/koai-0.0.3.2-cp39-cp39-manylinux_2_28_x86_64.manylinux_2_34_x86_64.whl/koai/__init__.py
@@ -0,0 +1,8 @@
+from . import benchmarks
+from .benchmarks import finetune, TaskInfo
+from .utils import *
+from rs_utils import *
+
+__version__ = "0.0.3.2"
+
+__all__ = ["benchmarks", "finetune", "TaskInfo", "BM25"]  # more functions will be added soon...
