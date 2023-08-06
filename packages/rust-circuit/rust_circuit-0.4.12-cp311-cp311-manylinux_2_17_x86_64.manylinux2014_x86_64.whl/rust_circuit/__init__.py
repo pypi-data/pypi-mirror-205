26a6d07f6f1e6d2148d3fde3712fc13578a62d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:00 +0000

    Add package rust-circuit/0.4.12/rust_circuit-0.4.12-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/rust-circuit/rust_circuit-0.4.12-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/rust_circuit/__init__.py
@@ -0,0 +1,7 @@
+# isort: skip_file
+
+from .general_function_spec_base import (
+    GeneralFunctionSpecBase as GeneralFunctionSpecBase,
+    MultiOutputGeneralFunctionSpecBase as MultiOutputGeneralFunctionSpecBase,
+)
+from ._rust import *
