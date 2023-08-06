b4e7a46230731b0efb4d7f110efcc1b9ab4d2a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:13 +0000

    Add package ext-mongo/0.4.0/ext-mongo-0.4.0.tar.gz

---
+++ packages/ext-mongo/ext-mongo-0.4.0.tar.gz/ext-mongo-0.4.0/tests/__main__.py
@@ -0,0 +1,47 @@
+from datetime import datetime, date
+
+from bson import ObjectId
+
+from mongo import Document, PrimaryKey
+
+
+class Doc(Document):
+    key: int | PrimaryKey
+    a1: str  # Required
+    a2: int | None
+    a3: bool | None
+    a4: float | None
+    a5: ObjectId | None
+    a6: datetime | None
+    a7: date | None
+    b1: list[str]
+    b2: list[int]
+    b3: list[bool]
+    b4: list[float]
+    b5: list[ObjectId]
+    b6: list[datetime]
+    b7: list[date]
+    c1: dict
+
+
+KEY = 0
+A1 = "a1"
+
+d = Doc(key=KEY, a1=A1).save()
+
+assert Doc.get(KEY) == d
+assert Doc.find(a1=A1) == d
+assert Doc.find_all(a1=A1) == [d]
+assert d.str_id == str(KEY)
+assert d.to_dict() == {
+    "_id": KEY,
+    "a1": A1,
+    "b1": [],
+    "b2": [],
+    "b3": [],
+    "b4": [],
+    "b5": [],
+    "b6": [],
+    "b7": [],
+    "c1": {},
+}
