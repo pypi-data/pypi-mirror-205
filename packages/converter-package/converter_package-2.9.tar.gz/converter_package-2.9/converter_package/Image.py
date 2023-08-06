3dfcb82f086f0bfb506b1c7be3e8a01f2ccf7b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:08 +0000

    Add package converter-package/2.9/converter_package-2.9.tar.gz

---
+++ packages/converter-package/converter_package-2.9.tar.gz/converter_package-2.9/converter_package/Image.py
@@ -0,0 +1,28 @@
+class Image:
+
+    def set_component(self, component):
+        self.__component = component
+
+    def get_component(self):
+        return self.__component
+
+    def set_image_name(self, image):
+        self.__image = image
+
+    def get_image_name(self):
+        return self.__image
+
+    def set_image_type(self, type):
+        self.__type = type
+
+    def get_image_type(self):
+        return self.__type
+
+    def set_path(self, path):
+        self.__path = path
+
+    def get_path(self):
+        return self.__path
+
+
+
