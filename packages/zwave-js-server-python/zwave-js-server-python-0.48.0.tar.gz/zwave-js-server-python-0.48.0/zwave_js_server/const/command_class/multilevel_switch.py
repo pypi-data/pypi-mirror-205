4f84c2535af73383f44bb53b5e467f43978a8cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:42 +0000

    Add package zwave-js-server-python/0.48.0/zwave-js-server-python-0.48.0.tar.gz

---
+++ packages/zwave-js-server-python/zwave-js-server-python-0.48.0.tar.gz/zwave-js-server-python-0.48.0/zwave_js_server/const/command_class/multilevel_switch.py
@@ -0,0 +1,20 @@
+"""Constants for the Multilevel Switch CC."""
+from __future__ import annotations
+
+from enum import IntEnum
+
+SET_TO_PREVIOUS_VALUE = 255
+
+COVER_OPEN_PROPERTY = "Open"
+COVER_UP_PROPERTY = "Up"
+COVER_ON_PROPERTY = "On"
+COVER_CLOSE_PROPERTY = "Close"
+COVER_DOWN_PROPERTY = "Down"
+COVER_OFF_PROPERTY = "Off"
+
+
+class MultilevelSwitchCommand(IntEnum):
+    """Enum for known multilevel switch notifications."""
+
+    START_LEVEL_CHANGE = 4
+    STOP_LEVEL_CHANGE = 5
