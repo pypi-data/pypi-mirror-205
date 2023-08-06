10566a5903894b7a88211409f44b94e7083e29a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:01 +0000

    Add package dingraia/1.0.1/dingraia-1.0.1.tar.gz

---
+++ packages/dingraia/dingraia-1.0.1.tar.gz/dingraia-1.0.1/README.md
@@ -0,0 +1,59 @@
+﻿# 这是什么？
+这个是一个关于能套用Graia-Ariadne项目的小部分模块
+并运行的异步钉钉机器人代码，用来运行 基础 的Webhook类型
+的微型机器人，只实现了10%的功能，将来可能会完善。
+作者是Python废物，请不要催，你行你上。
+
+# 特点
+支持协程，支持阻塞函数的并发，函数捕捉报错等，支持At含有userid的人员
+
+# 注意
+本项目适用于一般聊天机器人开发，不建议用于生产环境
+作者自身已经挂着该框架，保证框架确实可用
+
+# 需求
+首先得有一个公网IP，还得拥有企业内部开发权限
+
+# 实现方法
+通过使用简单的Flask服务端和非常简单的装饰器进行非常简单
+的广播方法。
+
+# 如何使用？
+把导入的模块相应地替换成Dingraia中的模块即可。
+具体请观看[Dingraia使用方法](https://wps.lxyddice.top/meihuaguangshuo/dingraia%E4%BD%BF%E7%94%A8%E6%96%B9%E6%B3%95/)
+
+由于Cloudflare和lj服务器的原因，博客可能无法使用，如需帮助请联系我
+## 接收函数
+```python
+@channel.use(ListenerSchema(listening_events[GroupMessage]))  # 目前只支持GroupMessage
+async def example(group: Group):  # 此处暂不支持传入机器人实例
+    ...
+```
+当然，也可以这样
+```python
+@channel.use(ListenerSchema(listening_events[GroupMessage]))
+def example(group: Group):  # 此处暂不支持传入机器人实例
+    ...
+```
+它们都会并发执行
+~~(你不会异步的话建议直接使用def，这样理论上效率会快一点)~~
+## 发送消息
+```python
+app = Dingtalk()
+app.send_message(MessageChain("Message")) # 当然也可以传入任意对象，前提是支持str方法
+# 从 element 导入元素即可发送 MarkDown, ActionCard等支持的消息卡片，如
+app.send_message(MarkDown(...))
+```
+注意：机器人的发送提示实际是在准备发送时提示的，不一定代表确实发送成功
+
+# 兼容度？
+30%...
+
+# TODO
+Flask服务端分离，Websocket链接，消息等待
+
+# 最后要说
+求梨膏，去看看[Graia](https://github.com/GraiaProject/Ariadne)项目吧，这个机器人框架真的很好用，
+至少目前用起来真的很不错。
+
+
---
+++ packages/dingraia/dingraia-1.0.1.tar.gz/dingraia-1.0.1/slog/__init__.py
@@ -0,0 +1,137 @@
+﻿import inspect
+import datetime
+from .element import *
+
+
+color_map = {
+        "red"    : "\033[0;31m",
+        "green"  : "\033[0;32m",
+        "yellow" : "\033[0;33m",
+        "blue"   : "\033[0;34m",
+        "magenta": "\033[0;35m",
+        "cyan"   : "\033[0;36m",
+        "grey"  : "\033[0;37m",
+        "white"  : "\033[0;38m",
+        "RED"    : "\033[0;41m",
+        "GREEN"  : "\033[0;42m",
+        "YELLOW" : "\033[0;43m",
+        "BLUE"   : "\033[0;44m",
+        "MAGENTA": "\033[0;45m",
+        "CYAN"   : "\033[0;46m",
+        "GREY"  : "\033[0;47m",
+        "WHITE"  : "\033[0;48m",
+        "reset"  : "\033[0m",
+    }
+
+
+def colorer(message: str, level_color: str = '', f_sign: str = "<", e_sign: str = ">", ignore_sign: str = "\\"):
+    if level_color and level_color not in color_map:
+        raise ValueError("Using level color with invalid color!")
+    message = message.replace(f"{ignore_sign}{f_sign}", "<Ignore>")
+    for color in color_map:
+        message = message.replace(f"{f_sign}/{color}{e_sign}", color_map["reset"] + (color_map[level_color] if level_color else ""))
+        message = message.replace(f"{f_sign}{color}{e_sign}", (color_map["reset"] if level_color else "") + color_map[color])
+    message = message.replace(f"{f_sign}/{e_sign}", color_map["reset"] + (color_map[level_color] if level_color else ""))
+    message = message.replace("<Ignore>", f"{f_sign}")
+    if not message.endswith(color_map['reset']):
+        message += color_map['reset']
+    return message
+
+
+class logger:
+    
+    @classmethod
+    def info(
+            cls,
+            __message,
+            module_name: str = "",
+            color: str = "",
+            colors: bool = False,
+            f_sign: str = "<",
+            e_sign: str = ">",
+            ignore_sign: str = "\\"
+    ):
+        __time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+        lies = cls._get_caller(inspect.currentframe())
+        if not color:
+            if colors:
+                __message = colorer(__message, f_sign=f_sign, e_sign=e_sign, ignore_sign=ignore_sign)
+            print(f"{__time} I/{f'{lies[1]}:{lies[2]}' if not module_name else module_name}: {__message}")
+        else:
+            if color in color_map:
+                if colors:
+                    __message = colorer(__message, level_color=color, f_sign=f_sign, e_sign=e_sign, ignore_sign=ignore_sign)
+                print(f"{color_map[color]}{__time} I/{f'{lies[1]}:{lies[2]}' if not module_name else module_name}: {__message}{color_map['reset']}")
+                
+    @classmethod
+    def warning(
+            cls,
+            __message,
+            module_name: str = "",
+            colors: bool = False,
+            f_sign: str = "<",
+            e_sign: str = ">",
+            ignore_sign: str = "\\"
+    ):
+        __time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+        lies = cls._get_caller(inspect.currentframe())
+        if colors:
+            __message = colorer(__message, level_color="yellow", f_sign=f_sign, e_sign=e_sign, ignore_sign=ignore_sign)
+        print(Yellow(f"{__time} W/{f'{lies[1]}:{lies[2]}' if not module_name else module_name}: {__message}"))
+        
+    @classmethod
+    def success(
+            cls,
+            __message,
+            module_name: str = "",
+            colors: bool = False,
+            f_sign: str = "<",
+            e_sign: str = ">",
+            ignore_sign: str = "\\"
+    ):
+        __time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+        lies = cls._get_caller(inspect.currentframe())
+        if colors:
+            __message = colorer(__message, level_color="green", f_sign=f_sign, e_sign=e_sign, ignore_sign=ignore_sign)
+        print(Green(f"{__time} S/{f'{lies[1]}:{lies[2]}' if not module_name else module_name}: {__message}"))
+        
+    @classmethod
+    def error(
+            cls,
+            __message,
+            module_name: str = "",
+            colors: bool = False,
+            f_sign: str = "<",
+            e_sign: str = ">",
+            ignore_sign: str = "\\"
+    ):
+        __time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+        lies = cls._get_caller(inspect.currentframe())
+        if colors:
+            __message = colorer(__message, level_color="red", f_sign=f_sign, e_sign=e_sign, ignore_sign=ignore_sign)
+        print(Red(f"{__time} E/{f'{lies[1]}:{lies[2]}' if not module_name else module_name}: {__message}"))
+        
+    @classmethod
+    def critical(
+            cls,
+            __message,
+            module_name: str = "",
+            colors: bool = False,
+            f_sign: str = "<",
+            e_sign: str = ">",
+            ignore_sign: str = "\\"
+    ):
+        __time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+        lies = cls._get_caller(inspect.currentframe())
+        if colors:
+            __message = colorer(__message, level_color="white RED", f_sign=f_sign, e_sign=e_sign, ignore_sign=ignore_sign)
+        print(White(RedBackGround(f"{__time} E/{f'{lies[1]}:{lies[2]}' if not module_name else module_name}: {__message}")))
+
+    @staticmethod
+    def _get_caller(f_back) -> list:
+        caller_frame = f_back.f_back
+        module_name = caller_frame.f_back.f_globals["__name__"]
+        line = caller_frame.f_lineno
+        func_name = caller_frame.f_code.co_name
+        return [module_name, func_name, line]
+    
