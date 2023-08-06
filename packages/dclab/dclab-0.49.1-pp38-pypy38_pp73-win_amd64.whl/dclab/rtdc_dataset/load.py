5c84a514273088a1a03fcd4118b2bdb130d30f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:27 +0000

    Add package dclab/0.49.1/dclab-0.49.1-pp38-pypy38_pp73-win_amd64.whl

---
+++ packages/dclab/dclab-0.49.1-pp38-pypy38_pp73-win_amd64.whl/dclab/rtdc_dataset/load.py
@@ -0,0 +1,57 @@
+"""Load RT-DC datasets"""
+import pathlib
+
+from .core import RTDCBase
+from . import fmt_dict, fmt_dcor, fmt_hdf5, fmt_tdms, fmt_hierarchy
+
+
+def load_file(path, identifier=None, **kwargs):
+    path = pathlib.Path(path).resolve()
+    for fmt in [fmt_hdf5.RTDC_HDF5, fmt_tdms.RTDC_TDMS]:
+        if fmt.can_open(path):
+            return fmt(path, identifier=identifier, **kwargs)
+    else:
+        raise ValueError("Unknown file format: '{}'".format(path.suffix))
+
+
+def new_dataset(data, identifier=None, **kwargs):
+    """Initialize a new RT-DC dataset
+
+    Parameters
+    ----------
+    data:
+        can be one of the following:
+
+        - dict
+        - .tdms file
+        - .rtdc file
+        - subclass of `RTDCBase`
+          (will create a hierarchy child)
+        - DCOR resource URL
+    identifier: str
+        A unique identifier for this dataset. If set to `None`
+        an identifier is generated.
+    kwargs: dict
+        Additional parameters passed to the RTDCBase subclass
+
+    Returns
+    -------
+    dataset: subclass of :class:`dclab.rtdc_dataset.RTDCBase`
+        A new dataset instance
+    """
+    if isinstance(data, dict):
+        return fmt_dict.RTDC_Dict(data, identifier=identifier, **kwargs)
+    elif fmt_dcor.is_dcor_url(data):
+        return fmt_dcor.RTDC_DCOR(data, identifier=identifier, **kwargs)
+    elif isinstance(data, RTDCBase):
+        return fmt_hierarchy.RTDC_Hierarchy(data, identifier=identifier,
+                                            **kwargs)
+    elif isinstance(data, (pathlib.Path, str)):
+        path = pathlib.Path(data).resolve()
+        if not path.exists():
+            raise FileNotFoundError(f"Could not find file '{path}'!")
+        else:
+            return load_file(data, identifier=identifier, **kwargs)
+    else:
+        msg = "data type not supported: {}".format(data.__class__)
+        raise NotImplementedError(msg)
