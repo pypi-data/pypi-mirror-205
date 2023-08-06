a58773f2124694cd3801fe68d85b5cbbdc50db3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:12 +0000

    Add package dinohub-seedwork-be/0.1.13/dinohub_seedwork_be-0.1.13.tar.gz

---
+++ packages/dinohub-seedwork-be/dinohub_seedwork_be-0.1.13.tar.gz/dinohub_seedwork_be-0.1.13/dino_seedwork_be/domain/value_object/FullName.py
@@ -0,0 +1,77 @@
+from dino_seedwork_be.domain.exceptions import DomainException
+from dino_seedwork_be.domain.value_object.AbstractValueObject import \
+    ValueObject
+
+__all__ = ["FullName"]
+
+
+class FullName(ValueObject):
+    _first_name: str | None = None
+    _last_name: str | None = None
+
+    def __init__(self, first_name: str | None, last_name: str | None):
+        super().__init__()
+        if first_name is not None:
+            self._set_first_name(first_name)
+        if last_name is not None:
+            self._set_last_name(last_name)
+
+    def __eq__(self, obj):
+        if isinstance(obj, FullName):
+            return (
+                self.first_name() == obj.first_name()
+                and self.last_name() == obj.last_name()
+            )
+        return False
+
+    def first_name(self) -> str | None:
+        return self._first_name
+
+    def last_name(self) -> str | None:
+        return self._last_name
+
+    def as_formatted_name(self):
+        return f"{self._last_name} {self._first_name}"
+
+    def with_changed_first_name(self, aFirstName: str):
+        return FullName(first_name=aFirstName, last_name=self._last_name)
+
+    def with_changed_last_name(self, aSecondName: str):
+        return FullName(last_name=aSecondName, first_name=self._first_name)
+
+    def _set_first_name(self, a_value: str):
+        try:
+            self.assert_argument_not_empty(a_value, "First name is required").unwrap()
+            self.assert_argument_length(
+                a_value,
+                a_minimum=1,
+                a_maximum=50,
+                a_message="First name must be 50 characters or less",
+            ).unwrap()
+            self.assert_argument_regex(
+                a_value,
+                r"^[A-Z\s](?:[A-Za-z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễếệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s])+$",
+                "First name must be at least one character in length, \
+                starting with a capital letter.",
+            ).unwrap()
+            self._first_name = a_value
+        except Exception as error:
+            raise DomainException(code="FIRST_NAME_INVALID", message=str(error))
+
+    def _set_last_name(self, a_value: str):
+        try:
+            self.assert_argument_not_empty(a_value, "Last name is required").unwrap()
+            self.assert_argument_length(
+                a_value,
+                a_minimum=1,
+                a_maximum=50,
+                a_message="last name must be 50 characters or less",
+            ).unwrap()
+            self.assert_argument_regex(
+                a_value,
+                r"^[A-Z\s](?:[A-Za-z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễếệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s])+$",
+                "Last name must be at least one character in length.",
+            ).unwrap()
+            self._last_name = a_value
+        except Exception as error:
+            raise DomainException(code="LAST_NAME_INVALID", message=str(error))
