859aca8142a2023d6e4641fac0fbee8012f7283b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:35 +0000

    Add package pie-extended/0.1.1/pie_extended-0.1.1-py2.py3-none-any.whl

---
+++ packages/pie-extended/pie_extended-0.1.1-py2.py3-none-any.whl/pie_extended/models/fro/tokenizer.py
@@ -0,0 +1,77 @@
+import regex as re
+from typing import List, Generator, Tuple
+
+from pie_extended.pipeline.tokenizers.memorizing import MemorizingTokenizer
+from pie_extended.pipeline.tokenizers.utils.excluder import (
+    ExcluderPrototype,
+    DottedNumberExcluder,
+    ApostropheExcluder,
+    ReferenceExcluder,
+    CharRegistry
+)
+
+_Dots_except_apostrophe = r".?!\"“”\"«»…\[\]\(\)„“"
+_Dots_collections = r"[" + _Dots_except_apostrophe + "‘’]"
+
+
+class FroMemorizingTokenizer(MemorizingTokenizer):
+    re_add_space_around_punct = re.compile(r"(\s*)([^\w\s])(\s*)")
+    re_remove_ending_apostrophe = re.compile(r"(?<=\w)([\'’ʼ])")
+    _sentence_boundaries = re.compile(
+        r"([" + _Dots_except_apostrophe + r"]+\s*)+"
+    )
+
+    def __init__(self):
+        super(FroMemorizingTokenizer, self).__init__()
+        self.tokens = []
+        self.char_registry: CharRegistry = CharRegistry()
+        self.normalizers: Tuple[ExcluderPrototype, ...] = (
+            ApostropheExcluder(char_registry=self.char_registry),
+            ReferenceExcluder(char_registry=self.char_registry),
+            DottedNumberExcluder(char_registry=self.char_registry),
+        )
+
+    @staticmethod
+    def _sentence_tokenizer_merge_matches(match):
+        """ Best way we found to deal with repeating groups"""
+        start, end = match.span()
+        return match.string[start:end] + "<SPLIT>"
+
+    def _real_sentence_tokenizer(self, string: str) -> List[str]:
+        string = self._sentence_boundaries.sub(self._sentence_tokenizer_merge_matches, string)
+
+        for normalizer in self.normalizers:
+            string = normalizer.after_sentence_tokenizer(string)
+
+        return string.split("<SPLIT>")
+
+    def _real_word_tokenizer(self, text: str, lower: bool = False) -> List[str]:
+        if lower:
+            text = text.lower()
+        text = text.split()
+        return text
+
+    def sentence_tokenizer(self, text: str, lower: bool = False) -> Generator[List[str], None, None]:
+        sentences = list()
+        data = self.normalizer(text)
+        for sent in self._real_sentence_tokenizer(data):
+            sent = sent.strip()
+            if sent:
+                sentences.append(self.word_tokenizer(sent))
+        yield from sentences
+
+    def normalizer(self, data: str) -> str:
+        for excluder in self.normalizers:
+            data = excluder.before_sentence_tokenizer(data)
+
+        data = self.re_add_space_around_punct.sub(
+            r" \g<2> ",
+            data
+        )
+        return data
+
+    def replacer(self, inp: str):
+        for excluder in self.normalizers:
+            if not excluder.can_be_replaced and excluder.exclude_regexp.match(inp):
+                return inp
+        return self.re_remove_ending_apostrophe.sub("", inp)
