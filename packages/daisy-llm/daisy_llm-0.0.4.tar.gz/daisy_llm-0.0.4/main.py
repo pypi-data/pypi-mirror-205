9a38f7f33b509d4a2a40942b26d90e48e3c6f004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:24 +0000

    Add package daisy-llm/0.0.4/daisy_llm-0.0.4.tar.gz

---
+++ packages/daisy-llm/daisy_llm-0.0.4.tar.gz/daisy_llm-0.0.4/main.py
@@ -0,0 +1,13 @@
+import threading
+
+import src.daisy_llm.ModuleLoader as module_loader
+import src.daisy_llm.ContextHandlers as context_handlers
+
+ch = context_handlers('daisy.db')
+
+#Instantiate ModuleLoader and ContextHandlers for global use by front-ends
+ml = module_loader(ch, "modules", "configs.yaml")
+update_modules_loop_thread = threading.Thread(target=ml.update_modules_loop)
+update_modules_loop_thread.start()
+
+ml.process_main_start_instances()
