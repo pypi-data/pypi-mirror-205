297f81d4e9423397f7e4d214f40b1316a919cecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:42 +0000

    Add package oreoTO/0.2/oreoTO-0.2.tar.gz

---
+++ packages/oreoTO/oreoTO-0.2.tar.gz/oreoTO-0.2/oreoTO/__init__.py
@@ -0,0 +1,1 @@
+from.to_info import *
---
+++ packages/oreoTO/oreoTO-0.2.tar.gz/oreoTO-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name= 'oreoTO',
+    version='0.2',
+    author= 'oreoTO',
+    description='Giting info for any account in the tiktok',
+    packages= ['oreoTO'],
+    author_email='zyaahee@gmail.com',
+    zip_safe=False,
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
