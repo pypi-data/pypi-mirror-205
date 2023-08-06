ed21518f48b55bb0c111eca9dde9fba66e2e1931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:14 +0000

    Add package anydoc/0.0.0/anydoc-0.0.0-py3-none-any.whl

---
+++ packages/anydoc/anydoc-0.0.0-py3-none-any.whl/anydoc/__init__.py
@@ -0,0 +1,4 @@
+"""`anydoc `: 🔌 Turn your GitHub Markdown documentation into a Q&A bot for Slack (more integrations to come soon!)"""
+
+__author__ = "Alvaro Bartolome <alvarobartt@gmail.com>"
+__version__ = "0.0.0"
