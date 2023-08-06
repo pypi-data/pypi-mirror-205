adf84d6f0e3db2c40edb404daf7af9be83d36a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:03 +0000

    Add package airportcities/1.0.0/airportcities-1.0.0.tar.gz

---
+++ packages/airportcities/airportcities-1.0.0.tar.gz/airportcities-1.0.0/airportcities/airportcities.py
@@ -0,0 +1,25 @@
+airport_cities = {
+        "NYC": "New York City",
+        "LA": "Los Angeles",
+        "CHI": "Chicago",
+        "HOU": "Houston",
+        "DAL": "Dallas",
+        "MIA": "Miami",
+        "ATL": "Atlanta",
+        "DC": "Washington D.C",
+        "BOS": "Boston",
+        "SEA": "Seattle",
+        "SFO": "San Francisco",
+        "DEN": "Denver",
+        "PHX": "Phoenix",
+        "LV": "Las Vegas",
+        "TOR": "Toronto",
+        "LON": "London",
+        "PAR": "Paris",
+        "ROM": "Rome",
+        "SYD": "Sydney",
+        "TOK": "Tokyo",
+        # Add more airport countires and names as needed
+    }
+def airport_city():
+    return airport_cities
