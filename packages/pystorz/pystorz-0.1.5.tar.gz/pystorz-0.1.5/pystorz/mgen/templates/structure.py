e87764492e15af90691a4145a51a0e4bd5abfc18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:27 +0000

    Add package pystorz/0.1.5/pystorz-0.1.5.tar.gz

---
+++ packages/pystorz/pystorz-0.1.5.tar.gz/pystorz-0.1.5/pystorz/mgen/templates/structure.py
@@ -0,0 +1,39 @@
+ 
+
+def {{ data.name }}Factory():
+	ret = _{{ data.name }}()
+	{% for prop in data.properties %}
+	ret.{{ prop.name }}_ = {{ prop.default }}{% endfor %}
+	
+	return ret
+
+
+class _{{ data.name }}({{data.implements}}):
+
+	def __init__(self):
+		{% for prop in data.properties %}
+		self.{{ prop.name }}_ = {{prop.default}}{% endfor %}
+
+	{% for prop in data.properties %}
+	{% if prop.name != "External" %}
+
+	def Set{{ prop.name }}(self, val):
+		{% if prop.type == "datetime" %}
+		self.{{ prop.name }}_ = utils.datetime_string(val)
+		{% else %}
+		self.{{ prop.name }}_ = val
+		{% endif %}
+
+	def {{ prop.name }}(self):
+		{% if prop.type == "datetime" %}
+		return utils.datetime_parse(self.{{ prop.name }}_)
+		{% else %}
+		return self.{{ prop.name }}_
+		{% endif %}
+
+	{% endif %}
+	{% endfor %}
+
+	def Clone(entity):
+		return utils.clone_object(entity, Schema())
+
