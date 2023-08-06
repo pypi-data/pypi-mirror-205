bdbb4be44a0adddb1069961c5625c13318a0b1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:01 +0000

    Add package cheeseshop/0.0.0/cheeseshop-0.0.0.tar.gz

---
+++ packages/cheeseshop/cheeseshop-0.0.0.tar.gz/cheeseshop-0.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""The setup script."""
+
+# Third party modules
+from setuptools import setup
+
+setup(name='cheeseshop',
+      version='0.0.0',
+      description='place holder',
+      url='http://github.com/SK/cheeseshop',
+      author='sking',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['cheeseshop'],
+      zip_safe=False)
