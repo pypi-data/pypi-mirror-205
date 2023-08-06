491a7d62baaff77bc316c8d72690d3cb377415f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:16 +0000

    Add package item-selector/1.0.0/item_selector-1.0.0.tar.gz

---
+++ packages/item-selector/item_selector-1.0.0.tar.gz/item_selector-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='item_selector',
+    version='1.0.0',
+    description='A Python library to select a random item from a list.',
+    author='Nitish',
+    author_email='ksainitish72@gmail.com',
+    py_modules=['item_selector'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
