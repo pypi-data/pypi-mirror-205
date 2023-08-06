37f892b41fd5dda82edd505bd5f40299bae627f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:37 +0000

    Add package bnlp-toolkit/3.3.1/bnlp_toolkit-3.3.1-py3-none-any.whl

---
+++ packages/bnlp-toolkit/bnlp_toolkit-3.3.1-py3-none-any.whl/bnlp/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "3.3.1"
+
+
+import os
+from bnlp.pos import POS
+from bnlp.ner import NER
+from bnlp.tokenizer.nltk import NLTKTokenizer
+from bnlp.tokenizer.basic import BasicTokenizer
+from bnlp.tokenizer.sentencepiece import SentencepieceTokenizer
+from bnlp.embedding.word2vec import BengaliWord2Vec
+from bnlp.embedding.glove import BengaliGlove
+from bnlp.embedding.doc2vec import BengaliDoc2vec
+from bnlp.cleantext.clean import CleanText
