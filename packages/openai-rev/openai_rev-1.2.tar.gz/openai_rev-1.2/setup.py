4ed9293a07a5a91ace4a27b28e000e1a6a518cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:06 +0000

    Add package openai-rev/1.2/openai_rev-1.2.tar.gz

---
+++ packages/openai-rev/openai_rev-1.2.tar.gz/openai_rev-1.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', encoding='utf-8') as f:
+  description = f.read()
+
+setuptools.setup(
+  name='openai_rev',
+  version='1.2',
+  license='Unlicense',
+  author='bobbill',
+  description='making music with OpenAI (GPT-4)',
+  packages=['openai_rev'],
+  install_requires=['simpleaudio', 'numpy'],
+  long_description=description,
+  long_description_content_type='text/markdown',
+  classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+  ],
+)
