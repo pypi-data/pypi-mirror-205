91ffa69e5f344f60a24e01b98ad5e554d12c8614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:03 +0000

    Add package leptonai/0.1/leptonai-0.1.tar.gz

---
+++ packages/leptonai/leptonai-0.1.tar.gz/leptonai-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="leptonai",
+    version="0.1",
+    description="Lepton AI Inc.",
+    author="Lepton AI Inc.",
+    author_email="admin@lepton.ai",
+    url="https://lepton.ai",
+    packages=find_packages(),
+)
