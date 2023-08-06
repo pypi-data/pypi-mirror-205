72204b06b67c836b4329698d45261a2d98c0eb63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:41 +0000

    Add package ptzipcam/0.0.5/ptzipcam-0.0.5.tar.gz

---
+++ packages/ptzipcam/ptzipcam-0.0.5.tar.gz/ptzipcam-0.0.5/setup.py
@@ -0,0 +1,55 @@
+import os
+import re
+
+import setuptools
+
+
+def read(filename):
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), filename)
+    with open(path, 'r') as f:
+        return f.read()
+
+
+def find_version(text):
+    match = re.search(r"^__version__\s*=\s*['\"](.*)['\"]\s*$", text,
+                      re.MULTILINE)
+    return match.group(1)
+
+
+AUTHOR = "Ian Ingram"
+DESC = "Package for controlling PTZ IP cameras that support ONVIF"
+
+setuptools.setup(
+    name="ptzipcam",
+    description=DESC,
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    version=find_version(read('ptzipcam/__init__.py')),
+    author=AUTHOR,
+    packages=['ptzipcam'],
+    include_package_data=True,
+    install_requires=[
+        'numpy==1.18.3',
+        'opencv-python==3.4.2.16',
+        'onvif-zeep',
+        'camml',
+    ],
+    extra_require={
+        'examples': [
+            'pyyaml',
+            'imutils',
+            'screeninfo',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: POSIX :: Linux',
+    ],
+    # would like to add this as a CLI tool but currently needs config file...
+    # entry_points = {
+    #     'console_scripts': ['aim_ptz_w_keyboard=ptzipcam.utils.aim_ptz_w_keyboard:main']
+    #},
+)
