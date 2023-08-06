611711e2a3b93bb9b27191937dc34f40da191739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:40 +0000

    Add package figures-07659443/0.1/figures_07659443-0.1.tar.gz

---
+++ packages/figures-07659443/figures_07659443-0.1.tar.gz/figures_07659443-0.1/figures_07659443/__init__.py
@@ -0,0 +1,8 @@
+from .circle import circle_area, circle_perimeter
+from .triangle import triangle_area, triangle_perimeter
+from .square import square_area, square_perimeter
+
+__all__ = ["circle_area", "circle_perimeter",
+           "triangle_area", "triangle_perimeter",
+           "square_area", "square_perimeter"
+           ]
---
+++ packages/figures-07659443/figures_07659443-0.1.tar.gz/figures_07659443-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name= "figures_07659443",
+      version= "0.1",
+      description= "With this program you can find areas and perimeters of 	different circles, triangles and squares",
+      packages= ["figures_07659443"],
+      author_email= "nevdokimov777@gmail.com",
+      zip_safe=False)
