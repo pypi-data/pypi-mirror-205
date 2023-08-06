3121c7bdc64fe825c78fa534cbc4785313cd9640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:27 +0000

    Add package xgh-say-hello/0.0.0/xgh_say_hello-0.0.0.tar.gz

---
+++ packages/xgh-say-hello/xgh_say_hello-0.0.0.tar.gz/xgh_say_hello-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xgh_say_hello',
+    author='xgh',
+    version='0.0.0',
+    description='Just for learning publish a python package',
+    packages=find_packages(),
+)
