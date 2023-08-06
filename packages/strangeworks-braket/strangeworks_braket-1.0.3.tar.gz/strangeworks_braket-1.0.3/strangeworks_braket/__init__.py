0346d23302085868974d2c06db9a2f578709aded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:13 +0000

    Add package strangeworks-braket/1.0.3/strangeworks_braket-1.0.3.tar.gz

---
+++ packages/strangeworks-braket/strangeworks_braket-1.0.3.tar.gz/strangeworks_braket-1.0.3/strangeworks_braket/__init__.py
@@ -0,0 +1,8 @@
+"""Strangeworks Braket SDK"""
+import importlib.metadata
+
+from .device import StrangeworksDevice  # noqa: F401
+from .task import StrangeworksQuantumTask  # noqa: F401
+
+
+__version__ = importlib.metadata.version("strangeworks-braket")
