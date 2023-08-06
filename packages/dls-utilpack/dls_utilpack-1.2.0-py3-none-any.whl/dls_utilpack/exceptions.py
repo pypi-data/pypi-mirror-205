6a0cd8350f255b10eec57e552c5d597c02ad82b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:02 +0000

    Add package dls-utilpack/1.2.0/dls_utilpack-1.2.0-py3-none-any.whl

---
+++ packages/dls-utilpack/dls_utilpack-1.2.0-py3-none-any.whl/dls_utilpack/exceptions.py
@@ -0,0 +1,26 @@
+class NotFound(RuntimeError):
+    pass
+
+
+# When something has no value set yet.
+class NotSet(RuntimeError):
+    pass
+
+
+class CapacityReached(RuntimeError):
+    pass
+
+
+class DuplicateLabelException(RuntimeError):
+    pass
+
+
+class DuplicateUuidException(RuntimeError):
+    pass
+
+
+class Factory:
+    def build(qualname):
+        if qualname == "dls_bxflow_api.exceptions.DuplicateLabelException":
+            return DuplicateLabelException
+        return None
