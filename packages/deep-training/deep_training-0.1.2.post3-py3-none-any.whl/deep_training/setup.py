ff8a658f17024fae138bfb8919cf114c29e66489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:17 +0000

    Add package deep-training/0.1.2.post3/deep_training-0.1.2.post3-py3-none-any.whl

---
+++ packages/deep-training/deep_training-0.1.2.post3-py3-none-any.whl/deep_training/setup.py
@@ -0,0 +1,26 @@
+#! -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+ignore = ['test','tests']
+setup(
+    name='deep_training',
+    version='0.1.2@post3',
+    description='an easy training architecture',
+    long_description='torch_training: https://github.com/ssbuild/deep_training.git',
+    license='Apache License 2.0',
+    url='https://github.com/ssbuild/deep_training',
+    author='ssbuild',
+    author_email='9727464@qq.com',
+    install_requires=['pytorch-lightning>=2',
+                      'fastdatasets>=0.9.6 , <= 1',
+                      'tfrecords >= 0.2.4 , <= 1',
+                      'sentencepiece',
+                      'numpy',
+                      'transformers >= 4.22',
+                      'seqmetric','scipy',
+                      'scikit-learn',
+                      'tqdm',
+                      'six'],
+    packages=[p for p in find_packages() if p not in ignore]
+)
