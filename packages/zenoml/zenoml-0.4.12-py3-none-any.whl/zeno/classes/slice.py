cdd305b00a26ee738d4c4c50d5fb9878b9d1e2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:40 +0000

    Add package zenoml/0.4.12/zenoml-0.4.12-py3-none-any.whl

---
+++ packages/zenoml/zenoml-0.4.12-py3-none-any.whl/zeno/classes/slice.py
@@ -0,0 +1,31 @@
+from typing import List, Optional, Union
+
+from zeno.classes.base import CamelModel, ZenoColumn
+
+
+class FilterPredicate(CamelModel):
+    column: ZenoColumn
+    # ==, !=, >, <, >=, <=, match
+    operation: str
+    value: Union[str, float, int, bool]
+    join: Optional[str]
+
+
+class FilterPredicateGroup(CamelModel):
+    predicates: List[Union["FilterPredicateGroup", FilterPredicate]]
+    join: str
+
+
+class FilterIds(CamelModel):
+    ids: Union[List[int], List[str]]
+
+
+class Slice(CamelModel):
+    slice_name: str
+    folder: str
+    filter_predicates: FilterPredicateGroup
+
+
+class SliceMetric(CamelModel):
+    metric: Union[float, None]
+    size: int
