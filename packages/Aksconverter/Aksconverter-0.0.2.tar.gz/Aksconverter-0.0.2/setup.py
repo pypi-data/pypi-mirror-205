374ceeda0f000fd56874a35c860e12ddbb5c914f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:35 +0000

    Add package Aksconverter/0.0.2/Aksconverter-0.0.2.tar.gz

---
+++ packages/Aksconverter/Aksconverter-0.0.2.tar.gz/Aksconverter-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: MacOS',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3.8'
+]
+ 
+setup(
+  name='Aksconverter',
+  version='0.0.2',
+  description='A very currency calculator',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Akshat Joshi',
+  author_email='x22137696@student.ncirl.ie',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Currency Calculator', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
