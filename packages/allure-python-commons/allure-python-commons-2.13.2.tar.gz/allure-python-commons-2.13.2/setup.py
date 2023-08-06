969cdf9c4b7908cc6f5f485f77ec0cd64f866e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:15 +0000

    Add package allure-python-commons/2.13.2/allure-python-commons-2.13.2.tar.gz

---
+++ packages/allure-python-commons/allure-python-commons-2.13.2.tar.gz/allure-python-commons-2.13.2/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+PACKAGE = "allure-python-commons"
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Topic :: Software Development :: Quality Assurance',
+    'Topic :: Software Development :: Testing',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3 :: Only',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+]
+
+install_requires = [
+    "attrs>=16.0.0",
+    "pluggy>=0.4.0",
+]
+
+
+def main():
+    setup(
+        name=PACKAGE,
+        use_scm_version={"root": "..", "relative_to": __file__},
+        setup_requires=['setuptools_scm'],
+        description="Common module for integrate allure with python-based frameworks",
+        url="https://github.com/allure-framework/allure-python",
+        author="QAMetaSoftware, Stanislav Seliverstov",
+        author_email="sseliverstov@qameta.io",
+        license="Apache-2.0",
+        classifiers=classifiers,
+        keywords="allure reporting report-engine",
+        packages=["allure_commons"],
+        package_dir={"allure_commons": 'src'},
+        install_requires=install_requires,
+        py_modules=['allure', 'allure_commons'],
+        python_requires='>=3.6'
+    )
+
+
+if __name__ == '__main__':
+    main()
