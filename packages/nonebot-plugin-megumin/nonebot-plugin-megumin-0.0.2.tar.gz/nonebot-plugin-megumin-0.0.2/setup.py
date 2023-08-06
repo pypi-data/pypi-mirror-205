d5cda29e0f58160929c8bc1ecc0cece0a4aedd45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:09 +0000

    Add package nonebot-plugin-megumin/0.0.2/nonebot-plugin-megumin-0.0.2.tar.gz

---
+++ packages/nonebot-plugin-megumin/nonebot-plugin-megumin-0.0.2.tar.gz/nonebot-plugin-megumin-0.0.2/setup.py
@@ -0,0 +1,36 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+
+VERSION = '0.0.2'
+DESCRIPTION = ' 让你的群友释放爆裂魔法吧！ '
+LONG_DESCRIPTION = ' Let your group members unleash their Explosion magic! '
+
+# Setting up
+setup(
+    name="nonebot-plugin-megumin",
+    version=VERSION,
+    author="youlanan",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python','nonebot','explosion'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
