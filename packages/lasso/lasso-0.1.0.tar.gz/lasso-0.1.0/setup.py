cddd86ec3ad48a31f79ad286c780060108069c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:22 +0000

    Add package lasso/0.1.0/lasso-0.1.0.tar.gz

---
+++ packages/lasso/lasso-0.1.0.tar.gz/lasso-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lasso",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Willem Pienaar",
+    description="A simple lasso package",
+    url="https://github.com/woop/lasso",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
