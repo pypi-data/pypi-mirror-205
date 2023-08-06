bd23c451c42038f772d605c8e974e21d919731a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:47 +0000

    Add package eia-odin-pkg-rioatmadja2018/0.1.1682651653/eia_odin_pkg_rioatmadja2018-0.1.1682651653-py3-none-any.whl

---
+++ packages/eia-odin-pkg-rioatmadja2018/eia_odin_pkg_rioatmadja2018-0.1.1682651653-py3-none-any.whl/eia/utils/constants.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from typing import List, Dict
+
+API_KEY: str = "CZdQsisRJzwOfqUWV3jiMPNEx3ZbHcuJ2VQus04i"
+STATE: List[str] = ["California", "Colorado", "Florida", "Massachusetts", "Minnesota", "New York", "Ohio", "Texas", "Washington"]
+REGION: Dict = {'New York': 'East Coast',
+                 'Massachusetts': 'East Coast',
+                 'Ohio': 'Midwest',
+                 'Minnesota': 'Midwest',
+                 'Texas': 'Gulf Coast',
+                 'Florida': 'Gulf Coast',
+                 'Colorado': 'Rocky Mountain',
+                 'California': 'West Coast',
+                 'Washington': 'West Coast'}
+
+REGION_REVERSE: Dict = {'East Coast': 'Massachusetts',
+                        'MidWest': 'Minnesota',
+                        'Gulf Coast': 'Florida',
+                        'Rocky Mountain': 'Colorado',
+                        'West Coast': 'Washington'}
+
+
