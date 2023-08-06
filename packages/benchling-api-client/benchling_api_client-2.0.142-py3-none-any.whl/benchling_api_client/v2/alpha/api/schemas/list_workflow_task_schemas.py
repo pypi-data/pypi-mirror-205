4a651e1ec3bd15a062a5b9d1c98d8c7bc7efc63d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:42 +0000

    Add package benchling-api-client/2.0.142/benchling_api_client-2.0.142-py3-none-any.whl

---
+++ packages/benchling-api-client/benchling_api_client-2.0.142-py3-none-any.whl/benchling_api_client/v2/alpha/api/schemas/list_workflow_task_schemas.py
@@ -0,0 +1,122 @@
+from typing import Any, Dict, Optional, Union
+
+import httpx
+
+from ...client import Client
+from ...models.not_found_error import NotFoundError
+from ...types import Response, UNSET, Unset
+
+
+def _get_kwargs(
+    *,
+    client: Client,
+    next_token: Union[Unset, str] = UNSET,
+    page_size: Union[Unset, int] = 50,
+) -> Dict[str, Any]:
+    url = "{}/workflow-task-schemas".format(client.base_url)
+
+    headers: Dict[str, Any] = client.httpx_client.headers
+    headers.update(client.get_headers())
+
+    cookies: Dict[str, Any] = client.httpx_client.cookies
+    cookies.update(client.get_cookies())
+
+    params: Dict[str, Any] = {}
+    if not isinstance(next_token, Unset) and next_token is not None:
+        params["nextToken"] = next_token
+    if not isinstance(page_size, Unset) and page_size is not None:
+        params["pageSize"] = page_size
+
+    return {
+        "url": url,
+        "headers": headers,
+        "cookies": cookies,
+        "timeout": client.get_timeout(),
+        "params": params,
+    }
+
+
+def _parse_response(*, response: httpx.Response) -> Optional[NotFoundError]:
+    if response.status_code == 404:
+        response_404 = NotFoundError.from_dict(response.json(), strict=False)
+
+        return response_404
+    return None
+
+
+def _build_response(*, response: httpx.Response) -> Response[NotFoundError]:
+    return Response(
+        status_code=response.status_code,
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(response=response),
+    )
+
+
+def sync_detailed(
+    *,
+    client: Client,
+    next_token: Union[Unset, str] = UNSET,
+    page_size: Union[Unset, int] = 50,
+) -> Response[NotFoundError]:
+    kwargs = _get_kwargs(
+        client=client,
+        next_token=next_token,
+        page_size=page_size,
+    )
+
+    response = client.httpx_client.get(
+        **kwargs,
+    )
+
+    return _build_response(response=response)
+
+
+def sync(
+    *,
+    client: Client,
+    next_token: Union[Unset, str] = UNSET,
+    page_size: Union[Unset, int] = 50,
+) -> Optional[NotFoundError]:
+    """ List workflow task schemas """
+
+    return sync_detailed(
+        client=client,
+        next_token=next_token,
+        page_size=page_size,
+    ).parsed
+
+
+async def asyncio_detailed(
+    *,
+    client: Client,
+    next_token: Union[Unset, str] = UNSET,
+    page_size: Union[Unset, int] = 50,
+) -> Response[NotFoundError]:
+    kwargs = _get_kwargs(
+        client=client,
+        next_token=next_token,
+        page_size=page_size,
+    )
+
+    async with httpx.AsyncClient() as _client:
+        response = await _client.get(**kwargs)
+
+    return _build_response(response=response)
+
+
+async def asyncio(
+    *,
+    client: Client,
+    next_token: Union[Unset, str] = UNSET,
+    page_size: Union[Unset, int] = 50,
+) -> Optional[NotFoundError]:
+    """ List workflow task schemas """
+
+    return (
+        await asyncio_detailed(
+            client=client,
+            next_token=next_token,
+            page_size=page_size,
+        )
+    ).parsed
