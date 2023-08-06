6ec57f15638a3356f0d1ddbe3cf88c3469cebe00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:27 +0000

    Add package simulai-toolkit/0.99.20/simulai_toolkit-0.99.20-py3-none-any.whl

---
+++ packages/simulai-toolkit/simulai_toolkit-0.99.20-py3-none-any.whl/simulai/__init__.py
@@ -0,0 +1,30 @@
+# (C) Copyright IBM Corp. 2019, 2020, 2021, 2022.
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#           http://www.apache.org/licenses/LICENSE-2.0
+
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+# (C) Copyright IBM Corporation 2017, 2018, 2019
+# U.S. Government Users Restricted Rights:  Use, duplication or disclosure restricted
+# by GSA ADP Schedule Contract with IBM Corp.
+#
+# Author: Joao Lucas S. Almeida <joao.lucas.sousa.almeida@ibm.com>
+
+import os
+
+engine_var = os.environ.get("engine")
+
+if engine_var is not None:
+    engine = engine_var
+else:
+    engine = "pytorch"
+
+__version__ = "1.3"
