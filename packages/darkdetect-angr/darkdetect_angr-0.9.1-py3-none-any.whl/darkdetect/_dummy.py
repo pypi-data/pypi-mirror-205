b32a761cf50d8c856471e8b635683d3681ec32f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:53 +0000

    Add package darkdetect-angr/0.9.1/darkdetect_angr-0.9.1-py3-none-any.whl

---
+++ packages/darkdetect-angr/darkdetect_angr-0.9.1-py3-none-any.whl/darkdetect/_dummy.py
@@ -0,0 +1,20 @@
+#-----------------------------------------------------------------------------
+#  Copyright (C) 2019 Alberto Sottile, Zachary Wimer
+#
+#  Distributed under the terms of the 3-clause BSD License.
+#-----------------------------------------------------------------------------
+
+from ._base_listener import BaseListener
+
+
+def theme():
+    return None
+
+
+class DummyListener(BaseListener):
+    """
+    A dummy listener class that implements nothing the abstract class does not
+    """
+
+
+__all__ = ("theme", "DummyListener")
