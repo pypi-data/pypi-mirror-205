ff7b73d6719be374856ee39e312c701f4b13ac55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:44 +0000

    Add package dead-simple-framework/1.3.8/dead_simple_framework-1.3.8.tar.gz

---
+++ packages/dead-simple-framework/dead_simple_framework-1.3.8.tar.gz/dead_simple_framework-1.3.8/setup.py
@@ -0,0 +1,23 @@
+
+"""Setup for Dead Simple Framework"""
+
+import setuptools
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Peter Swanson",
+    author_email="pswanson@ucdavis.edu",
+    name='dead_simple_framework',
+    license="MIT",
+    description='RESTful Flask framework with builtin MongoDB, Redis, Celery, Sentry and Slack integrations',
+    version='v1.3.8',
+    long_description=README,
+    url='https://github.com/Topazoo/dead_simple_framework',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=['flask', 'pymongo', 'celery', 'flask-cors', 'requests', 'redis', 'eventlet', 'pyOpenSSL', 'Flask-JWT-Extended', 'passlib', 'jsonschema', 'sentry-sdk[flask]', 'slack-sdk', 'Flask-PyMongo'],
+    long_description_content_type='text/markdown',
+    classifiers=[]
+)
