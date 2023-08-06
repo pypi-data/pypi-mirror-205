11b9b492c1adc148cd32afb1cbb7523fc7014548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:28 +0000

    Add package serverless-sdk/0.4.5/serverless-sdk-0.4.5.tar.gz

---
+++ packages/serverless-sdk/serverless-sdk-0.4.5.tar.gz/serverless-sdk-0.4.5/sls_sdk/base.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import List, Union
+
+from importlib_metadata import version
+from typing_extensions import Final
+
+SLS_ORG_ID: Final[str] = "SLS_ORG_ID"
+
+# module metadata
+__name__: Final[str] = "serverless-sdk"
+__version__: Final[str] = version(__name__)
+
+
+TraceId = str
+Nanoseconds = int
+DateStr = str
+
+TagType = Union[str, int, float, DateStr, bool, datetime]
+TagList = List[TagType]
+ValidTags = Union[TagType, TagList]
