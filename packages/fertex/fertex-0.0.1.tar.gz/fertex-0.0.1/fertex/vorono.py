104729e3148083e86ff214a01445574233394101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:30 +0000

    Add package fertex/0.0.1/fertex-0.0.1.tar.gz

---
+++ packages/fertex/fertex-0.0.1.tar.gz/fertex-0.0.1/fertex/vorono.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def the_func(n):
+    return n 
+
+F = the_func(1)
+
+print(F)
---
+++ packages/fertex/fertex-0.0.1.tar.gz/fertex-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='fertex',
+    version='0.0.1',
+    packages=find_packages(),
+    description='Test 2023-04-27',
+    
+)
