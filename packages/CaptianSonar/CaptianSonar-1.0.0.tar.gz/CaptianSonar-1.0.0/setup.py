ed62a3c03508b66c3dde3d143b8888de4490b8eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:50 +0000

    Add package CaptianSonar/1.0.0/CaptianSonar-1.0.0.tar.gz

---
+++ packages/CaptianSonar/CaptianSonar-1.0.0.tar.gz/CaptianSonar-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='CaptianSonar',
+    packages=['pypi'],
+    zip_safe=True,
+    version='1.0.0',
+    author='Michael Caggeso',
+    author_email='laurencaggeso@gmail.com',
+    long_description='Sending a folder to my brother',
+    description='Sending a folder to my brother',
+    url="https://pypi.org/search/?q=tradchat&o=",
+    requires=['girl'],
+    license='Michael`s License',
+    download_url="https://pypi.org/search/?q=tradchat&o=")
