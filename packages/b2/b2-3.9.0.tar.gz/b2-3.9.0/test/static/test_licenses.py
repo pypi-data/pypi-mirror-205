eeb37c1c2c0d745d9acbd5a6ccd58f0acc93c95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:53 +0000

    Add package b2/3.9.0/b2-3.9.0.tar.gz

---
+++ packages/b2/b2-3.9.0.tar.gz/b2-3.9.0/test/static/test_licenses.py
@@ -0,0 +1,30 @@
+######################################################################
+#
+# File: test/static/test_licenses.py
+#
+# Copyright 2020 Backblaze Inc. All Rights Reserved.
+#
+# License https://www.backblaze.com/using_b2_code.html
+#
+######################################################################
+
+from glob import glob
+from itertools import islice
+
+import pytest
+
+
+def test_files_headers():
+    for file in glob('**/*.py', recursive=True):
+        if file.startswith('build/'):
+            # built files naturally have a different file path than source files
+            continue
+        with open(file) as fd:
+            file = file.replace(
+                '\\', '/'
+            )  # glob('**/*.py') on Windows returns "b2\console_tool.py" (wrong slash)
+            head = ''.join(islice(fd, 9))
+            if 'All Rights Reserved' not in head:
+                pytest.fail('Missing "All Rights Reserved" in the header in: {}'.format(file))
+            if file not in head:
+                pytest.fail('Wrong file name in the header in: {}'.format(file))
