6d3c5894e84308be7c225a5c14c1c324ed96d3aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:04 +0000

    Add package PyMPDATA-examples/1.0.11/PyMPDATA-examples-1.0.11.tar.gz

---
+++ packages/PyMPDATA-examples/PyMPDATA-examples-1.0.11.tar.gz/PyMPDATA-examples-1.0.11/setup.py
@@ -0,0 +1,40 @@
+""" the magick behind ``pip install ...`` """
+from setuptools import setup, find_packages
+
+
+def get_long_description():
+    """ returns contents of README.md file """
+    with open("README.md", "r", encoding="utf8") as file:
+        long_description = file.read()
+    return long_description
+
+
+setup(
+    name='PyMPDATA-examples',
+    description='PyMPDATA usage examples reproducing results from literature'
+                ' and depicting how to use PyMPDATA in Python from Jupyter notebooks',
+    use_scm_version={"local_scheme": lambda _: "", "version_scheme": "post-release"},
+    setup_requires=['setuptools_scm'],
+    install_requires=['PyMPDATA',
+                      'open-atmos-jupyter-utils',
+                      'pystrict',
+                      'matplotlib',
+                      'ipywidgets',
+                      'scipy',
+                      'pint',
+                      'joblib',
+                      'sympy',
+                      'ghapi'],
+    author='https://github.com/open-atmos/PyMPDATA/graphs/contributors',
+    license="GPL-3.0",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=['PyMPDATA_examples', 'PyMPDATA_examples.*']),
+    package_data={'': ['*/*/*.txt']},
+    include_package_data=True,
+    project_urls={
+        "Tracker": "https://github.com/open-atmos/PyMPDATA/issues",
+        "Documentation": "https://open-atmos.github.io/PyMPDATA-examples",
+        "Source": "https://github.com/open-atmos/PyMPDATA-examples"
+    }
+)
