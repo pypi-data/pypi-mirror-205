4d9fba8edb7f3f4ed9498d0aa12d830ade4a18e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:29 +0000

    Add package nrobo/2.0.13/nrobo-2.0.13-py2.py3-none-any.whl

---
+++ packages/nrobo/nrobo-2.0.13-py2.py3-none-any.whl/nrobo/requirements.py
@@ -0,0 +1,61 @@
+####################
+# nRoBo requirements
+####################
+
+# A tool for creating isolated virtual python environments.
+virtualenv==20.17.1
+
+# YAML is a data serialization format designed
+# for human readability and interaction with scripting languages.
+# PyYAML is a YAML parser and emitter for Python.
+PyYAML==6.0
+
+# The pytest framework makes it easy to write small tests-backup,
+# yet scales to support complex functional testing for applications and libraries.
+pytest==7.2.0
+
+# Makes ANSI escape character sequences (for producing colored terminal text and cursor positioning) work under MS Windows.
+colorama==0.4.5
+
+# A simple wrapper for termcolor
+termcolor2==0.0.3
+
+# Python language bindings for Selenium WebDriver 4 :).
+selenium==4.7.2
+# Webdriver Manager
+webdriver-manager==3.8.5
+
+# Allure reports
+# Doc: https://docs.qameta.io/allure-report/
+allure-pytest==2.12.0
+
+# Allure Behave
+# https://pypi.org/project/allure2-behave/
+#allure2-behave==2.3.4
+
+# Take full control of your keyboard with this small Python library.
+# Hook global events, register hotkeys, simulate key presses and much more.
+keyboard==0.13.5
+
+py==1.11.0
+
+pytest-rerunfailures==10.3
+
+pytest-forked==1.4.0
+
+pytest-html==3.2.0
+
+pytest-metadata==2.0.4
+
+pytest-xdist==3.1.0
+
+termcolor~=2.2.0
+
+beautifulsoup4>=4.11.1
+
+pandas>=1.5.2
+
+wget>=3.2
+
+# Drop duplicate tests at runtime and keep only single copy
+pytest-drop-dup-tests>=0.3.0
