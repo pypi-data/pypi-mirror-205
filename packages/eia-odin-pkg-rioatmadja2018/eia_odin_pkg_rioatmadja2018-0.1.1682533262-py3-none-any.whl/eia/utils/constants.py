9333f94acb73cf370f26d6073c99146bc8b36d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:30 +0000

    Add package eia-odin-pkg-rioatmadja2018/0.1.1682533262/eia_odin_pkg_rioatmadja2018-0.1.1682533262-py3-none-any.whl

---
+++ packages/eia-odin-pkg-rioatmadja2018/eia_odin_pkg_rioatmadja2018-0.1.1682533262-py3-none-any.whl/eia/utils/constants.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from typing import List
+
+API_KEY: str = "CZdQsisRJzwOfqUWV3jiMPNEx3ZbHcuJ2VQus04i"
+STATE: List[str] = ["California", "Colorado", "Florida", "Massachusetts", "Minnesota", "New York", "Ohio", "Texas", "Washington"]
