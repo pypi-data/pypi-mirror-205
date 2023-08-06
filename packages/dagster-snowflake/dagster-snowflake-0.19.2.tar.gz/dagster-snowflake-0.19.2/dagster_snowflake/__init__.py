de5ad9609fe1f86ab81cc03859d7c75bd6cf4d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:51 +0000

    Add package dagster-snowflake/0.19.2/dagster-snowflake-0.19.2.tar.gz

---
+++ packages/dagster-snowflake/dagster-snowflake-0.19.2.tar.gz/dagster-snowflake-0.19.2/dagster_snowflake/__init__.py
@@ -0,0 +1,18 @@
+from dagster._core.libraries import DagsterLibraryRegistry
+
+from .resources import SnowflakeConnection, snowflake_resource
+from .snowflake_io_manager import (
+    SnowflakeIOManager as SnowflakeIOManager,
+    build_snowflake_io_manager as build_snowflake_io_manager,
+)
+from .solids import snowflake_op_for_query
+from .version import __version__
+
+DagsterLibraryRegistry.register("dagster-snowflake", __version__)
+
+__all__ = [
+    "snowflake_op_for_query",
+    "snowflake_resource",
+    "build_snowflake_io_manager",
+    "SnowflakeConnection",
+]
