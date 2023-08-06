dc0aa1a56e13114f1ee4e0a15a49ce040919f45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:43 +0000

    Add package toolkit4life/0.2.1/toolkit4life-0.2.1-py3-none-any.whl

---
+++ packages/toolkit4life/toolkit4life-0.2.1-py3-none-any.whl/toolkit4life/clients/postgres.py
@@ -0,0 +1,29 @@
+# Standard imports
+from sqlalchemy import create_engine
+from urllib.parse import quote_plus as urlquote
+
+# Third-party imports
+from ._sqlalchemy import SQLAlchemy
+
+
+class PostgresClient(SQLAlchemy):
+
+    def __init__(self, host: str, port: str, username: str, password: str, database: str) -> None:
+        """
+            Creates and initializes a PostgreSQL engine instance that connects to the database
+
+            Parameters:
+                host (str): Host IP address
+                port (str): Port number                
+                username (str): Username for authentication/privileges
+                password (str): Password for authentication
+                database (str): Name of the database
+        """
+        super().__init__(engine = "postgresql", host = host, port = port, database = database, username = username, password = password)
+        self.engine = create_engine(self.connection_string)
+
+
+    @property
+    def connection_string(self) -> str:
+        """ Returns the connection string """
+        return f"{self.engine}://{self.username}:%s@{self.host}:{self.port}/{self.database}" % urlquote(self.password)
