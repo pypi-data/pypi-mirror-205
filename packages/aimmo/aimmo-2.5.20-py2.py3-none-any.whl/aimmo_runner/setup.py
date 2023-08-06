d902fc8d222cce8a3cb0b957516e00cdb9775054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:06 +0000

    Add package aimmo/2.5.20/aimmo-2.5.20-py2.py3-none-any.whl

---
+++ packages/aimmo/aimmo-2.5.20-py2.py3-none-any.whl/aimmo_runner/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+setup(
+    name="aimmo_runner",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "django==3.2.18",
+        "django-forms-bootstrap",
+        "django-js-reverse",
+        "eventlet",
+        "requests",
+        "six",
+        "hypothesis",
+        "psutil",
+        "docker<6",
+        "kubernetes==23.6.0",
+    ],
+    zip_safe=False,
+)
