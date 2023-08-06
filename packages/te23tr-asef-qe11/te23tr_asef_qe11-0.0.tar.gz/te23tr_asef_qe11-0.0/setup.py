ee53e06498ba8cd29ca973b9a4e5abf0e1805767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:40 +0000

    Add package te23tr-asef-qe11/0.0/te23tr_asef_qe11-0.0.tar.gz

---
+++ packages/te23tr-asef-qe11/te23tr_asef_qe11-0.0.tar.gz/te23tr_asef_qe11-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='te23tr_asef_qe11',
+      version='0.0',
+      description='test',
+      packages=['te23tr_asef_qe11'],
+      author_email='testdd12d@mail.ru',
+      zip_safe=False)
