56139a94188700fc4569abdeadf5b841f43baabd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:00 +0000

    Add package ansible-navigator/3.2.0/ansible_navigator-3.2.0-py3-none-any.whl

---
+++ packages/ansible-navigator/ansible_navigator-3.2.0-py3-none-any.whl/ansible_navigator/actions/select.py
@@ -0,0 +1,46 @@
+"""Menu selection implementation.
+
+Processor of a menu selection from a numeric key press
+or entry at the single line prompt. e.g ``:42``
+"""
+
+import logging
+
+from ..app_public import AppPublic
+from ..configuration_subsystem.definitions import ApplicationConfiguration
+from ..ui_framework import Interaction
+from . import _actions as actions
+
+
+@actions.register
+class Action:
+    """Menu selection implementation."""
+
+    KEGEX = r"^\d+$"
+
+    def __init__(self, args: ApplicationConfiguration):
+        """Initialize the select action.
+
+        :param args: The current settings for the application
+        """
+        self._args = args
+        self._logger = logging.getLogger(__name__)
+
+    def run(self, interaction: Interaction, app: AppPublic) -> None:
+        """Execute a menu selection for mode interactive.
+
+        :param interaction: The interaction from the user
+        :param app: The app instance
+        """
+        self._logger.debug("selection made")
+        interaction.ui.scroll(0)
+        interaction.ui.clear()
+        this = app.steps.back_one()  # remove this
+        app.steps.current.index = interaction.action.value  # update index
+        app.steps.append(app.steps.current.select_func())  # add next
+        app.steps.append(this)  # put this back on stack
+        self._logger.debug(
+            "Requested next step in %s will be %s",
+            app.name,
+            app.steps.previous.name,
+        )
---
+++ packages/ansible-navigator/ansible_navigator-3.2.0-py3-none-any.whl/ansible_navigator/actions/serialize_yaml.py
@@ -0,0 +1,37 @@
+"""``:yaml`` command implementation."""
+import logging
+
+from ansible_navigator.content_defs import ContentFormat
+
+from ..app_public import AppPublic
+from ..configuration_subsystem.definitions import ApplicationConfiguration
+from ..ui_framework import Interaction
+from . import _actions as actions
+
+
+@actions.register
+class Action:
+    """``:yaml`` command implementation."""
+
+    KEGEX = r"^y(?:aml)?$"
+
+    def __init__(self, args: ApplicationConfiguration):
+        """Initialize the ``:yaml`` action.
+
+        :param args: The current settings for the application
+        """
+        self._args = args
+        self._logger = logging.getLogger(__name__)
+
+    # pylint: disable=unused-argument
+    def run(self, interaction: Interaction, app: AppPublic) -> None:
+        """Execute the ``:yaml`` request for mode interactive.
+
+        :param interaction: The interaction from the user, action and value
+        :param app: The app instance
+        """
+        self._logger.debug("yaml requested")
+        if interaction.ui is not None:
+            interaction.ui.scroll(0)
+            content_format = interaction.ui.content_format(ContentFormat.YAML, default=True)
+            self._logger.debug("Serialization set to %s", content_format)
