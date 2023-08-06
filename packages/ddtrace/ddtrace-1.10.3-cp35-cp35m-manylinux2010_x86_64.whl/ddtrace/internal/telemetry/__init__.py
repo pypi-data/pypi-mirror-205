6b8f6ec68b544e0ac29117867aa5990f352bec81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:13 +0000

    Add package ddtrace/1.10.3/ddtrace-1.10.3-cp35-cp35m-manylinux2010_x86_64.whl

---
+++ packages/ddtrace/ddtrace-1.10.3-cp35-cp35m-manylinux2010_x86_64.whl/ddtrace/internal/telemetry/__init__.py
@@ -0,0 +1,16 @@
+"""
+Instrumentation Telemetry API.
+This is normally started automatically by ``ddtrace-run`` when the
+``DD_INSTRUMENTATION_TELEMETRY_ENABLED`` variable is set.
+To start the service manually, invoke the ``enable`` method::
+    from ddtrace.internal.telemetry import telemetry_writer
+    telemetry_writer.enable()
+"""
+from .writer import TelemetryMetricsWriter
+from .writer import TelemetryWriter
+
+
+telemetry_metrics_writer = TelemetryMetricsWriter()
+telemetry_writer = TelemetryWriter()
+
+__all__ = ["telemetry_writer"]
