31af9cee0fc7e20494819900492e299ef85dd2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:39 +0000

    Add package lago-python-client/0.30.0/lago_python_client-0.30.0-py3-none-any.whl

---
+++ packages/lago-python-client/lago_python_client-0.30.0-py3-none-any.whl/lago_python_client/models/credit.py
@@ -0,0 +1,23 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from .invoice_item import InvoiceItemResponse
+from ..base_model import BaseResponseModel
+
+
+class InvoiceShortDetails(BaseModel):
+    lago_id: Optional[str]
+    payment_status: Optional[str]
+
+
+class CreditResponse(BaseResponseModel):
+    lago_id: Optional[str]
+    amount_cents: Optional[int]
+    amount_currency: Optional[str]
+    item: Optional[InvoiceItemResponse]
+    invoice: Optional[InvoiceShortDetails]
+
+
+class CreditsResponse(BaseResponseModel):
+    __root__: List[CreditResponse]
