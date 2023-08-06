64af3af598f87e228d8f2b2e5484d0b9a988b81e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:15 +0000

    Add package IPAkor/2.0/IPAkor-2.0.tar.gz

---
+++ packages/IPAkor/IPAkor-2.0.tar.gz/IPAkor-2.0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(name='IPAkor',
+                 version='2.0',
+                 packages=setuptools.find_packages(),
+                 include_package_data=True,
+                 classifiers=[
+                     'Programming Language :: Python :: 3',
+                     'Operating System :: OS Independent',
+                     'Topic :: Scientific/Engineering'
+                 ],
+                 install_requires=['konlpy'],
+                 python_requires='>=3',
+                 author_email='neminova2.0@gmail.com'
+                 )
