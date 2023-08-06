1836870262a59a63ae00e770a5032c4fd5d08a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:36 +0000

    Add package process-cap/0.0.0/process_cap-0.0.0.tar.gz

---
+++ packages/process-cap/process_cap-0.0.0.tar.gz/process_cap-0.0.0/setup.py
@@ -0,0 +1,10 @@
+"""Run upon call of pip install cri-diagnose."""
+
+from pathlib import Path
+
+from setuptools import setup
+
+here = Path(__file__).parent
+name = "process_cap"
+
+setup(name=name, version="0.0.0")
