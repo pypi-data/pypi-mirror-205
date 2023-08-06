127f7c2be38f8d21c74ea761b71a4679caf3bc6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:59 +0000

    Add package eia-odin-pkg-rioatmadja2018/0.1.1682735277/eia_odin_pkg_rioatmadja2018-0.1.1682735277-py3-none-any.whl

---
+++ packages/eia-odin-pkg-rioatmadja2018/eia_odin_pkg_rioatmadja2018-0.1.1682735277-py3-none-any.whl/eia/crude_oil/refining_processing.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from eia.utils.browser import Browser
+from eia.utils.constants import API_KEY, REGION
+from eia.utils.facets import get_facets
+from typing import List, Dict
+
+class CrudeOilRefinigAndProcessing(object):
+
+    def __init__(self, frequency: str = "weekly"):
+        self.frequency: str = frequency
+        self.all_processing: Dict=  {}
+        self.visited: Dict = {}
+
+    def get_crude_oil_refining_processing(self, length: int = 5000):
+
+        facets: Dict = get_facets(facet_name="petroleum/pnp/wprode")
+        for state,series in facets.items():
+            endpoint: str = f"https://api.eia.gov/v2/petroleum/pnp/wprode/data/?api_key={API_KEY}&frequency={self.frequency}&data[0]=value&facets[series][]={series}&sort[0][column]=period&sort[0][direction]=desc&offset=0&length={length}"
+
+            if not self.visited.get(state, {}):
+                resp: Dict = Browser(endpoint=endpoint).parse_content().get('response').get('data')
+                self.all_processing[state] = resp
+                self.visited[REGION.get(state)] = resp
+
+            else:
+                self.all_processing[state] = self.visited.get(REGION.get(state))
+    @property
+    def get_all_productions(self):
+        return self.all_processing
