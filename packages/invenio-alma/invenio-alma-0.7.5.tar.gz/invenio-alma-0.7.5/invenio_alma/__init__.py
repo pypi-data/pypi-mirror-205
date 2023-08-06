43cb26ddf659acd6c08d27f872ab174107b40c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:45 +0000

    Add package invenio-alma/0.7.5/invenio-alma-0.7.5.tar.gz

---
+++ packages/invenio-alma/invenio-alma-0.7.5.tar.gz/invenio-alma-0.7.5/invenio_alma/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021-2022 Graz University of Technology.
+#
+# invenio-alma is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio module to connect InvenioRDM to Alma."""
+
+from .ext import InvenioAlma
+
+__version__ = "0.7.5"
+
+__all__ = ("__version__", "InvenioAlma")
