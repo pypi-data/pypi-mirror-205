a896e0284252f33e7bb2e11dc19fbad8fedbfdbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:00 +0000

    Add package moodle-dl/2.3.1.9/moodle-dl-2.3.1.9.tar.gz

---
+++ packages/moodle-dl/moodle-dl-2.3.1.9.tar.gz/moodle-dl-2.3.1.9/moodle_dl/notifications/xmpp/xmpp_shooter.py
@@ -0,0 +1,20 @@
+import xmpp
+
+from moodle_dl.utils import check_verbose
+
+
+class XmppShooter:
+    def __init__(self, jid, password, recipient):
+        self.jid = xmpp.protocol.JID(jid)
+        self.connection = xmpp.Client(server=self.jid.getDomain(), debug=check_verbose())
+        self.is_connected = False
+        self.password = password
+        self.recipient = xmpp.protocol.JID(recipient)
+
+    def send(self, message):
+        if not self.is_connected:
+            self.connection.connect()
+            self.connection.auth(user=self.jid.getNode(), password=self.password, resource=self.jid.getResource())
+            self.is_connected = True
+
+        self.connection.send(xmpp.protocol.Message(to=self.recipient, body=message))
