cb3b6ddc4fde1540ef1c42dc8edc1a194684a0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:29 +0000

    Add package SaGaF-Email/0.1/SaGaF_Email-0.1.tar.gz

---
+++ packages/SaGaF-Email/SaGaF_Email-0.1.tar.gz/SaGaF_Email-0.1/SaGaF_Email/__init__.py
@@ -0,0 +1,5 @@
+from .SaGaF_Email import S1
+from .SaGaF_Email import S2
+from .SaGaF_Email import S3
+from .SaGaF_Email import S4
+from .SaGaF_Email import S5
---
+++ packages/SaGaF-Email/SaGaF_Email-0.1.tar.gz/SaGaF_Email-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name= 'SaGaF_Email',
+    version='0.1',
+    author= '@CCIIUU',
+    description='Checking emails for any domain , for example : gmail,yahoo,aol,hotmail,outlook,mail_ru',
+    packages= ['SaGaF_Email'],
+    author_email='dxa00776@gmail.com',
+    zip_safe=False,
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
