54b0ad894bfc91650c73a4ff3482db7d8001933a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:41 +0000

    Add package streamlit-chatbot/0.0.2.2/streamlit_chatbot-0.0.2.2-py3-none-any.whl

---
+++ packages/streamlit-chatbot/streamlit_chatbot-0.0.2.2-py3-none-any.whl/streamlit_chat/frontend/build/precache-manifest.b07dcacca392a0fa95672bace19f7fb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed7bf79803222c87557dfc1cd8b9b37",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1167ab71a7ce87346556",
+    "url": "./static/js/2.82d6880e.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.82d6880e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67ef7fc95d96c6fad72",
+    "url": "./static/js/main.342b5169.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
