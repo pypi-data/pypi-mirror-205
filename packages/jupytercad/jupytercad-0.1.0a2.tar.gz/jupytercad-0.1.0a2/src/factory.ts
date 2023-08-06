d6ddc9272120de5fcafed80bb8718f9a0b5b06c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:26 +0000

    Add package jupytercad/0.1.0a2/jupytercad-0.1.0a2.tar.gz

---
+++ packages/jupytercad/jupytercad-0.1.0a2.tar.gz/jupytercad-0.1.0a2/src/factory.ts
@@ -0,0 +1,48 @@
+import { IJupyterCadTracker } from './token';
+import { ABCWidgetFactory, DocumentRegistry } from '@jupyterlab/docregistry';
+
+import { CommandRegistry } from '@lumino/commands';
+
+import { JupyterCadModel } from './model';
+import { JupyterCadPanel, JupyterCadWidget } from './widget';
+import { ToolbarWidget } from './toolbar/widget';
+import { ToolbarModel } from './toolbar/model';
+// import { WidgetTracker } from '@jupyterlab/apputils';
+
+interface IOptios extends DocumentRegistry.IWidgetFactoryOptions {
+  tracker: IJupyterCadTracker;
+  commands: CommandRegistry;
+}
+
+export class JupyterCadWidgetFactory extends ABCWidgetFactory<
+  JupyterCadWidget,
+  JupyterCadModel
+> {
+  private _commands: CommandRegistry;
+
+  constructor(options: IOptios) {
+    const { ...rest } = options;
+    super(rest);
+
+    this._commands = options.commands;
+  }
+
+  /**
+   * Create a new widget given a context.
+   *
+   * @param context Contains the information of the file
+   * @returns The widget
+   */
+  protected createNewWidget(
+    context: DocumentRegistry.IContext<JupyterCadModel>
+  ): JupyterCadWidget {
+    const { model } = context;
+    const content = new JupyterCadPanel({ model });
+    const toolbarModel = new ToolbarModel({ panel: content, context });
+    const toolbar = new ToolbarWidget({
+      model: toolbarModel,
+      commands: this._commands
+    });
+    return new JupyterCadWidget({ context, content, toolbar });
+  }
+}
---
+++ packages/jupytercad/jupytercad-0.1.0a2.tar.gz/jupytercad-0.1.0a2/src/toolbar/widget.tsx
@@ -0,0 +1,65 @@
+import { CommandToolbarButton } from '@jupyterlab/apputils';
+import {
+  Toolbar,
+  ReactWidget,
+  undoIcon,
+  redoIcon
+} from '@jupyterlab/ui-components';
+
+import { CommandRegistry } from '@lumino/commands';
+
+import * as React from 'react';
+
+import { ToolbarReact } from './toolbar';
+import { ToolbarModel } from './model';
+
+export class ToolbarWidget extends Toolbar {
+  constructor(options: ToolbarWidget.IOptions) {
+    const { model, ...rest } = options;
+    super(rest);
+    this.addClass('jpcad-toolbar-widget');
+    this._model = model;
+    if (options.commands) {
+      this.addItem(
+        'undo',
+        new CommandToolbarButton({
+          id: ToolbarWidget.CommandIDs.undo,
+          label: '',
+          icon: undoIcon,
+          commands: options.commands
+        })
+      );
+      this.addItem(
+        'redo',
+        new CommandToolbarButton({
+          id: ToolbarWidget.CommandIDs.redo,
+          label: '',
+          icon: redoIcon,
+          commands: options.commands
+        })
+      );
+    }
+
+    const body = ReactWidget.create(
+      <ToolbarReact toolbarModel={this._model} />
+    );
+    this.addItem('body', body);
+  }
+
+  private _model: ToolbarModel;
+}
+
+export namespace ToolbarWidget {
+  export interface IOptions extends Toolbar.IOptions {
+    model: ToolbarModel;
+    commands?: CommandRegistry;
+  }
+
+  /**
+   * The command IDs used by the FreeCAD plugin.
+   */
+  export namespace CommandIDs {
+    export const redo = 'jupytercad:redo';
+    export const undo = 'jupytercad:undo';
+  }
+}
