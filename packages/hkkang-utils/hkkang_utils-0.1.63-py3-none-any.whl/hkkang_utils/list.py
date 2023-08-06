51d31dcc0910014ee5671292fc91c8117fecb67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:56 +0000

    Add package hkkang-utils/0.1.63/hkkang_utils-0.1.63-py3-none-any.whl

---
+++ packages/hkkang-utils/hkkang_utils-0.1.63-py3-none-any.whl/hkkang_utils/list.py
@@ -0,0 +1,30 @@
+import functools
+import math
+import operator
+from typing import Any, List
+
+
+def do_flatten_list(list_of_list: List[List[Any]])-> List[Any]:
+    """Flatten a list of list to a list
+
+    :param list_of_list: list to flatten
+    :type list_of_list: List[List[Any]]
+    :return: flatten list
+    :rtype: List[Any]
+    """
+    return functools.reduce(operator.iconcat, list_of_list, [])
+
+def map_many(functions: List, iterable:List[Any]) -> List[Any]:
+    return list(functools.reduce(lambda x, y: map(y, x), functions, iterable))
+
+def get(items: List[Any], idx: int, default: Any="") -> Any:
+    if idx < 0:
+        return default
+    try:
+        return items[idx]
+    except IndexError:
+        return default
+    
+def divide_into_chunks(lst: List[Any], num_chunks: int) -> List[List[Any]]:
+    num_of_items_in_chunk = math.ceil(len(lst) / num_chunks)
+    return [lst[i:i+num_of_items_in_chunk] for i in range(0, len(lst), num_of_items_in_chunk)]
