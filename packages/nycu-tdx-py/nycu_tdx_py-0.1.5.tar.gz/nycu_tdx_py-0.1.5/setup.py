2674e14137d7387563e4eeb4a6f64ba327e1b53f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:32 +0000

    Add package nycu-tdx-py/0.1.5/nycu_tdx_py-0.1.5.tar.gz

---
+++ packages/nycu-tdx-py/nycu_tdx_py-0.1.5.tar.gz/nycu_tdx_py-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='nycu_tdx_py',
+    version='0.1.5',
+    license='NYCU',
+    author="Robert Yeh",
+    author_email='robert1328.mg10@nycu.edu.tw',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/ChiaJung-Yeh/nycu_tdx_py',
+    keywords='example project',
+    install_requires=[
+          'pandas',
+          'geopandas',
+          'numpy',
+          'shapely',
+          'requests'
+      ],
+
+)
