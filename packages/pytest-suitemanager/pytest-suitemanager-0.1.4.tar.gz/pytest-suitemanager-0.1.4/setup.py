4e374724475da8024a180f6463b045e39d4baa41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:19 +0000

    Add package pytest-suitemanager/0.1.4/pytest-suitemanager-0.1.4.tar.gz

---
+++ packages/pytest-suitemanager/pytest-suitemanager-0.1.4.tar.gz/pytest-suitemanager-0.1.4/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-suitemanager',
+    version='0.1.4',
+    author='mf.liang',
+    author_email='mf.liang@outlook.com',
+    maintainer='mf.liang',
+    maintainer_email='mf.liang@outlook.com',
+    license='BSD-3',
+    url='https://github.com/mf.liang/pytest-suitemanager',
+    description='A simple plugin to use with pytest',
+    long_description=read('README.rst'),
+    py_modules=['pytest_suitemanager'],
+    python_requires='>=3.5',
+    install_requires=['pytest>=3.5.0'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+    ],
+    entry_points={
+        'pytest11': [
+            'suitemanager = pytest_suitemanager',
+        ],
+    },
+)
