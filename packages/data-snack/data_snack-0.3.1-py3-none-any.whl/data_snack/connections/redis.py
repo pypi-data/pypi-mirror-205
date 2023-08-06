ed0040cc71f7019b576aebab9f774d82a5cd8278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:05 +0000

    Add package data-snack/0.3.1/data_snack-0.3.1-py3-none-any.whl

---
+++ packages/data-snack/data_snack-0.3.1-py3-none-any.whl/data_snack/connections/redis.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from typing import Dict, List, Optional, Text
+
+from .base import Connection
+
+
+@dataclass
+class RedisConnection(Connection):
+    connection: "Redis"
+
+    def get(self, key: Text) -> Optional[bytes]:
+        return self.connection.get(key)
+
+    def set(self, key: Text, value: Text, expire: int = 0) -> bool:
+        ex = expire if expire > 0 else None
+        return self.connection.set(key, value, ex=ex)
+
+    def delete(self, key: Text) -> bool:
+        n_deleted = self.connection.delete(key)
+        return n_deleted == 1
+
+    def get_many(self, keys: List[Text]) -> Dict[Text, Optional[bytes]]:
+        return dict(zip(keys, self.connection.mget(keys)))
+
+    def set_many(self, values: Dict[Text, Text]) -> List[Text]:
+        return self.connection.mset(values)
+
+    def delete_many(self, keys: List[Text]) -> bool:
+        n_deleted = self.connection.delete(*keys)
+        return len(keys) == n_deleted
+
+    def keys(self, pattern: Text) -> List[Text]:
+        return self.connection.keys(pattern)
