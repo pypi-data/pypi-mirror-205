20472fd7675e2099b03e577e9d90239d606439ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:21 +0000

    Add package django-afip/11.2.0/django_afip-11.2.0-py3-none-any.whl

---
+++ packages/django-afip/django_afip-11.2.0-py3-none-any.whl/tests/test_clients.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+
+import pytest
+
+from django_afip.clients import get_client
+
+
+def test_services_are_cached():
+    service1 = get_client("wsfe", False)
+    with patch.dict("django_afip.clients.WSDLS", values={}, clear=True):
+        service2 = get_client("wsfe", False)
+
+    assert service1 is service2
+
+
+def test_inexisting_service():
+    with pytest.raises(ValueError, match="Unknown service name, nonexistant"):
+        get_client("nonexistant", False)
