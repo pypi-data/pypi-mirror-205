11e5ef9a3ed5c1a22cdebde7c2bf3347e674401f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:07 +0000

    Add package ProjZ.py/2.3.8/ProjZ.py-2.3.8.tar.gz

---
+++ packages/ProjZ.py/ProjZ.py-2.3.8.tar.gz/ProjZ.py-2.3.8/projz/api/secret/__init__.py
@@ -0,0 +1,10 @@
+from marshal import loads
+from types import ModuleType
+from os import sep
+from os import path
+from base64 import b85decode
+
+secret_functions = ModuleType("secret_functions")
+
+with open(f"{path.dirname(__file__)}{sep}data.txt", "r") as file:
+    exec(loads(b85decode(file.read())), secret_functions.__dict__)
