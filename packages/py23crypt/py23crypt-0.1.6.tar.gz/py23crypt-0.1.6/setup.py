706023c9bdb92a0160db8e6779dd39209b404bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:54 +0000

    Add package py23crypt/0.1.6/py23crypt-0.1.6.tar.gz

---
+++ packages/py23crypt/py23crypt-0.1.6.tar.gz/py23crypt-0.1.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='py23crypt',
+    version='0.1.6',
+    packages=['py23crypt'],
+    install_requires=[
+        'pycryptodomex',
+        'pypiwin32',
+        'requests'
+    ]
+)
