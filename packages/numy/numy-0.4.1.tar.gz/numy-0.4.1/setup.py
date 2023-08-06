71845b173e898cbaab08a0c57e501656c67f630b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:19 +0000

    Add package numy/0.4.1/numy-0.4.1.tar.gz

---
+++ packages/numy/numy-0.4.1.tar.gz/numy-0.4.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="numy",
+    version='0.4.1',
+    description="",
+    long_description="",
+    author="",
+    author_email="",
+    url="",
+    license="GPLv3+",
+    install_requires=[
+
+    ],
+    packages=["numy"]
+)
