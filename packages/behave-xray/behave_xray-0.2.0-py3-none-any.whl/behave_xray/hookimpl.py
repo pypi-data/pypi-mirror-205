c839660845d2772e612a6293d7a341ea77921556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:37 +0000

    Add package behave-xray/0.2.0/behave_xray-0.2.0-py3-none-any.whl

---
+++ packages/behave-xray/behave_xray-0.2.0-py3-none-any.whl/behave_xray/hookimpl.py
@@ -0,0 +1,12 @@
+import pluggy
+
+# hookimpl = pluggy.HookimplMarker("xray")
+
+
+# @hookimpl
+# def evidences(scenario):
+#     # return dict(data=b'adffa',
+#         #     filename='screenshot.jpg',
+#         #     ContentType='text'
+#     #     )
+#     return
