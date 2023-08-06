3c56476eacc4b461eef990ede50dfe0ab39a5c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:17 +0000

    Add package iGetMusic/0.1.0/iGetMusic-0.1.0.tar.gz

---
+++ packages/iGetMusic/iGetMusic-0.1.0.tar.gz/iGetMusic-0.1.0/iGetMusic/__init__.py
@@ -0,0 +1,7 @@
+from .iGet import resizeImage
+from .iGet import get
+from .iGet import getArtist
+from .iGet import getAllArtistSongs
+
+
+__all__ = ('resizeImage', 'get', 'getArtist', 'getAllArtistSongs')
