4ae729645cca6374ef03a0551336ab01c56932ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:32 +0000

    Add package fineslice/0.0.2/fineslice-0.0.2-py3-none-any.whl

---
+++ packages/fineslice/fineslice-0.0.2-py3-none-any.whl/fineslice/sampler_0d.py
@@ -0,0 +1,34 @@
+from typing import Any, Optional
+
+import numpy as np
+
+from .types import SamplerResult0D, as_affine, AffineLike, SamplerPointLike, sampler_point_3d, Texture3D, \
+    check_valid_texture_3d
+
+
+def sample_0d(
+        texture: Texture3D,
+        affine: AffineLike,
+        out_position: SamplerPointLike,
+        out_default: Any = None
+) -> Optional[SamplerResult0D]:
+    """
+    Sample a single point from a texture.
+
+    :param texture: Image texture.
+    :param affine: Affine matrix.
+    :param out_position: Point to sample. E.g. (1, 2, 3).
+    :param out_default: Default value that will be returned when sampling outside of texture bounds.
+    :return: Sampled value (type equals texture array type).
+    """
+    check_valid_texture_3d(texture)
+
+    affine = as_affine(affine)
+    affine_inv = np.linalg.inv(affine)
+    out_position = sampler_point_3d(out_position)
+
+    p = np.dot(affine_inv, out_position).astype(int)
+    if np.any(p < 0) or np.any(p[:3] >= texture.shape):
+        return out_default
+    print(p)
+    return texture[p[0], p[1], p[2]]
---
+++ packages/fineslice/fineslice-0.0.2-py3-none-any.whl/fineslice/utils.py
@@ -0,0 +1,25 @@
+from typing import Union, Optional
+
+import numpy as np
+
+
+def eye_1d(
+        n: int,
+        eye_index: int,
+        eye_value: Union[int, float, complex, np.ndarray] = 1,
+        fill_value: Union[int, float, complex, np.ndarray] = 0,
+        dtype: Optional[object] = None
+):
+    """
+    Similar to ``np.full`` but a single index is changed.
+
+    :param n: Vector length.
+    :param eye_index: Index.
+    :param eye_value: Index value.
+    :param fill_value: Fill value.
+    :param dtype: The desired data-type for the array The default, None, means ``np.array(f).dtype``.
+    :return:
+    """
+    a = np.full((n,), fill_value=fill_value, dtype=dtype)
+    a[eye_index] = eye_value
+    return a
