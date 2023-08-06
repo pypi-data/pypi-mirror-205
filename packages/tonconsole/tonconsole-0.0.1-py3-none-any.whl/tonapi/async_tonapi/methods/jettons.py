7102fb386312ea81540b1fb8fc80a3cae7e3ce99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:16 +0000

    Add package tonconsole/0.0.1/tonconsole-0.0.1-py3-none-any.whl

---
+++ packages/tonconsole/tonconsole-0.0.1-py3-none-any.whl/tonapi/async_tonapi/methods/jettons.py
@@ -0,0 +1,18 @@
+from tonapi.async_tonapi import AsyncTonapiClient
+
+from tonapi.schema.jettons import JettonInfo
+
+
+class JettonMethod(AsyncTonapiClient):
+
+    async def get_info(self, account_id: str) -> JettonInfo:
+        """
+        Get jetton metadata by jetton master address.
+
+        :param account_id: account ID
+        :return: :class:`JettonInfo`
+        """
+        method = f"v2/jettons/{account_id}"
+        response = await self._request(method=method)
+
+        return JettonInfo(**response)
---
+++ packages/tonconsole/tonconsole-0.0.1-py3-none-any.whl/tonapi/async_tonapi/methods/traces.py
@@ -0,0 +1,18 @@
+from tonapi.async_tonapi import AsyncTonapiClient
+
+from tonapi.schema.traces import Trace
+
+
+class TraceMethod(AsyncTonapiClient):
+
+    async def get_trace(self, trace_id: str) -> Trace:
+        """
+        Get the trace by trace ID or hash of any transaction in trace.
+
+        :param trace_id: trace ID or transaction hash in hex (without 0x) or base64url format
+        :return: :class:`Trace`
+        """
+        method = f"v2/traces/{trace_id}"
+        response = await self._request(method=method)
+
+        return Trace(**response)
