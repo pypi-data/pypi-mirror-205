5597dc9b29880dbcef3bcb71205d3245ad3c5f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:14 +0000

    Add package Video-Audio-Image-Downloader/0.1.1/Video_Audio_Image_Downloader-0.1.1-py3-none-any.whl

---
+++ packages/Video-Audio-Image-Downloader/Video_Audio_Image_Downloader-0.1.1-py3-none-any.whl/source/lib/help.py
@@ -0,0 +1,86 @@
+class help:
+        
+        
+    def Youtube_Download_video_Resolution_help(self):
+        print(f"Usage: <Downloader> youtube video -get_resolution [Options]")
+        print()
+        print("Description :: ")
+        print("In this command is used to show's Resolution of Youtube Video")
+        print()
+        print("Options  are :")
+        print()
+        print("positional arguments : ")
+        print("   -get_resolution       Show's the Resolution of the Youtube")
+        print()
+        print("Optional arguments : ")
+        print("-h, --help   show this help message and exit")
+        
+        
+    def Youtube_Download_video_help(self):
+        
+        print(f"Usage: <Downloader> Youtube Video [Options]...")
+        print()
+        print("Description :: ")
+        print("             In this command is used to Download Youtube Video's with Specified Path with Specifed Resolution")
+        print()
+        print("Options  are :")
+        print()
+        print("positional arguments :: ")
+        print("         -source             Source of our Youtube Video")
+        print()
+        print("Optional arguments :: ")
+        print("         -resolution         Resolution of our Youtube Video")
+        print("         -path               which path the file to be saved")
+        print("-h,      --help              show this help message and exit")
+        
+    def Youtube_Download_Audio_help(self):
+        print("Usage: <Downloader> youtube audio [Options]...")
+        print()
+        print("Description :: ")
+        print("In this command is used to Download Youtube Audio's with Specified Path with Specifed Resolution")
+        print()
+        print("Options  are :")
+        print()
+        print("positional arguments : ")
+        print("         -source       Source of our Youtube Audio")
+        print("         -filename     Name of the Audio File")
+        print("         -path         which path the file to be saved")
+        print()
+        print("Optional arguments : ")
+        print("   -h, --help   show this help message and exit")
+
+    def Image_Download_help(self):
+        print("Usage: <Downloader> Image [Options]...")
+        print()
+        print("Description :: ")
+        print("In this command is used to Download Image's with Specified Path with Filename")
+        print()
+        print("Options  are :")
+        print()
+        print("positional arguments : ")
+        print("   -source       Source of our Image to be download")
+        print("   -filename     Name of the Image File")
+        print()
+        print("Optional arguments : ")
+        print()
+        print("   -path         which path the file to be saved")
+        print("   -h, --help   show this help message and exit")
+    
+    def Youtube_Download_video_Description_help(self):
+        print("Usage: <Downloader>  Youtube Video Description [Options]...")
+        print()
+        print("Description :: ")
+        print("In this command is used to get the all information about the video file")
+        print()
+        print("Options  are :")
+        print()
+        print("positional arguments : ")
+        print("   -url       Source of our Image to be download")
+        print()
+        print("Optional arguments : ")
+        print()
+        print("   -a,-all      show's all information about the video file")
+        print("   -h, --help   show this help message and exit")
+        
+        
+            
