24482550737620bb27d69bc12ba7291072116640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:53 +0000

    Add package tableY/0.1.3/tableY-0.1.3.tar.gz

---
+++ packages/tableY/tableY-0.1.3.tar.gz/tableY-0.1.3/setup.py
@@ -0,0 +1,44 @@
+
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+import io,os
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open("readme.md", "r", encoding='utf-8') as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = ''
+# def read_file(filename):
+#     with open(filename) as fp:
+#         return fp.read().strip()
+#
+# def read_requirements(filename):
+#     return [line.strip() for line in read_file(filename).splitlines()
+#             if not line.startswith('#')]
+
+# REQUIRED = read_requirements('requirements.txt')
+setup(
+    name='tableY',
+    version='0.1.3',
+    author='White.tie',
+    author_email='1042798703@qq.com',
+    url='https://github.com/tyj-1995',
+    description='Convert the columns of the table to json',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['tableY'],
+    install_requires=["pandas"],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 3.6",
+        ],
+)
