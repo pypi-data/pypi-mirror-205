4ee0ab6e886e58900260baedb26bb608af7e0a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:32 +0000

    Add package tello-asyncio/2.1.3/tello_asyncio-2.1.3.tar.gz

---
+++ packages/tello-asyncio/tello_asyncio-2.1.3.tar.gz/tello_asyncio-2.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='tello_asyncio',
+      version='2.1.3',
+      description='Asyncio-based control library for the Tello drone',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/robagar/tello-asyncio',
+      project_urls={
+            'Documentation': 'https://tello-asyncio.readthedocs.io/en/latest/'
+      },
+      author='Rob Agar',
+      author_email='tello_asyncio@fastmail.net',
+      license='LGPL',
+      packages=['tello_asyncio'],
+      zip_safe=False,
+      python_requires=">=3.6")
