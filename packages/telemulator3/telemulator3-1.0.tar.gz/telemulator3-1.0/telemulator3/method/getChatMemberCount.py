3aa60f9c36cdba756302394456678ba7470e8389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:31 +0000

    Add package telemulator3/1.0/telemulator3-1.0.tar.gz

---
+++ packages/telemulator3/telemulator3-1.0.tar.gz/telemulator3-1.0/telemulator3/method/getChatMemberCount.py
@@ -0,0 +1,10 @@
+# pylint: disable=invalid-name
+"""Module for getChatMemberCount method emulator."""
+from . import with_chat
+
+
+@with_chat
+def response(_api, _params, chat):
+    """Return chat members count."""
+    # print "#getChatMemberCount params", params
+    return len(chat.members)
