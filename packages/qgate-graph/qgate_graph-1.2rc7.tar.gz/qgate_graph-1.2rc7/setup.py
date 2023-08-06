dfed21d65e4ba3be13df69381c992d321c5af724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:53 +0000

    Add package qgate-graph/1.2rc7/qgate_graph-1.2rc7.tar.gz

---
+++ packages/qgate-graph/qgate_graph-1.2rc7.tar.gz/qgate_graph-1.2rc7/setup.py
@@ -0,0 +1,38 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+import json
+import logging
+from qgate_graph.version import __version__
+#import dependencies
+#import packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+#with open('HISTORY.md') as history_file:
+#    HISTORY = history_file.read()
+setup_args = dict(
+    name='qgate_graph',
+    version=__version__,
+    description='Generate graphs based on outputs from Quality Gate',
+    long_description_content_type="text/markdown",
+    long_description=README, # + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    author='Jiri Steuer',
+    author_email='steuer.jiri@gmail.com',
+    keywords=['Quality', 'QualityGate', 'Graph'],
+    url='https://github.com/george0st/qgate-graph/',
+    download_url='https://pypi.org/project/qgate_graph/'
+)
+
+install_requires = [
+    'matplotlib~=3.7',
+    'click~=8.1'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
