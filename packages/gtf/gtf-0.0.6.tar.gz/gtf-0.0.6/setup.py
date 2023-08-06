372ce2c761f02af69bb109a01cc82754eb3d9d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:07 +0000

    Add package gtf/0.0.6/gtf-0.0.6.tar.gz

---
+++ packages/gtf/gtf-0.0.6.tar.gz/gtf-0.0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gtf',
+    packages=find_packages(),
+    include_package_data=True,
+    version="0.0.6",
+    description='A GTF installer is a command line tool that quickly and efficiently installs software and libraries while managing software dependencies. It\'s essential for deployment and development pipelines and automates the installation process.',
+    long_description="An installer for GTF (command line installer) is a useful tool that allows developers, beginners, and system administrators to install software and libraries from the command line interface quickly and efficiently. These installers use package managers to manage software dependencies and ensure a seamless installation process. They simplify the installation process and are an essential tool for deployment or development pipelines, particularly for automating installation processes. GTF installers are highly valuable in ensuring correct installation of software dependencies to prevent delays in the pipeline.",
+    long_description_content_type="text/markdown",
+    author='MR_GT',
+    author_email='friendyt89@gmail.com',
+    url='https://github.com/GreyTechno/gtf',
+    keywords=['installer', 'gtf', 'cli', 'software', 'libraries', 'packagemanagers', 'dependencies', 'automation', 'deploymentpipeline'],
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Operating System :: OS Independent',
+            'Environment :: Console',
+    ],
+    install_requires=["requests", "random2"],
+    license='MIT',
+    entry_points={
+            'console_scripts': [
+                'gtf = gtf.main:Main',
+            ],
+    },
+    python_requires='>=3.5'
+)
