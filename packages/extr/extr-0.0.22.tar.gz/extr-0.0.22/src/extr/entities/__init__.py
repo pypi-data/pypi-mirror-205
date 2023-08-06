50641889fc4fa8927043064d2fd910f40441174d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:08 +0000

    Add package extr/0.0.22/extr-0.0.22.tar.gz

---
+++ packages/extr/extr-0.0.22.tar.gz/extr-0.0.22/src/extr/entities/__init__.py
@@ -0,0 +1,5 @@
+from .extractor import EntityExtractor
+from .annotator import EntityAnnotator, \
+                       HtmlEntityAnnotator, \
+                       LabelOnlyEntityAnnotator
+from .factories import create_entity_extractor
