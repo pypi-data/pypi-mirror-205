ea545611c57fb2d04d4182237d868abfa656289c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:51 +0000

    Add package spatialpy/1.2.2/spatialpy-1.2.2-py3-none-any.whl

---
+++ packages/spatialpy/spatialpy-1.2.2-py3-none-any.whl/test/models/cylinder_demo3D.py
@@ -0,0 +1,88 @@
+# SpatialPy is a Python 3 package for simulation of
+# spatial deterministic/stochastic reaction-diffusion-advection problems
+# Copyright (C) 2019 - 2023 SpatialPy developers.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU GENERAL PUBLIC LICENSE Version 3 as
+# published by the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU GENERAL PUBLIC LICENSE Version 3 for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import numpy
+import sys
+from collections import OrderedDict
+sys.path.append("../..")
+import spatialpy
+# Global Constants
+MAX_X_DIM = 5.0
+MIN_X_DIM = -5.0
+TOL = 1e-9
+
+class Edge1(spatialpy.Geometry):
+    def inside(self, x, on_boundary):
+        return abs(x[0] - MAX_X_DIM) < 0.05
+
+class Edge2(spatialpy.Geometry):
+    def inside(self, x, on_boundary):
+        return abs(x[0] - MIN_X_DIM) < 0.05
+
+class Middle(spatialpy.Geometry):
+    def inside(self, x, on_boundary):
+        return abs(x[0] - MIN_X_DIM) >= 0.05
+
+def create_cylinder_demo_3D(model_name="cylinder_demo3d", parameter_values=None):
+    model = spatialpy.Model(model_name)
+
+    # Set domain type ids
+    model.EDGE1 = Edge1.__name__
+    model.EDGE2 = Edge2.__name__
+    model.MIDDLE = Middle.__name__
+
+    # System constants
+    D_const = 0.1
+
+    # Define Geometry
+    # Make sure that we have the correct path to the mesh file even if we are not executing from the basedir.
+    basedir = os.path.dirname(os.path.abspath(__file__))
+    domain = spatialpy.Domain.read_xml_mesh(f'{basedir}cylinder.xml')
+    
+    # Define Types
+    domain.set_properties(Middle(), model.MIDDLE)
+    domain.set_properties(Edge1(), model.EDGE1)
+    domain.set_properties(Edge2(), model.EDGE2)
+
+    model.add_domain(domain)
+
+    # Define Species
+    A = spatialpy.Species(name="A", diffusion_coefficient=D_const, restrict_to=[model.MIDDLE, model.EDGE1])
+    B = spatialpy.Species(name="B", diffusion_coefficient=D_const, restrict_to=[model.MIDDLE, model.EDGE2])
+    model.add_species([A, B])
+
+    vol = model.domain.get_vol()
+    type_id = model.domain.type
+    left = numpy.sum(vol[type_id == model.domain.get_type_def(model.EDGE1)])
+    right = numpy.sum(vol[type_id == model.domain.get_type_def(model.EDGE2)])
+    print(f"left {left} right {right}")
+
+    k_react = spatialpy.Parameter(name="k_react", expression=1.0)
+    k_creat1 = spatialpy.Parameter(name="k_creat1", expression=100 / left)
+    k_creat2 = spatialpy.Parameter(name="k_creat2", expression=100 / right)
+    model.add_parameter([k_react, k_creat1, k_creat2])
+
+    # Define Reactions
+    R1 = spatialpy.Reaction(reactants=None, products={A: 1}, rate=k_creat1, restrict_to=model.EDGE1)
+    R2 = spatialpy.Reaction(reactants=None, products={B: 1}, rate=k_creat2, restrict_to=model.EDGE2)
+    R3 = spatialpy.Reaction(reactants={A: 1, B: 1}, products=None, rate=k_react)
+    model.add_reaction([R1, R2, R3])
+
+    # Define simulation timespan
+    # self.set_timesteps(output_interval=1, num_steps=200, timestep_size=1)
+    tspan = spatialpy.TimeSpan(range(500), timestep_size=1)
+    model.timespan(tspan)
---
+++ packages/spatialpy/spatialpy-1.2.2-py3-none-any.whl/test/models/mincde.py
@@ -0,0 +1,113 @@
+# SpatialPy is a Python 3 package for simulation of
+# spatial deterministic/stochastic reaction-diffusion-advection problems
+# Copyright (C) 2019 - 2023 SpatialPy developers.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU GENERAL PUBLIC LICENSE Version 3 as
+# published by the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU GENERAL PUBLIC LICENSE Version 3 for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#!/usr/bin/env python3
+
+from spatialpy.Model import SpeciesError
+import numpy
+import os.path
+import spatialpy
+
+class DomainSize(spatialpy.DataFunction):
+    def __init__(self, domain):
+        spatialpy.DataFunction.__init__(self, name="DomainSize")
+        self.domain = domain
+        self.h = domain.get_domain_size()
+    
+    def map(self, point):
+        ret = self.h[self.domain.closest_vertex(point)]
+        return ret
+
+def create_mincde(model_name="mincde", parameter_values=None):
+    """ Model of MinD oscillations in E. Coli, based on the model by Huang. et. al. in """
+    model = spatialpy.Model(model_name)
+
+    # Set domain type ids
+    model.CYTOSOL = "Cytosol"
+    model.MEMBRANE = "Membrane"
+
+    # System constants
+    D_m = 1e-14
+    D_c = 2.5e-12
+
+    # Make sure that we have the correct path to the mesh file even if we are not executing from the basedir.
+    basedir = os.path.dirname(os.path.abspath(__file__))
+    domain = spatialpy.Domain.read_xml_mesh(f"{basedir}/data/coli.xml")
+
+    # Set types
+    domain.set_properties(spatialpy.GeometryInterior(), model.CYTOSOL)
+    domain.set_properties(spatialpy.GeometryExterior(), model.MEMBRATE)
+
+    model.add_domain(domain)
+
+    # Species
+    # Restrict to membrane protiens to membrane domain (2)
+    MinD_m = spatialpy.Species(name="MinD_m", diffusion_coefficient=D_m, restrict_to=model.MEMBRANE)
+    MinD_c_atp = spatialpy.Species(name="MinD_c_atp", diffusion_coefficient=D_c)
+    MinD_c_adp = spatialpy.Species(name="MinD_c_adp", diffusion_coefficient=D_c)
+    MinD_e = spatialpy.Species(name="MinD_e", diffusion_coefficient=D_c)
+    MinDE = spatialpy.Species(name="MinDE", diffusion_coefficient=D_m, restrict_to=model.MEMBRANE)
+    model.add_species([MinD_m, MinD_c_atp, MinD_c_adp, MinD_e, MinDE])
+
+    # Parameters
+    sigma_d = spatialpy.Parameter(name="sigma_d", expression=2.5e-8)
+    sigma_dD = spatialpy.Parameter(name="sigma_dD", expression=0.0016e-18)
+    sigma_e = spatialpy.Parameter(name="sigma_e", expression=0.093e-18)
+    sigma_de = spatialpy.Parameter(name="sigma_de", expression=0.7)
+    sigma_dt = spatialpy.Parameter(name="sigma_dt", expression=1.0)
+    model.add_parameter([sigma_d, sigma_dD, sigma_e, sigma_de, sigma_dt])
+
+    # Data function (spatially varying constant)
+    model.add_data_function(DomainSize(model.domain))
+
+    # Reactions
+    R1 = spatialpy.Reaction(propensity_function="MinD_c_atp * sigma_d / DomainSize", 
+                            restrict_to=model.MEMBRANE,
+                            reactants={MinD_c_atp: 1}, products={MinD_m: 1})
+    R2 = spatialpy.Reaction(rate=sigma_dD,
+                            reactants={MinD_c_atp: 1, MinD_m: 1}, products={MinD_m: 2})
+    R3 = spatialpy.Reaction(rate=sigma_e,
+                            reactants={MinD_m: 1, MinD_e: 1}, products={MinDE: 1})
+    R4 = spatialpy.Reaction(rate=sigma_de,
+                            reactants={MinDE: 1}, products={MinD_c_adp: 1, MinD_e: 1})
+    R5 = spatialpy.Reaction(rate=sigma_dt,
+                            reactants={MinD_c_adp: 1}, products={MinD_c_atp: 1})
+    R6 = spatialpy.Reaction(rate=sigma_dD,
+                            reactants={MinDE: 1, MinD_c_atp: 1}, products={MinD_m: 1, MinDE: 1})
+    model.add_reaction([R1, R2, R3, R4, R5, R6])
+
+    # Initial Conditions
+    model.add_initial_condition(spatialpy.ScatterInitialCondition(MinD_c_adp, 4500))
+    model.add_initial_condition(spatialpy.ScatterInitialCondition(MinD_e, 1575))
+
+    # tspan = spatialpy.TimeSpan(range(200), timestep_size=1)
+    tspan = spatialpy.TimeSpan(range(500), timestep_size=1)
+    model.timespan(tspan)
+
+
+if __name__=="__main__":
+    """ Dump model to a file. """
+
+    model = create_mincde()
+    result = model.run(report_level=1)
+
+    try:
+        mindm = result.get_species("MinD_m")
+        y_vals = model.mesh.coordinates()[:, 1]
+        idx = (y_vals < 1e-6)
+        mindmsum = numpy.sum(mindm[:,idx],axis=1)
+    except:
+        pass
