a120a32b7a53dc18a97606a5da6028f119653e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:11 +0000

    Add package findmyorder/1.0.23/findmyorder-1.0.23-py3-none-any.whl

---
+++ packages/findmyorder/findmyorder-1.0.23-py3-none-any.whl/findmyorder/main.py
@@ -0,0 +1,88 @@
+import asyncio, logging, re
+from datetime import datetime
+from pyparsing import Regex, Optional
+from .config import settings
+
+
+class findmyorder:
+
+    def __init__(self,
+                 ):
+        self.logger =  logging.getLogger(__name__)
+
+    def search(self,message_to_parse: str = None,):
+      try:
+      #  print(settings.identifier)
+        myDict = settings.identifier
+
+        for word in myDict:
+            #self.logger.debug(f"Loop check {word}")
+            if re.search(word, message_to_parse):
+              self.logger.debug(f"found {word} in {message_to_parse}")
+              return True
+        self.logger.debug(f"no word identified in {message_to_parse}")
+        return False
+      except Exception as e:
+        self.logger.debug(f"error search {e}")
+        return False
+
+    def identify_order(self,mystring: str = None,):
+      self.logger.debug(f"identify_order for {mystring}")
+      try:
+        action = Regex(r'/(SELL|BUY|long|short)/i')
+        instrument = Regex(r'(?<=SELL|BUY|long|short\s).\w+')
+        stop_loss = Regex(r'sl=(\d+)')
+        take_profit = Regex(r'tp=(\d+)')
+        quantity = Regex(r'q=(\d+)')
+        ordertype = Regex(settings.order_type)
+        leverage_type = Regex(r'/(isolated|cross|margin)/i')
+
+        order_grammar = action('action') + instrument('instrument') + Optional(stop_loss) + Optional(take_profit) + Optional(quantity) 
+
+        order = order_grammar.parse_string(instring=mystring,parse_all=False)
+        self.logger.debug(f"identify_order order {order}")
+        return order
+
+      except Exception as e:
+          self.logger.error(f"identify_order {e}")
+          return None
+
+    def get_order(self,mystring: str = None,):
+      try:
+        self.logger.debug(f"get_order for {mystring}")
+
+        if (self.search(mystring)):
+            self.logger.info(msg=f"get_order found: {mystring}")
+            parsed_order = self.identify_order(mystring)
+            self.logger.info(msg=f"get_order parsed_order: {parsed_order}")
+
+            # order_raw = mystring.split()
+            # self.logger.info(msg=f"Order identified: {order_raw}")
+
+            order = {}
+            order['action'] = 'BUY'
+            order['instrument'] = 'EURUSD'
+            order['stoploss'] = 1000
+            order['takeprofit'] = {'tp1':10}
+            order['quantity'] = 10
+            order['timestamp'] = datetime.utcnow()
+            # order['comments'] = 'findmyorder'
+            # order['ordertype'] = 'spot'
+            # order['instrument_type'] = 'Any'
+            # order['market'] = 'Any'
+            # order['exchange'] = 'Any'
+            # order['leverage'] = 1
+            # order['leverage_type'] = 'isolated'
+            # order['amount'] = 100
+
+            self.logger.info(msg=f"get_order order: {order}")
+
+            return order
+
+        else:
+          return None
+
+      except Exception as e:
+          self.logger.error(f"get order {e}")
+          #return
+
