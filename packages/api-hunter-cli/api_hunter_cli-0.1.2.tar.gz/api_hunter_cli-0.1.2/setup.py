d0e2fdba32f22fc54227bfe0e50a9eac37a99880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:50 +0000

    Add package api-hunter-cli/0.1.2/api_hunter_cli-0.1.2.tar.gz

---
+++ packages/api-hunter-cli/api_hunter_cli-0.1.2.tar.gz/api_hunter_cli-0.1.2/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+import sys
+
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        self.spawn([sys.executable, "post_install.py"])
+
+
+setup(
+    name="api_hunter_cli",
+    version="0.1.2",
+    description="CLI tool for finding hidden apis in a certain url.",
+    author="Charly Molero",
+    author_email="perez.moleroc@gmail.com",
+    url="https://github.com/engcarlosperezmolero/",
+    packages=find_packages(),
+    install_requires=[
+        "playwright",
+        "typer[all]",
+        "yaspin",
+        "inquirer",
+    ],
+    entry_points={
+        "console_scripts": [
+            "apihunter=apihunter.__main__:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.7",
+    cmdclass={"post_install": PostInstallCommand},
+)
