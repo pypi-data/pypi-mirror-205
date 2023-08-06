bd5fd995fab806c36c988155a0219809423f9513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:58 +0000

    Add package QPUIQ/0.1.7/QPUIQ-0.1.7-py3-none-any.whl

---
+++ packages/QPUIQ/QPUIQ-0.1.7-py3-none-any.whl/PUI/PySide6/__init__.py
@@ -0,0 +1,37 @@
+from .application import *
+from .button import *
+from .canvas import *
+from .label import *
+from .layout import *
+from .progressbar import *
+from .textfield import *
+from .window import *
+
+Application = QtApplication
+Window = QtWindow
+HBox = QtHBox
+VBox = QtVBox
+Label = QtLabel
+Button = QtButton
+Canvas = QtCanvas
+CanvasText = QtCanvasText
+CanvasLine = QtCanvasLine
+TextField = QtLineEdit
+ProgressBar = QtProgressBar
+
+def PUI(func):
+    def func_wrapper(*args):
+        class PUIViewWrapper(QPUIView):
+            def __init__(self, name):
+                self.name = name
+                super().__init__()
+
+            def content(self):
+                return self.__wrapped_content__()
+
+            def __wrapped_content__(self):
+                return func(*args)
+        ret = PUIViewWrapper(func.__name__)
+        return ret
+
+    return func_wrapper
