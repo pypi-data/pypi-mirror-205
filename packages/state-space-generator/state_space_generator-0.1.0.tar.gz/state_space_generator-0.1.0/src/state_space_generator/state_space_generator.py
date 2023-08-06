8cc9897a3641e45312282972b62f2bea321b17fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:57 +0000

    Add package state-space-generator/0.1.0/state_space_generator-0.1.0.tar.gz

---
+++ packages/state-space-generator/state_space_generator-0.1.0.tar.gz/state_space_generator-0.1.0/src/state_space_generator/state_space_generator.py
@@ -0,0 +1,31 @@
+import subprocess
+from pathlib import Path
+
+HERE = Path(__file__).resolve().parent
+
+def build_callstring(max_time: float):
+    result = "dump_reachable_search_space("
+    if max_time is not None:
+        result += f"max_time={max_time}"
+    result +=  ")"
+    return result
+
+def generate_state_space(domain: str, problem: str, max_time: str = None):
+    command = [
+        "python3",
+        Path(HERE / 'scorpion/fast-downward.py').resolve(),
+        "--keep-sas-file",
+        Path(domain).resolve(),
+        Path(problem).resolve(),
+        "--translate-options",
+        "--dump-static-predicates",
+        "--dump-predicates",
+        "--dump-constants",
+        "--dump-static-atoms",
+        "--dump-goal-atoms",
+        "--search-options",
+        "--search",
+        build_callstring(max_time)]
+    print(f'Executing "{" ".join(map(str, command))}"')
+    with open("run.log", "w") as file:
+        subprocess.run(command, stdout=file,text=True)
