63980078b3e9ef46c407678e1b57df6c8086de36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:04 +0000

    Add package pyrew/0.16.1/pyrew-0.16.1.tar.gz

---
+++ packages/pyrew/pyrew-0.16.1.tar.gz/pyrew-0.16.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.rst", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='pyrew',
+    version='0.16.1',
+    description='A Python library for writing shorter and more efficient Python code.',
+    long_description=long_description,
+    url="https://github.com/AquaQuokka/pyrew",
+    author="AquaQuokka",
+    license='BSD-3-Clause',
+    py_modules=['pyrew'],
+    scripts=['pyrew.py'],
+    install_requires=["humanize", "pillow"],
+)
