4d897f68de31c153e20f1644a1ad73abbe54f405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:48 +0000

    Add package goxlrutilityapi/1.0.0.dev6/goxlrutilityapi-1.0.0.dev6.tar.gz

---
+++ packages/goxlrutilityapi/goxlrutilityapi-1.0.0.dev6.tar.gz/goxlrutilityapi-1.0.0.dev6/goxlrutilityapi/__main__.py
@@ -0,0 +1,106 @@
+"""GoXLR Utility API: Main"""
+from __future__ import annotations
+
+import asyncio
+from collections.abc import Awaitable, Callable
+from typing import Optional
+
+import typer
+
+from ._version import __version__
+from .const import KEY_DATA, KEY_ID, KEY_TYPE
+from .exceptions import (
+    BadMessageException,
+    ConnectionClosedException,
+    ConnectionErrorException,
+)
+from .logger import setup_logger
+from .models.patch import Patch
+from .models.response import Response
+from .models.status import Status
+from .websocket_client import WebsocketClient
+
+app = typer.Typer()
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+websocket_client = WebsocketClient()
+
+
+def setup_websocket(
+    callback: Optional[Callable[[Response[Patch]], Awaitable[None]]] = None
+) -> bool:
+    """Listen for messages on another thread"""
+    try:
+        loop.run_until_complete(websocket_client.connect())
+        loop.create_task(
+            websocket_client.listen(callback),
+            name="Websocket Listener",
+        )
+    except (
+        BadMessageException,
+        ConnectionClosedException,
+        ConnectionErrorException,
+    ) as error:
+        typer.secho(f"Error: {error}", fg=typer.colors.RED)
+        loop.stop()
+        return False
+    return True
+
+
+async def patch_callback(response: Response[Patch]) -> None:
+    """Patch response callback function"""
+    typer.secho(
+        response.json(
+            include={
+                KEY_ID,
+                KEY_TYPE,
+                KEY_DATA,
+            },
+            exclude_unset=True,
+        ),
+        fg=typer.colors.GREEN,
+    )
+
+
+@app.command(name="get_status", short_help="Get Status of GoXLR")
+def get_status(debug: bool = False) -> None:
+    """Get Status of GoXLR"""
+    if debug:
+        setup_logger("DEBUG")
+    if setup_websocket() is False:
+        typer.secho("Failed to connect to GoXLR", fg=typer.colors.RED)
+        return
+    try:
+        status: Status = loop.run_until_complete(websocket_client.get_status())
+    except BadMessageException as error:
+        typer.secho(
+            f"Failed to get status from GoXLR: {error}",
+            fg=typer.colors.RED,
+        )
+        return
+    typer.secho(status.json(), fg=typer.colors.GREEN)
+
+
+@app.command(
+    name="listen_for_messages",
+    short_help="Listen for patch messages from GoXLR",
+)
+def listen_for_messages(debug: bool = False) -> None:
+    """Listen for patch messages from GoXLR"""
+    if debug:
+        setup_logger("DEBUG")
+    setup_websocket(patch_callback)
+    typer.secho("Listening for messages...", fg=typer.colors.GREEN)
+    loop.run_forever()
+
+
+@app.command(name="version", short_help="Module Version")
+def version() -> None:
+    """Module Version"""
+    typer.secho(__version__.public(), fg=typer.colors.CYAN)
+
+
+if __name__ == "__main__":
+    app()
+    loop.stop()
---
+++ packages/goxlrutilityapi/goxlrutilityapi-1.0.0.dev6.tar.gz/goxlrutilityapi-1.0.0.dev6/goxlrutilityapi/const.py
@@ -0,0 +1,37 @@
+"""GoXLR Utility API: Constants"""
+
+from typing import Any, Final
+
+from .models import DefaultBaseModel
+from .models.patch import Patch
+from .models.request import Request
+from .models.response import Response
+from .models.status import Status
+
+# Connection
+DEFAULT_HOST: Final[str] = "localhost"
+DEFAULT_PORT: Final[int] = 14564
+
+# Mixer
+VOLUME_MAX: Final[int] = 254
+
+# Request/Response Keys
+KEY_DATA: Final[str] = "data"
+KEY_ID: Final[str] = "id"
+KEY_TYPE: Final[str] = "type"
+
+# Request/Response Types
+REQUEST_TYPE_GET_STATUS: Final[str] = "GetStatus"
+RESPONSE_TYPE_PATCH: Final[str] = "Patch"
+RESPONSE_TYPE_STATUS: Final[str] = "Status"
+
+# Models
+MODEL_PATCH: type[Patch] = Patch
+MODEL_REQUEST: type[Request] = Request
+MODEL_RESPONSE: type[Response[Any]] = Response
+MODEL_STATUS: type[Status] = Status
+
+MODEL_MAP: Final[dict[str, type[DefaultBaseModel]]] = {
+    RESPONSE_TYPE_PATCH: MODEL_PATCH,
+    RESPONSE_TYPE_STATUS: MODEL_STATUS,
+}
