84aa1881425cbf3655b43c1112dabdd6c5156830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:52 +0000

    Add package netin/1.0.5.8.9/netin-1.0.5.8.9-py3.9.egg

---
+++ packages/netin/netin-1.0.5.8.9-py3.9.egg/netin/generators/pa.py
@@ -0,0 +1,105 @@
+from typing import Set
+from typing import Union
+
+import numpy as np
+
+from netin.generators.undirected import UnDiGraph
+from netin.utils import constants as const
+
+
+class PA(UnDiGraph):
+    """Creates a new PA instance. An undirected graph with preferential attachment.
+
+    Parameters
+    ----------
+    n: int
+        number of nodes (minimum=2)
+
+    k: int
+        minimum degree of nodes (minimum=1)
+
+    f_m: float
+        fraction of minorities (minimum=1/n, maximum=(n-1)/n)
+
+    seed: object
+        seed for random number generator
+
+    Notes
+    -----
+    The initialization is an undirected graph with n nodes and no edges.
+    Then, everytime a node is selected as source, it gets connected to k target nodes.
+    Target nodes are selected via preferential attachment (in-degree), see [BarabasiAlbert1999]_.
+    """
+
+    ############################################################
+    # Constructor
+    ############################################################
+
+    def __init__(self, n: int, k: int, f_m: float, seed: object = None):
+        super().__init__(n, k, f_m, seed)
+
+    ############################################################
+    # Init
+    ############################################################
+
+    def _infer_model_name(self):
+        """
+        Infers the name of the model.
+        """
+        return self.set_model_name(const.PA_MODEL_NAME)
+
+    ############################################################
+    # Generation
+    ############################################################
+
+    def get_target_probabilities(self, source: Union[None, int], target_set: Union[None, Set[int]],
+                                 special_targets: Union[None, object, iter] = None) -> tuple[np.array, set[int]]:
+        """
+        Returns the probabilities of the target nodes to be selected given a source node.
+        This probability is proportional to the degree of the target node.
+
+        Parameters
+        ----------
+        source: int
+            source node (id)
+
+        target_set: set
+            set of target nodes (ids)
+
+        special_targets: object
+            special targets
+
+        Returns
+        -------
+        probs: np.array
+            probabilities of the target nodes to be selected
+
+        target_set: set
+            set of target nodes (ids)
+        """
+        probs = np.array([(self.degree(target) + const.EPSILON) for target in target_set])
+        probs /= probs.sum()
+        return probs, target_set
+
+    def _makecopy(self):
+        """
+        Makes a copy of the current object.
+        """
+        return self.__class__(n=self.n,
+                              k=self.k,
+                              f_m=self.f_m,
+                              seed=self.seed)
+
+    @staticmethod
+    def fit(g, n=None, k=None, seed=None):
+        n = n or g.number_of_nodes()
+        k = k or g.calculate_minimum_degree()
+        f_m = g.calculate_fraction_of_minority()
+
+        new_g = PA(n=n,
+                     k=k,
+                     f_m=f_m,
+                     seed=seed)
+        new_g.generate()
+
+        return new_g
