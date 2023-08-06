64557fbf96dbc3c7b35fb310982fb7966e0a1414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:34 +0000

    Add package test-package-opqr/0.1.0/test-package-opqr-0.1.0.tar.gz

---
+++ packages/test-package-opqr/test-package-opqr-0.1.0.tar.gz/test-package-opqr-0.1.0/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name = "test-package-opqr",
+    version = "0.1.0",
+    py_modules = ["test_lib"],
+    description = "A test package.",
+    long_description = "A test package.",
+)
