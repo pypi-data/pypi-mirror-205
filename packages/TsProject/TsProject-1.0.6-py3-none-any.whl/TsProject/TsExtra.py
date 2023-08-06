134617dde51c1dc4523e6701ae0660a73afb6567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:46 +0000

    Add package TsProject/1.0.6/TsProject-1.0.6-py3-none-any.whl

---
+++ packages/TsProject/TsProject-1.0.6-py3-none-any.whl/TsProject/TsExtra.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time : 2023/04/29 21:06
+# @Author : Kqy
+# @Version : 1.0.6
+# @Website : https://www.tsginkgo.cn/
+
+def printc(text, color="white"):
+    """
+        输出彩色字符
+    """
+    # if color==""
+
+
+    # BeginColorRed = '\033[1;31m';
+    # BeginColorBlue = '\033[1;34m';
+    # EndColor = '\033[0m';
+    # print(f"{BeginColor}{text}{EndColor}")
+    print(text)
