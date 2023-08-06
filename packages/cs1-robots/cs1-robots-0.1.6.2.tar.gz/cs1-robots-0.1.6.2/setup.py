8c3fbe806e3a23f367b65571475ca6382a54c492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:09 +0000

    Add package cs1-robots/0.1.6.2/cs1-robots-0.1.6.2.tar.gz

---
+++ packages/cs1-robots/cs1-robots-0.1.6.2.tar.gz/cs1-robots-0.1.6.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name="cs1-robots",
+    version='0.1.6.2',
+    author='David Letscher and Michael H. Goldwasser',
+    packages=find_packages('src'),
+    long_description=long_description,
+    package_dir={'': 'src'},
+    url='https://github.com/hunderaweke/cs1',
+    author_email="hunderaweke@gmail.com",
+    keywords='cs1robots',
+    install_requires=[
+        'pillow',
+        'tkinter',
+    ],
+)
