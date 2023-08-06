d3ad7ba586e550080f4e87176bdb4d2d24485293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:23 +0000

    Add package aiogram-ext/0.1.19/aiogram-ext-0.1.19.tar.gz

---
+++ packages/aiogram-ext/aiogram-ext-0.1.19.tar.gz/aiogram-ext-0.1.19/botty/errors.py
@@ -0,0 +1,15 @@
+from deps import BadRequest
+
+
+class NoSendTextRights(BadRequest):
+    msg = "Not enough rights to send text messages to the chat"
+
+    def __str__(self):
+        return self.msg
+
+
+class NoSendPhotoRights(BadRequest):
+    msg = "Not enough rights to send photos to the chat"
+
+    def __str__(self):
+        return self.msg
