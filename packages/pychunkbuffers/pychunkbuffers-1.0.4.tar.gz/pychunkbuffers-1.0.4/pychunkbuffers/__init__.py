ace060c6c9874386caf89126b6889771d737384e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:23 +0000

    Add package pychunkbuffers/1.0.4/pychunkbuffers-1.0.4.tar.gz

---
+++ packages/pychunkbuffers/pychunkbuffers-1.0.4.tar.gz/pychunkbuffers-1.0.4/pychunkbuffers/__init__.py
@@ -0,0 +1,64 @@
+from typing import Any, Callable, Iterable, List, Mapping, Tuple
+
+import threading
+import math
+
+class ChunkedFunction:
+    def __init__(self, func:Callable, idx:int, chunksz:int, minidx:int, maxidx:int, args:Iterable[Any]=None, kwargs:Mapping[str,Any]=None, daemon:bool=False, statuslist:Iterable[bool]=None):
+        self.func = func
+        self.idx = idx
+        self.startidx = minidx+idx*chunksz
+        self.endidx = min((idx+1)*chunksz, maxidx)
+        self.args = args
+        self.kwargs = kwargs
+        self.statuslist = statuslist
+        self.thread = threading.Thread(target=self.run)
+        self.thread.setDaemon(daemon)
+    def run(self):
+        if self.args==None: self.args=[]
+        if self.kwargs==None: self.kwargs={}
+        self.func(*self.args, **self.kwargs, startidx=self.startidx, endidx=self.endidx)
+        self.statuslist[self.idx] = True
+    def start(self):
+        self.thread.start()
+
+def run_chunked(func:Callable, chunksz:int, minidx:int, maxidx:int, args:Iterable[Any]=None, kwargs:Mapping[str,Any]=None, daemon:bool=False) -> List[bool]:
+    """
+    Params:
+     - func    : Function or callable, must take keyword arguments `startidx` and `endidx`
+     - chunksz : Integer, size of each chunk
+     - minidx  : Integer, minimum index ie starting index of first chunk
+     - maxidx  : Integer, maximum index ie ending index of last chunk
+     - daemon  : Bool, whether the chunks should be run as a daemon thread (killed upon program completion)
+     - args    : Iterable (like list), passed directly to the function
+     - kwargs  : Mapping (like dict), passed directly to the function
+    Returns: List of Bool values.
+        This list holds the completion status of all chunks.
+        Initially, all values are False.
+        When a chunk finishes its function, it's corresponding status is set to True.
+        `while not all(LIST): pass` can be used to wait for all chunks to finish
+    """
+    assert type(chunksz)==int
+    assert type(minidx)==int
+    assert type(maxidx)==int
+    n = math.ceil((maxidx-minidx)/chunksz)
+    statuslist = [False]*n
+    for i in range(n):
+        ChunkedFunction(func, i, chunksz, minidx, maxidx, args, kwargs, daemon, statuslist).start()
+    return statuslist
+
+def run(func:Callable, *args, **kwargs) -> List[bool]:
+    return run_chunked(func, *args, **kwargs)
+
+run.__doc__ = run_chunked.__doc__
+
+if __name__ == "__main__":
+    # Task: To write the squares values for numbers 1 to 10**8 (inclusive)
+    squares = [0]*10**8
+    CHUNKSIZE = 10**5
+    def func(startidx, endidx):
+        for i in range(startidx, endidx):
+            squares[i] = (i+1)**2
+    status = run_chunked(func, CHUNKSIZE, 0, len(squares))
+    while not all(status): pass
+    print("Done")
---
+++ packages/pychunkbuffers/pychunkbuffers-1.0.4.tar.gz/pychunkbuffers-1.0.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md") as fl: ldesc = fl.read()
+setuptools.setup(
+    name="pychunkbuffers",
+    version="1.0.4",
+    author="AdityaIyer2k7",
+    author_email="adityaiyer2007@gmail.com",
+    description="An open-source python library for writing large amounts of data to buffers via chunks",
+    long_description=ldesc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AdityaIyer2k7/pychunkbuffers",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities"
+    ],
+    keywords=[
+        "python 3",
+        "threading",
+        "thread",
+        "chunking",
+        "buffers",
+        "pychunk",
+        "pybuffer"
+    ],
+    python_requires='>=3.6'
+)
