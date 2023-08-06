440ab28d8fccdb232d24605cf6e647c1baadf695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:06 +0000

    Add package usepy/0.1.46/usepy-0.1.46.tar.gz

---
+++ packages/usepy/usepy-0.1.46.tar.gz/usepy-0.1.46/src/usepy/__init__.py
@@ -0,0 +1,101 @@
+from ._datetime import useDateTime
+from ._path import usePath
+from ._thread import useThread
+from .data import (
+    useDict,
+    useList,
+    useString,
+    useAdDict
+)
+from .decorator import (
+    useDecorator,
+    useSingleton,
+    useTimeIt,
+    useRunInThread,
+    useCatchError,
+    useExceptDebug,
+    useListify,
+    useCachedProperty,
+    useRetry
+)
+
+from .parser import (
+    useParser,
+    useCURL,
+    useURL
+)
+from .utils import (
+    useTimer,
+    useTimerManager,
+    useUserAgent,
+    useImport,
+    useLazyImport,
+    useUniqueId,
+    useDataToDict,
+    useCookieToDict,
+    useHeadersToDict,
+    useCleanHtml,
+    useBloomFilter,
+    useTo,
+    useIs
+)
+from .utils.bloom_filter import BloomFilter as useBloomFilter
+
+# plugins
+try:
+    from usepy_notify import useNotify, channels as useNotifyChannels
+except ImportError:
+    pass
+try:
+    from usepy_logger import *
+except ImportError:
+    pass
+
+__all__ = [
+    # data
+    'useDict',
+    'useList',
+    'useString',
+    'useAdDict',
+    # decorator
+    'useDecorator',
+    'useSingleton',
+    'useTimeIt',
+    'useRunInThread',
+    'useCatchError',
+    'useExceptDebug',
+    'useListify',
+    'useCachedProperty',
+    'useRetry',
+    # parser
+    'useParser',
+    'useCURL',
+    'useURL',
+    # utils
+    'useIs',
+    'useTo',
+    'useTimer',
+    'useTimerManager',
+    'useUserAgent',
+    'useImport',
+    'useLazyImport',
+    'useUniqueId',
+    'useDataToDict',
+    'useCookieToDict',
+    'useHeadersToDict',
+    'useCleanHtml',
+    'useBloomFilter',
+    'useDateTime',
+    'usePath',
+    'useThread',
+
+    # plugins
+    'useNotify',
+    'useNotifyChannels',
+
+    'useLogger',
+    'useLoggerIntercept',
+    'useLoggerInterceptUvicorn',
+    'useLoggerHandlers',
+    'useLoggerFormatters'
+]
