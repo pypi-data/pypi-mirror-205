28d90fc95b82619fbad664686ad36821ea1cdfe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:23 +0000

    Add package promsoft-pecom-interface/0.1.3/promsoft_pecom_interface-0.1.3.tar.gz

---
+++ packages/promsoft-pecom-interface/promsoft_pecom_interface-0.1.3.tar.gz/promsoft_pecom_interface-0.1.3/promsoft_pecom_interface/__init__.py
@@ -0,0 +1,20 @@
+from promsoft_pecom_interface.common import IdentityCard
+
+from promsoft_pecom_interface.cargopickup import (
+    CommonCargopickup,
+    ServicesCargopickup,
+    PersonInfoCargopickup,
+    SenderCargopickup,
+    ReceiverCargopickup,
+    PaymentsCargopickup,
+    BaseCargopickup,
+)
+
+from promsoft_pecom_interface.preregistration import (
+    SenderPreregistration,
+    CommonPreregistration,
+    ReceiverPreregistration,
+    ServicesPreregistration,
+    _get_def_payer,
+    CargoPreregistration,
+)
