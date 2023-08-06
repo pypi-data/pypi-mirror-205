1ff1318e9e28fd643aa1d1673dc7559d22a15b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:15 +0000

    Add package formant/1.123.65/formant-1.123.65.tar.gz

---
+++ packages/formant/formant-1.123.65.tar.gz/formant-1.123.65/formant/sdk/cloud/v2/src/resources/count/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_query_api_client.models import CountHistoryEntryCount, CountHistoryQuery, ActiveDevicesQuery, CountHistoryEntry, CountHistoryQueryType, UuidListResponse, CountHistory, ActiveDevicesQueryType
+from formant.sdk.cloud.v2.formant_query_api_client.api.count import count_controller_history, count_controller_active_devices
+from formant.sdk.cloud.v2.formant_query_api_client.types import *
