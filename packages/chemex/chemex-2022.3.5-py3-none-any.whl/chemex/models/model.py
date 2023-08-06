6c2bb05e69740734e7fb852ce199bde7a0a15443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:09 +0000

    Add package chemex/2022.3.5/chemex-2022.3.5-py3-none-any.whl

---
+++ packages/chemex/chemex-2022.3.5-py3-none-any.whl/chemex/models/model.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from string import ascii_lowercase
+
+from chemex.models.factory import model_factory
+
+
+@dataclass
+class _Model:
+    _name: str = "2st"
+    _states: str = "ab"
+    _model_free: bool = False
+    _temp_coef: bool = False
+
+    @staticmethod
+    def validate_model_name(name: str) -> str:
+        if name not in model_factory.set:
+            print("Warning: The 'model' option should either be:")
+            for model_name in sorted(model_factory.set):
+                print(f"    - '{model_name}'")
+            print("Set to the default value: '2st'.")
+            return "2st"
+        return name
+
+    def set_model(self, name: str) -> None:
+        kinetic_model_name, *ext = name.split(".")
+        self._name = self.validate_model_name(kinetic_model_name)
+        state_nb = int(kinetic_model_name[0])
+        self._states = ascii_lowercase[:state_nb]
+        self._model_free = "mf" in ext if ext else False
+        self._temp_coef = "tc" in ext if ext else False
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def states(self) -> str:
+        return self._states
+
+    @property
+    def model_free(self) -> bool:
+        return self._model_free
+
+    @property
+    def temp_coef(self) -> bool:
+        return self._temp_coef
+
+
+model = _Model()
+
+set_model = model.set_model
