7958a5bc4a4882087b4cb4879f5a2257e3a4aff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:52 +0000

    Add package gpt-index/0.6.0a1/gpt_index-0.6.0a1.tar.gz

---
+++ packages/gpt-index/gpt_index-0.6.0a1.tar.gz/gpt_index-0.6.0a1/gpt_index/constants.py
@@ -0,0 +1,16 @@
+"""Set of constants."""
+
+MAX_CHUNK_SIZE = 3900
+MAX_CHUNK_OVERLAP = 200
+NUM_OUTPUTS = 256
+
+
+INDEX_STRUCT_KEY = "index_struct"
+DOCSTORE_KEY = "docstore"
+VECTOR_STORE_KEY = "vector_store"
+ADDITIONAL_QUERY_CONTEXT_KEY = "query_context"
+ALL_INDICES_KEY = "all_indices"
+ROOT_INDEX_ID_KEY = "root_index_id"
+
+TYPE_KEY = "__type__"
+DATA_KEY = "__data__"
