9598c80f64ccba58febbe3533a5377daf1e185ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:59 +0000

    Add package bfactory/0.4.1/bfactory-0.4.1-py3-none-any.whl

---
+++ packages/bfactory/bfactory-0.4.1-py3-none-any.whl/bfactory/tests/engine_tests.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import unittest
+
+from bfactory.inputs.manifest import Manifest
+from bfactory.core.engine import Engine
+from bfactory.utils.paths import Paths
+
+fpaths = Paths()
+
+class EngineTest(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        to_path = '/tmp/test_bfactory'
+        if not fpaths.check_path(to_path, force=True):
+            exit(2)
+        
+        self.manifest_path = fpaths.abspath('tests/manifest_test.json')
+        self.manifest = Manifest(file_path_manifest=self.manifest_path)
+        
+        fpaths.manifest = self.manifest    
+        fpaths.to_path = to_path
+        
+        self.engine = Engine(manifest=self.manifest)
+
+    def test_model_creation(self):
+        self.engine.create_api()
