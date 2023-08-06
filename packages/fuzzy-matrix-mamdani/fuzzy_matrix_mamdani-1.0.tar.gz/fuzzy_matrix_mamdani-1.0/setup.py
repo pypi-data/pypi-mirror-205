9c0a3b0a9ffa74a0260046678d463699476968a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:00 +0000

    Add package fuzzy-matrix-mamdani/1.0/fuzzy_matrix_mamdani-1.0.tar.gz

---
+++ packages/fuzzy-matrix-mamdani/fuzzy_matrix_mamdani-1.0.tar.gz/fuzzy_matrix_mamdani-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fuzzy_matrix_mamdani',
+      version='1.0',
+      description='matrix and mamdani fuzzy inference',
+      packages=['fuzzy_matrix_mamdani'],
+      author_email='epishina.nata@bk.ru',
+      zip_safe=False)
