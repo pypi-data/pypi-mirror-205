ea0ef18100e3f2468b203abad128e9017486db8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:56 +0000

    Add package msgraphlib/0.1.3/msgraphlib-0.1.3-py3-none-any.whl

---
+++ packages/msgraphlib/msgraphlib-0.1.3-py3-none-any.whl/msgraphlib/__init__.py
@@ -0,0 +1,9 @@
+from .core import Metadata
+from .core import Config, Basic, ConfigException
+# from .drive import DriveExplorer
+# from .excel import Excel
+# from .workbook import Workbook
+# from .sharepoint import _SiteBasic, Site, List
+__version__ = Metadata.PACKAGE_VERSION
+#__all__ = ["files.Files", "client.Config"]
+
