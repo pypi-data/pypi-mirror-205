43474c767c494110b545235f4abeb974b0cd8bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:56 +0000

    Add package nitrado/1.0.15/nitrado-1.0.15-py3-none-any.whl

---
+++ packages/nitrado/nitrado-1.0.15-py3-none-any.whl/nitrado/globals/globals.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+from ..lib import Client
+from .maintenance import Maintenance
+from .game import Game
+
+
+class Global:
+    @classmethod
+    def health_check(cls) -> Global:
+        """Health information about the Nitrado API."""
+        response = Client.get('/ping')
+        kwargs: dict = response.json()
+        return cls(**kwargs)
+
+    @classmethod
+    def maintenance_status(cls) -> Maintenance:
+        response = Client.get('/maintenance')
+        kwargs: dict = response.json()
+        return Maintenance(**kwargs['data']['maintenance'])
+
+    @classmethod
+    def version(cls) -> str:
+        """Nitrado API version."""
+        response = Client.get('/version')
+        kwargs: dict = response.json()
+        return cls(**kwargs).message
+
+    @classmethod
+    def full_game_list(cls) -> list[Game]:
+        """Get all games supported by Nitrado."""
+        path = '/gameserver/games'
+        response = Client.get(path=path)
+        data: dict = response.json()['data']
+        return Game.from_data(**data['games'])
+
+    def __init__(self, data: dict = None, message: str = None, status: str = None):
+        self.success = status == 'success'
+        self.data = data
+        self.message = message
+        self.status = status
+
+    def __repr__(self):
+        params = [f'{k}={repr(v)}' for k, v in self.__dict__.items()]
+        return f"<{self.__class__.__name__}({', '.join(params)})>"
