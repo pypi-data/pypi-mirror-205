db94a016f4a2a112b57b272aa22f9227907a1354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:11 +0000

    Add package lammps/2022.6.23.3.1/lammps-2022.6.23.3.1-py2.py3-none-win_amd64.whl

---
+++ packages/lammps/lammps-2022.6.23.3.1-py2.py3-none-win_amd64.whl/lammps/etc/profile.d/lammps.sh
@@ -0,0 +1,5 @@
+# set environment for LAMMPS and msi2lmp executables
+# to find potential and force field files
+LAMMPS_POTENTIALS=${LAMMPS_POTENTIALS-C:/Users/runneradmin/LAMMPS/share/lammps/potentials}
+MSI2LMP_LIBRARY=${MSI2LMP_LIBRARY-C:/Users/runneradmin/LAMMPS/share/lammps/frc_files}
+export LAMMPS_POTENTIALS MSI2LMP_LIBRARY
