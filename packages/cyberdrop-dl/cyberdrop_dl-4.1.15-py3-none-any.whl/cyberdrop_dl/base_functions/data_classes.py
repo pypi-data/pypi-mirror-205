3b407065eacb4b88c830514d29529f3d107a0863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:08 +0000

    Add package cyberdrop-dl/4.1.15/cyberdrop_dl-4.1.15-py3-none-any.whl

---
+++ packages/cyberdrop-dl/cyberdrop_dl-4.1.15-py3-none-any.whl/cyberdrop_dl/base_functions/data_classes.py
@@ -0,0 +1,243 @@
+from __future__ import annotations
+
+import asyncio
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, ClassVar, List, Tuple
+
+if TYPE_CHECKING:
+    from yarl import URL
+
+
+@dataclass
+class MediaItem:
+    url: URL
+    referer: URL
+    complete: bool
+    filename: str
+    ext: str
+    original_filename: str
+
+    async def is_complete(self):
+        return self.complete
+
+    async def mark_completed(self):
+        self.complete = True
+
+
+@dataclass
+class AlbumItem:
+    """Class for keeping track of download links for each album"""
+    title: str
+    media: List[MediaItem]
+
+    async def add_media(self, media_item: MediaItem):
+        self.media.append(media_item)
+
+    async def set_new_title(self, new_title: str):
+        self.title = new_title
+
+    async def append_title(self, title):
+        new_title = title + '/' + self.title
+        self.title = new_title
+
+    async def extend(self, album):
+        self.media.extend(album.media)
+
+    async def is_empty(self):
+        if not self.media:
+            return True
+        return False
+
+
+@dataclass
+class DomainItem:
+    """Class for keeping track of albums for each scraper type"""
+    domain: str
+    albums: dict
+
+    async def add_to_album(self, title: str, media: MediaItem):
+        if title in self.albums:
+            await self.albums[title].add_media(media)
+        else:
+            self.albums[title] = AlbumItem(title=title, media=[media])
+
+    async def add_media(self, title: str, media: MediaItem):
+        if title in self.albums:
+            album = self.albums[title]
+            await album.add_media(media)
+        else:
+            self.albums[title] = AlbumItem(title, [media])
+
+    async def add_album(self, title: str, album: AlbumItem):
+        if title in self.albums:
+            stored_album = self.albums[title]
+            for media_item in album.media:
+                if media_item in stored_album.media:
+                    continue
+                await stored_album.add_media(media_item)
+        else:
+            self.albums[title] = album
+
+    async def set_new_domain(self, domain: str):
+        self.domain = domain
+
+    async def extend(self, domain):
+        for title, album in domain.albums.items():
+            await self.add_album(title, album)
+
+    async def append_title(self, title):
+        if not title:
+            return
+        new_albums = {}
+        for album_str, album in self.albums.items():
+            new_title = title+'/'+album_str
+            new_albums[new_title] = album
+            album.title = new_title
+        self.albums = new_albums
+
+
+@dataclass
+class CascadeItem:
+    """Class for keeping track of domains for each scraper type"""
+    domains: dict
+
+    async def add_albums(self, domain_item: DomainItem):
+        domain = domain_item.domain
+        albums = domain_item.albums
+        for title, album in albums.items():
+            await self.add_album(domain, title, album)
+
+    async def add_to_album(self, domain: str, title: str, media_item: MediaItem):
+        if domain in self.domains:
+            await self.domains[domain].add_to_album(title, media_item)
+        else:
+            self.domains[domain] = DomainItem(domain, {title: AlbumItem(title, [media_item])})
+
+    async def add_album(self, domain: str, title: str, album: AlbumItem):
+        if domain in self.domains:
+            await self.domains[domain].add_album(title, album)
+        else:
+            self.domains[domain] = DomainItem(domain, {title: album})
+
+    async def is_empty(self):
+        for domain in self.domains.values():
+            for album in domain.albums.values():
+                if album.media:
+                    return False
+        return True
+
+    async def get_total(self):
+        total = 0
+        for domain in self.domains.values():
+            for album in domain.albums.values():
+                total += len(album.media)
+        return total
+
+    async def append_title(self, title: str):
+        if not title:
+            return
+        for domain in self.domains.values():
+            new_albums = {}
+            for album_str, album in domain.albums.items():
+                new_title = title+'/'+album_str
+                new_albums[new_title] = album
+                album.title = new_title
+            domain.albums = new_albums
+
+    async def extend(self, Cascade):
+        if Cascade and Cascade.domains:
+            for domain_str, domain in Cascade.domains.items():
+                for album_str, album in domain.albums.items():
+                    await self.add_album(domain_str, album_str, album)
+
+    async def dedupe(self):
+        for domain in self.domains.values():
+            for album in domain.albums.values():
+                check = []
+                allowed = []
+                for media_item in album.media:
+                    if media_item.url in check:
+                        continue
+                    check.append(media_item.url)
+                    allowed.append(media_item)
+                album.media = allowed
+
+
+@dataclass
+class ForumItem:
+    """Class for keeping track of forum threads"""
+    threads: dict
+
+    async def add_album_to_thread(self, title: str, domain: str, album: AlbumItem):
+        if title not in self.threads:
+            self.threads[title] = CascadeItem({domain: DomainItem(domain, {album.title: album})})
+        else:
+            await self.threads[title].add_album(domain, album.title, album)
+
+    async def add_thread(self, title: str, cascade: CascadeItem):
+        if title not in self.threads:
+            self.threads[title] = cascade
+        else:
+            await self.threads[title].extend(cascade)
+
+    async def is_empty(self):
+        for Cascade in self.threads.values():
+            for domain in Cascade.domains.values():
+                for album in domain.albums.values():
+                    if album.media:
+                        return False
+        return True
+
+    async def get_total(self):
+        total = 0
+        for Cascade in self.threads.values():
+            for domain in Cascade.domains.values():
+                for album in domain.albums.values():
+                    total += len(album.media)
+        return total
+
+    async def dedupe(self):
+        for Cascade in self.threads.values():
+            for domain in Cascade.domains.values():
+                for album in domain.albums.values():
+                    check = []
+                    allowed = []
+                    for media_item in album.media:
+                        if media_item.url in check:
+                            continue
+                        check.append(media_item.url)
+                        allowed.append(media_item)
+                    album.media = allowed
+
+    async def extend_thread(self, title: str, cascade: CascadeItem):
+        if title in self.threads:
+            await self.threads[title].extend(cascade)
+        else:
+            self.threads[title] = cascade
+
+@dataclass
+class FileLock:
+    """Rudimentary file lock system"""
+    locked_files = []
+
+    async def check_lock(self, filename):
+        await asyncio.sleep(.1)
+        return filename.lower() in self.locked_files
+
+    async def add_lock(self, filename):
+        self.locked_files.append(filename.lower())
+
+    async def remove_lock(self, filename):
+        self.locked_files.remove(filename.lower())
+
+
+@dataclass
+class SkipData:
+    """The allows optoins for domains to skip when scraping"""
+    supported_hosts: ClassVar[Tuple[str]] = ("anonfiles", "bayfiles", "bunkr", "coomer.party", "cyberdrop",
+                                             "cyberfile", "e-hentai", "erome", "fapello", "gfycat", "gofile",
+                                             "hgamecg", "img.kiwi", "imgbox", "jpg.church", "jpg.fish",
+                                             "gallery.deltaporno.com", "kemono.party",
+                                             "lovefap", "nsfw.xxx", "pimpandhost", "pixeldrain", "pixl.li", "postimg",
+                                             "saint", "simpcity", "socialmediagirls", "xbunker", "xbunkr")
+    sites: List[str]
---
+++ packages/cyberdrop-dl/cyberdrop_dl-4.1.15-py3-none-any.whl/cyberdrop_dl/crawlers/Xenforo_Spider.py
@@ -0,0 +1,301 @@
+from __future__ import annotations
+
+import asyncio
+import re
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
+
+import aiofiles
+from bs4 import BeautifulSoup
+from yarl import URL
+
+from ..base_functions.base_functions import (
+    get_filename_and_ext,
+    log,
+    logger,
+    make_title_safe,
+)
+from ..base_functions.data_classes import CascadeItem, MediaItem
+from ..base_functions.error_classes import FailedLoginFailure, NoExtensionFailure
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from ..base_functions.sql_helper import SQLHelper
+    from ..client.client import ScrapeSession
+
+
+async def write_last_post_file(file: Path, url: str):
+    """Writes the last post url from a thread to the specified file"""
+    async with aiofiles.open(file, mode='a') as f:
+        await f.write(url + '\n')
+
+
+@dataclass
+class ParseSpec:
+    """Class for specific selectors of supported domains"""
+    domain: str
+    title_clutter_tag: str = field(init=False)
+
+    def __post_init__(self):
+        if self.domain in ("simpcity", "xbunker"):
+            self.title_clutter_tag = "a"
+        elif self.domain == "socialmediagirls":
+            self.title_clutter_tag = "span"
+
+
+class ForumLogin:
+    def __init__(self, name: str, username: str, password: str):
+        self.name = name
+        self.logged_in = False
+        self.username = username
+        self.password = password
+
+    async def login(self, session: ScrapeSession, url: URL, quiet: bool):
+        """Handles forum logging in"""
+        if not self.username or not self.password:
+            await log(f"Login wasn't provided for {self.name}", quiet=quiet, style="red")
+            raise FailedLoginFailure()
+        attempt = 0
+        while True:
+            try:
+                if self.logged_in:
+                    return
+                if attempt == 5:
+                    raise FailedLoginFailure()
+
+                domain = URL("https://" + url.host) / "login"
+                text = await session.get_text(domain)
+                await asyncio.sleep(5)
+                soup = BeautifulSoup(text, 'html.parser')
+
+                inputs = soup.select('form input')
+                data = {
+                    elem['name']: elem['value']
+                    for elem in inputs
+                    if elem.get('name') and elem.get('value')
+                }
+                data.update({
+                    "login": self.username,
+                    "password": self.password,
+                    "_xfRedirect": str(URL("https://" + url.host))
+                })
+                await session.post_data_no_resp(domain / "login", data=data)
+                await asyncio.sleep(5)
+                text = await session.get_text(domain)
+                if "You are already logged in" not in text:
+                    raise FailedLoginFailure()
+
+                self.logged_in = True
+            except asyncio.exceptions.TimeoutError:
+                attempt += 1
+                continue
+
+
+class XenforoCrawler:
+    domains = ("simpcity", "socialmediagirls", "xbunker")
+
+    def __init__(self, *, scraping_mapper, args: dict, SQL_Helper: SQLHelper, quiet: bool):
+        self.include_id = args["Runtime"]["include_id"]
+        self.quiet = quiet
+        self.separate_posts = args["Forum_Options"]["separate_posts"]
+        self.output_last = args["Forum_Options"]["output_last_forum_post"]
+        self.output_last_file = args["Files"]["output_last_forum_post_file"]
+
+        self.simpcity = ForumLogin("SimpCity",
+                                   args["Authentication"]["simpcity_username"],
+                                   args["Authentication"]["simpcity_password"])
+
+        self.socialmediagirls = ForumLogin("SocialMediaGirls",
+                                           args["Authentication"]["socialmediagirls_username"],
+                                           args["Authentication"]["socialmediagirls_password"])
+
+        self.xbunker = ForumLogin("XBunker",
+                                  args["Authentication"]["xbunker_username"],
+                                  args["Authentication"]["xbunker_password"])
+
+        self.scraping_mapper = scraping_mapper
+        self.SQL_Helper = SQL_Helper
+
+    async def fetch(self, session: ScrapeSession, url: URL):
+        """Xenforo forum director"""
+        await log(f"Starting: {str(url)}", quiet=self.quiet, style="green")
+        cascade = CascadeItem({})
+
+        scrape_url, post_num = await self.get_thread_url_and_post_num(url)
+        title = None
+        try:
+            if "simpcity" in url.host:
+                await self.simpcity.login(session, url, self.quiet)
+            elif "socialmediagirls" in url.host:
+                await self.socialmediagirls.login(session, url, self.quiet)
+            elif "xbunker" in url.host:
+                await self.xbunker.login(session, url, self.quiet)
+
+            domain = next((domain for domain in self.domains if domain in url.host), None)
+            if domain:
+                title = await self.parse_forum(session, scrape_url, ParseSpec(domain), cascade, "", post_num)
+        except Exception as e:
+            logger.debug("Error encountered while handling %s", str(url), exc_info=True)
+            await log(f"Error: {str(url)}", quiet=self.quiet, style="red")
+            logger.debug(e)
+            return cascade, ""
+
+        await self.SQL_Helper.insert_cascade(cascade)
+        await log(f"Finished: {str(url)}", quiet=self.quiet, style="green")
+        return cascade, title
+
+    async def get_thread_url_and_post_num(self, url: URL):
+        """Splits the thread url and returns the url and post number if provided"""
+        post_number = 0
+        if "post-" in str(url):
+            post_number_parts = str(url).rsplit("post-", 1)
+            post_number = int(post_number_parts[-1].strip("/")) if len(post_number_parts) == 2 else 0
+            url = URL(post_number_parts[0].rstrip("#"))
+        return url, post_number
+
+    async def get_links(self, post_content, selector, attribute, domain, temp_title):
+        """Grabs links from the post content based on the given selector and attribute"""
+        found_links = []
+        if not post_content:
+            return found_links
+        links = post_content.select(selector)
+        for link in links:
+            link = link.get(attribute)
+            if link.endswith("/"):
+                link = link[:-1]
+            if link.startswith('//'):
+                link = "https:" + link
+            elif link.startswith('/'):
+                link = domain / link[1:]
+            found_links.append([URL(link), temp_title])
+        return found_links
+
+    async def get_embedded(self, post_content, selector, attribute, domain, temp_title):
+        """Gets embedded media from post content based on selector and attribute provided"""
+        found_links = []
+        links = post_content.select(selector)
+        for link in links:
+            embed_data = link.get(attribute)
+            embed_data = embed_data.replace("\/\/", "https://www.")
+            embed_data = embed_data.replace("\\", "")
+
+            embed_url = re.search(
+                r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)",
+                embed_data)
+            if embed_url:
+                embed_url = URL(embed_url.group(0).replace("www.", ""))
+                found_links.append([embed_url, temp_title])
+
+            embed_url = re.search(
+                r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\/[-a-zA-Z0-9@:%._\+~#=]*\/[-a-zA-Z0-9@:?&%._\+~#=]*",
+                embed_data)
+            if embed_url:
+                embed_url = URL(embed_url.group(0).replace("www.", ""))
+                found_links.append([embed_url, temp_title])
+        return found_links
+
+    async def filter_content_links(self, cascade: CascadeItem, content_links: list, url: URL, domain: str):
+        """Splits given links into direct links and external links,
+        returns external links, adds internal to the cascade"""
+        forum_direct_urls = [x for x in content_links if x[0].host.replace(".st", ".su") in url.host]
+        forum_direct_urls.extend([x for x in content_links if url.host in x[0].host.replace(".st", ".su")])
+        forum_direct_urls.extend([x for x in content_links if "smgmedia" in x[0].host])
+        content_links = [x for x in content_links if x not in forum_direct_urls]
+        for link_title_bundle in forum_direct_urls:
+            link = link_title_bundle[0]
+            temp_title = link_title_bundle[1]
+            in_prog_title = temp_title + "/Attachments"
+            if str(link).endswith("/"):
+                link = URL(str(link)[:-1])
+            if 'attachments' in link.parts or 'content' in link.parts or 'data' in link.parts:
+                completed = await self.SQL_Helper.check_complete_singular(domain, link)
+                try:
+                    filename, ext = await get_filename_and_ext(link.name, True)
+                except NoExtensionFailure:
+                    continue
+                media = MediaItem(link, url, completed, filename, ext, filename)
+                await cascade.add_to_album(domain, in_prog_title, media)
+        return content_links
+
+    async def handle_external_links(self, content_links: list, referer: URL):
+        """Maps external links to the scraper class"""
+        tasks = []
+        for link_title_bundle in content_links:
+            link = link_title_bundle[0]
+            temp_title = link_title_bundle[1]
+            tasks.append(asyncio.create_task(self.scraping_mapper.map_url(link, temp_title, referer)))
+        if tasks:
+            await asyncio.wait(tasks)
+
+    async def parse_forum(self, session: ScrapeSession, url: URL, spec: ParseSpec, cascade: CascadeItem,
+                          title: str, post_number: int):
+        """Parses forum threads"""
+        soup = await session.get_BS4(url)
+
+        domain = URL("https://" + url.host)
+        post_num_str = None
+        content_links = []
+
+        title_block = soup.select_one("h1[class=p-title-value]")
+        for elem in title_block.find_all(spec.title_clutter_tag):
+            elem.decompose()
+
+        if title:
+            pass
+        else:
+            title = title_block.text
+            title = await make_title_safe(title.replace("\n", "").strip())
+
+        posts = soup.select("div[class='message-main uix_messageContent js-quickEditTarget']")
+
+        for post in posts:
+            post_num_str = post.select_one("li[class=u-concealed] a").get('href').split('/')[-1]
+            post_num_int = int(post_num_str.split('post-')[-1])
+            if post_number > post_num_int:
+                continue
+
+            temp_title = title + "/" + post_num_str if self.separate_posts else title
+
+            for elem in post.find_all('blockquote'):
+                elem.decompose()
+            post_content = post.select_one("div[class=bbWrapper]")
+
+            # Get Links
+            content_links.extend(await self.get_links(post_content, "a", "href", domain, temp_title))
+
+            # Get Images
+            content_links.extend(await self.get_links(post_content, "div[class='bbImageWrapper js-lbImage']", "data-src", domain, temp_title))
+            content_links.extend(await self.get_links(post_content, "div[class='bbImageWrapper lazyload js-lbImage']", "data-src", domain, temp_title))
+
+            # Get Videos:
+            content_links.extend(await self.get_links(post_content, "video source", "src", domain, temp_title))
+            content_links.extend(await self.get_links(post_content, "iframe[class=saint-iframe]", "src", domain, temp_title))
+
+            # Get Other Embedded Content
+            content_links.extend(await self.get_embedded(post_content, "span[data-s9e-mediaembed-iframe]", "data-s9e-mediaembed-iframe",
+                                 domain, temp_title))
+
+            # Get Attachments
+            attachments_block = post.select_one("section[class=message-attachments]")
+            content_links.extend(await self.get_links(attachments_block, "a[class='file-preview js-lbImage']", "href", domain, temp_title))
+
+        # Handle links
+        content_links = await self.filter_content_links(cascade, content_links, url, spec.domain)
+        await self.handle_external_links(content_links, url)
+
+        next_page = soup.select_one('a[class="pageNav-jump pageNav-jump--next"]')
+        if next_page is not None:
+            next_page = next_page.get('href')
+            if next_page is not None:
+                if next_page.startswith('/'):
+                    next_page = domain / next_page[1:]
+                next_page = URL(next_page)
+                await self.parse_forum(session, next_page, spec, cascade, title, post_number)
+        elif self.output_last:
+            if 'page-' in url.raw_name or 'post-' in url.raw_name:
+                last_post_url = url.parent / post_num_str
+            else:
+                last_post_url = url / post_num_str
+            await write_last_post_file(self.output_last_file, str(last_post_url))
+        return title
