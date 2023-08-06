4fe11e0bd26b85148124a47158ad529d8ca5d398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:43 +0000

    Add package gradio-tools/0.0.7/gradio_tools-0.0.7.tar.gz

---
+++ packages/gradio-tools/gradio_tools-0.0.7.tar.gz/gradio_tools-0.0.7/gradio_tools/__init__.py
@@ -0,0 +1,21 @@
+from gradio_tools.tools import (BarkTextToSpeechTool, ClipInterrogatorTool,
+                                DocQueryDocumentAnsweringTool, GradioTool,
+                                ImageCaptioningTool, ImageToMusicTool,
+                                StableDiffusionPromptGeneratorTool,
+                                StableDiffusionTool, TextToVideoTool,
+                                WhisperAudioTranscriptionTool,
+                                SAMImageSegmentationTool)
+
+__all__ = [
+    "GradioTool",
+    "StableDiffusionTool",
+    "ClipInterrogatorTool",
+    "ImageCaptioningTool",
+    "ImageToMusicTool",
+    "WhisperAudioTranscriptionTool",
+    "StableDiffusionPromptGeneratorTool",
+    "TextToVideoTool",
+    "DocQueryDocumentAnsweringTool",
+    "BarkTextToSpeechTool",
+    "SAMImageSegmentationTool"
+]
