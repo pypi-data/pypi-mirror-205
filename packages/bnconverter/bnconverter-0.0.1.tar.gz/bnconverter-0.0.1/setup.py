58b05416f9c6bd50d0f206eb2c24eda6a833b16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:00 +0000

    Add package bnconverter/0.0.1/bnconverter-0.0.1.tar.gz

---
+++ packages/bnconverter/bnconverter-0.0.1.tar.gz/bnconverter-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Python converting Bangla <=> Unicode (UTF-8) for Bengali to Bangla.'
+LONG_DESCRIPTION = 'Python converting Bangla <=> Unicode (UTF-8) for Bengali to Bangla.'
+
+# Setting up
+setup(
+    name="bnconverter",
+    version=VERSION,
+    author="Nazrul Islam (Nadeem)",
+    author_email="<nazrul.devs@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['regex'],
+    keywords=['python', 'converting', 'Unicode', 'Bengali to Bangla'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
