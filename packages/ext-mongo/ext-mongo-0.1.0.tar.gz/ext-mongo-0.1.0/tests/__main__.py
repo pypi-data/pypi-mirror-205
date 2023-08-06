9c3dc702b246df6b5c4fe4a5e6315c3909b2fc49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:39 +0000

    Add package ext-mongo/0.1.0/ext-mongo-0.1.0.tar.gz

---
+++ packages/ext-mongo/ext-mongo-0.1.0.tar.gz/ext-mongo-0.1.0/tests/__main__.py
@@ -0,0 +1,49 @@
+from datetime import datetime, date
+
+from bson import ObjectId
+
+from mongo import Document, PrimaryKey
+
+
+class Doc(Document):
+    key: PrimaryKey[str]
+    a1: str | None
+    a2: int | None
+    a3: bool | None
+    a4: float | None
+    a5: ObjectId | None
+    a6: datetime | None
+    a7: date | None
+    b1: list[str]
+    b2: list[int]
+    b3: list[bool]
+    b4: list[float]
+    b5: list[ObjectId]
+    b6: list[datetime]
+    b7: list[date]
+    c1: dict
+    c2: list[dict]
+
+
+KEY = "key"
+A1 = "a1"
+
+d = Doc(key=KEY, a1=A1).save()
+
+assert Doc.get(KEY) == d
+assert Doc.find(a1=A1) == d
+assert Doc.find_all(a1=A1) == [d]
+assert d.str_id == KEY
+assert d.to_dict() == {
+    "_id": "key",
+    "a1": "a1",
+    "b1": [],
+    "b2": [],
+    "b3": [],
+    "b4": [],
+    "b5": [],
+    "b6": [],
+    "b7": [],
+    "c1": {},
+    "c2": [],
+}
