c232adaed12792ecdd8416ffdb6799772f46e854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:34 +0000

    Add package acnaweblib/0.1.2/acnaweblib-0.1.2.tar.gz

---
+++ packages/acnaweblib/acnaweblib-0.1.2.tar.gz/acnaweblib-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+requirements = []
+
+setup(
+    name='acnaweblib',
+    version='0.1.002',
+    description='Description',
+    long_description=readme(),
+    author='Antonio Carlos de Lima Junior',
+    author_email='antonioclj.ac@gmail.com',
+    url='https://github.com/acnaweb/acnaweb-lib',
+    packages=find_packages(include=['acnaweblib', 'acnaweblib.*']),
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+    keywords='funniest joke comedy flying circus',
+    license='MIT',
+)
