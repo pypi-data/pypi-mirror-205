d5556beea076d3dd3534b7990b572a59c161a91e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:58 +0000

    Add package airptlib1/1.0.0/airptlib1-1.0.0.tar.gz

---
+++ packages/airptlib1/airptlib1-1.0.0.tar.gz/airptlib1-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="airptlib1",
+    version="1.0.0",
+    description="A library for storing airport codes",
+    packages=find_packages(),
+    install_requires=[],
+    author="Your Name",
+    author_email="your_email@example.com",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
