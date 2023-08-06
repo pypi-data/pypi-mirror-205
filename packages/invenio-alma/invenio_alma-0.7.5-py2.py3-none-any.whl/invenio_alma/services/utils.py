6d0325a68d0a09dda58011a97871b043dbd1ebd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:45 +0000

    Add package invenio-alma/0.7.5/invenio_alma-0.7.5-py2.py3-none-any.whl

---
+++ packages/invenio-alma/invenio_alma-0.7.5-py2.py3-none-any.whl/invenio_alma/services/utils.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 Graz University of Technology.
+#
+# invenio-alma is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Alma utils."""
+
+
+# pylint: disable-next=unused-argument
+def jpath_to_xpath(field_json_path: str) -> str:
+    """Convert json path to xpath."""
+    # TODO
+    return ""
