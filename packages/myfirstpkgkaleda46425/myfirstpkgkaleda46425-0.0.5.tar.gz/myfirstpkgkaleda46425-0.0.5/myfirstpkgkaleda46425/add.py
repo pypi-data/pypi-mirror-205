346fdee495faf9b807af7d69fb4cc433d3a9d2d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:08 +0000

    Add package myfirstpkgkaleda46425/0.0.5/myfirstpkgkaleda46425-0.0.5.tar.gz

---
+++ packages/myfirstpkgkaleda46425/myfirstpkgkaleda46425-0.0.5.tar.gz/myfirstpkgkaleda46425-0.0.5/myfirstpkgkaleda46425/add.py
@@ -0,0 +1,14 @@
+def add():
+    x = int(input("Enter the first number: "))
+    y = int(input("Enter the second number: "))
+    math_type = input("Press 0 to add or 1 to multiply: ")
+    if math_type == 0:
+        return x+y
+    elif math_type == 1:
+        return x*y
+
+
+# def main():
+#    print("Hi")
+# if __name__ == '__main__':
+#    main()
