52c02fb0cc8e622ee13a68c017e1ccfb8b68304f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:20 +0000

    Add package pyconch/0.0.4/pyconch-0.0.4-py3-none-any.whl

---
+++ packages/pyconch/pyconch-0.0.4-py3-none-any.whl/pyconch/main.py
@@ -0,0 +1,23 @@
+"""
+main entry point to the program
+"""
+
+
+import pylogconf.core
+from pytconf import register_main, config_arg_parse_and_launch
+
+from pyconch.static import APP_NAME, VERSION_STR, DESCRIPTION
+
+
+@register_main(
+    main_description=DESCRIPTION,
+    app_name=APP_NAME,
+    version=VERSION_STR,
+)
+def main():
+    pylogconf.core.setup()
+    config_arg_parse_and_launch()
+
+
+if __name__ == '__main__':
+    main()
