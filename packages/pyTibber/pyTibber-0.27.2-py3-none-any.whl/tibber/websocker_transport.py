6fe4d64eece61561f155096f624f3bb51407f8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:17 +0000

    Add package pyTibber/0.27.2/pyTibber-0.27.2-py3-none-any.whl

---
+++ packages/pyTibber/pyTibber-0.27.2-py3-none-any.whl/tibber/websocker_transport.py
@@ -0,0 +1,50 @@
+"""Websocket transport for Tibber."""
+import asyncio
+import datetime as dt
+import logging
+
+from gql.transport.exceptions import TransportClosed
+from gql.transport.websockets import WebsocketsTransport
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class TibberWebsocketsTransport(WebsocketsTransport):
+    """Tibber websockets transport."""
+
+    def __init__(self, url: str, access_token: str, user_agent: str) -> None:
+        """Initialize TibberWebsocketsTransport."""
+        super().__init__(
+            url=url,
+            init_payload={"token": access_token},
+            headers={"User-Agent": user_agent},
+            ping_interval=10,
+        )
+        self._timeout: int = 90
+        self.reconnect_at: dt.datetime = dt.datetime.now() + dt.timedelta(
+            seconds=self._timeout
+        )
+
+    @property
+    def running(self) -> bool:
+        """Is real time subscription running."""
+        return (
+            self.websocket is not None
+            and self.websocket.open
+            and self.reconnect_at > dt.datetime.now()
+            and self.receive_data_task in asyncio.all_tasks()
+        )
+
+    async def _receive(self) -> str:
+        """Wait the next message from the websocket connection."""
+        try:
+            msg = await asyncio.wait_for(super()._receive(), timeout=self._timeout)
+        except asyncio.TimeoutError:
+            _LOGGER.error("No data received from Tibber for %s seconds", self._timeout)
+            raise
+        self.reconnect_at = dt.datetime.now() + dt.timedelta(seconds=self._timeout)
+        return msg
+
+    async def close(self) -> None:
+        await self._fail(TransportClosed("Tibber websocket closed by pyTibber"))
+        await self.wait_closed()
