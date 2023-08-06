bb5f911ee2ee24f5a472a56e134489b2ade446a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:59 +0000

    Add package pdm-conda/0.9.3/pdm_conda-0.9.3-py3-none-any.whl

---
+++ packages/pdm-conda/pdm_conda-0.9.3-py3-none-any.whl/pdm_conda/__init__.py
@@ -0,0 +1,21 @@
+from pdm.core import Core
+
+
+def main(core: Core):
+    from pdm_conda import utils  # noqa
+    from pdm_conda.cli import utils as cli_utils  # noqa
+    from pdm_conda.cli.commands.add import Command as AddCommand
+    from pdm_conda.cli.commands.remove import Command as RemoveCommand
+    from pdm_conda.models.config import CONFIGS
+    from pdm_conda.project import CondaProject
+
+    core.project_class = CondaProject
+
+    core.register_command(AddCommand)
+    core.register_command(RemoveCommand)
+
+    for name, config in CONFIGS:
+        core.add_config(name, config)
+
+
+__version__ = "0.9.3"
