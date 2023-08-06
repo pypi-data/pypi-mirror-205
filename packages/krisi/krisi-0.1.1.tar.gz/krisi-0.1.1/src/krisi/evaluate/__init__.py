d26c3a338f0b3ac1c3cea9d53cc326cf8a80bcd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:09 +0000

    Add package krisi/0.1.1/krisi-0.1.1.tar.gz

---
+++ packages/krisi/krisi-0.1.1.tar.gz/krisi-0.1.1/src/krisi/evaluate/__init__.py
@@ -0,0 +1,6 @@
+from .compare import compare
+from .dataset import check_consistency
+from .metric import Metric
+from .score import score, score_in_outsample
+from .scorecard import ScoreCard
+from .type import Calculation, MetricCategories, SampleTypes
