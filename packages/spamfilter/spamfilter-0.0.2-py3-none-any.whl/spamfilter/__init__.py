7064c33143e71577a358916dd303d868c5314a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:10 +0000

    Add package spamfilter/0.0.2/spamfilter-0.0.2-py3-none-any.whl

---
+++ packages/spamfilter/spamfilter-0.0.2-py3-none-any.whl/spamfilter/__init__.py
@@ -0,0 +1,6 @@
+"""
+The spamfilter module is a lightweight, fast and straightforward way to build your own spam filtering machines in order to keep applications using user-generated content clean.
+It's object-oriented and makes a quick, yet concise approach to remove spam very easy.
+"""
+
+__version__ = "v0.0.2"
