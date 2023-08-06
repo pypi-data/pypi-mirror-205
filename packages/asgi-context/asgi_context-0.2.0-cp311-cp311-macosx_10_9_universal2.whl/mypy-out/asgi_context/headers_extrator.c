bd6fb00c19284e2e740f329c4651d7c51e23ff2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:03 +0000

    Add package asgi-context/0.2.0/asgi_context-0.2.0-cp311-cp311-macosx_10_9_universal2.whl

---
+++ packages/asgi-context/asgi_context-0.2.0-cp311-cp311-macosx_10_9_universal2.whl/mypy-out/asgi_context/headers_extrator.c
@@ -0,0 +1,18 @@
+#include <Python.h>
+
+PyMODINIT_FUNC
+PyInit_headers_extrator(void)
+{
+    PyObject *tmp;
+    if (!(tmp = PyImport_ImportModule("20278fa609a360dbc58d__mypyc"))) return NULL;
+    Py_DECREF(tmp);
+    void *init_func = PyCapsule_Import("20278fa609a360dbc58d__mypyc.init_asgi_context___headers_extrator", 0);
+    if (!init_func) {
+        return NULL;
+    }
+    return ((PyObject *(*)(void))init_func)();
+}
+
+// distutils sometimes spuriously tells cl to export CPyInit___init__,
+// so provide that so it chills out
+PyMODINIT_FUNC PyInit___init__(void) { return PyInit_headers_extrator(); }
