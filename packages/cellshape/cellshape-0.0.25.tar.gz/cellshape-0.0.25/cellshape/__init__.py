148d861c0ce16f59f2fe5d57277b67c0a8981db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:59 +0000

    Add package cellshape/0.0.25/cellshape-0.0.25.tar.gz

---
+++ packages/cellshape/cellshape-0.0.25.tar.gz/cellshape-0.0.25/cellshape/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.0.25"
+
+import cellshape_cloud as cloud
+import cellshape_cluster as cluster
---
+++ packages/cellshape/cellshape-0.0.25.tar.gz/cellshape-0.0.25/setup.py
@@ -0,0 +1,70 @@
+from os import path
+
+from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+requirements = [
+    "torch",
+    "torchvision",
+    "pyntcloud",
+    "numpy==1.21",
+    "matplotlib",
+    "cellshape-helper",
+    "cellshape-cloud",
+    "cellshape-voxel",
+    "cellshape-cluster",
+    "umap-learn",
+    "scikit-learn",
+    "plotly",
+    "napari[all]",
+]
+
+
+setup(
+    name="cellshape",
+    version="0.0.25",
+    description="3D shape analysis using deep learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "black",
+            "black[jupyter]",
+            "pytest-cov",
+            "pytest",
+            "gitpython",
+            "coverage>=5.0.3",
+            "bump2version",
+            "pre-commit",
+            "flake8",
+        ]
+    },
+    extry_points={"console_scripts": ["cellshape-train=cellshape.train:main"]},
+    python_requires=">=3.7",
+    packages=["cellshape"],
+    package_dir={"cellshape": "cellshape"},
+    include_package_data=True,
+    project_urls={
+        "Source Code": "https://github.com/Sentinal4D/cellshape",
+        "Bug Tracker": "https://github.com/Sentinal4D/cellshape/issues",
+    },
+    author="Matt De Vries, Lucas Dent, Adam Tyson",
+    author_email="mattdevries.ai@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+    zip_safe=False,
+)
