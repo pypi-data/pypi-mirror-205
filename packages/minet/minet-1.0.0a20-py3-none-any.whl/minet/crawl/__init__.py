5e6e2d213d8784c001ea5c31dd0f4c3291068565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:16 +0000

    Add package minet/1.0.0a20/minet-1.0.0a20-py3-none-any.whl

---
+++ packages/minet/minet-1.0.0a20-py3-none-any.whl/minet/crawl/__init__.py
@@ -0,0 +1,18 @@
+from minet.crawl.types import (
+    CrawlResult,
+    SomeCrawlResult,
+    SuccessfulCrawlResult,
+    ErroredCrawlResult,
+    CrawlJob,
+    UrlOrCrawlJob,
+)
+from minet.crawl.state import CrawlerState
+from minet.crawl.spiders import (
+    Spider,
+    SpiderResult,
+    SpiderNextJobs,
+    FunctionSpider,
+    DefinitionSpider,
+    DefinitionSpiderOutput,
+)
+from minet.crawl.crawler import Crawler
