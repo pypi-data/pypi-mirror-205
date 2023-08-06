9f8c849d18683356b0781a5fb7e869c0b2880042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:21 +0000

    Add package vijaikfirstpackage/0.1.0/vijaikfirstpackage-0.1.0-py3-none-any.whl

---
+++ packages/vijaikfirstpackage/vijaikfirstpackage-0.1.0-py3-none-any.whl/extras/divide.py
@@ -0,0 +1,13 @@
+def divide(x,y):
+    """
+    Sum of given arguments.
+    Keyword arguments:
+    -----------------
+    x : number format.
+    y : number format.
+    Returns:
+    --------
+    x+y : Number.
+        Returns division of two Numbers.
+    """   
+    return x/y
