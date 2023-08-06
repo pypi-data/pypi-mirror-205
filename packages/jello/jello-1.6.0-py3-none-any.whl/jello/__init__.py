e003f72d1f500ca609d05faba47a28fcd9e2685f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:06 +0000

    Add package jello/1.6.0/jello-1.6.0-py3-none-any.whl

---
+++ packages/jello/jello-1.6.0-py3-none-any.whl/jello/__init__.py
@@ -0,0 +1,8 @@
+"""jello - query JSON at the command line with python syntax"""
+
+
+__version__ = '1.6.0'
+AUTHOR = 'Kelly Brazil'
+WEBSITE = 'https://github.com/kellyjonbrazil/jello'
+COPYRIGHT = '© 2020-2023 Kelly Brazil'
+LICENSE = 'MIT License'
