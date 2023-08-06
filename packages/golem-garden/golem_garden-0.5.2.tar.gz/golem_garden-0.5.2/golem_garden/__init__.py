6ec1abb7de92bc1628a773c4b2cf1b82b370e1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:08 +0000

    Add package golem-garden/0.5.2/golem_garden-0.5.2.tar.gz

---
+++ packages/golem-garden/golem_garden-0.5.2.tar.gz/golem_garden-0.5.2/golem_garden/__init__.py
@@ -0,0 +1,9 @@
+""" Welcome to the Garden - We're so glad you're here <3 """
+__version__ = "v0.5.2"
+import sys
+from pathlib import Path
+
+
+
+base_package_path = Path(__file__).parent.parent
+sys.path.insert(0, str(base_package_path))  # add parent directory to sys.path
