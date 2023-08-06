91466fe01f6160688a9fe9ab3c6abeba09f6691a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:16 +0000

    Add package cythonbuilder/0.0.18/cythonbuilder-0.0.18-py3-none-any.whl

---
+++ packages/cythonbuilder/cythonbuilder-0.0.18-py3-none-any.whl/cythonbuilder/__init__.py
@@ -0,0 +1,7 @@
+# import code from your package here to make them available outside the module like:
+
+__version__ = "0.0.18"
+__last_build_datetime__ ="2023-04-28 16:48:09"
+__last_publish_version__ ="0.0.17"
+__last_publish_datetime__ ="2023-04-25 08:42:22"
+
