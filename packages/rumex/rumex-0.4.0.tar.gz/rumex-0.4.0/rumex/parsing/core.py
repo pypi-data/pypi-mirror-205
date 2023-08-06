bc2af652c0d7a3fc4a26db849e5d9e343af80460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:19 +0000

    Add package rumex/0.4.0/rumex-0.4.0.tar.gz

---
+++ packages/rumex/rumex-0.4.0.tar.gz/rumex-0.4.0/rumex/parsing/core.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from typing import Protocol, Sequence
+
+
+@dataclass(frozen=True, kw_only=True)
+class InputFile:
+    """Container for a test file to be parsed.
+
+    Does not have to represent an actual file.
+    Could be e.g. an entry in a database.
+
+    Params
+    ------
+
+    uri: A unique identifer. If it's a file,
+        this could be a path to this file.
+
+    text: The content of the file.
+    """
+
+    uri: str
+    text: str
+
+
+@dataclass(frozen=True, kw_only=True)
+class Step:
+
+    sentence: str
+    data: type
+
+
+@dataclass(frozen=True, kw_only=True)
+class Scenario:
+
+    name: str
+    description: str
+    steps: Sequence[Step]
+    tags: Sequence[str]
+
+
+@dataclass(frozen=True, kw_only=True)
+class ParsedFile:
+
+    name: str
+    description: str
+    scenarios: Sequence[Scenario]
+    uri: str
+
+
+class ParserProto(Protocol):
+
+    def __call__(self, input_file: InputFile, /) -> ParsedFile:
+        """Build test files."""
