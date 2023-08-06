7c4aadad2aeeb8666efb73ac1d2254d9398d864b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:07 +0000

    Add package aixlab.cn/0.1/aixlab.cn-0.1.tar.gz

---
+++ packages/aixlab.cn/aixlab.cn-0.1.tar.gz/aixlab.cn-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aixlab.cn',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib'
+    ],
+    author='Xu Ziyi',
+    author_email='759946140@qq.com',
+    description='Support for aixlab.cn',
+    url='https://github.com/xuzycuan/aixlab.cn',
+    license='MIT'
+)
