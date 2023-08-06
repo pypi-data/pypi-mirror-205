9349160123e6fb4e819cf770e13df36852232567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:13 +0000

    Add package aiogram-ext/0.1.24/aiogram_ext-0.1.24-py3-none-any.whl

---
+++ packages/aiogram-ext/aiogram_ext-0.1.24-py3-none-any.whl/botty/loader.py
@@ -0,0 +1,34 @@
+import logging
+from asyncio import new_event_loop
+
+from aiohttp.web import Application
+
+from .bot import Bot
+from .config import BOT_TOKEN, MONGO, APP_URL
+from .deps import MongoStorage, Update
+from .dispatcher import Dispatcher
+
+storage = MongoStorage(
+    db_name=MONGO.DB,
+    host=MONGO.HOST,
+    username=MONGO.USER,
+    password=MONGO.PASSWORD,
+)
+
+loop = new_event_loop()
+bot = Bot(BOT_TOKEN, loop)
+dp = Dispatcher(bot, storage, loop)
+logger = logging.getLogger()
+app = Application(loop=loop)
+
+Dispatcher.set_current(dp)
+
+
+@dp.errors_handler()
+async def _(update: Update, error: Exception):
+    logger.exception(f"{error=} on {update=}")
+    return True
+
+
+def run():
+    dp.run_server(APP_URL, app)
