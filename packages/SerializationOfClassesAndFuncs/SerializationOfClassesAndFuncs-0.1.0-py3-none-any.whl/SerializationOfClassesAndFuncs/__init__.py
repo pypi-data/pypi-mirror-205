90e9e0b575032f79b0b30580578ef921894a3b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:49 +0000

    Add package SerializationOfClassesAndFuncs/0.1.0/SerializationOfClassesAndFuncs-0.1.0-py3-none-any.whl

---
+++ packages/SerializationOfClassesAndFuncs/SerializationOfClassesAndFuncs-0.1.0-py3-none-any.whl/SerializationOfClassesAndFuncs/__init__.py
@@ -0,0 +1,13 @@
+from SerializationOfClassesAndFuncs.type_constants import \
+                           nonetype, moduletype, codetype, celltype, \
+                           functype, bldinfunctype, smethodtype, cmethodtype, \
+                           mapproxytype, wrapdesctype, metdesctype, getsetdesctype, \
+                           CODE_PROPS, UNIQUE_TYPES
+
+from SerializationOfClassesAndFuncs.base_serializer import BaseSerializer
+from SerializationOfClassesAndFuncs.dict_serializer import DictSerializer
+from SerializationOfClassesAndFuncs.json_serializer import JsonSerializer
+from SerializationOfClassesAndFuncs.xml_serializer import XmlSerializer
+from SerializationOfClassesAndFuncs.serializers_factory import SerializersFactory, SerializerType
+
+
