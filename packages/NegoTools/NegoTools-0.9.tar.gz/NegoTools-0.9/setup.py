3a4c5ab40d0afa489848af77431b02d7af30c5c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:08 +0000

    Add package NegoTools/0.9/NegoTools-0.9.tar.gz

---
+++ packages/NegoTools/NegoTools-0.9.tar.gz/NegoTools-0.9/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='NegoTools', 
+    version='0.9', 
+    description='nego tools',
+    author='Alexandro Rocha', 
+    url='https://github.com/AlexandroLuis/NegoTools',
+    package_dir = {
+    	"hyperheuristic": "hyperheuristic",
+    	"Scientific": "Scientific",
+    },
+    packages=[
+    	'hyperheuristic',
+    	'Scientific',
+    	'Scientific.Clustering',
+    ],
+    install_requires=[
+        'thompson-sampling',
+        'pandas',
+        'random2',
+        'numpy',
+        'matplotlib',
+        'scikit-learn'
+    ],
+    zip_safe=False
+)
