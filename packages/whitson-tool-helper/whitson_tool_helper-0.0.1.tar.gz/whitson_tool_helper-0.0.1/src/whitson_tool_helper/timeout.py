b4a6010d8210a72193a85ebaef984a2537cac663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:17 +0000

    Add package whitson-tool-helper/0.0.1/whitson_tool_helper-0.0.1.tar.gz

---
+++ packages/whitson-tool-helper/whitson_tool_helper-0.0.1.tar.gz/whitson_tool_helper-0.0.1/src/whitson_tool_helper/timeout.py
@@ -0,0 +1,22 @@
+from whitson_tool_helper import CalculationTimeoutError
+
+
+def apply_timeout(timeout_in_seconds: int):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            import signal
+
+            def handler(signum, frame):
+                raise CalculationTimeoutError
+
+            signal.signal(signal.SIGALRM, handler)
+            signal.alarm(timeout_in_seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wrapper
+
+    return decorator
