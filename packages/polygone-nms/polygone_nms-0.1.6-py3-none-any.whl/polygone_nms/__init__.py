0122a7794cf509ab056acac3218e218e556bd7f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:14 +0000

    Add package polygone-nms/0.1.6/polygone_nms-0.1.6-py3-none-any.whl

---
+++ packages/polygone-nms/polygone_nms-0.1.6-py3-none-any.whl/polygone_nms/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+from polygone_nms.nms import nms
+
+
+def read_version():
+    version_file_path = os.path.join(os.path.dirname(__file__), "..", "VERSION")
+    with open(version_file_path, "r") as version_file:
+        return version_file.read().strip()
+
+
+__version__ = read_version()
+
+__all__ = ["nms"]
