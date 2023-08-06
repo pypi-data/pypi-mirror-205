5f93d5b449d6c7661c74c2ebefe45644486ee11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:59 +0000

    Add package sweetbean/0.0.35/sweetbean-0.0.35.tar.gz

---
+++ packages/sweetbean/sweetbean-0.0.35.tar.gz/sweetbean-0.0.35/examples/image/backup/main_20230429_170147.js
@@ -0,0 +1,9 @@
+import jsPsychImageKeyboardResponse from '@jspsych/plugin-image-keyboard-response'
+import 'jspsych/css/jspsych.css'
+const main = async (id, condition) => {
+
+const trials = [
+{timeline: [{type: jsPsychImageKeyboardResponse,stimulus: () => {let src = "test.png";return src},choices: () => {let choices = [' '];return choices},on_finish: (data) => {data["bean_type"] = 'jsPsychImageKeyboardResponse';let src = "test.png";data["bean_src"] = src;let choices = [' '];data["bean_choices"] = choices;let correct_key = "";data["bean_correct_key"] = correct_key;data["bean_correct"] = correct_key== data["response"]}}], timeline_variables: [<sweetbean.parameter.CodeVariable object at 0x103113fd0>]}]
+return await condition
+}
+export default main
