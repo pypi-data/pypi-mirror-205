8f0a212f55e438f1cbe39e1196dee08d1f8be2f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:36 +0000

    Add package eeg-blinks/0.0.4/eeg_blinks-0.0.4.tar.gz

---
+++ packages/eeg-blinks/eeg_blinks-0.0.4.tar.gz/eeg_blinks-0.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='eeg_blinks',
+      version='0.0.4',
+      description='make life easier',
+      author='rpb',
+      packages=['eeg_blinks','eeg_blinks.utilities',
+                  'eeg_blinks.vislab','eeg_blinks.viz'],
+      long_description='Get EOG from EEG signal recording',
+      install_requires=['seaborn']
+      )
