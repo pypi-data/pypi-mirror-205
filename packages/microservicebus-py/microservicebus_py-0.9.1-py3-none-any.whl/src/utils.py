990dd84a46c78410e75669593f03b15be6ecc2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:21 +0000

    Add package microservicebus-py/0.9.1/microservicebus_py-0.9.1-py3-none-any.whl

---
+++ packages/microservicebus-py/microservicebus_py-0.9.1-py3-none-any.whl/src/utils.py
@@ -0,0 +1,13 @@
+import subprocess, sys, importlib, socket, array, struct, fcntl 
+
+def get_public_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+def getHwAddr(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', bytes(ifname, 'utf-8')[:15]))
+    return ':'.join('%02x' % b for b in info[18:24])
