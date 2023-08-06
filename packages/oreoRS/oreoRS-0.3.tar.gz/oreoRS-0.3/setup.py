4ae45244c28019da88edee8d4081164838920d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:02 +0000

    Add package oreoRS/0.3/oreoRS-0.3.tar.gz

---
+++ packages/oreoRS/oreoRS-0.3.tar.gz/oreoRS-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name= 'oreoRS',
+    version='0.3',
+    author= 'oreoRS',
+    description='Giting info for any account in the instagram account',
+    packages= ['oreoRs'],
+    author_email='zyaahee@gmail.com',
+    zip_safe=False,
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
