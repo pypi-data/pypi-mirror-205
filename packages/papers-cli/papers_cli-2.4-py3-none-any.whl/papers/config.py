5a997b55e44b04d86f91641171f5e5c52b8656a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:59 +0000

    Add package papers-cli/2.4/papers_cli-2.4-py3-none-any.whl

---
+++ packages/papers-cli/papers_cli-2.4-py3-none-any.whl/papers/config.py
@@ -0,0 +1,245 @@
+import os, json
+from pathlib import Path
+import subprocess as sp, sys
+import hashlib
+import bibtexparser
+from papers import logger
+from papers.filename import Format, NAMEFORMAT, KEYFORMAT
+from papers import __version__
+from papers.utils import bcolors, check_filesdir, search_config
+
+# GIT = False
+DRYRUN = False
+
+# config directory location
+HOME = os.environ.get('HOME',os.path.expanduser('~'))
+CONFIG_HOME = os.environ.get('XDG_CONFIG_HOME', os.path.join(HOME, '.config'))
+CACHE_HOME = os.environ.get('XDG_CACHE_HOME', os.path.join(HOME, '.cache'))
+DATA_HOME = os.environ.get('XDG_DATA_HOME', os.path.join(HOME, '.local','share'))
+
+CONFIG_FILE_LEGACY = os.path.join(CONFIG_HOME, 'papersconfig.json')
+CONFIG_FILE = os.path.join(DATA_HOME, 'config.json')
+DATA_DIR = os.path.join(DATA_HOME, 'papers')
+CACHE_DIR = os.path.join(CACHE_HOME, 'papers')
+
+
+class Config:
+    """configuration class to specify system-wide collections and files-dir
+    """
+    def __init__(self, file=None, data=DATA_DIR,
+        bibtex=None, filesdir=None,
+        keyformat=KEYFORMAT,
+        nameformat=NAMEFORMAT,
+        editor=None,
+        gitdir=None, git=False, gitlfs=False, local=None, absolute_paths=None, backup_files=False):
+        self.file = file
+        self.local = local
+        self.data = data
+        self.filesdir = filesdir
+        self.editor = editor
+        self.bibtex = bibtex
+        self.keyformat = keyformat
+        self.nameformat = nameformat
+        if absolute_paths is None:
+            absolute_paths = False if local else True
+        self.absolute_paths = absolute_paths
+        self.gitdir = gitdir  or data
+        self.git = git
+        self.gitlfs = gitlfs
+        self.backup_files = backup_files
+
+
+    @property
+    def editor(self):
+        return self._editor
+
+    @editor.setter
+    def editor(self, value):
+        if value is not None:
+            os.environ['EDITOR'] = value
+        self._editor = value
+
+    def collections(self):
+        files = []
+        for root, dirs, files in os.walk(os.path.dirname(self.bibtex)):
+            break
+        # return sorted(f[:-4] for f in files if f.endswith('.bib'))
+        return sorted(f for f in files if f.endswith('.bib'))
+
+    @property
+    def root(self):
+        if self.local and self.bibtex:
+            return Path(self.bibtex).parent.resolve()
+        else:
+            return Path(os.path.sep)
+
+    def gitcmd(self, cmd, check=True, **kw):
+        return (sp.check_call if check else sp.call)(f"git {cmd}", shell=True, cwd=self.gitdir, **kw)
+
+
+    def _relpath(self, p):
+        if p is None: return p
+        if not self.local:
+            return str(Path(p).resolve())  # abspath
+
+        # otherwise express path relative to bibtex (parent of config file)
+        try:
+            # logger.info(f"rel path: (p)", p)
+            return str((self.root / p).relative_to(self.root))
+        except Exception as error:
+            print(error)
+            logger.warn(f"config :: can't save {p} as relative path to {self.root}")
+            return p
+
+    def _abspath(self, p, root=None):
+        if p is None: return p
+        if not self.local:
+            return str(Path(p).resolve())  # abspath
+        p2 = str((Path(root).resolve() if root is not None else self.root) / p)
+        return p2
+
+
+    def save(self):
+        json.dump({
+            "filesdir": self._relpath(self.filesdir),
+            "bibtex": self._relpath(self.bibtex),
+            "gitdir": self._relpath(self.gitdir),
+            "editor": self.editor,
+            "keyformat": self.keyformat.todict(),
+            "nameformat": self.nameformat.todict(),
+            "local": self.local,
+            "absolute_paths": self.absolute_paths,
+            "git": self.git,
+            "gitlfs": self.gitlfs,
+            "backup_files": self.backup_files,
+            }, open(self.file, 'w'), sort_keys=True, indent=2, separators=(',', ': '))
+
+
+    @classmethod
+    def load(cls, file):
+        js = json.load(open(file))
+        if 'nameformat' in js:
+            js['nameformat'] = Format(**js.get('nameformat'))
+        if 'keyformat' in js:
+            js['keyformat'] = Format(**js.get('keyformat'))
+        cfg = cls(file=file, **js)
+        cfg._update_paths_to_absolute()
+        return cfg
+
+
+    def _update_paths_to_absolute(self):
+        if self.file is None:
+            logger.warn("_update_paths_to_absolute: only works if Config.file is defined")
+            return
+        root = Path(self.file).parent.parent
+        for field in ['bibtex', 'filesdir', 'gitdir']:
+            setattr(self, field, self._abspath(getattr(self, field), root))
+
+
+    def status(self, check_files=False, verbose=False):
+
+        def _fmt_path(p):
+            if self.local:
+                return os.path.relpath(p, ".")
+            else:
+                return p
+
+        lines = []
+        if self.file and os.path.exists(self.file):
+            status = "(local)" if self.local else "(global)"
+        else:
+            status = bcolors.WARNING+"(default, not installed)"+bcolors.ENDC
+        lines.append(bcolors.BOLD+f'papers configuration {status}'+bcolors.ENDC)
+        lines.append(bcolors.BOLD+f'version {__version__}'+bcolors.ENDC)
+        if verbose:
+            lines.append('* configuration file: '+(_fmt_path(self.file) if self.file and os.path.exists(self.file) else bcolors.WARNING+'none'+bcolors.ENDC))
+            lines.append('* cache directory:    '+CACHE_DIR)
+            lines.append('* absolute paths:     '+str(self.absolute_paths))
+            # lines.append('* app data directory: '+self.data)
+            lines.append('* git-tracked:        '+str(self.git))
+            if self.git:
+                lines.append('* git-lfs tracked:    '+str(self.gitlfs))
+                lines.append('* git directory :     '+self.gitdir)
+            lines.append('* editor:             '+str(self.editor))
+
+        if self.filesdir is None:
+            status = bcolors.WARNING+' (unset)'+bcolors.ENDC
+        elif not os.path.exists(self.filesdir):
+            status = bcolors.WARNING+' (missing)'+bcolors.ENDC
+        elif not os.listdir(self.filesdir):
+            status = bcolors.WARNING+' (empty)'+bcolors.ENDC
+        elif check_files:
+            file_count, folder_size = check_filesdir(self.filesdir)
+            status = bcolors.OKBLUE+" ({} files, {:.1f} MB)".format(file_count, folder_size/(1024*1024.0))+bcolors.ENDC
+        else:
+            status = ''
+
+        lines.append(f'* files directory:    {_fmt_path(self.filesdir) if self.filesdir else self.filesdir}'+status)
+
+        if self.bibtex is None:
+            status = bcolors.WARNING+' (unset)'+bcolors.ENDC
+        elif not os.path.exists(self.bibtex):
+            status = bcolors.WARNING+' (missing)'+bcolors.ENDC
+        elif check_files:
+            try:
+                bibtexstring = open(self.bibtex).read()
+                db = bibtexparser.loads(bibtexstring)
+                if len(db.entries):
+                    status = bcolors.OKBLUE+' ({} entries)'.format(len(db.entries))+bcolors.ENDC
+                else:
+                    status = bcolors.WARNING+' (empty)'+bcolors.ENDC
+            except:
+                status = bcolors.FAIL+' (corrupted)'+bcolors.ENDC
+        elif os.path.getsize(self.bibtex) == 0:
+            status = bcolors.WARNING+' (empty)'+bcolors.ENDC
+        else:
+            status = ''
+        lines.append(f'* bibtex:             {_fmt_path(self.bibtex) if self.bibtex else self.bibtex}'+status)
+
+        # if verbose:
+        #     collections = self.collections()
+        #     status = bcolors.WARNING+' none'+bcolors.ENDC if not collections else ''
+        #     lines.append('* other collections:'+status)
+        #     for i, nm in enumerate(collections):
+        #         if i > 10:
+        #             lines.append('    '+'({} more collections...)'.format(len(collections)-10))
+        #             break
+        #         status = ' (*)' if nm == self.collection else ''
+        #         lines.append('    '+nm+status)
+
+
+
+        return '\n'.join(lines)
+
+
+def _init_cache():
+    if not os.path.exists(CACHE_DIR):
+        logger.info('make cache directory for DOI requests: '+CACHE_DIR)
+        os.makedirs(CACHE_DIR)
+
+_init_cache()
+
+def cached(file, hashed_key=False):
+
+    file = os.path.join(CACHE_DIR, file)
+
+    def decorator(fun):
+        if os.path.exists(file):
+            cache = json.load(open(file))
+        else:
+            cache = {}
+        def decorated(doi):
+            if hashed_key: # use hashed parameter as key (for full text query)
+                key = hashlib.sha256(doi.encode('utf-8')).hexdigest()[:6]
+            else:
+                key = doi
+            if key in cache:
+                logger.debug('load from cache: '+repr((file, key)))
+                return cache[key]
+            else:
+                res = cache[key] = fun(doi)
+                if not DRYRUN:
+                    json.dump(cache, open(file,'w'))
+            return res
+        return decorated
+    return decorator
---
+++ packages/papers-cli/papers_cli-2.4-py3-none-any.whl/papers/utils.py
@@ -0,0 +1,150 @@
+import os
+import shutil
+import hashlib
+
+from contextlib import contextmanager
+from pathlib import Path
+
+from papers import logger
+
+
+class bcolors:
+    # https://stackoverflow.com/a/287944/2192272
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def strip_colors(s):
+    for name, c in vars(bcolors).items():
+        if name.startswith("_"):
+            continue
+        s = s.replace(c, '')
+    return s
+
+
+def check_filesdir(folder):
+    folder_size = 0
+    file_count = 0
+    for (path, dirs, files) in os.walk(folder):
+      for file in files:
+        filename = os.path.join(path, file)
+        if filename.endswith('.pdf'):
+            folder_size += os.path.getsize(filename)
+            file_count += 1
+    return file_count, folder_size
+
+
+def search_config(filenames, start_dir, default=None):
+    """Thanks Chat GPT !"""
+    current_dir = os.path.abspath(start_dir)
+    root_dir = os.path.abspath(os.sep)
+    while True:
+        for filename in filenames:
+            file_path = os.path.join(current_dir, filename)
+            if os.path.exists(file_path):
+                return file_path
+
+        parent_dir = os.path.dirname(current_dir)
+        if parent_dir == current_dir:
+            return default
+
+        # root
+        if parent_dir == root_dir:
+            return default
+        current_dir = parent_dir
+
+    return default
+
+
+def hash_bytestr_iter(bytesiter, hasher, ashexstr=False):
+    for block in bytesiter:
+        hasher.update(block)
+    return (hasher.hexdigest() if ashexstr else hasher.digest())
+
+def file_as_blockiter(afile, blocksize=65536):
+    with afile:
+        block = afile.read(blocksize)
+        while len(block) > 0:
+            yield block
+            block = afile.read(blocksize)
+
+def checksum(fname):
+    """memory-efficient check sum (sha256)
+
+    source: https://stackoverflow.com/a/3431835/2192272
+    """
+    return hash_bytestr_iter(file_as_blockiter(open(fname, 'rb')), hashlib.sha256())
+
+
+
+# move / copy
+def move(f1, f2, copy=False, interactive=True, dryrun=False, hardlink=True):
+    maybe = 'dry-run:: ' if dryrun else ''
+    dirname = os.path.dirname(f2)
+    if dirname and not os.path.exists(dirname):
+        logger.info(f'{maybe}create directory: {dirname}')
+        if not dryrun: os.makedirs(dirname)
+    if f1 == f2:
+        logger.info('dest is identical to src: '+f1)
+        return
+
+    if os.path.exists(f2):
+        # if identical file, pretend nothing happened, skip copying
+        if os.path.samefile(f1, f2) or checksum(f2) == checksum(f1):
+            if not copy and not dryrun:
+                logger.info(f'{maybe}rm {f1}')
+                os.remove(f1)
+            return
+
+        elif interactive:
+            ans = input(f'dest file already exists: {f2}. Replace? (y/n) ')
+            if ans.lower() != 'y':
+                return
+        else:
+            logger.info(f'{maybe}rm {f2}')
+            if not dryrun:
+                os.remove(f2)
+
+    if copy:
+        # If we can do a hard-link instead of copy-ing, let's do:
+        if hardlink:
+            cmd = f'{maybe}ln {f1} {f2}'
+            logger.info(cmd)
+            if not dryrun:
+                os.link(f1, f2)
+
+        else:
+            logger.info(f'{maybe}cp {f1} {f2}')
+            if not dryrun:
+                shutil.copy(f1, f2)
+    else:
+        cmd = f'{maybe}mv {f1} {f2}'
+        logger.info(cmd)
+        if not dryrun:
+            shutil.move(f1, f2)
+
+
+
+
+@contextmanager
+def set_directory(path: Path):
+    """Sets the cwd within the context
+
+    Args:
+        path (Path): The path to the cwd
+
+    Yields:
+        None
+    """
+
+    origin = Path().absolute()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(origin)
