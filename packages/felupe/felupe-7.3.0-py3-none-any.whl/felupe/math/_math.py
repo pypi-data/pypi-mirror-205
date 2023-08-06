f35a07c8a6b02fbcb7583929382aa86f7027982d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:50 +0000

    Add package felupe/7.3.0/felupe-7.3.0-py3-none-any.whl

---
+++ packages/felupe/felupe-7.3.0-py3-none-any.whl/felupe/math/_math.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+This file is part of FElupe.
+
+FElupe is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FElupe is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FElupe.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import numpy as np
+
+
+def linsteps(points, num=10, endpoint=True):
+
+    points = np.array(points).ravel()
+    start = points[:-1]
+    end = points[1:]
+    num = np.array([num]).ravel()
+
+    if len(num) == 1:
+        num = np.tile(num, max(1, len(start)))
+
+    num = np.pad(num, (0, max(0, len(start) - len(num))), mode="edge")
+
+    steplist = [
+        np.linspace(a, b, n, endpoint=False) for a, b, n in zip(start, end, num)
+    ]
+    if len(steplist) > 0:
+        steps = np.concatenate(
+            [np.linspace(a, b, n, endpoint=False) for a, b, n in zip(start, end, num)]
+        )
+    else:
+        steps = np.array([])
+
+    if endpoint:
+        steps = np.append(steps, points[-1])
+
+    return steps
