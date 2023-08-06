877fa8c1fd4e4fe0f107e8c6ac5d503eb502ad3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:46 +0000

    Add package mage-ai/0.8.61/mage-ai-0.8.61.tar.gz

---
+++ packages/mage-ai/mage-ai-0.8.61.tar.gz/mage-ai-0.8.61/mage_ai/data_integrations/destinations/constants.py
@@ -0,0 +1,15 @@
+DESTINATIONS = [
+    dict(name='Amazon S3'),
+    dict(name='BigQuery'),
+    dict(name='Delta Lake S3'),
+    dict(
+        module_name='MSSQL',
+        name='Microsoft SQL Server',
+        uuid='mssql',
+    ),
+    dict(name='MySQL'),
+    dict(name='PostgreSQL'),
+    dict(name='Redshift'),
+    dict(name='Snowflake'),
+    dict(name='Trino'),
+]
