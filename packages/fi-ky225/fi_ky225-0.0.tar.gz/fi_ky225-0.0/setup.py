6b4c6eb87138ecfb7e15323bff6e43cf045b1339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:16 +0000

    Add package fi-ky225/0.0/fi_ky225-0.0.tar.gz

---
+++ packages/fi-ky225/fi_ky225-0.0.tar.gz/fi_ky225-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fi_ky225',
+      version='0.0',
+      description='circle, tiangle, square',
+      packages=['fi_ky225.circle', 'fi_ky225.triangle', 'fi_ky225.square'],
+      author_email='yablochkinkirill95@gmail.com',
+      zip_safe=False)
