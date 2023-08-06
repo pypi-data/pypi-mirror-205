e003be1b66cfd0624defc5b87d018eac916c08ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:37 +0000

    Add package anyplusone/0.0.1/anyplusone-0.0.1.tar.gz

---
+++ packages/anyplusone/anyplusone-0.0.1.tar.gz/anyplusone-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='anyplusone',
+      version='0.0.1',
+      description='Simple math library',
+      url='https://github.com/<username>/anyplusone',
+      author='ay',
+      author_email='',
+      license='MIT',
+      packages=['anyplusone'],
+      zip_safe=False,
+      )
