766479925894fa06a1e276b02c8b1c33d6f6af13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:36 +0000

    Add package ucsdasone/1.0.0a3/ucsdasone-1.0.0a3-py3-none-any.whl

---
+++ packages/ucsdasone/ucsdasone-1.0.0a3-py3-none-any.whl/asone/__init__.py
@@ -0,0 +1,39 @@
+import argparse
+from importlib import metadata
+
+from .scraper import GradeArchive
+
+
+NAME = metadata.metadata("ucsdasone")["name"]
+VERSION = metadata.version("ucsdasone")
+
+
+def _parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-v", "--version", action="version",
+        version=f"{NAME} {VERSION}"
+    )
+    parser.add_argument("-q", "--quarter", type=str)
+    parser.add_argument("-y", "--year", type=int)
+    parser.add_argument("-i", "--instructor", type=str)
+    parser.add_argument("-s", "--subject", type=str)
+    parser.add_argument("-c", "--code", type=str)
+
+    return parser
+
+
+def main() -> None:
+    parser = _parser()
+    args = parser.parse_args()
+
+    archive = GradeArchive(
+        quarter=args.quarter,
+        year=args.year,
+        instructor=args.instructor,
+        subject=args.subject,
+        code=args.code
+    )
+
+    print(archive.data())
