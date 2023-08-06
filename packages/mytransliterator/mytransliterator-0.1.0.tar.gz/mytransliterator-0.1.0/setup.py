2215df08c5f64b4fe5f4ce03430729a40e04624b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:34 +0000

    Add package mytransliterator/0.1.0/mytransliterator-0.1.0.tar.gz

---
+++ packages/mytransliterator/mytransliterator-0.1.0.tar.gz/mytransliterator-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytransliterator',
+    version='0.1.0',
+    author='Sanjana',
+    author_email='sanjana64885@gmail.com',
+    description='transliteration',
+    packages=find_packages(),
+    install_requires=[
+        'dependency1>=1.0',
+        'dependency2>=2.0',],
+)
