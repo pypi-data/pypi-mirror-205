06f1345e4c2cd57c9c2c2161a0b5c70c1b39798a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:13 +0000

    Add package pygame-json-ui/0.0.0.4/pygame_json_ui-0.0.0.4-py3-none-any.whl

---
+++ packages/pygame-json-ui/pygame_json_ui-0.0.0.4-py3-none-any.whl/pygame_ui/__init__.py
@@ -0,0 +1,29 @@
+"""
+Pygame UI
+=========
+
+A python library for quickly building user interfaces for pygame.
+
+Make sure you read the ``Getting started`` section of the documentation in the library folder.
+"""
+
+from pygame_ui.base import *
+from pygame_ui.elements import *
+
+# release, year, month, session
+__version__ = '0.0.0.4'
+
+#print('Pygame UI ' + __version__)
+
+def test():
+	try:
+		import pygame_ui.base
+		import pygame_ui.constants
+		import pygame_ui.elements
+		print('Successfully installed! enjoy :)\nversion: '+__version__)
+		del pygame_ui.base, pygame_ui.constants, pygame_ui.elements
+	except:
+		print(ISSUE_REPORT)
+
+# Base64
+# VGhpcyBjb2RlIHdhcyB3cml0dGVuIGJ5IFJlZG5heEdhbWluZyBvbiBnaXRodWI=
