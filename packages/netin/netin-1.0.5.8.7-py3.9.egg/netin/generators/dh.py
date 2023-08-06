4c009411d9014cafae2508f14420027bf292af8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:15 +0000

    Add package netin/1.0.5.8.7/netin-1.0.5.8.7-py3.9.egg

---
+++ packages/netin/netin-1.0.5.8.7-py3.9.egg/netin/generators/dh.py
@@ -0,0 +1,180 @@
+from typing import Union, Set, Tuple
+
+import numpy as np
+
+from netin.generators.directed import DiGraph
+from netin.generators.h import Homophily
+from netin.utils import constants as const
+
+
+class DH(DiGraph, Homophily):
+    """ Creates a new DH instance. A directed graph with homophily.
+
+    Parameters
+    ----------
+    n: int
+        number of nodes (minimum=2)
+
+    d: float
+        edge density (minimum=0, maximum=1)
+
+    f_m: float
+        fraction of minorities (minimum=1/n, maximum=(n-1)/n)
+
+    plo_M: float
+        activity (out-degree power law exponent) majority group (minimum=1)
+
+    plo_m: float
+        activity (out-degree power law exponent) minority group (minimum=1)
+
+    h_MM: float
+        homophily within majority group (minimum=0, maximum=1)
+
+    h_mm: float
+        homophily within minority group (minimum=0, maximum=1)
+
+    seed: object
+        seed for random number generator
+
+    Notes
+    -----
+    The initialization is a directed with n nodes and no edges.
+    Source nodes are selected based on their activity given by plo_M (if majority) or plo_m (if minority).
+    Target nodes are selected via homophily, see [Espin-Noboa2022]_.
+    """
+
+    ############################################################
+    # Constructor
+    ############################################################
+
+    def __init__(self, n: int, d: float, f_m: float, plo_M: float, plo_m: float, h_MM: float, h_mm: float,
+                 seed: object = None):
+        DiGraph.__init__(self, n=n, d=d, f_m=f_m, plo_M=plo_M, plo_m=plo_m, seed=seed)
+        Homophily.__init__(self, n=n, f_m=f_m, h_MM=h_MM, h_mm=h_mm, seed=seed)
+
+    ############################################################
+    # Init
+    ############################################################
+
+    def _infer_model_name(self):
+        """
+        Infers the name of the model.
+        """
+        return self.set_model_name(const.DH_MODEL_NAME)
+
+    ############################################################
+    # Generation
+    ############################################################
+
+    def _initialize(self, class_attribute: str = 'm', class_values: list = None, class_labels: list = None):
+        """
+        Initializes the model.
+
+        Parameters
+        ----------
+        class_attribute: str
+            name of the attribute that represents the class
+
+        class_values: list
+            values of the class attribute
+
+        class_labels: list
+            labels of the class attribute mapping the class_values.
+        """
+        DiGraph._initialize(self, class_attribute, class_values, class_labels)
+        Homophily._initialize(self, class_attribute, class_values, class_labels)
+
+    def get_target_probabilities(self, source: Union[None, int], target_set: Union[None, Set[int], np.array],
+                                 special_targets: Union[None, object, iter] = None) -> np.array:
+        """
+        Returns the probabilities of the target nodes to be selected given a source node.
+
+        Parameters
+        ----------
+        source: int
+            source node (id)
+
+        target_set: set
+            set of target nodes (ids)
+
+        special_targets: object
+            special targets
+
+        Returns
+        -------
+        probs: np.array
+            probabilities of the target nodes to be selected
+
+        """
+        probs, ts = Homophily.get_target_probabilities(self, source, target_set, special_targets)
+        return probs
+
+    ############################################################
+    # Calculations
+    ############################################################
+
+    def info_params(self):
+        """
+        Shows the parameters of the model.
+        """
+        DiGraph.info_params(self)
+        Homophily.info_params(self)
+
+    def info_computed(self):
+        """
+        Shows the computed properties of the graph.
+        """
+        Homophily.info_computed(self)
+
+    def infer_homophily_values(self) -> Tuple[float, float]:
+        """
+        Infers analytically the homophily values for the majority and minority classes.
+
+        Returns
+        -------
+        h_MM: float
+            homophily within majority group
+
+        h_mm: float
+            homophily within minority group
+        """
+        from sympy import symbols
+        from sympy import Eq
+        from sympy import solve
+
+        f_m = self.calculate_fraction_of_minority()
+        f_M = 1 - f_m
+
+        e = self.calculate_edge_type_counts()
+        e_MM = e['MM']
+        e_mm = e['mm']
+        e_Mm = e['Mm']
+        e_mM = e['mM']
+
+        p_MM = e_MM / (e_MM + e_Mm)
+        p_mm = e_mm / (e_mm + e_mM)
+
+        # equations
+        hmm, hMM, hmM, hMm = symbols('hmm hMM hmM hMm')
+        eq1 = Eq((f_m * hmm) / ((f_m * hmm) + (f_M * hmM)), p_mm)
+        eq2 = Eq(hmm + hmM, 1)
+
+        eq3 = Eq((f_M * hMM) / ((f_M * hMM) + (f_m * hMm)), p_MM)
+        eq4 = Eq(hMM + hMm, 1)
+
+        solution = solve((eq1, eq2, eq3, eq4), (hmm, hmM, hMM, hMm))
+        h_MM, h_mm = solution[hMM], solution[hmm]
+        return h_MM, h_mm
+
+    def _makecopy(self):
+        """
+        Makes a copy of the current object.
+        """
+        return self.__class__(n=self.n,
+                              d=self.d,
+                              f_m=self.f_m,
+                              plo_M=self.plo_M,
+                              plo_m=self.plo_m,
+                              h_MM=self.h_MM,
+                              h_mm=self.h_mm,
+                              seed=self.seed)
---
+++ packages/netin/netin-1.0.5.8.7-py3.9.egg/netin/generators/patch.py
@@ -0,0 +1,203 @@
+from typing import Union, Set, Tuple
+
+import numpy as np
+
+from netin.utils import constants as const
+from netin.generators.tc import TriadicClosure
+from .pah import PAH
+
+
+class PATCH(PAH, TriadicClosure):
+    """Creates a new PATCH instance. An undirected graph with preferential attachment, homophily, and triadic closure.
+
+    Parameters
+    ----------
+    n: int
+        number of nodes (minimum=2)
+
+    k: int
+        minimum degree of nodes (minimum=1)
+
+    f_m: float
+        fraction of minorities (minimum=1/n, maximum=(n-1)/n)
+
+    h_MM: float
+        homophily (similarity) between majority nodes (minimum=0, maximum=1.)
+
+    h_mm: float
+        homophily (similarity) between minority nodes (minimum=0, maximum=1.)
+
+    tc: float
+        probability of a new edge to close a triad (minimum=0, maximum=1.)
+
+    Notes
+    -----
+    The initialization is an undirected with n nodes and no edges.
+    Then, everytime a node is selected as source, it gets connected to k target nodes.
+    Target nodes are selected via preferential attachment (in-degree) [BarabasiAlbert1999]_,
+    homophily (h_**; see :class:`netin.Homophily`) [Karimi2018]_,
+    and triadic closure (see :class:`netin.TriadicClosure`) [HolmeKim2002]_.
+    """
+
+    ############################################################
+    # Constructor
+    ############################################################
+
+    def __init__(self, n: int, k: int, f_m: float, h_mm: float, h_MM: float, tc: float, seed: object = None):
+        PAH.__init__(self, n=n, k=k, f_m=f_m, h_MM=h_MM, h_mm=h_mm, seed=seed)
+        TriadicClosure.__init__(self, n=n, f_m=f_m, tc=tc, seed=seed)
+
+    ############################################################
+    # Init
+    ############################################################
+
+    def _infer_model_name(self):
+        """
+        Infers the name of the model.
+        """
+        return self.set_model_name(const.PATCH_MODEL_NAME)
+
+    def _validate_parameters(self):
+        """
+        Validates the parameters of the undirected.
+        """
+        PAH._validate_parameters(self)
+        TriadicClosure._validate_parameters(self)
+
+    def get_metadata_as_dict(self) -> dict:
+        """
+        Returns a dictionary with the metadata of the PATCH graph.
+
+        Returns
+        -------
+        dict
+            the graph  metadata as a dictionary
+        """
+        obj1 = PAH.get_metadata_as_dict(self)
+        obj2 = TriadicClosure.get_metadata_as_dict(self)
+        obj1.update(obj2)
+        return obj1
+
+    ############################################################
+    # Generation
+    ############################################################
+
+    def get_target_probabilities(self, source: Union[None, int], target_set: Union[None, Set[int]],
+                                 special_targets: Union[None, object, iter] = None) -> Tuple[np.array, set[int]]:
+        """
+        Returns the probabilities of nodes to be selected as target nodes.
+
+        Parameters
+        ----------
+        source: int
+            source node id
+
+        target_set: set
+            set of target node ids
+
+        special_targets: dict
+            dictionary of special target node ids to be considered
+
+        Returns
+        -------
+        tuple
+            probabilities of nodes to be selected as target nodes, and set of target of nodes
+
+        """
+        return TriadicClosure.get_target_probabilities(self, source, target_set, special_targets)
+
+    def get_target_probabilities_regular(self, source: Union[None, int], target_set: Union[None, Set[int]],
+                                         special_targets: Union[None, object, iter] = None) -> Tuple[
+        np.ndarray, set[int]]:
+        """
+        Returns the probability of nodes to be selected as target nodes using the
+        preferential attachment with homophily mechanism.
+
+        Parameters
+        ----------
+        source: int
+            source node id
+
+        target_set: set
+            set of target node ids
+
+        special_targets: dict
+            dictionary of special target node ids to be considered
+
+        Returns
+        -------
+        tuple
+            probabilities of nodes to be selected as target nodes, and set of target of nodes
+        """
+        return PAH.get_target_probabilities(self, source, target_set, special_targets)
+
+    def get_special_targets(self, source: int) -> object:
+        """
+        Returns an empty dictionary (source node ids)
+
+        Parameters
+        ----------
+        source : int
+            Newly added node
+
+        Returns
+        -------
+        Dict
+            Empty dictionary
+        """
+        return TriadicClosure.get_special_targets(self, source)
+
+    ############################################################
+    # Calculations
+    ############################################################
+
+    def info_params(self):
+        """
+        Shows the (input) parameters of the graph.
+        """
+        PAH.info_params(self)
+        TriadicClosure.info_params(self)
+
+    def info_computed(self):
+        """
+        Shows the (computed) properties of the graph.
+        """
+        PAH.info_computed(self)
+        TriadicClosure.info_computed(self)
+
+    def infer_homophily_values(self) -> Tuple[float, float]:
+        """
+        Infers analytically the homophily values of the graph.
+
+        Returns
+        -------
+        tuple
+            homophily values of the graph (majority, minority)
+        """
+        h_MM = None
+        h_mm = None
+        return h_MM, h_mm
+
+    def infer_triadic_closure(self) -> float:
+        """
+        Infers analytically the triadic closure value of the graph.
+
+        Returns
+        -------
+        float
+            triadic closure probability of the graph
+        """
+        tc = None
+        return tc
+
+    def _makecopy(self):
+        """
+        Makes a copy of the current object.
+        """
+        return self.__class__(n=self.n,
+                              k=self.k,
+                              f_m=self.f_m,
+                              tc=self.tc,
+                              h_MM=self.h_MM,
+                              h_mm=self.h_mm,
+                              seed=self.seed)
