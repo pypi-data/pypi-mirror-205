6144336ade55b2ec97abd733c2e4e0f5021e77bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:21 +0000

    Add package fig-23dec/0.0/fig_23dec-0.0.tar.gz

---
+++ packages/fig-23dec/fig_23dec-0.0.tar.gz/fig_23dec-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fig_23dec',
+      version='0.0',
+      description='circle, tiangle, square',
+      packages=['fig_23dec.circle', 'fig_23dec.triangle', 'fig_23dec.square'],
+      author_email='daraaleksandrovic49627@gmail.com',
+      zip_safe=False)
