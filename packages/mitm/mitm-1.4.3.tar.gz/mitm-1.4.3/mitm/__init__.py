b56613f3f2336e8eb8e5691014f8950bfd73185f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:20 +0000

    Add package mitm/1.4.3/mitm-1.4.3.tar.gz

---
+++ packages/mitm/mitm-1.4.3.tar.gz/mitm-1.4.3/mitm/__init__.py
@@ -0,0 +1,42 @@
+"""
+Github: https://github.com/synchronizing/mitm
+Docs: https://synchronizing.github.io/mitm/
+"""
+
+# pylint: disable=wrong-import-order, wrong-import-position
+
+__author__ = "Felipe Faria"
+__project__ = "mitm"
+
+import pathlib
+import appdirs
+from pbr.version import VersionInfo
+
+__version__ = VersionInfo(__project__).release_string()
+__data__ = pathlib.Path(appdirs.user_data_dir(__package__, __author__))
+
+import logging
+import sys
+
+logging.basicConfig(
+    stream=sys.stdout,
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
+
+from mitm.core import *
+from mitm.crypto import *
+from mitm.extension import *
+from mitm.mitm import *
+
+__all__ = [
+    "Host",
+    "Connection",
+    "Flow",
+    "MITM",
+    "Middleware",
+    "Protocol",
+    "InvalidProtocol",
+    "CertificateAuthority",
+]
