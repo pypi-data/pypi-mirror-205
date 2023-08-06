e9157a812c5463a2d93f165a62cdccc6a9318a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:39 +0000

    Add package feiticeiro-tec/1.0.0/feiticeiro_tec-1.0.0.tar.gz

---
+++ packages/feiticeiro-tec/feiticeiro_tec-1.0.0.tar.gz/feiticeiro_tec-1.0.0/feiticeiro_tec/api/__init__.py
@@ -0,0 +1,11 @@
+from flask_restx import Api
+from loguru import logger
+from flask import Flask
+
+api = Api()
+
+
+def init_app(app: Flask):
+    logger.info('Inicializando API')
+    api.init_app(app)
+    return api
