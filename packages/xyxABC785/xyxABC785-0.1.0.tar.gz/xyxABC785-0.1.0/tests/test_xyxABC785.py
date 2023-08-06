4f886a0fa04dfe005d7836be084c1ebed66ca20d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:23 +0000

    Add package xyxABC785/0.1.0/xyxABC785-0.1.0.tar.gz

---
+++ packages/xyxABC785/xyxABC785-0.1.0.tar.gz/xyxABC785-0.1.0/tests/test_xyxABC785.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `xyxABC785` package."""
+
+
+import unittest
+
+from xyxABC785 import xyxABC785
+
+
+class TestXyxabc785(unittest.TestCase):
+    """Tests for `xyxABC785` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
---
+++ packages/xyxABC785/xyxABC785-0.1.0.tar.gz/xyxABC785-0.1.0/xyxABC785/xyxABC785.py
@@ -0,0 +1,4 @@
+"""Main module."""
+class Welcome:
+    def greet(self):
+        print("Hello! Welcome to my package.")
