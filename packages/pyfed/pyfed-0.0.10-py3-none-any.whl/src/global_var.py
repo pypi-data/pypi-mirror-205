a23e0f043a64f295037aabe0f9d5a93ca7a3b5fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:57 +0000

    Add package pyfed/0.0.10/pyfed-0.0.10-py3-none-any.whl

---
+++ packages/pyfed/pyfed-0.0.10-py3-none-any.whl/src/global_var.py
@@ -0,0 +1,20 @@
+import os
+import time
+import h5py
+import copy
+import socket
+import datetime
+import warnings
+import numpy as np
+from io import BytesIO
+import tensorflow as tf
+import concurrent.futures
+from joblib import load, dump
+from tensorflow.keras.models import save_model, load_model
+
+
+IP = "127.0.0.1"
+PORT = 124
+SIZE = 1024
+FORMAT = "utf-8"
+PATH = "pyfl_logs/fit"
