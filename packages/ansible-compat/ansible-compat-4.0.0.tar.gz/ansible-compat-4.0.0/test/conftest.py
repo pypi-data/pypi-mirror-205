e8111ac38dff79a50ee94a26c26ebb28e412c0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:46 +0000

    Add package ansible-compat/4.0.0/ansible-compat-4.0.0.tar.gz

---
+++ packages/ansible-compat/ansible-compat-4.0.0.tar.gz/ansible-compat-4.0.0/test/conftest.py
@@ -0,0 +1,27 @@
+"""Pytest fixtures."""
+import pathlib
+from typing import Generator
+
+import pytest
+
+from ansible_compat.runtime import Runtime
+
+
+@pytest.fixture()
+# pylint: disable=unused-argument
+def runtime(scope: str = "session") -> Generator[Runtime, None, None]:
+    """Isolated runtime fixture."""
+    instance = Runtime(isolated=True)
+    yield instance
+    instance.clean()
+
+
+@pytest.fixture()
+# pylint: disable=unused-argument
+def runtime_tmp(
+    tmp_path: pathlib.Path, scope: str = "session"
+) -> Generator[Runtime, None, None]:
+    """Isolated runtime fixture using a temp directory."""
+    instance = Runtime(project_dir=str(tmp_path), isolated=True)
+    yield instance
+    instance.clean()
