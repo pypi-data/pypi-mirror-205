348c38eeb4c83107aa5aff5ee14f743fc46dc2f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:47 +0000

    Add package fig-an225/0.0/fig_an225-0.0.tar.gz

---
+++ packages/fig-an225/fig_an225-0.0.tar.gz/fig_an225-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fig_an225',
+      version='0.0',
+      description='circle, tiangle, square',
+      packages=['fig_an225.circle', 'fig_an225.triangle', 'fig_an225.square'],
+      author_email='alexeykozhakin@gmail.com',
+      zip_safe=False)
