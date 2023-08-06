b1813fc95c1143f4a88098f60917523e09e146f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:30 +0000

    Add package goxlrutilityapi/1.0.0.dev9/goxlrutilityapi-1.0.0.dev9.tar.gz

---
+++ packages/goxlrutilityapi/goxlrutilityapi-1.0.0.dev9.tar.gz/goxlrutilityapi-1.0.0.dev9/goxlrutilityapi/helper.py
@@ -0,0 +1,22 @@
+"""GoXLR Utility API: Helper"""
+from __future__ import annotations
+
+from typing import Optional
+
+from .const import VOLUME_MAX
+from .models.status import Mixer, Status
+
+
+def get_mixer_from_status(status: Status) -> Optional[Mixer]:
+    """Get first mixer from status"""
+    return next(iter(status.mixers.values()), None)
+
+
+def get_volume_percentage(
+    mixer: Mixer,
+    key: str,
+) -> Optional[int]:
+    """Return the volume in percent."""
+    if (value := getattr(mixer.levels.volumes, key, None)) is not None:
+        return round(value / VOLUME_MAX * 100)
+    return None
