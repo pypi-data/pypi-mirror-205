ee6d1e983275e4533b9841f778782171de0020e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:05 +0000

    Add package formic-opcua/1.2.5/formic_opcua-1.2.5-py3-none-any.whl

---
+++ packages/formic-opcua/formic_opcua-1.2.5-py3-none-any.whl/formic_opcua/core/type_conversions.py
@@ -0,0 +1,30 @@
+# Copyright Formic Technologies 2023
+import uuid
+from datetime import datetime
+
+from asyncua.ua.uatypes import VariantType
+
+type_map = {
+    VariantType.SByte: int,
+    VariantType.Byte: int,
+    VariantType.ByteString: bytes,
+    VariantType.Int16: int,
+    VariantType.Int32: int,
+    VariantType.Int64: int,
+    VariantType.UInt16: int,
+    VariantType.UInt32: int,
+    VariantType.UInt64: int,
+    VariantType.Boolean: bool,
+    VariantType.Double: float,
+    VariantType.Float: float,
+    VariantType.String: str,
+    VariantType.DateTime: datetime,
+    VariantType.Guid: uuid.UUID,
+}
+
+
+def convert_type(value, var_type):
+    if var_type == VariantType.String and (type_map[var_type] == int or type_map[var_type] == float):
+        value = hash(value)
+
+    return type_map[var_type](value)
