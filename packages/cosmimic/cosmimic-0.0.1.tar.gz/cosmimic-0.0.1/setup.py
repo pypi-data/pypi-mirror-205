b3ac15e19bfe71344c403c5761f40729c7a5de3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:30 +0000

    Add package cosmimic/0.0.1/cosmimic-0.0.1.tar.gz

---
+++ packages/cosmimic/cosmimic-0.0.1.tar.gz/cosmimic-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cosmimic",
+    version="0.0.1",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+    ],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A short description of your package",
+    long_description="A longer description of your package",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jack-white1/cosmimic",
+)
