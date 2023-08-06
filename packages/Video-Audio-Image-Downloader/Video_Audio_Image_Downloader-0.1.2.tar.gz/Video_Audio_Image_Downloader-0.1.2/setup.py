ed01bef5ed5bf5f502f475b5713f211efb86952f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:34 +0000

    Add package Video-Audio-Image-Downloader/0.1.2/Video_Audio_Image_Downloader-0.1.2.tar.gz

---
+++ packages/Video-Audio-Image-Downloader/Video_Audio_Image_Downloader-0.1.2.tar.gz/Video_Audio_Image_Downloader-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import os
+
+requirements = os.popen("/usr/local/bin/pipreqs main --print").read().splitlines()
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='Video_Audio_Image_Downloader',
+    version='0.1.2',
+    author='Sridhar',
+    author_email='dcsvsridhar@gmail.com',
+    #  to help download videos and audio files from Youtube
+    description="In this tool is help to Download the Youtube Video,Audio and Any type of Google and other site's Images",
+    packages=find_packages(),
+    url='https://git.selfmade.ninja/SRIDHARDSCV/audio_video_image_downloder-1',
+    install_requires=requirements,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            'Downloader=source.source:main',
+        ],
+    },
+)
