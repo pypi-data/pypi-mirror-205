71cc72f11b3a65b82a91ee653c75a8f05a62fdd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:33 +0000

    Add package tox-backtocks/0.4.0/tox_backtocks-0.4.0-py3-none-any.whl

---
+++ packages/tox-backtocks/tox_backtocks-0.4.0-py3-none-any.whl/tox_backtocks/__init__.py
@@ -0,0 +1,31 @@
+"""A tox plugin that allows backquote expansion in set_env section.
+"""
+
+from tox.config.sets import EnvConfigSet
+from tox.plugin import impl
+from tox.session.state import State
+from tox.tox_env.api import ToxEnv
+
+from .backquote import SHELL, has_backticks, \
+    eval_backquote, set_env_backquote_items
+from .setenv import set_env_items
+
+
+@impl
+def tox_add_env_config(env_conf: EnvConfigSet, state: State) -> None:
+    """Post process config after parsing."""
+    # pylint: disable=unused-argument
+    set_env = env_conf["set_env"]
+    for _, value in set_env_items(set_env):
+        if has_backticks(value):
+            # Add bash in order to be able to evaluate backquotes.
+            env_conf["allowlist_externals"].append(SHELL)
+            return
+
+
+@impl
+def tox_before_run_commands(tox_env: ToxEnv) -> None:
+    """Eval and replace backquotes expressions"""
+    set_env = tox_env.conf["set_env"]
+    set_env.update({var: eval_backquote(tox_env, cmd, var)
+                    for var, cmd in set_env_backquote_items(set_env)})
