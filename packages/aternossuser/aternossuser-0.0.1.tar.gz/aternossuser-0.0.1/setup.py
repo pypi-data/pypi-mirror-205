b1b43c09f60a2b1a6d7ca99cc4f05884127c094c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:15 +0000

    Add package aternossuser/0.0.1/aternossuser-0.0.1.tar.gz

---
+++ packages/aternossuser/aternossuser-0.0.1.tar.gz/aternossuser-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='aternossuser',
+    version='0.0.1',
+    description='Модуль использующий, апи Aternos-а с 3 классами Auth, Server, Files.',
+    long_description=open('README.md').read(),     long_description_content_type='text/markdown',
+    py_modules=['aternossuser'],
+)
+
