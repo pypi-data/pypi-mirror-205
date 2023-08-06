ba61a784d04fb526d2dc9bf2593c1cf60c815ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:03 +0000

    Add package invenio-records-lom/0.8.1/invenio-records-lom-0.8.1.tar.gz

---
+++ packages/invenio-records-lom/invenio-records-lom-0.8.1.tar.gz/invenio-records-lom-0.8.1/invenio_records_lom/cli.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 Graz University of Technology.
+#
+# invenio-records-lom is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Click command-line interface for LOM module."""
+
+import click
+from flask.cli import with_appcontext
+from invenio_access.permissions import system_identity
+
+from .fixtures import publish_fake_records
+from .proxies import current_records_lom
+
+
+@click.group()
+def lom():
+    """CLI-group for "invenio lom" commands."""
+
+
+@lom.command("rebuild-index")
+@with_appcontext
+def rebuild_index():
+    """Reindex all drafts, records."""
+    click.secho("Reindexing records and drafts...", fg="green")
+
+    rec_service = current_records_lom.records_service
+    rec_service.rebuild_index(identity=system_identity)
+
+    click.secho("Reindexed records!", fg="green")
+
+
+@lom.command()
+@with_appcontext
+@click.option(
+    "--number",
+    "-n",
+    default=100,
+    show_default=True,
+    type=int,
+    help="Number of records to be created.",
+)
+@click.option("--seed", "-s", default=42, type=int, help="Seed for RNG.")
+def demo(number, seed):
+    """Publish `number` fake LOM records to the database, for demo purposes."""
+    click.secho(f"Creating {number} LOM demo records", fg="green")
+    for __ in publish_fake_records(number, seed):
+        pass
+    click.secho("Published fake LOM records to the database!", fg="green")
---
+++ packages/invenio-records-lom/invenio-records-lom-0.8.1.tar.gz/invenio-records-lom-0.8.1/invenio_records_lom/resources/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Graz University of Technology.
+#
+# invenio-records-lom is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio-Records-LOM module for creating REST APIs."""
+
+from .config import (
+    LOMDraftFilesResourceConfig,
+    LOMRecordFilesResourceConfig,
+    LOMRecordResourceConfig,
+)
+from .resources import LOMRecordResource
+
+__all__ = (
+    "LOMDraftFilesResourceConfig",
+    "LOMRecordFilesResourceConfig",
+    "LOMRecordResource",
+    "LOMRecordResourceConfig",
+)
