ea494861fa1ab39f3274dc105b58e4c3346e6226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:21 +0000

    Add package decorworld-database-model/0.6.3/decorworld_database_model-0.6.3.tar.gz

---
+++ packages/decorworld-database-model/decorworld_database_model-0.6.3.tar.gz/decorworld_database_model-0.6.3/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['decorworld_database_model',
+ 'decorworld_database_model.alembic',
+ 'decorworld_database_model.alembic.versions',
+ 'decorworld_database_model.tables']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['marshmallow==3.14.1', 'sqlalchemy==1.3.18']
+
+setup_kwargs = {
+    'name': 'decorworld-database-model',
+    'version': '0.6.3',
+    'description': 'Database models for DecorWorld',
+    'long_description': None,
+    'author': 'Pintér Tamás',
+    'author_email': 'tamas.pinter@pannonszoftver.hu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
