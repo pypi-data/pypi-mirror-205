f350bc9e602a7b0ab5239aebe4c2450fab4382c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:59 +0000

    Add package cocojamboo/2.1.2/cocojamboo-2.1.2.tar.gz

---
+++ packages/cocojamboo/cocojamboo-2.1.2.tar.gz/cocojamboo-2.1.2/README.md
@@ -0,0 +1,3 @@
+# COCO Ecaluation Metrics Extension
+
+An extension to COCO evaluation metrics to provide mAP scores with custom paramters.
---
+++ packages/cocojamboo/cocojamboo-2.1.2.tar.gz/cocojamboo-2.1.2/cocojamboo/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "2.1.2"
+
+from .utils import *
+from .transforms import *
+from .cocoeval2 import *
+from .coco_eval2 import *
+from .coco_utils import *
+from .engine2 import *
