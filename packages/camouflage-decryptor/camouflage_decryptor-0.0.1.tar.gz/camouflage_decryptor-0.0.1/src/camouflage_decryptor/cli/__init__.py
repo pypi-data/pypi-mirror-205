1817a047dec5e9e130ceef750ee7ffb967ccad3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:18 +0000

    Add package camouflage-decryptor/0.0.1/camouflage_decryptor-0.0.1.tar.gz

---
+++ packages/camouflage-decryptor/camouflage_decryptor-0.0.1.tar.gz/camouflage_decryptor-0.0.1/src/camouflage_decryptor/cli/__init__.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2023-present anjomro <py@anjomro.de>
+#
+# SPDX-License-Identifier: EUPL-1.2
+import click
+
+from camouflage_decryptor.__about__ import __version__
+from camouflage_decryptor.decryptor import extract_camouflage_password
+
+
+@click.group(context_settings={"help_option_names": ["-h", "--help"]})
+@click.version_option(version=__version__, prog_name="camouflage-decryptor")
+def camouflage_decryptor():
+    pass
+
+
+@click.command()
+@click.argument('input', type=click.File('rb'))
+def get_key(input):
+    """Extract key from input file. """
+    img_raw = input.read()
+    extract_camouflage_password(img_raw)
+
+
+camouflage_decryptor.add_command(get_key)
