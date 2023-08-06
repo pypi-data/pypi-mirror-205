80e0b72fcc5c5e0dd1fbb2686d43d94010358921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:40 +0000

    Add package sdkit/1.0.82/sdkit-1.0.82.tar.gz

---
+++ packages/sdkit/sdkit-1.0.82.tar.gz/sdkit-1.0.82/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    install_requires=[
+        "stable-diffusion-sdkit==2.1.4",  # wrapper around stable-diffusion, to allow pip install
+        "gfpgan",
+        "piexif",
+        "realesrgan",
+        "requests",
+        "picklescan",
+        "safetensors",
+        "k-diffusion",
+        "diffusers==0.15.1",
+        "compel==1.0.5",
+    ],
+)
