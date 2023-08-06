61a50ac8a8316fde837f882f5173074a5e326853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:57 +0000

    Add package xsocs/2020.11.0/xsocs-2020.11.0-cp27-cp27mu-manylinux_2_5_x86_64.manylinux1_x86_64.whl

---
+++ packages/xsocs/xsocs-2020.11.0-cp27-cp27mu-manylinux_2_5_x86_64.manylinux1_x86_64.whl/xsocs/gui/project/AcqDataGroup.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+# /*##########################################################################
+#
+# Copyright (c) 2015-2016 European Synchrotron Radiation Facility
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# ###########################################################################*/
+
+from __future__ import absolute_import
+
+__authors__ = ["D. Naudet"]
+__license__ = "MIT"
+__date__ = "15/09/2016"
+
+
+from .ProjectItem import ProjectItem
+from .ProjectDef import ItemClassDef
+
+
+@ItemClassDef('AcqDataGroup')
+class AcqDataGroup(ProjectItem):
+    itemName = 'Acquisition'
+
+    ScanCommandPath = 'Scan'
+    SamplePath = 'Sample'
+    EntriesPath = 'Entries'
+
+    def _createItem(self):
+        with self.xsocsH5 as xsocsH5:
+            with self:
+                # adding parameter values to this item
+                entries = xsocsH5.entries()
+                # TODO : make sure that all parameters are consistent
+                scan_params = xsocsH5.scan_params(entries[0])
+                path_tpl = '{0}/{1}/{{0}}'.format(self.path,
+                                                  AcqDataGroup.ScanCommandPath)
+                for key, value in scan_params.items():
+                    self._set_scalar_data(path_tpl.format(key), value)
+
+                path = self.path + '/' + AcqDataGroup.EntriesPath
+                self.add_soft_link(path, ProjectItem.XsocsH5FilePath)
