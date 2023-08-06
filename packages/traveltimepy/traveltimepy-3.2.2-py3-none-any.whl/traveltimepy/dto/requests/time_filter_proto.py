21b72d4d61fd8fb29ed65f429ac4c7e466cef724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:16 +0000

    Add package traveltimepy/3.2.2/traveltimepy-3.2.2-py3-none-any.whl

---
+++ packages/traveltimepy/traveltimepy-3.2.2-py3-none-any.whl/traveltimepy/dto/requests/time_filter_proto.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+@dataclass
+class TransportationInfo:
+    code: int
+    name: str
+
+
+class ProtoTransportation(Enum):
+    PUBLIC_TRANSPORT = TransportationInfo(0, "pt")
+    DRIVING_FERRY = TransportationInfo(3, "driving+ferry")
+    CYCLING_FERRY = TransportationInfo(6, "cycling+ferry")
+    WALKING_FERRY = TransportationInfo(7, "walking+ferry")
+
+
+class ProtoCountry(str, Enum):
+    NETHERLANDS = "nl"
+    AUSTRIA = "at"
+    UNITED_KINGDOM = "uk"
+    BELGIUM = "be"
+    GERMANY = "de"
+    FRANCE = "fr"
+    IRELAND = "ie"
+    LITHUANIA = "lt"
