ce4a5e7ae86b01c872c85e19940583ddf01f4306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:12 +0000

    Add package airgiant/0.1.0/airgiant-0.1.0.tar.gz

---
+++ packages/airgiant/airgiant-0.1.0.tar.gz/airgiant-0.1.0/src/airgiant/state/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+        # if finalstate<6:
+        #     await DORMANT(ws)
+        # elif finalstate<15:
+        #     await ALERT(ws)
+        # elif finalstate<20:
+        #     await ACTIVE(ws)
+        # else:
+        #     print("working")
