81b71ab8835bf74ef6ab2deddbde878592a9ba76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:40 +0000

    Add package mercury/2.2.9/mercury-2.2.9.tar.gz

---
+++ packages/mercury/mercury-2.2.9.tar.gz/mercury-2.2.9/mercury/__init__.py
@@ -0,0 +1,4 @@
+
+__version__ = "2.2.9"
+
+from mercury.mercury import *
---
+++ packages/mercury/mercury-2.2.9.tar.gz/mercury-2.2.9/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
+    long_description = fh.read()
+
+def list_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join(path, filename))
+    return paths
+
+setup(
+    name="mercury",
+    version="2.2.9",
+    maintainer="MLJAR Sp. z o.o.",
+    maintainer_email="contact@mljar.com",
+    description="Turn Jupyter Notebook to Web App and share with non-technical users",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=open("mercury/requirements.txt").readlines(),
+    url="https://github.com/mljar/mercury",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": ["mercury=mercury.mercury:main"],
+    },
+    package_data={"mercury": list_files("frontend-dist") + ["requirements.txt"]},
+    include_package_data=True,
+)
