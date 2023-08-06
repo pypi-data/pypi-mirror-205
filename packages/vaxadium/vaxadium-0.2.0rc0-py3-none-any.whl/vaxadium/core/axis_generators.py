de702d47ac5f6f2c4dbb0df1a8b998d079800deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:57 +0000

    Add package vaxadium/0.2.0rc0/vaxadium-0.2.0rc0-py3-none-any.whl

---
+++ packages/vaxadium/vaxadium-0.2.0rc0-py3-none-any.whl/vaxadium/core/axis_generators.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def generate_axis(start, stop, step):
+    # calculate number of points
+    return np.arange(start, stop, step) + step / 2
+
+
+def get_pyfai_range_npts(start, stop, step):
+    rnge = (start, stop)
+    npts = (stop - start) / step
+    return rnge, npts
+
+
+def get_ai_params(start, stop, step):
+    q = generate_axis(start, stop, step)
+    r, n = get_pyfai_range_npts(start, stop, step)
+    return q, r, n
