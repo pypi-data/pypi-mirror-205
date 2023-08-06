868188cc7aa6f2ac4bf139e39656613e9ab7a00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:47 +0000

    Add package screeny/0.0.1/Screeny-0.0.1-py3-none-any.whl

---
+++ packages/screeny/Screeny-0.0.1-py3-none-any.whl/screeny.py
@@ -0,0 +1,11 @@
+
+
+
+class Screeny:
+
+    def __init__(self):
+        pass
+
+    def take_screenshot(self):
+        # Todo: Implement
+        print("This function should take a screenshot from the computer screen.")
