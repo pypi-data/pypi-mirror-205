25f0f012c5665c5d22630bd631acd3e93ffb08ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:33 +0000

    Add package TheSilent/0.0.334/TheSilent-0.0.334.tar.gz

---
+++ packages/TheSilent/TheSilent-0.0.334.tar.gz/TheSilent-0.0.334/src/TheSilent/web_scanner.py
@@ -0,0 +1,33 @@
+from TheSilent.clear import clear
+from TheSilent.sql_injection_scanner import sql_injection_scanner
+from TheSilent.xss_scanner import xss_scanner
+
+CYAN = "\033[1;36m"
+
+# scans for security flaws and bad practices
+def web_scanner(url, secure=True, tor=False, delay=1, report=True):
+    clear()
+
+    my_sql_injection_scanner = sql_injection_scanner(url=url, secure=secure, tor=tor, delay=delay)
+    my_xss_scanner = xss_scanner(url=url, secure=secure, tor=tor, delay=delay)
+
+    clear()
+
+    print(CYAN + "sql injection:")
+
+    for i in my_sql_injection_scanner:
+        if report:
+            with open(url +  ".txt", "a") as f:
+                f.write(i + "\n")
+
+        print(CYAN + i)
+
+    print(CYAN + "")
+    print(CYAN + "xss:")
+
+    for i in my_xss_scanner:
+        if report:
+            with open(url +  ".txt", "a") as f:
+                f.write(i + "\n")
+                
+        print(CYAN + i)
