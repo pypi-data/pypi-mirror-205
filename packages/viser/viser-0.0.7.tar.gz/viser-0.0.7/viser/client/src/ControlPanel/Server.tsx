63b29ef2fb13789955f13ad7ca959b509d3784a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:46 +0000

    Add package viser/0.0.7/viser-0.0.7.tar.gz

---
+++ packages/viser/viser-0.0.7.tar.gz/viser-0.0.7/viser/client/src/ControlPanel/Server.tsx
@@ -0,0 +1,79 @@
+import { button, LevaPanel, useControls, useCreateStore } from "leva";
+import Box from "@mui/material/Box";
+import { levaTheme } from "./Generated";
+import { UseGui } from "./GuiState";
+import { RefObject, useContext } from "react";
+import { SceneContext } from "..";
+import { isTexture } from "../WebsocketInterface";
+
+export default function ServerControls(props: {
+  useGui: UseGui;
+  wrapperRef: RefObject<HTMLDivElement>;
+}) {
+  const scene = useContext(SceneContext);
+  const server = props.useGui((state) => state.server);
+  const label = props.useGui((state) => state.label);
+  const backgroundAvailable = props.useGui(
+    (state) => state.backgroundAvailable
+  );
+
+  const levaStore = useCreateStore();
+  useControls(
+    {
+      Label: {
+        value: label,
+        onChange: (value) => props.useGui.setState({ label: value }),
+      },
+      Websocket: {
+        value: server,
+        onChange: (value) => props.useGui.setState({ server: value }),
+      },
+      "Download Background": button(
+        () => {
+          const wrapper = props.wrapperRef.current;
+          if (wrapper === null) return;
+
+          if (!isTexture(scene!.background)) {
+            // This should never happen.
+            alert("No background to download!");
+            return;
+          }
+
+          const data = scene!.background.image;
+          const link = document.createElement("a");
+          link.download = "background";
+          link.href = data;
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        },
+        { disabled: !backgroundAvailable }
+      ),
+    },
+    { store: levaStore },
+    [backgroundAvailable]
+  );
+
+  // Leva theming is a bit limited, so we hack at styles here...
+  return (
+    <Box
+      component="div"
+      sx={{
+        "& label": { color: "#777" },
+        "& input[type='checkbox']~label svg path": {
+          stroke: "#fff !important",
+        },
+        "& button": { color: "#fff !important", height: "2em" },
+      }}
+    >
+      <LevaPanel
+        fill
+        flat
+        titleBar={false}
+        theme={levaTheme}
+        store={levaStore}
+        hideCopyButton
+      />
+    </Box>
+  );
+}
