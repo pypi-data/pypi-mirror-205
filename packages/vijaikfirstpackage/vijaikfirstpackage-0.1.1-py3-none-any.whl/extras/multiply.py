04d97c89518a6fb95d6d6c589b0940af3cd50a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:10 +0000

    Add package vijaikfirstpackage/0.1.1/vijaikfirstpackage-0.1.1-py3-none-any.whl

---
+++ packages/vijaikfirstpackage/vijaikfirstpackage-0.1.1-py3-none-any.whl/extras/multiply.py
@@ -0,0 +1,13 @@
+def multiply(x,y):
+    """
+    Sum of given arguments.
+    Keyword arguments:
+    -----------------
+    x : number format.
+    y : number format.
+    Returns:
+    --------
+    x+y : Number.
+        Returns product of two Numbers.
+    """   
+    return x*y
