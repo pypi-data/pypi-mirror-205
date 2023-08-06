6029cb345cf1affeeb68d1e114a8ecb730b9350b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:26 +0000

    Add package te22str-asef-qe11/0.0/te22str_asef_qe11-0.0.tar.gz

---
+++ packages/te22str-asef-qe11/te22str_asef_qe11-0.0.tar.gz/te22str_asef_qe11-0.0/te22str_asef_qe11/main.py
@@ -0,0 +1,16 @@
+@property
+class Employee:  
+    """Базовый класс для всех сотрудников"""  
+    emp_count = 0  
+  
+    def __init__(self, name, salary):  
+        self.name = name  
+        self.salary = salary  
+        Employee.emp_count += 1  
+  
+    def display_count(self):  
+        print('Всего сотрудников: %d' % Employee.empCount)  
+  
+    def display_employee(self):  
+        print('Имя: {}. Зарплата: {}'.format(self.name, self.salary))
+
