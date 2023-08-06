082ea8fab265b6a122e5095a90f417f45ba98f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:26 +0000

    Add package refractio/1.0.1/refractio-1.0.1.tar.gz

---
+++ packages/refractio/refractio-1.0.1.tar.gz/refractio-1.0.1/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.1'
+DESCRIPTION = 'REFRACT IO'
+LONG_DESCRIPTION = 'To read and write dataframe from different connectors'
+
+extras_require = {
+    "all": [
+        "snowflake-connector-python[pandas]==3.0.2",
+        "boto3==1.26.116",
+        "azure==4.0.0",
+        "openpyxl==3.1.2",
+        "xlrd==2.0.1",
+        "pysftp==0.2.9",
+        "pymysql==1.0.3"
+    ],
+    "snowflake": [
+        "snowflake-connector-python[pandas]==3.0.2"
+    ],
+    "s3": [
+        "boto3==1.26.116"
+    ],
+    "azureblob": [
+        "azure==4.0.0"
+    ],
+    "local": [
+        "openpyxl==3.1.2",
+        "xlrd==2.0.1",
+    ],
+    "sftp": [
+        "pysftp==0.2.9"
+    ],
+    "mysql": [
+        "pymysql==1.0.3"
+    ]
+}
+
+# Setting up
+setup(
+    name="refractio",
+    version=VERSION,
+    author="Abhishek Chaurasia",
+    author_email="<abhishek1.chaurasia@fosfor.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "pandas==2.0.0"
+    ],
+    keywords=['refractio'],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+    ],
+    extras_require=extras_require,
+    project_urls={
+        "Product": "https://www.fosfor.com/refract/",
+        "Source": "https://git.lti-aiq.in/refract-sdk/refract-sdk",
+    }
+)
