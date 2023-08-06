4faf08f661aae4c8ce1df5d137881782ba5aeb2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:49 +0000

    Add package tegda/0.1.1/tegda-0.1.1-py2.py3-none-any.whl

---
+++ packages/tegda/tegda-0.1.1-py2.py3-none-any.whl/tegda/__init__.py
@@ -0,0 +1,14 @@
+"""Top-level package for titorium EGDA."""
+
+__author__ = """Roberto Arce Aguirre"""
+__email__ = 'roberto_arce_@hotmail.com'
+#__version__ = '0.1.1'
+
+
+
+from . import time_series  # noqa : F401
+from . import flow  # noqa : F401
+from . import proportion  # noqa : F401
+from . import distribution  # noqa : F401
+from . import correlation  # noqa : F401
+from . import ranking  # noqa : F401
