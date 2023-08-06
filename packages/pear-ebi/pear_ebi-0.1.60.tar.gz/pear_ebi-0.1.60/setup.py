8f1ea0f9edf6cba44065e15a67a2c4d40c7cc798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:19 +0000

    Add package pear-ebi/0.1.60/pear_ebi-0.1.60.tar.gz

---
+++ packages/pear-ebi/pear_ebi-0.1.60.tar.gz/pear_ebi-0.1.60/setup.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name="pear_ebi",
+    version="0.1.60",
+    license="MIT License",
+    description="Embeds phylogenetic tree distances and produce representations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Andrea Rubbi",
+    author_email="andrea.rubbi.98@gmail.com",
+    url="https://github.com/AndreaRubbi/TreeEmbedding",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib>=3.4",
+        "scikit-learn",
+        "plotly",
+        "rich",
+        "pyDRMetrics",
+        "tqdm",
+        "toml",
+        "ipywidgets==7.7.2",
+    ],
+    entry_points={"console_scripts": ["pear_ebi = pear_ebi.__main__:main"]},
+)
