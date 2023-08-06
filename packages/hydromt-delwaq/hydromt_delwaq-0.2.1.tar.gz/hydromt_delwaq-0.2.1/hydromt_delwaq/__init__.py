ae2011d3aee124d871c244111f8ebc9d49447da4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:37 +0000

    Add package hydromt-delwaq/0.2.1/hydromt_delwaq-0.2.1.tar.gz

---
+++ packages/hydromt-delwaq/hydromt_delwaq-0.2.1.tar.gz/hydromt_delwaq-0.2.1/hydromt_delwaq/__init__.py
@@ -0,0 +1,16 @@
+"""hydroMT plugin for DELWAQ models."""
+
+from os.path import dirname, join, abspath
+
+
+__version__ = "0.2.1"
+
+DATADIR = join(dirname(abspath(__file__)), "data")
+
+# Set environment variables (this will be temporary)
+# to use shapely 2.0 in favor of pygeos (if installed)
+import os
+
+os.environ["USE_PYGEOS"] = "0"
+
+from .delwaq import *
---
+++ packages/hydromt-delwaq/hydromt_delwaq-0.2.1.tar.gz/hydromt_delwaq-0.2.1/hydromt_delwaq/workflows/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""HydroMT delwaq workflows"""
+
+from .emissions import *
+from .segments import *
+from .roads import *
