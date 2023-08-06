752f9e676b10f616a97ca26816b7e379b00993fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:04 +0000

    Add package sar-handler/0.1.0/sar_handler-0.1.0.tar.gz

---
+++ packages/sar-handler/sar_handler-0.1.0.tar.gz/sar_handler-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sar_handler',
+    packages=find_packages(include=['sar_handler']),
+    version='0.1.0',
+    description='Handler SAR images',
+    author='Felecort',
+    # author_email="@gmail.com",
+    url="https://github.com/Felecort/SAR_Handler",
+    license='MIT',
+    install_requires=["numpy"],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
