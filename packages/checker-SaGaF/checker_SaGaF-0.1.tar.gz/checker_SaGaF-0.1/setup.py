d15814af72cbe22fa1b02c5829aa70512a3f8a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:01 +0000

    Add package checker-SaGaF/0.1/checker_SaGaF-0.1.tar.gz

---
+++ packages/checker-SaGaF/checker_SaGaF-0.1.tar.gz/checker_SaGaF-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name= 'checker_SaGaF',
+    version='0.1',
+    author= '@CCIIUU',
+    description='Checker vaild emails for tiktok or instagram .',
+    packages= ['checker_SaGaF'],
+    author_email='dxa00776@gmail.com',
+    zip_safe=False,
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
