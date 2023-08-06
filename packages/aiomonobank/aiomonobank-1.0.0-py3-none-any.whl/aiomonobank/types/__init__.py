771ab669d4a5c1c6de8adf50ef85c0942e47022e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:50 +0000

    Add package aiomonobank/1.0.0/aiomonobank-1.0.0-py3-none-any.whl

---
+++ packages/aiomonobank/aiomonobank-1.0.0-py3-none-any.whl/aiomonobank/types/__init__.py
@@ -0,0 +1,15 @@
+from .currency import Currency
+from .client_info import Client
+from .account import Account
+from .jar import Jar
+from .statement_item import Statement
+from .webhook_data import WebhookData
+
+__all__ = [
+    'Currency',
+    'Client',
+    'Account',
+    'Jar',
+    'Statement',
+    'WebhookData'
+]
