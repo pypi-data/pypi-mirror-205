512fad544e24b53dd7e0edfc2e6079bb62dfd2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:47 +0000

    Add package prettyqt/1.24.0/prettyqt-1.24.0.tar.gz

---
+++ packages/prettyqt/prettyqt-1.24.0.tar.gz/prettyqt-1.24.0/prettyqt/gui/pdfwriter.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from prettyqt import core, gui
+from prettyqt.qt import QtCore, QtGui
+from prettyqt.utils import InvalidParamError, datatypes
+
+
+class PdfWriter(core.ObjectMixin, gui.PagedPaintDeviceMixin, QtGui.QPdfWriter):
+    def set_desktop_resolution(self):
+        primary = gui.app().primaryScreen()
+        dpi = primary.logicalDotsPerInch()
+        self.setResolution(int(dpi))
+
+    def set_page_margins(
+        self,
+        margins: datatypes.MarginsFType,
+        unit: gui.pagelayout.UnitStr | None = None,
+    ) -> bool:
+        if isinstance(margins, tuple):
+            margins = QtCore.QMarginsF(*margins)
+        if unit is None:
+            return self.setPageMargins(margins)
+        if unit not in gui.pagelayout.UNITS:
+            raise InvalidParamError(unit, gui.pagelayout.UNITS)
+        return self.setPageMargins(margins, gui.pagelayout.UNITS[unit])
+
+
+if __name__ == "__main__":
+    app = gui.app()
+    writer = PdfWriter("")
+    writer.set_desktop_resolution()
---
+++ packages/prettyqt/prettyqt-1.24.0.tar.gz/prettyqt-1.24.0/prettyqt/widgets/widgetaction.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from prettyqt import widgets
+from prettyqt.qt import QtCore, QtWidgets
+
+
+class WidgetAction(widgets.ActionMixin, QtWidgets.QWidgetAction):
+    def __init__(self, parent: QtCore.QObject | None = None):
+        super().__init__(parent)  # type: ignore
+        self._menu = None  # bc of PySide inheritance also defined here
+
+    #     self.set_text(text)
+    #     self.set_icon(icon)
+    #     self.set_shortcut(shortcut)
+    #     self.set_tooltip(tooltip)
+
+
+if __name__ == "__main__":
+    w = WidgetAction()
