cda7d973a437a73d52097bb80280e8f01532d01d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:15 +0000

    Add package pointclass/0.4/pointclass-0.4.tar.gz

---
+++ packages/pointclass/pointclass-0.4.tar.gz/pointclass-0.4/pointclass/__init__.py
@@ -0,0 +1,20 @@
+import math
+class point:
+    def __init__(self,x=0,y=0):
+        self.x=x
+        self.y=y
+    def show(self):
+        return self.x
+        return self.y
+    def __add__(self,p):
+        return(self.x+p.x,self.y+p.y)
+    def __sub__(self,p):
+        return(self.x-p.x,self.y-p.y)
+    def __mult__(self,p):
+        return(self.x * p.x,self.y * p.y)
+    def __eq__(self,p):
+         return(self.x==p.x,self.y==p.y)
+    def __repro__():
+        return "point(self.x,self.y)"
+    def distance(p1,p2):
+        return math.hypot(p2.x-p1.x,p2.y-p1.y)
