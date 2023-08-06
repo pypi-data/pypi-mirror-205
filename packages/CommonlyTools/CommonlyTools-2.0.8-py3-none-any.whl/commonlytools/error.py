146808ea64245a7898514740aa5e15bf9c62f081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:40 +0000

    Add package CommonlyTools/2.0.8/CommonlyTools-2.0.8-py3-none-any.whl

---
+++ packages/CommonlyTools/CommonlyTools-2.0.8-py3-none-any.whl/commonlytools/error.py
@@ -0,0 +1,10 @@
+def UrlError(Exception):
+    print("Please confirm that your link is correct!")
+
+
+def NoneMessageError(Exception):
+    print("You didn't provide the required information!")
+
+
+def ColourError(Exception):
+    print("Please confirm that the color you type is correct!")
