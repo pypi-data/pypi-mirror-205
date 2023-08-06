b52f289dacaa19663dddd8a47979f034d72d364d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:01 +0000

    Add package ansible-navigator/3.1.0/ansible-navigator-3.1.0.tar.gz

---
+++ packages/ansible-navigator/ansible-navigator-3.1.0.tar.gz/ansible-navigator-3.1.0/src/ansible_navigator/actions/back.py
@@ -0,0 +1,53 @@
+"""``:back`` command implementation.
+
+Additionally triggered by the escape key.
+"""
+import logging
+
+from ..app_public import AppPublic
+from ..configuration_subsystem.definitions import ApplicationConfiguration
+from ..steps import Step
+from ..ui_framework import Interaction
+from . import _actions as actions
+
+
+@actions.register
+class Action:
+    """``:back`` command implementation."""
+
+    KEGEX = r"^\^\[|\x1b|back$"
+
+    def __init__(self, args: ApplicationConfiguration):
+        """Initialize the ``:back`` action.
+
+        :param args: The current settings for the application
+        """
+        self._args = args
+        self._logger = logging.getLogger(__name__)
+
+    def run(self, interaction: Interaction, app: AppPublic) -> None:
+        """Execute the ``:back`` request.
+
+        :param interaction: The interaction from the user
+        :param app: The app instance
+        """
+        self._logger.debug("back requested")
+        interaction.ui.scroll(0)
+        interaction.ui.clear()
+        this = app.steps.back_one()  # pop this
+        step = app.steps.back_one()  # pop current
+
+        if app.steps:
+            if isinstance(step, Step) and isinstance(app.steps.current, Step):
+                if step.type == "menu" and app.steps.current.type == "menu":
+                    interaction.ui.menu_filter(None)
+            self._logger.debug(
+                "Stepping back in %s from %s to %s",
+                app.name,
+                step.name,
+                app.steps.current.name,
+            )
+        else:
+            self._logger.debug("Return to %s, at last step", app.name)
+
+        app.steps.append(this)  # put this back on
---
+++ packages/ansible-navigator/ansible-navigator-3.1.0.tar.gz/ansible-navigator-3.1.0/src/ansible_navigator/version.py
@@ -0,0 +1,14 @@
+"""Ansible-navigator version information."""
+try:
+    from ._version import version as __version__
+except ImportError:  # pragma: no branch
+    try:
+        from importlib.metadata import version
+
+        __version__ = version("ansible-navigator")
+    except Exception:  # pylint: disable=broad-except
+        # this is the fallback SemVer version picked by setuptools_scm when tag
+        # information is not available.
+        __version__ = "0.1.dev1"
+
+__all__ = ("__version__",)
