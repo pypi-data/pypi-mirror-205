b8fd7e951978c1622edace9e7cb52a682bc57ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:24 +0000

    Add package joes-giant-toolbox/0.1.0/joes_giant_toolbox-0.1.0.tar.gz

---
+++ packages/joes-giant-toolbox/joes_giant_toolbox-0.1.0.tar.gz/joes_giant_toolbox-0.1.0/joes_giant_toolbox/text.py
@@ -0,0 +1,6 @@
+"""Tools for working with text data"""
+
+from joes_giant_toolbox.all.longest_sentence_subsequence_plagiarism_detector import (
+    longest_sentence_subsequence_plagiarism_detector,
+)
+from joes_giant_toolbox.all.string_cleaner import StringCleaner
