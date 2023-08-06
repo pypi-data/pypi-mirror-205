08b3ff28f18093ccfb4c99b71139174aed7ac905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:16 +0000

    Add package amrendras/1.0.1/amrendras-1.0.1-py3-none-any.whl

---
+++ packages/amrendras/amrendras-1.0.1-py3-none-any.whl/amrendras/__init__.py
@@ -0,0 +1,2 @@
+from amrendras import amrendras
+    
---
+++ packages/amrendras/amrendras-1.0.1-py3-none-any.whl/amrendras/amrendras.py
@@ -0,0 +1,16 @@
+def amrendras():
+    print(
+'''
+Amrendra naam ab ek brand ban chuka hai.
+
+Uske naam se jo bhi chiz ban raha hai sab famous hai.
+
+aur ek chiz, agar aap abhi bhi code run karke ye chiz
+pad rahe ho tho aap legend ho, maan gaya mai. 
+
+'''
+        )
+
+    
+    
+    
