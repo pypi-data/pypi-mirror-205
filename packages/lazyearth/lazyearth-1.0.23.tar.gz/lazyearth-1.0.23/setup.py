68e41194cee64b7bb8c82eeb79771ec549f43837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:50 +0000

    Add package lazyearth/1.0.23/lazyearth-1.0.23.tar.gz

---
+++ packages/lazyearth/lazyearth-1.0.23.tar.gz/lazyearth-1.0.23/setup.py
@@ -0,0 +1,36 @@
+﻿import io
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+HERE = dirname(abspath(__file__))
+LOAD_TEXT = lambda name: io.open(join(HERE, name), encoding='UTF-8').read()
+DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
+    'README.rst'
+])
+
+setup(
+  name = 'lazyearth',                # Name project the same with folder
+  packages = ['lazyearth'],          # Name project the same with folder
+  version = '1.0.23',                 # version
+  license='MIT', 
+  description = 'Library for earth science',    #Show on PyPi
+  long_description=DESCRIPTION,
+  author = 'Tun Kedsaro',            #          
+  author_email = 'Tun.k@ku.th',      #
+  url = 'https://github.com/Tun555/lazyearth',  #
+  download_url = 'https://github.com/Tun555/lazyearth/archive/refs/tags/v0.0.15.zip',                                      #  
+  keywords = ['geo','ODC','earth'],      # When someone search
+  # Dont add any library bz It's gonna error waiting
+  install_requires=[                 # Package that use
+        'numpy',
+        'matplotlib'
+    ],  
+  classifiers=[    
+    'Development Status :: 1 - Planning',  
+    'Intended Audience :: Education', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Version pathon that we test    
+    'Programming Language :: Python :: 3.8',
+  ],
+)
