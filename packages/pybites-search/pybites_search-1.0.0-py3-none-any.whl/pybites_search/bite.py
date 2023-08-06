75048647173dfc872e4789680d8029e929f977a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:55 +0000

    Add package pybites-search/1.0.0/pybites_search-1.0.0-py3-none-any.whl

---
+++ packages/pybites-search/pybites_search-1.0.0-py3-none-any.whl/pybites_search/bite.py
@@ -0,0 +1,29 @@
+from .base import ContentPiece, PybitesSearch
+
+BITES_ENDPOINT = "https://codechalleng.es/api/bites/"
+PLATFORM_BASE_URL = "https://codechalleng.es/bites/"
+
+
+class BiteSearch(PybitesSearch):
+    def __init__(self) -> None:
+        self.title = "Pybites Bite Exercises"
+
+    def match_content(self, search: str) -> list[ContentPiece]:
+        entries = self.get_data(BITES_ENDPOINT)
+        results = []
+        for entry in entries:
+            if search.lower() in (entry["title"] + entry["description"]).lower():
+                results.append(
+                    ContentPiece(
+                        title=entry["title"],
+                        url=f"{PLATFORM_BASE_URL}{entry['number']}",
+                        channel=self.title,
+                    )
+                )
+        return results
+
+
+if __name__ == "__main__":
+    searcher = BiteSearch()
+    results = searcher.match_content("fastapi")
+    searcher.show_matches(results)
---
+++ packages/pybites-search/pybites_search-1.0.0-py3-none-any.whl/pybites_search/youtube.py
@@ -0,0 +1,29 @@
+from .base import ContentPiece, PybitesSearch
+
+YOUTUBE_ENDPOINT = "https://codechalleng.es/api/videos/"
+YOUTUBE_BASE_URL = "https://www.youtube.com/watch?v="
+
+
+class YouTubeSearch(PybitesSearch):
+    def __init__(self) -> None:
+        self.title = "Pybites YouTube Videos"
+
+    def match_content(self, search: str) -> list[ContentPiece]:
+        entries = self.get_data(YOUTUBE_ENDPOINT)
+        results = []
+        for entry in entries:
+            if search.lower() in (entry["title"] + entry["description"]).lower():
+                results.append(
+                    ContentPiece(
+                        title=entry["title"],
+                        url=YOUTUBE_BASE_URL + entry["video_id"],
+                        channel=self.title,
+                    )
+                )
+        return results
+
+
+if __name__ == "__main__":
+    searcher = YouTubeSearch()
+    results = searcher.match_content("django")
+    searcher.show_matches(results)
