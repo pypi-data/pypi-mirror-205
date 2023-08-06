ad563869836cf90824f62a1ff7777498f39c4ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:22 +0000

    Add package uib-experiments/1.6.1/uib_experiments-1.6.1.tar.gz

---
+++ packages/uib-experiments/uib_experiments-1.6.1.tar.gz/uib_experiments-1.6.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+LONG_DESCRIPTION = "Package to handle scientific experiments easily"
+
+setup(name='uib_experiments',
+      version='1.6.1',
+      description='Handle the experiments.',
+      long_description=LONG_DESCRIPTION,
+      url='https://github.com/miquelmn/uib_experiments',
+      author='Miquel Miró Nicolau, Dr. Gabriel Moyà Alcover',
+      author_email='miquel.miro@uib.cat, gabriel_moya@uib.es',
+      download_url="https://github.com/miquelmn/uib_experiments/archive/refs/tags/v0.9.2.tar.gz",
+      packages=find_packages(),
+      install_requires=[
+          'telegram_send',
+          'opencv-python',
+          'matplotlib',
+          'scipy',
+          'numpy',
+          'peewee'
+      ],
+      zip_safe=False)
