550f4f3a08479017b5b2ae6844eef8cc7e10cb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:34 +0000

    Add package mentpy/0.1.0a4/mentpy-0.1.0a4-py3-none-any.whl

---
+++ packages/mentpy/mentpy-0.1.0a4-py3-none-any.whl/mentpy/mbqc/states/cluster.py
@@ -0,0 +1,7 @@
+# TODO: Add AKLT state class inheritance from graph state
+import networkx as nx
+
+
+class ClusterState(nx.Graph):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
