f7cdc8356b6fd0d1fa2a1438bb4c45b152cf6421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:50 +0000

    Add package bthome-ble/2.10.1/bthome_ble-2.10.1.tar.gz

---
+++ packages/bthome-ble/bthome_ble-2.10.1.tar.gz/bthome_ble-2.10.1/src/bthome_ble/__init__.py
@@ -0,0 +1,34 @@
+"""Parser for BLE advertisements in BTHome format."""
+from __future__ import annotations
+
+from sensor_state_data import (
+    BinarySensorDeviceClass,
+    DeviceClass,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceClass,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .const import SLEEPY_BINARY_SENSORS, SLEEPY_SENSORS
+from .parser import BTHomeBluetoothDeviceData
+
+__version__ = "2.10.1"
+
+__all__ = [
+    "SLEEPY_BINARY_SENSORS",
+    "SLEEPY_SENSORS",
+    "BinarySensorDeviceClass",
+    "BTHomeBluetoothDeviceData",
+    "DeviceClass",
+    "DeviceKey",
+    "SensorDescription",
+    "SensorDeviceClass",
+    "SensorDeviceInfo",
+    "SensorUpdate",
+    "SensorValue",
+    "Units",
+]
