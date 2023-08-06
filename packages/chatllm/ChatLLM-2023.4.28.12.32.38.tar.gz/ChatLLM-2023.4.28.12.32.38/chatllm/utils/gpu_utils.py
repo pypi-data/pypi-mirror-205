844de095fc3886a15cce7fe3470e203378ad44b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:59 +0000

    Add package chatllm/2023.4.28.12.32.38/ChatLLM-2023.4.28.12.32.38.tar.gz

---
+++ packages/chatllm/ChatLLM-2023.4.28.12.32.38.tar.gz/ChatLLM-2023.4.28.12.32.38/chatllm/utils/gpu_utils.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : muti_gpu
+# @Time         : 2023/4/28 12:16
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 多卡 训练 多卡加载
+
+from meutils.pipe import *
