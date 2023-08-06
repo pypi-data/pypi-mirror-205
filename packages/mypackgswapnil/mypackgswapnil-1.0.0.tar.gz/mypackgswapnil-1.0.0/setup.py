ac512392833e9c999709bc194509c4d62d3fcaaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:45 +0000

    Add package mypackgswapnil/1.0.0/mypackgswapnil-1.0.0.tar.gz

---
+++ packages/mypackgswapnil/mypackgswapnil-1.0.0.tar.gz/mypackgswapnil-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='mypackgswapnil',
+    version='1.0.0',
+    description='A short description of my package',
+    packages=['bookcount'],
+    install_requires=[
+        'requests>=2.25.1',
+        'numpy>=1.19.5',
+        
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    url='https://github.com/my_username/my_package',
+    author='Swapnil',
+    author_email='swapnild333@gmail.com'
+)
