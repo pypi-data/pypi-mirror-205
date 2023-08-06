ff795463381cf17ed5ecaf3bf768c2d529824bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:30 +0000

    Add package airpt2/1.0.0/airpt2-1.0.0-py2.py3-none-any.whl

---
+++ packages/airpt2/airpt2-1.0.0-py2.py3-none-any.whl/airpt2/airpt.py
@@ -0,0 +1,8 @@
+airport_codes = {
+        "LAX": "Los Angeles International Airport",
+        "JFK": "John F. Kennedy International Airport",
+        "ORD": "Chicago O'Hare International Airport",
+        # Add more airport codes and names as needed
+    }
+def airport_code():
+    return airport_codes
