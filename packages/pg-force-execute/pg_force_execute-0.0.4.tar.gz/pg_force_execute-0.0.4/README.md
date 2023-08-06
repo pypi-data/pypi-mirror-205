366779192369c5891a5a4ca35c860566131a8bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:06 +0000

    Add package pg-force-execute/0.0.4/pg_force_execute-0.0.4.tar.gz

---
+++ packages/pg-force-execute/pg_force_execute-0.0.4.tar.gz/pg_force_execute-0.0.4/README.md
@@ -0,0 +1,66 @@
+# pg-force-execute
+
+Utility function to run a PostgreSQL query with SQLAlchemy, terminating any other clients that continue to block it after a configurable delay.
+
+Using this function to run queries is somewhat of a last resort, but is useful in certain Extract Transform Load (ETL) pipeline contexts. For example, if it is more important to replace one table with another than to allow running queries on the table to complete, then this function can be used to run the relevant `ALTER TABLE RENAME TO` query.
+
+
+## Installation
+
+```bash
+pip install pg-force-execute
+```
+
+
+## Example usage
+
+```python
+import datetime
+import sqlalchemy as sa
+from pg_force_execute import pg_force_execute
+
+# Run postgresql locally should allow the below to run
+# docker run --rm -it -e POSTGRES_HOST_AUTH_METHOD=trust -p 5432:5432 postgres
+
+engine = sa.create_engine('postgresql://postgres@127.0.0.1:5432/')
+query = 'SELECT 1'  # A more realistic example would be something that needs an exclusive lock on a table
+
+with engine.begin() as conn:
+    results = pg_force_execute(
+        sa.text(query), # SQL query to execute
+        conn,           # SQLAlchemy connection to run the query
+        engine,         # SQLAlchemy engine that will create new connections to cancel blocking queries
+        delay=datetime.timedelta(minutes=5),  # Amount of time to wait before cancelling queries
+    )
+    print(results.fetchall())
+```
+
+
+## API
+
+The API a single function `pg_force_execute`.
+
+`pg_force_execute`(query, conn, engine, delay=datetime.timedelta(minutes=5), check_interval=datetime.timedelta(seconds=1), termination_thread_timeout=datetime.timedelta(seconds=10), logger=logging.getLogger("pg_force_execute"))
+
+- `query` - A SQLAlchemy text instance of the query to run
+
+- `conn` - A SQLAlchemy connection to run `query` on
+
+- `engine` - A SQLAlchemy engine to create a new connection that will be used to terminate backends blocking `query`
+
+- `delay` (optional) - How long to wait before attempting to terminate backends blocking `query`
+
+- `check_interval` (optional) - The interval between repeated attempted to terminate backends blocking `query`
+
+- `termination_thread_timeout` (optional) - How long to wait for the termination to complete
+
+- `logger` (optional) The Python logger instance through which to log
+
+
+## Running tests locally
+
+```bash
+pip install -e ".[dev]"  # Only needed once
+./start-services.sh      # Only needed once
+pytest
+```
---
+++ packages/pg-force-execute/pg_force_execute-0.0.4.tar.gz/pg_force_execute-0.0.4/pg_force_execute.py
@@ -0,0 +1,66 @@
+import datetime
+import logging
+from threading import Event, Thread
+
+import sqlalchemy as sa
+
+
+def pg_force_execute(query, conn, engine,
+                     delay=datetime.timedelta(minutes=5),
+                     check_interval=datetime.timedelta(seconds=1),
+                     termination_thread_timeout=datetime.timedelta(seconds=10),
+                     logger=logging.getLogger("pg_force_execute"),
+):
+    cancel_exception = None
+
+    def force_unblock(pid, exit):
+        nonlocal cancel_exception
+
+        try:
+            exit.wait(timeout=(delay - check_interval).total_seconds())
+
+            # Repeatedly check for other backends that block `query`, and cancel them if
+            # they are. The repeat check is to avoid race conditions - if another backend
+            # blocks this backend just after pg_blocking_pids returns its PIDs. If it's
+            # determined that PostgreSQL forbids this case the looping can be removed
+            while not exit.wait(timeout=check_interval.total_seconds()):
+                logger.info('Cancelling queries blocking PID %s running %s', pid, query)
+                with engine.begin() as conn:
+                    # pg_cancel_backend isn't strong enough - the blocking PIDs might not be
+                    # actually running a query, so there is nothing to cancel. They might
+                    # just be holding locks. To force release of the locks, we have to call
+                    # pg_terminate_backend
+                    cancelled_queries = conn.execute(
+                        sa.text("""
+                            SELECT
+                                activity.usename AS usename,
+                                activity.query AS query,
+                                age(clock_timestamp(), activity.query_start) AS age,
+                                pg_terminate_backend(pids.pid)
+                            FROM
+                                UNNEST(pg_blocking_pids(:pid)) AS pids(pid)
+                            INNER JOIN
+                                pg_stat_activity activity ON activity.pid = pids.pid;
+                        """), {"pid": pid},
+                    ).fetchall()
+                    if not cancelled_queries:
+                        logger.info('No blocking queries to cancel')
+                    for cancelled_query in cancelled_queries:
+                        logger.error('Cancelled blocking query %s', cancelled_query)
+        except Exception as e:
+            cancel_exception = e
+
+    pid = conn.execute(
+        sa.text('SELECT pg_backend_pid()')
+    ).fetchall()[0][0]
+    exit = Event()
+    t = Thread(target=force_unblock, args=(pid, exit))
+    t.start()
+
+    try:
+        return conn.execute(query)
+    finally:
+        exit.set()
+        t.join(timeout=termination_thread_timeout.total_seconds())
+        if cancel_exception is not None:
+            raise cancel_exception
