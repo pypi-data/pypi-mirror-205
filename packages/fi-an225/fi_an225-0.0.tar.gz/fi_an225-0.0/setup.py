b49489c698338a5baad091c972ad28b61eeb80b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:42 +0000

    Add package fi-an225/0.0/fi_an225-0.0.tar.gz

---
+++ packages/fi-an225/fi_an225-0.0.tar.gz/fi_an225-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fi_an225',
+      version='0.0',
+      description='circle, tiangle, square',
+      packages=['fi_an225.circle', 'fi_an225.triangle', 'fi_an225.square'],
+      author_email='alexeykozhakin@gmail.com',
+      zip_safe=False)
