cbe2b1efbd5250ad1920ec466ded693f09ca2d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:49 +0000

    Add package terrace/0.0.77/terrace-0.0.77.tar.gz

---
+++ packages/terrace/terrace-0.0.77.tar.gz/terrace-0.0.77/src/terrace/categorical_tensor.py
@@ -0,0 +1,163 @@
+from copy import deepcopy
+from typing import Tuple, Union
+import functools
+import torch
+
+from torch.utils._pytree import tree_map
+
+# https://pytorch.org/docs/stable/notes/extending.html
+
+# https://colab.research.google.com/drive/1MLeSCMrc6a5Yf_6uAh0bH-IPf5c_BTQf#scrollTo=dM0sl2X6epBX
+
+NumClassesType = Union[int, Tuple[int, ...]]
+
+HANDLED_FUNCTIONS = {}
+# if num_classes is a tuple, it decribes the number of classes along the last
+# dimension of the tensor
+class CategoricalTensor(torch.Tensor):
+    @staticmethod
+    def __new__(cls, tensor: torch.Tensor, num_classes: NumClassesType):
+        return torch.Tensor._make_subclass(cls, tensor.to('meta'))
+
+    def __init__(self, tensor, num_classes: NumClassesType):
+        assert tensor.dtype == torch.long
+        if isinstance(num_classes, int):
+            num_classes = tuple([num_classes]*tensor.shape[-1])
+        assert len(num_classes) == tensor.shape[-1]
+        self.tensor = tensor
+        self.num_classes = num_classes
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(tensor={self.tensor}, num_classes={self.num_classes})"
+
+    def __getstate__(self):
+        return (self.tensor, self.num_classes)
+
+    def __setstate__(self, state):
+        self.tensor, self.num_classes = state
+
+    # Only difference with above is to add this function.
+    @classmethod
+    def __torch_function__(cls, func, types, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = {}
+        if func in HANDLED_FUNCTIONS:
+            return HANDLED_FUNCTIONS[func](*args, **kwargs)
+        # return func(*args, **kwargs)
+        print(func)
+        raise NotImplementedError(func)
+
+class NoStackCatTensor(CategoricalTensor):
+    pass
+
+
+def implements(torch_function):
+    """Register a torch function override for ScalarTensor"""
+    @functools.wraps(torch_function)
+    def decorator(func):
+        HANDLED_FUNCTIONS[torch_function] = func
+        return func
+    return decorator
+
+@implements(torch.Tensor.__deepcopy__)
+def ct_deepcopy(ct, memo):
+    return CategoricalTensor(deepcopy(ct.tensor, memo), deepcopy(ct.num_classes, memo))
+
+@implements(torch.split)
+def split(ct, *args, **kwargs):
+    """ No error checking for now"""
+    ret = torch.split(ct.tensor, *args, **kwargs)
+    return [ CategoricalTensor(t, ct.num_classes) for t in ret ]
+
+@implements(torch.Tensor.__len__)
+def cat_tensor_len(ct):
+    return len(ct.tensor)
+
+@implements(torch.Tensor.is_sparse.__get__)
+def cat_is_sparse(ct):
+    return ct.tensor.is_sparse
+
+@implements(torch.Tensor.storage)
+def cat_storage(ct):
+    return ct.tensor.storage()
+
+@implements(torch.Tensor.element_size)
+def cat_element_size(ct):
+    return ct.tensor.element_size()
+
+@implements(torch.Tensor.size)
+def cat_size(ct, *args, **kwargs):
+    return ct.tensor.size(*args, **kwargs)
+    
+@implements(torch.Tensor.numel)
+def cat_numel(ct):
+    return ct.tensor.numel()
+
+@implements(torch.Tensor.cuda)
+def cuda(ct):
+    return CategoricalTensor(ct.tensor.cuda(), num_classes=ct.num_classes)
+
+@implements(torch.Tensor.cpu)
+def cpu(ct):
+    return CategoricalTensor(ct.tensor.cpu(), num_classes=ct.num_classes)
+
+@implements(torch.Tensor.to)
+def to(ct, device):
+    return CategoricalTensor(ct.tensor.to(device), num_classes=ct.num_classes)
+
+def construct_cat_tensor(*args):
+    num_classes, (tensor_func, tensor_args) = args
+    tensor = tensor_func(*tensor_args)
+    return CategoricalTensor(tensor, num_classes=num_classes)
+
+@implements(torch.Tensor.__reduce_ex__)
+def cat_reduce_ex(self, proto):
+    return (construct_cat_tensor, (self.num_classes, self.tensor.__reduce_ex__(proto)))
+
+@implements(torch.cat)
+def cat(inputs: Tuple[CategoricalTensor, ...], dim: int = -1) -> CategoricalTensor:
+    if dim < 0:
+        dim = len(inputs[0].shape) - dim
+    if dim == len(inputs[0].shape):
+        num_classes = []
+        for t in inputs:
+            num_classes += t.num_classes
+    else:
+        num_classes = inputs[0].num_classes
+        for inp in inputs:
+            assert inp.num_classes == num_classes
+    tensor = torch.cat([t.tensor for t in inputs], dim)
+    return CategoricalTensor(tensor, num_classes)
+
+@implements(torch.stack)
+def stack(inputs: Tuple[CategoricalTensor, ...]):
+    assert len(inputs) > 0
+    num_classes = inputs[0].num_classes
+    for t in inputs:
+        assert t.num_classes == num_classes
+    return CategoricalTensor(torch.stack([t.tensor for t in inputs]), num_classes)
+
+@implements(torch.Tensor.__getitem__)
+def getitem(t: CategoricalTensor, idx: int):
+    num_classes = t.num_classes
+    if isinstance(idx, tuple):
+        if len(idx) == len(t.shape):
+            num_classes = t.num_classes[idx[-1]]
+    else:
+        if len(t.shape) == 1:
+            num_classes = t.num_classes[idx]
+    if isinstance(num_classes, int):
+        num_classes = [ num_classes ]
+    return CategoricalTensor(t.tensor[idx], num_classes)
+
+@implements(torch.Tensor.shape.__get__)
+def get_shape(t: CategoricalTensor):
+    return t.tensor.shape
+
+@implements(torch.Tensor.device.__get__)
+def get_device(t: CategoricalTensor):
+    return t.tensor.device
+
+@implements(torch.Tensor.dtype.__get__)
+def get_dtype(t: CategoricalTensor):
+    return t.tensor.dtype
---
+++ packages/terrace/terrace-0.0.77.tar.gz/terrace-0.0.77/src/terrace/module.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn as nn
+
+from .categorical_tensor import CategoricalTensor
+
+class Module(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self._initialized = False
+        self._started_forward = False
+        self._submodule_index = 0
+        self._submodules = nn.ModuleList()
+        self._submodule_list = []
+        self._param_index = 0
+        self._params = nn.ParameterList()
+        self._checkpoints = []
+        self._checkpoint_index = 0
+
+    def start_forward(self):
+        # todo: these boolean values getting unweildly -- cut down!
+        if self.__dict__["_submodule_index"] != 0:
+            self.__dict__["_initialized"] = True
+        self.__dict__["_submodule_index"] = 0
+        self.__dict__["_param_index"] = 0
+        self.__dict__["_started_forward"] = True
+        self.__dict__["_checkpoint_index"] = 0
+
+    def make(self, cls, *args, **kwargs):
+        if not self.__dict__["_started_forward"]:
+            raise RuntimeError("You must call start_forward before you call make")
+        if not self.__dict__["_initialized"]:
+            submod = cls(*args, **kwargs)
+            self._submodules.append(submod)
+            self._submodule_list.append(submod)
+        submod = self.__dict__["_submodule_list"][self.__dict__["_submodule_index"]]
+        self.__dict__["_submodule_index"] += 1
+        return submod
+
+    def make_param(self, cls, *args, **kwargs):
+        if not self._started_forward:
+            raise RuntimeError("You must call start_forward before you call make_param")
+        if not self._initialized:
+            self._params.append(cls(*args, **kwargs))
+        param = self._params[self._param_index]
+        self._param_index+= 1
+        return param
+
+    def checkpoint(self):
+        if self._initialized:
+            self._submodule_index, self._param_index = self._checkpoints[self._checkpoint_index]
+            self._checkpoint_index += 1
+        else:
+            self._checkpoints.append((self._submodule_index, self._param_index))
+
+    def loop_start(self):
+        raise NotImplementedError
+
+    def loop_body(self):
+        raise NotImplementedError
+
+    def is_initialized(self):
+        return self._initialized or self._submodule_index > 0 or self._param_index > 0
+
+    def parameters(self, recurse: bool = True):
+        assert self.is_initialized(), "Terrace Module needs to be run on data before parameters method can be called"
+        return super().parameters(recurse)
+
+class WrapperModule(Module):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.args = args
+        self.kwargs = kwargs
+
+class LazyLinear(WrapperModule):
+    """ torch >=1.13 has this, but wanted to try my hand at implementing it myself
+    (and ensuring that older torch versions work) """
+
+    def forward(self, x):
+        self.start_forward()
+        in_feats = x.shape[-1]
+        return self.make(nn.Linear, in_feats, *self.args, **self.kwargs)(x)
+
+
+class LazyEmbedding(Module):
+
+    def __init__(self, embedding_dims):
+        super().__init__()
+        self.embedding_dims = embedding_dims
+
+    def forward(self, x: CategoricalTensor):
+        self.start_forward()
+        embedding_dims = [self.embedding_dims]*len(x.num_classes) if isinstance(self.embedding_dims, int) else self.embedding_dims
+        ret = []
+        for idx in range(x.shape[-1]):
+            max_val = x.num_classes[idx]
+            ret.append(self.make(nn.Embedding, max_val, embedding_dims[idx])(x.tensor[..., idx]))
+        return torch.cat(ret, -1)
+
+class LazyMultiheadAttention(WrapperModule):
+
+    def forward(self, q, k, v):
+        self.start_forward()
+        embed_dim = q.shape[-1]
+        self.kwargs["kdim"] = k.shape[-1]
+        self.kwargs["vdim"] = v.shape[-1]
+        return self.make(nn.MultiheadAttention, embed_dim, *self.args, **self.kwargs)(q, k, v)
+
+class LazyLayerNorm(WrapperModule):
+
+    def forward(self, x):
+        self.start_forward()
+        return self.make(nn.LayerNorm, x.shape[1:], *self.args, **self.kwargs)(x)
