09c025250aff4bb6214a6b8953ab08441e9bcac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:09 +0000

    Add package acnaweblib/0.1.18/acnaweblib-0.1.18.tar.gz

---
+++ packages/acnaweblib/acnaweblib-0.1.18.tar.gz/acnaweblib-0.1.18/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+REQUIREMENTS = []
+SETUP_REQUIRES = ['flake8', 'pytest-runner']
+
+setup(
+    name='acnaweblib',
+    version='0.1.18',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author='Antonio Carlos de Lima Junior',
+    author_email='antonioclj.ac@gmail.com',
+    url='https://github.com/acnaweb/acnaweb-lib',
+    packages=find_packages(include=['acnaweblib', 'acnaweblib.*']),
+    install_requires=REQUIREMENTS,
+    setup_requires=SETUP_REQUIRES,
+    tests_require=['pytest'],
+    classifiers=[               
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',        
+        'Operating System :: OS Independent',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Natural Language :: English',
+
+      ],
+    keywords='library test',
+    license='MIT',
+)
