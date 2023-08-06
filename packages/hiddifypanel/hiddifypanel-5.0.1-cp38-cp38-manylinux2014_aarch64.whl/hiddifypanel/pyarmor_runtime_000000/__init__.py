4327ad4fdcc006cb5bf49afb8f2cfcef1eeec2a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:59 +0000

    Add package hiddifypanel/5.0.1/hiddifypanel-5.0.1-cp38-cp38-manylinux2014_aarch64.whl

---
+++ packages/hiddifypanel/hiddifypanel-5.0.1-cp38-cp38-manylinux2014_aarch64.whl/hiddifypanel/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,4 @@
+#  2023-04-26T23:19:59.629577
+from .pyarmor_runtime import __pyarmor__
+#واقعا برای 5 دلار میخوای کرک میکنی؟ حاجی ارزش وقت خودت بیشتره
+#Email hiddify@gmail.com for free permium version. Do not crack this cheap product
