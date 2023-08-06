1c2abb55cbc2336e84d41b94260463ab4ba897eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:15 +0000

    Add package pyunique/0.0.10/pyunique-0.0.10-py3-none-any.whl

---
+++ packages/pyunique/pyunique-0.0.10-py3-none-any.whl/pyunique/static.py
@@ -0,0 +1,5 @@
+""" version which can be consumed from within the module """
+VERSION_STR = "0.0.10"
+DESCRIPTION = "Pyunique helps you get rid of duplicate files"
+APP_NAME = "pyunique"
+LOGGER_NAME = "pyunique"
