02be8cd93e7a8ff6e22416746b8823ae6403af1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:44 +0000

    Add package lockstep-sdk/2023.17.21/lockstep-sdk-2023.17.21.tar.gz

---
+++ packages/lockstep-sdk/lockstep-sdk-2023.17.21.tar.gz/lockstep-sdk-2023.17.21/src/lockstep/errorresult.py
@@ -0,0 +1,29 @@
+#
+# Lockstep Platform SDK for Python
+#
+# (c) 2021-2023 Lockstep, Inc.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @author     Lockstep Network <support@lockstep.io>
+# @copyright  2021-2023 Lockstep, Inc.
+# @link       https://github.com/Lockstep-Network/lockstep-sdk-python
+#
+
+
+from dataclasses import dataclass
+
+@dataclass
+class ErrorResult:
+    """
+    Represents a failed API request.
+    """
+
+    type: str | None = None
+    title: str | None = None
+    status: int | None = None
+    detail: str | None = None
+    instance: str | None = None
+    content: str | None = None
+
