8d362a13cb0313b8b95bff1f6d35763fac310f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:33 +0000

    Add package spectrum-utils/0.4.2/spectrum_utils-0.4.2-py3-none-any.whl

---
+++ packages/spectrum-utils/spectrum_utils-0.4.2-py3-none-any.whl/spectrum_utils/__init__.py
@@ -0,0 +1,24 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+
+    try:
+        __version__ = version(__name__)
+    except PackageNotFoundError:
+        pass
+except ImportError:
+    from pkg_resources import get_distribution, DistributionNotFound
+
+    try:
+        __version__ = get_distribution(__name__).version
+    except DistributionNotFound:
+        pass
+
+
+__all__ = [
+    "fragment_annotation",
+    "iplot",
+    "plot",
+    "proforma",
+    "spectrum",
+    "utils",
+]
