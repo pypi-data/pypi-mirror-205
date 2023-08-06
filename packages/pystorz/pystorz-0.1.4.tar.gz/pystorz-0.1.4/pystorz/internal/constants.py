c67de9ce270c121a459e4aeffd6820c448478b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:42 +0000

    Add package pystorz/0.1.4/pystorz-0.1.4.tar.gz

---
+++ packages/pystorz/pystorz-0.1.4.tar.gz/pystorz-0.1.4/pystorz/internal/constants.py
@@ -0,0 +1,9 @@
+
+ErrObjectNil = "object is nil"
+ErrInvalidMethod = "method not allowed"
+ErrObjectExists = "object already exists"
+ErrNoSuchObject = "object does not exist"
+ErrInvalidFilter = "invalid filter key"
+ErrInvalidPath = "invalid request path"
+
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
