51affcf90760cad7f0432c46c0aeb615f373004a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:25 +0000

    Add package sqlmath/0.0.1/sqlmath-0.0.1.tar.gz

---
+++ packages/sqlmath/sqlmath-0.0.1.tar.gz/sqlmath-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='sqlmath',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='me',
+    author_email='me@example.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
