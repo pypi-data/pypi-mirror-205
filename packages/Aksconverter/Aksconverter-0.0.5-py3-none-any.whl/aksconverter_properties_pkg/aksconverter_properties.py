2cf90a83aa3c27250c061faa3ac7f8338aee5978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:18 +0000

    Add package Aksconverter/0.0.5/Aksconverter-0.0.5-py3-none-any.whl

---
+++ packages/Aksconverter/Aksconverter-0.0.5-py3-none-any.whl/aksconverter_properties_pkg/aksconverter_properties.py
@@ -0,0 +1,10 @@
+import requests
+
+def aksconverter(base , to , amount):
+		url = "https://anyapi.io/api/v1/exchange/convert?apiKey=pg2hovi5anohf0unsi656obkus8dfi43o9a6t6e735sg8v5p187n33&base="+base+"&to="+to+"&amount="+amount
+		payload = {}
+		headers = {}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		print(response.text)
+		return response.text
+
