19aa6a5b8d6a696e5c57640d85604934a4c79dcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:03 +0000

    Add package openz/0.1.1/openz-0.1.1-py3-none-any.whl

---
+++ packages/openz/openz-0.1.1-py3-none-any.whl/openz/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# 
+# Copyright (c) 2023~2999 - Cologler <skyoflw@gmail.com>
+# ----------
+# 
+# ----------
+
+from ._core import open_for_write, try_rollback
+
+__all__ = [
+    'open_for_write',
+    'try_rollback'
+]
