755c67b85c83615e4ec804545b8e256c51712dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:44 +0000

    Add package jmcomic/1.8.0/jmcomic-1.8.0-py3-none-any.whl

---
+++ packages/jmcomic/jmcomic-1.8.0-py3-none-any.whl/jmcomic/api.py
@@ -0,0 +1,147 @@
+from .jm_option import *
+
+
+def download_album(jm_album_id, option=None):
+    """
+    下载一个本子集，入口api
+    @param jm_album_id: 禁漫的本子的id，类型可以是str/int/iterable[str]。
+    如果是iterable[str]，则会调用批量下载方法 download_album_batch
+    @param option: 下载选项，为空默认是 JmOption.default()
+    """
+
+    if not isinstance(jm_album_id, (str, int)):
+        return download_album_batch(jm_album_id, option)
+
+    option, jm_client = build_client(option)
+    album_detail: JmAlbumDetail = jm_client.get_album_detail(jm_album_id)
+
+    jm_debug('download_album',
+             f'获得album_detail成功，准备下载。'
+             f'本子作者是【{album_detail.author}】，一共有{len(album_detail)}集本子')
+
+    def download_photo(index: int,
+                       photo_detail: JmPhotoDetail,
+                       debug_topic='download_album_photo',
+                       ):
+        jm_client.ensure_photo_can_use(photo_detail)
+
+        jm_debug(debug_topic,
+                 f"下载第[{index + 1}]集: "
+                 f"图片数为[{len(photo_detail)}]，"
+                 f"标题为：({photo_detail.title}) "
+                 f"-- photo {photo_detail.photo_id}")
+
+        download_by_photo_detail(
+            photo_detail,
+            option,
+        )
+
+        jm_debug(debug_topic,
+                 f"下载完成：({photo_detail.title}) "
+                 f"-- photo {photo_detail.photo_id}")
+
+    thread_pool_executor(
+        iter_objs=enumerate(album_detail),
+        apply_each_obj_func=download_photo,
+    )
+
+
+def download_album_batch(jm_album_id_iter: Union[Iterable, Generator],
+                         option=None,
+                         wait_finish=True,
+                         ) -> List[Thread]:
+    """
+    批量下载album，每个album一个线程，使用的是同一个option。
+
+    @param jm_album_id_iter: album_id的可迭代对象
+    @param option: 下载选项，为空默认是 JmOption.default()
+    @param wait_finish: 是否要等待这些下载线程全部完成
+    @return 返回值是List[Thread]，里面是每个下载漫画的线程。
+    """
+    if option is None:
+        option = JmOption.default()
+
+    return thread_pool_executor(
+        iter_objs=((album_id, option) for album_id in jm_album_id_iter),
+        apply_each_obj_func=download_album,
+        wait_finish=wait_finish,
+    )
+
+
+def download_photo(jm_photo_id, option=None):
+    """
+    下载一个本子的一章，入口api
+    """
+    option, jm_client = build_client(option)
+    photo_detail = jm_client.get_photo_detail(jm_photo_id)
+    download_by_photo_detail(photo_detail, option)
+
+
+def download_by_photo_detail(photo_detail: JmPhotoDetail,
+                             option=None,
+                             ):
+    """
+    下载一个本子的一章，根据 photo_detail
+    @param photo_detail: 本子章节信息
+    @param option: 选项
+    """
+    option, jm_client = build_client(option)
+
+    # 下载准备
+    use_cache = option.download_use_disk_cache
+    decode_image = option.download_image_then_decode
+    jm_client.ensure_photo_can_use(photo_detail)
+
+    # 下载每个图片的函数
+    def download_image(index, img_detail, debug_topic='download_images_of_photo'):
+        img_save_path = option.decide_image_filepath(photo_detail, index)
+
+        # 已下载过，缓存命中
+        if use_cache is True and file_exists(img_save_path):
+            jm_debug(debug_topic, f'photo-{img_detail.aid}: '
+                                  f'图片{img_detail.filename}已下载过，'
+                                  f'命中磁盘缓存（{img_save_path}）')
+            return
+
+        # 开始下载
+        jm_client.download_by_image_detail(
+            img_detail,
+            img_save_path,
+            decode_image=decode_image,
+        )
+
+        jm_debug(debug_topic, f'photo-{img_detail.aid}: '
+                              f'图片{img_detail.filename}下载完成：'
+                              f'[{img_detail.img_url}] → [{img_save_path}]')
+
+    length = len(photo_detail)
+    # 根据图片数，决定下载策略
+    if length <= option.download_multi_thread_photo_len_limit:
+        # 如果图片数小的话，直接使用多线程下载，一张图一个线程。
+        multi_thread_launcher(
+            iter_objs=enumerate(photo_detail),
+            apply_each_obj_func=download_image,
+        )
+    else:
+        # 如果图片数多的话，还是分批下载。
+        multi_task_launcher_batch(
+            iter_objs=enumerate(photo_detail),
+            apply_each_obj_func=download_image,
+            batch_size=option.download_multi_thread_photo_batch_count
+        )
+
+
+def build_client(option: Optional[JmOption]) -> Tuple[JmOption, JmcomicClient]:
+    """
+    处理option的判空，并且创建jm_client
+    """
+    if option is None:
+        option = JmOption.default()
+
+    jm_client = option.build_jm_client()
+    return option, jm_client
+
+
+def create_option(filepath: str) -> JmOption:
+    option = JmOption.create_from_file(filepath)
+    return option
---
+++ packages/jmcomic/jmcomic-1.8.0-py3-none-any.whl/jmcomic/jm_config.py
@@ -0,0 +1,107 @@
+class JmModuleConfig:
+    # 网站相关
+    PROT = "https://"
+    _DOMAIN = None
+    JM_REDIRECT_URL = f'{PROT}jm365.xyz/3YeBdF'  # 永久網域，怕走失的小伙伴收藏起来
+    JM_PUB_URL = f'{PROT}jmcomic1.bet'
+    JM_CDN_IMAGE_URL_TEMPLATE = PROT + 'cdn-msp.{domain}/media/photos/{photo_id}/{index:05}{suffix}'  # index 从1开始
+    JM_IMAGE_SUFFIX = ['.jpg', '.webp', '.png', '.gif']
+
+    # 访问JM可能会遇到的异常网页
+    JM_ERROR_RESPONSE_HTML = {
+        "Could not connect to mysql! Please check your database settings!": "禁漫服务器内部报错",
+        "Restricted Access!": "禁漫拒绝你所在ip地区的访问，你可以选择: 换域名/换代理",
+    }
+
+    # 图片分隔相关
+    SCRAMBLE_0 = 220980
+    SCRAMBLE_10 = 268850
+    SCRAMBLE_NUM_8 = 421926  # 2023-02-08后改了图片切割算法
+
+    # 下载时的一些默认值
+    default_author = 'default-author'
+    default_photo_title = 'default-photo-title'
+    default_photo_id = 'default-photo-id'
+
+    # debug
+    enable_jm_debug = True
+    debug_printer = print
+
+    # 缓存
+    jm_client_caches = {}
+
+    @classmethod
+    def domain(cls, postman=None):
+        """
+        由于禁漫的域名经常变化，调用此方法可以获取一个当前可用的最新的域名 domain，
+        并且设置把 domain 设置为禁漫模块的默认域名。
+        这样一来，配置文件也不用配置域名了，一切都在运行时动态获取。
+        """
+        if cls._DOMAIN is None:
+            cls._DOMAIN = cls.get_jmcomic_url(postman).replace(cls.PROT, '')
+
+        return cls._DOMAIN  # jmcomic默认域名
+
+    @classmethod
+    def headers(cls, authority=None):
+        return {
+            'authority': authority or cls.domain(),
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
+                      'application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 '
+                          'Safari/537.36',
+        }
+
+    # noinspection PyUnusedLocal
+    @classmethod
+    def jm_debug(cls, topic: str, msg: str, from_class=None):
+        if cls.enable_jm_debug is True:
+            cls.debug_printer(f'【{topic}】{msg}')
+
+    @classmethod
+    def disable_jm_debug(cls):
+        cls.enable_jm_debug = False
+
+    @classmethod
+    def get_jmcomic_url(cls, postman=None):
+        """
+        访问禁漫的永久网域，从而得到一个可用的禁漫网址
+        @return: https://jm-comic2.cc
+        """
+        if postman is None:
+            from common import Postmans
+            postman = Postmans.get_impl_clazz('cffi_Session').create()
+
+        url = postman.with_redirect_catching().get(cls.JM_REDIRECT_URL)
+        cls.jm_debug('获取禁漫地址', f'[{cls.JM_REDIRECT_URL}] → [{url}]')
+        return url
+
+    @classmethod
+    def get_jmcomic_url_all(cls, postman=None):
+        """
+        访问禁漫发布页，得到所有禁漫的域名
+        @return：['18comic.vip', ..., 'jm365.xyz/ZNPJam'], 最后一个是【APP軟件下載】
+        """
+        if postman is None:
+            from common import Postmans
+            postman = Postmans.get_impl_clazz('cffi').create()
+
+        resp = postman.get(cls.JM_PUB_URL)
+        if resp.status_code != 200:
+            raise AssertionError(resp.text)
+
+        from .jm_toolkit import JmcomicText
+        return JmcomicText.analyse_jm_pub_html(resp.text)
+
+
+jm_debug = JmModuleConfig.jm_debug
+disable_jm_debug = JmModuleConfig.disable_jm_debug
