25031b6076f2febcd1775fd9119f2697122fa340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:11 +0000

    Add package balena-sdk/12.6.19/balena_sdk-12.6.19.tar.gz

---
+++ packages/balena-sdk/balena_sdk-12.6.19.tar.gz/balena_sdk-12.6.19/balena/models/__init__.py
@@ -0,0 +1,41 @@
+"""
+This module implements all models for balena python SDK.
+
+"""
+
+from .device import Device
+from .application import Application
+from .config import Config
+from .environment_variables import EnvironmentVariable
+from .key import Key
+from .device_os import DeviceOs
+from .device_type import DeviceType
+from .supervisor import Supervisor
+from .image import Image
+from .service import Service
+from .release import Release
+from .config_variable import ConfigVariable
+from .api_key import ApiKey
+from .tag import Tag
+from .organization import Organization
+from .history import History
+
+
+class Models:
+    def __init__(self):
+        self.device = Device()
+        self.application = Application()
+        self.config = Config()
+        self.environment_variables = EnvironmentVariable()
+        self.key = Key()
+        self.device_os = DeviceOs()
+        self.device_type = DeviceType()
+        self.supervisor = Supervisor()
+        self.image = Image()
+        self.service = Service()
+        self.release = Release()
+        self.config_variable = ConfigVariable()
+        self.api_key = ApiKey()
+        self.tag = Tag()
+        self.organization = Organization()
+        self.history = History()
