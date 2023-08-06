65d4ad46f84a6545befc93958cab3ab07f1a1e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:19 +0000

    Add package bytez/0.1.7/bytez-0.1.7-py2.py3-none-any.whl

---
+++ packages/bytez/bytez-0.1.7-py2.py3-none-any.whl/bytez/pipeline.py
@@ -0,0 +1,41 @@
+import json
+from typing import BinaryIO, Literal, get_args
+from dataclasses import dataclass
+import requests
+
+with open('tasks.json', 'r') as file:
+    tasks = json.loads(file.read())
+
+
+@dataclass
+class Handler:
+    url: str
+
+    def inference(self, input_img: BinaryIO) -> bytes:
+        files = {'image': input_img}
+        response = requests.post(self.url, files=files)
+
+        # reset seek index to start of file
+        input_img.seek(0)
+
+        if not response.ok:
+            raise Exception(f'Request failed with {response.status_code}')
+
+        return response.content
+
+# TODO use enums to accomplish this
+
+
+def pipeline(task,
+             model) -> Handler:
+    task = tasks.get(task)
+
+    if not task:
+        raise Exception(f"{task} is not supported by this package.")
+
+    url = task.get(model)
+
+    if not url:
+        raise Exception(f"{model} is not supported by this package.")
+
+    return Handler(url=url)
