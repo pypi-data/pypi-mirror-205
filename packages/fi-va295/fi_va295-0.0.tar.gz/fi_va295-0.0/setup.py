45df975a24b7fea3f76a8f2a9c25d952136e5630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:24 +0000

    Add package fi-va295/0.0/fi_va295-0.0.tar.gz

---
+++ packages/fi-va295/fi_va295-0.0.tar.gz/fi_va295-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fi_va295',
+      version='0.0',
+      description='circle, tiangle, square',
+      packages=['fi_va295.circle', 'fi_va295.triangle', 'fi_va295.square'],
+      author_email='vanisimov2905@gmail.com',
+      zip_safe=False)
