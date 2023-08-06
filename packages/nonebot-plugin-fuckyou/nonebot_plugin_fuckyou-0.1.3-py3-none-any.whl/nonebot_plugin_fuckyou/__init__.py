effa0ee7e3fea89d6b4f4c9ee81089e2a8fa5a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:18 +0000

    Add package nonebot-plugin-fuckyou/0.1.3/nonebot_plugin_fuckyou-0.1.3-py3-none-any.whl

---
+++ packages/nonebot-plugin-fuckyou/nonebot_plugin_fuckyou-0.1.3-py3-none-any.whl/nonebot_plugin_fuckyou/__init__.py
@@ -0,0 +1,13 @@
+from nonebot.plugin import PluginMetadata
+
+from .__main__ import get_phase
+from .config import ConfigModel
+
+__version__ = "0.1.3"
+__plugin_meta__ = PluginMetadata(
+    "FuckYou",
+    get_phase(),
+    "与机器人对骂即可（关键词触发）",
+    ConfigModel,
+    {"License": "MIT", "Author": "student_2333"},
+)
