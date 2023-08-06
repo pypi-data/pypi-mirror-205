c42b2f7126ae86b8611a77584c5f4bf09f5c1443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:23 +0000

    Add package nrutility/0.0.1/nrutility-0.0.1.tar.gz

---
+++ packages/nrutility/nrutility-0.0.1.tar.gz/nrutility-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Data access module'
+LONG_DESCRIPTION = 'A package that allows to access database.'
+
+# Setting up
+setup(
+    name="nrutility",
+    version=VERSION,
+    author="Raghavendra Madugundi",
+    author_email="mrv8794@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
