f7217e256998faf1b038fcfd80312b9f8989bcc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:04 +0000

    Add package rolfsen/0.0.1/rolfsen-0.0.1.tar.gz

---
+++ packages/rolfsen/rolfsen-0.0.1.tar.gz/rolfsen-0.0.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='rolfsen',
+    version='0.0.1',
+    description='A really simple module to introduce everyone to James.',
+    author='James Rolfsen',
+    author_email='james.rolfsen@think.dev',
+    url='https://www.linkedin.com/in/jamesrolfsen/', 
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=[
+        # List your library dependencies here
+    ],
+) 
+
+
+
