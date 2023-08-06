c89547344066280940ebdd79dc30022bb9234a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:52 +0000

    Add package chopper/0.6.0/chopper-0.6.0.linux-x86_64.tar.gz

---
+++ packages/chopper/chopper-0.6.0.linux-x86_64.tar.gz/usr/local/lib/python3.10/dist-packages/chopper/css/rules.py
@@ -0,0 +1,11 @@
+class FontFaceRule:
+    """
+    @font-face rule
+    """
+
+    at_keyword = "@font-face"
+
+    def __init__(self, declarations, line, column):
+        self.declarations = declarations
+        self.line = line
+        self.column = column
