46cb789b85e6941d17276029aefed092c0aa4151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:16 +0000

    Add package shift-left-secure/0.2.2/shift_left_secure-0.2.2.tar.gz

---
+++ packages/shift-left-secure/shift_left_secure-0.2.2.tar.gz/shift_left_secure-0.2.2/shift_left_secure/chatgpt_api.py
@@ -0,0 +1,89 @@
+from .utils import join_openai_response
+
+import asyncio
+import logging
+import openai
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s] [%(levelname)s] - %(message)s')
+
+class CodeAnalyzer:
+    def __init__(self, api_key:str, model_engine:str="gpt-3.5-turbo"):
+        if not isinstance(api_key, str):
+            raise ValueError('API key should be str')
+        
+        openai.api_key = api_key
+        self.model_engine = model_engine
+
+    async def find_bugs_in_code(self, code):
+        prompt = f"What are the bugs in this code? Also find logical errors or issues with implementation\n\n{code}\n\nBugs:"
+        messages = [{"role":"user", "content":prompt}]
+
+        response = openai.ChatCompletion.create(
+            model=self.model_engine,
+            messages=messages,
+        )
+
+        return response
+    
+    async def find_vulns_in_code(self, code):
+        prompt = f"What are the vulnerabilities in this code? and how can we fix it?\n\n{code}"
+        messages = [{"role":"user", "content":prompt}]
+
+        response = openai.ChatCompletion.create(
+            model=self.model_engine,
+            messages=messages,
+        )
+
+        return response
+    
+    async def analyze_code(self, file_name:str, code:str):
+        logger.info(f'Analyzing code of file: {file_name}')
+
+        tasks = [
+            # for bug
+            asyncio.ensure_future(self.find_bugs_in_code(code)),
+            # for vulns
+            asyncio.ensure_future(self.find_vulns_in_code(code)),
+        ]
+        responses = await asyncio.gather(*tasks)
+
+        bugs_response = responses[0]
+        vulns_response = responses[1]
+
+        # extract data 
+        if bugs_response:
+            bugs_response = join_openai_response(bugs_response)
+        else:
+            logging.warning(f'unable to join messsages for {file_name} bug response')
+
+        if vulns_response:
+            vulns_response = join_openai_response(vulns_response)
+        else:
+            logging.warning(f'unable to join messsages for {file_name} vulns response')
+
+        analyzed_data = {
+            "file_name": file_name,
+            "bugs_response": bugs_response,
+            "vulns_response": vulns_response,
+            "code":code,
+        }
+
+        return analyzed_data
+
+    async def analyze_git_changes(self, diff_changes:dict):
+        '''
+        accepts data in dict form from GitHandler component
+        '''
+        tasks = []
+        for diff in diff_changes:
+            file_name = diff.get('file_path')
+            changed_lines = diff.get('changed_lines')
+
+            tasks.append(asyncio.ensure_future(self.analyze_code(file_name=file_name, code=changed_lines)))
+        
+        analyzed_code_snippets = await asyncio.gather(*tasks)
+
+        return analyzed_code_snippets
---
+++ packages/shift-left-secure/shift_left_secure-0.2.2.tar.gz/shift_left_secure-0.2.2/shift_left_secure/utils.py
@@ -0,0 +1,108 @@
+from json import dumps
+from textwrap import dedent
+import logging
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s] [%(levelname)s] - %(message)s')
+
+def join_diffs(diffs:list):
+    for file_change_data in diffs:
+        changed_lines = '\n'.join(file_change_data.get('changed_lines',[]))
+        file_change_data['changed_lines'] = changed_lines
+
+    return diffs
+
+def join_openai_response(response:dict):
+    choices = response.get('choices')
+    if not choices:
+        return False
+    
+    message = ''
+    for choice in choices:
+        message += choice.get('message', {'content':''}).get('content','')
+
+    return message
+
+def json_to_file(file_path:str, data:dict or list):
+    '''
+    dumps json data to a file
+    '''
+    if not file_path.endswith('.json'):
+        file_path += '.json'
+    
+    try:
+        with open(file_path, 'w') as f:
+            f.write(dumps(data))
+            logger.info(f'responses stored in file: {file_path}')
+
+    except Exception as e:
+        logger.exception(e)
+
+    return True
+
+def analyzed_data_to_console(analyzed_data:list):
+    if not isinstance(analyzed_data, list):
+        logger.warning('analyzed_data should be a list')
+        return 
+
+    for code_analysis in analyzed_data:
+        file_name = code_analysis.get('file_name')
+        bugs_response = code_analysis.get('bugs_response')
+        vulns_response = code_analysis.get('vulns_response')
+
+        print('FILE NAME:\t', file_name,'\n')
+        print('BUGS:\n',bugs_response, '\n')
+        print('VULNS:\n', vulns_response, '\n')
+        print('-'*20)
+
+def filter_gh_file_data(lines:list, file_name:str):
+    final_code = []
+    section = 0
+    for line in lines:
+        if line.startswith("---") or line.startswith("+++"):
+            continue
+        elif line.startswith("@@"):
+            section += 1
+            final_code.append(f'\n>> Interpret below code as section {section} inside {file_name} file.')
+        elif line.startswith("-"):
+            continue
+        elif line.startswith("+"):
+            final_code.append(line[1:])
+        else:
+            final_code.append(line)
+        
+    return '\n'.join(final_code)
+
+
+def analyzed_code_snippet_to_description(response:dict):
+    '''
+    converts analyzed code dict/json to github description
+    '''
+    return dedent(f'''## :file_folder: {response.get('file_name')}
+    Bugs and Vulnerabilities can be found in below section along with their fixes.
+
+    
+    ### :bug: Bugs
+
+    {response.get('bugs_response')}
+
+
+    ### :lock: Vulns
+
+    {response.get('vulns_response')}
+
+
+    ''')
+
+
+def create_gh_description(analyzed_code_snippets:str):
+    '''
+    create github description from analyzed code snippets response from openai api
+    '''
+    description = ''
+    for analyzed_code_snippet in analyzed_code_snippets:
+        description += analyzed_code_snippet_to_description(analyzed_code_snippet)
+
+    return description
