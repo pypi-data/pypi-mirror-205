3002fc13597c03cf2c412770d375dedafc4dea07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:53 +0000

    Add package inviz/0.0.6/inviz-0.0.6.tar.gz

---
+++ packages/inviz/inviz-0.0.6.tar.gz/inviz-0.0.6/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "inviz",
+    version = "0.0.6",
+    author = "James Wen",
+    author_email = "jswen@usc.edu",
+    description = ("An interactive visualizer to help explore the results of running MCMC posterior sampling on a cosmological model."),
+    license = "MIT",
+    keywords = "interactive visualizer cosmology",
+    url = "http://packages.python.org/inviz",
+    py_modules=["inviz"],
+    package_dir={'': 'inviz'},
+#    packages=['inviz'],
+    #package_dir={'': 'inviz'},
+    #long_description=long_description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',
+    install_requires=["holoviews==1.15.4",
+                    #   "bokeh==2.4.3",
+                      "spatialpandas==0.4.7"]
+                    #   "numpy<=1.24",
+                    #   "matplotlib==3.7.1"]
+    )
+
