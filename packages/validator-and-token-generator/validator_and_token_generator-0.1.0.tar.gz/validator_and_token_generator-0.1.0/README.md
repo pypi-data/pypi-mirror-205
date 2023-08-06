762444ca78e9b339a672e9c488fd46b99abc4f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:06 +0000

    Add package validator-and-token-generator/0.1.0/validator_and_token_generator-0.1.0.tar.gz

---
+++ packages/validator-and-token-generator/validator_and_token_generator-0.1.0.tar.gz/validator_and_token_generator-0.1.0/README.md
@@ -0,0 +1,17 @@
+### Description
+Validator_and_token_generator is a package created for a class project to validate certain strings such as credit/debit card numbers, and generate alphanumeric tokens.
+
+### Install validator_and_token_generator from PyPi.
+```bash
+pip install validator_and_token_generator
+```
+
+#### Example
+```python
+  # Import library
+  import validator_and_token_generator
+  # Generate token
+  _token = validator_and_token_generator.token_generator(length=10)
+```
+-------
+
---
+++ packages/validator-and-token-generator/validator_and_token_generator-0.1.0.tar.gz/validator_and_token_generator-0.1.0/validator_and_token_generator/__init__.py
@@ -0,0 +1,29 @@
+# Change the content according to your package.
+# from directory.filename.py import classname
+from validator_and_token_generator.validator_and_token_generator import *
+
+author='SasheO, Xaiver71, emilyyjx'
+author_email='mezisashe.ojuba@bison.howard.edu, malcolm.gray@bison.howard.edu, emily.jones@bison.howard.edu'
+__version__ = '0.1.0'
+
+# module level doc-string
+__doc__ = """
+validator_and_token_generator
+================
+
+Description
+-----------
+validator_and_token_generator is a package created for a class project to validate certain strings such as credit/debit card numbers, and generate alphanumeric tokens.
+
+Example
+-------
+>>> # Import library
+>>> import validator_and_token_generator
+>>> # Generate token
+>>> _token = validator_and_token_generator.token_generator(length=10)
+
+References
+----------
+* https://towardsdatascience.com/a-step-by-step-guide-in-detecting-causal-relationships-using-bayesian-structure-learning-in-python-c20c6b31cee5
+
+"""
