1df4d3672ba8ca69d3c0439656a0ffac8f69454b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:59 +0000

    Add package Aksconverter/0.0.4/Aksconverter-0.0.4.tar.gz

---
+++ packages/Aksconverter/Aksconverter-0.0.4.tar.gz/Aksconverter-0.0.4/aksconverter_properties_pkg/aksconverter_properties.py
@@ -0,0 +1,12 @@
+import requests
+
+def aksconverter(number):
+		print("test")
+		return number
+		# url = "https://anyapi.io/api/v1/exchange/convert?apiKey=pg2hovi5anohf0unsi656obkus8dfi43o9a6t6e735sg8v5p187n33&base="+base+"&to="+to+"&amount="+amount
+		# payload = {}
+		# headers = {}
+		# response = requests.request("GET", url, headers=headers, data=payload)
+		# print(response.text)
+		# return response.text
+
