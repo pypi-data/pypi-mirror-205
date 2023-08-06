e136f42e432751f1320aabcfd773a5596a041bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:27 +0000

    Add package mrdoxmrgt/0.1.4/mrdoxmrgt-0.1.4.tar.gz

---
+++ packages/mrdoxmrgt/mrdoxmrgt-0.1.4.tar.gz/mrdoxmrgt-0.1.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mrdoxmrgt',
+    packages=find_packages(),
+    include_package_data=True,
+    version="0.1.4",
+    description='A small example package',
+    long_description="A small example package HI",
+    long_description_content_type="text/markdown",
+    author='MR_GT',
+    author_email='friendyt89@gmail.com',
+    url='https://github.com/GreyTechno/gtf',
+    download_url="https://github.com/GreyTechno/gtf/archive/pypi.zip",
+        keywords=['a1', 'a2', 'a3', 'a3', 'a4',
+                  'a5', 'a6', 'a7', 'a8', 'a9'],
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: Python :: 3',
+            'Operating System :: OS Independent',
+            'Environment :: Console',
+    ],
+    install_requires=["requests", "random2"],
+    license='GPL',
+    entry_points={
+            'console_scripts': [
+                'mrdoxmrgt = mrdoxmrgt.main:Main',
+            ],
+    },
+    python_requires='>=3.5'
+)
