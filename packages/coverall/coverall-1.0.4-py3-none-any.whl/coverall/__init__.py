5c2c6aee1612b56f6f092ff27b9acc49cdfddc14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:19 +0000

    Add package coverall/1.0.4/coverall-1.0.4-py3-none-any.whl

---
+++ packages/coverall/coverall-1.0.4-py3-none-any.whl/coverall/__init__.py
@@ -0,0 +1,11 @@
+from cover import exactCover
+from cover import maxCover
+from cover import setCover
+from cover import createMatrix
+
+__all__ = [
+    'exactCover',
+    'maxCover',
+    'setCover',
+    'createMatrix'
+]
