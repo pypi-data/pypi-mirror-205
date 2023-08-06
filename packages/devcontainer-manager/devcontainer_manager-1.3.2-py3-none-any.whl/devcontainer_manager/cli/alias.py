4ed1f33b96ea1e72575611582085ab2ee2816c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:44 +0000

    Add package devcontainer-manager/1.3.2/devcontainer_manager-1.3.2-py3-none-any.whl

---
+++ packages/devcontainer-manager/devcontainer_manager-1.3.2-py3-none-any.whl/devcontainer_manager/cli/alias.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+from typing import List
+
+import typer
+
+from ..global_config import GlobalConfig
+
+app = typer.Typer()
+
+
+def complete_aliases(param_name: str = "alias"):
+    def _complete(ctx: typer.Context, args: List[str], incomplete: str):
+        global_config = GlobalConfig.load()
+        param_aliases = ctx.params.get(param_name) or []
+        for alias, path in global_config.load_alias_config().aliases.items():
+            if alias.startswith(incomplete) and alias not in param_aliases:
+                yield (alias, path.as_posix())
+
+    return _complete
+
+
+@app.command()
+def add(alias: str = typer.Argument(...), config_path: str = typer.Argument(...)):
+    path = Path(config_path)
+    alias_config = GlobalConfig.load().load_alias_config()
+    alias_config.aliases[alias] = path.resolve().as_posix()
+    alias_config.write_yaml()
+
+
+@app.command()
+def remove(
+    alias: List[str] = typer.Argument(..., autocompletion=complete_aliases("alias")),
+):
+    alias_config = GlobalConfig.load().load_alias_config()
+
+    for alias_remove in alias:
+        if alias_remove in alias_config.aliases:
+            alias_config.aliases.pop(alias_remove)
+            alias_config.write_yaml()
+        else:
+            message = (
+                f"{typer.style('Error: ', fg=typer.colors.RED)}"
+                "Nothing removed, alias "
+                f"'{typer.style(alias_remove, fg=typer.colors.BLUE)}' "
+                "does not exist"
+            )
+            typer.echo(message)
+
+
+@app.command()
+def list():
+    global_config = GlobalConfig.load()
+    typer.echo("Available aliases:")
+    aliases_msg = ""
+    for alias, path in global_config.load_alias_config().aliases.items():
+        file_exists = Path(path).exists()
+        file_color = typer.colors.GREEN if file_exists else typer.colors.RED
+        aliases_msg += (
+            f"  {typer.style(alias, fg=typer.colors.BLUE)}: "
+            f"{typer.style(path, fg=file_color)}"
+            "\n"
+        )
+    typer.echo(aliases_msg)
---
+++ packages/devcontainer-manager/devcontainer_manager-1.3.2-py3-none-any.whl/devcontainer_manager/global_config.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+from pydantic import Field, validator
+
+from .alias import DEFAULT_ALIAS_FILENAME, AliasConfig
+from .base_config import BaseYamlConfigModelWithBase, default_if_none
+from .config import Config
+from .settings import Settings
+
+
+class GlobalConfig(BaseYamlConfigModelWithBase):
+    defaults: Config = Field(
+        default_factory=Config,
+        description="default values for all configs for the current environment",
+    )
+    template_dir: Path = Field(
+        Path("./templates"),
+        description="directory for global templates",
+    )
+    override_config_path: Path = Field(
+        Path(".devcontainer/overrides.yaml"),
+        description=("default path for per-project override config (. is in '.devcontainer/')"),
+    )
+
+    _not_none = validator("*", pre=True, allow_reuse=True)(default_if_none)
+
+    def load_alias_config(self) -> AliasConfig:
+        alias_config_path = self.config_path.parent / DEFAULT_ALIAS_FILENAME
+        if not alias_config_path.exists():
+            AliasConfig().write_yaml(alias_config_path)
+
+        return AliasConfig.parse_file(alias_config_path)
+
+    @classmethod
+    def load(
+        cls, settings: Settings = Settings(), create_if_not_exist: bool = False
+    ) -> "GlobalConfig":
+        config_path = settings.global_config_path
+        if not config_path.exists():
+            if create_if_not_exist:
+                global_config = GlobalConfig()
+                global_config.write_yaml(config_path, with_descriptions=True)
+                global_config.config_path = config_path
+                return global_config
+            return None
+        return GlobalConfig.parse_file(config_path)
