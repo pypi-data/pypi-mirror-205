7cb7ecb25e539003950e2f675ef39e5bfbd186fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:43 +0000

    Add package netin/1.0.5.9.1/netin-1.0.5.9.1.tar.gz

---
+++ packages/netin/netin-1.0.5.9.1.tar.gz/netin-1.0.5.9.1/netin/__init__.py
@@ -0,0 +1,18 @@
+"""
+NetIn is a Python package for the analysis of network inequalities.
+It is based on the NetworkX package and provides a set of functions to study inequalities (e.g., in ranking, inference)
+in social networks.
+"""
+
+__version__ = '1.0.5.9.1'
+
+from netin import generators
+from netin.generators import *
+
+from netin import utils
+from netin.utils import *
+
+from netin import algorithms
+from netin.algorithms import sampling
+
+from netin.generators import convert_networkx_to_netin
