ad084080e13264be241ce2e2c5bb3050fc70fe2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:18 +0000

    Add package selfcord.py/0.1.2/selfcord.py-0.1.2-py3-none-any.whl

---
+++ packages/selfcord.py/selfcord.py-0.1.2-py3-none-any.whl/selfcord/models/__init__.py
@@ -0,0 +1,12 @@
+"""Data objects from discord, used throughout selfcord
+"""
+from .user import User, Profile, Connected_Account
+from .client import Client
+from .guild import Guild
+from .channel import TextChannel, Category, VoiceChannel, DMChannel, GroupChannel
+from .message import Message
+from .emoji import Emoji
+from .permission import Permission
+from .role import Role
+from .webhook import Webhook
+from .member import Member
