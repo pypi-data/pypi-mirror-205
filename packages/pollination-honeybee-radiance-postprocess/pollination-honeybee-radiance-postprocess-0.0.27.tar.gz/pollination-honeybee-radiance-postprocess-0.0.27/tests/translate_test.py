f166b6504d00e85f281459efbc5e034f06b3c54f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:23 +0000

    Add package pollination-honeybee-radiance-postprocess/0.0.27/pollination-honeybee-radiance-postprocess-0.0.27.tar.gz

---
+++ packages/pollination-honeybee-radiance-postprocess/pollination-honeybee-radiance-postprocess-0.0.27.tar.gz/pollination-honeybee-radiance-postprocess-0.0.27/tests/translate_test.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+from shutil import rmtree
+
+from pollination.honeybee_radiance_postprocess.translate import BinaryToNpy, \
+    TxtToNpy
+
+from queenbee.plugin.function import Function
+
+
+def test_binary_to_npy():
+    function = BinaryToNpy()
+    qb_function = function.queenbee
+    assert qb_function.name == 'binary-to-npy'
+    assert isinstance(qb_function, Function)
+
+
+def test_txt_to_npy():
+    function = TxtToNpy()
+    qb_function = function.queenbee
+    assert qb_function.name == 'txt-to-npy'
+    assert isinstance(qb_function, Function)
+
+
+def test_binary_to_npy_convert():
+    function = BinaryToNpy()
+    inputs = {
+        'matrix_file': Path('./tests/assets/translate/results.ill')
+    }
+    folder = Path('./tests/assets/temp')
+    output_file = folder.joinpath('output.npy')
+    if not folder.exists():
+        folder.mkdir(parents=True)
+    function._try(inputs, folder=folder)
+    assert output_file.is_file()
+
+    for path in folder.glob('*'):
+        if path.is_file():
+            path.unlink()
+        elif path.is_dir():
+            rmtree(path)
+
+
+def test_txt_to_npy_convert():
+    function = TxtToNpy()
+    inputs = {
+        'txt_file': Path('./tests/assets/translate/results.txt')
+    }
+    folder = Path('./tests/assets/temp')
+    output_file = folder.joinpath('output.npy')
+    if not folder.exists():
+        folder.mkdir(parents=True)
+    function._try(inputs, folder=folder)
+    assert output_file.is_file()
+
+    for path in folder.glob('*'):
+        if path.is_file():
+            path.unlink()
+        elif path.is_dir():
+            rmtree(path)
