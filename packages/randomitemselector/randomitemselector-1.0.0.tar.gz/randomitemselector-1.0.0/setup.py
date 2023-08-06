44fb76cfe9f3d6a6c938af3a5f81ad2a3346ae19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:47 +0000

    Add package randomitemselector/1.0.0/randomitemselector-1.0.0.tar.gz

---
+++ packages/randomitemselector/randomitemselector-1.0.0.tar.gz/randomitemselector-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='randomitemselector',
+    version='1.0.0',
+    description='A Python library to select a random item from a list.',
+    author='Nitish',
+    author_email='ksainitish@gmail.com',
+    py_modules=['randomitemselector'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
