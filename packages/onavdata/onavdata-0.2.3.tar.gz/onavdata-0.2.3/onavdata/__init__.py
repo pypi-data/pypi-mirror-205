aa3978346d343a6d8e912a90efacac8a8a0595e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:41 +0000

    Add package onavdata/0.2.3/onavdata-0.2.3.tar.gz

---
+++ packages/onavdata/onavdata-0.2.3.tar.gz/onavdata-0.2.3/onavdata/__init__.py
@@ -0,0 +1,5 @@
+from .onavdata import add_dataset
+from .onavdata import get_data
+from .onavdata import get_data_direct
+from .onavdata import apply_meta
+from .onavdata import print_shortnames
---
+++ packages/onavdata/onavdata-0.2.3.tar.gz/onavdata-0.2.3/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="onavdata",
+    version="0.2.3",
+    author="Organic Navigation",
+    author_email="team@organicnavigation.com",
+    description=(("Easily import both reference and user-specified datasets "
+                  "for navigation system design and testing projects.")),
+    packages=['onavdata', 'onavdata.utils'],
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    scripts=['bin/onavdata-print-shortnames'],
+    install_requires=[
+          'toml',
+          'pandas',
+          'numpy',
+          'idelib==3.1.0'
+      ],
+)
