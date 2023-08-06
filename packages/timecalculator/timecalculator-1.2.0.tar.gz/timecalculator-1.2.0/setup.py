32e3c8ad5fddb65ea10397046edaaf7814f776eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:55 +0000

    Add package timecalculator/1.2.0/timecalculator-1.2.0.tar.gz

---
+++ packages/timecalculator/timecalculator-1.2.0.tar.gz/timecalculator-1.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Programming Language :: Python :: 3",
+    "Operating System :: Microsoft :: Windows :: Windows 10",
+]
+
+setup(
+    name="timecalculator",
+    version="1.2.0",
+    packages=find_packages(),
+    description="A Python package for converting time units to seconds",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Xyndra",
+    author_email="sammy@deutschergamingserver.de",
+    license="MIT",
+    url="https://github.com/Xyndra/Timecalculator",
+    keywords=["time", "calculator"],
+    classifiers=classifiers,
+)
+# twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+# py setup.py sdist
