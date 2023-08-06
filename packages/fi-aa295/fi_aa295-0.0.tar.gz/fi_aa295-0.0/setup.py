e58fa180d7073d878cbf5280cc8474b9fb6fac4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:25 +0000

    Add package fi-aa295/0.0/fi_aa295-0.0.tar.gz

---
+++ packages/fi-aa295/fi_aa295-0.0.tar.gz/fi_aa295-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fi_aa295',
+      version='0.0',
+      description='circle, triangle, square',
+      packages=['fi_aa295.circle', 'fi_aa295.triangle', 'fi_aa295.square'],
+      author_email='sachaglushchenko08@gmail.com',
+      zip_safe=False)
