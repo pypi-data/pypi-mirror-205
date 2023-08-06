8d903bd6ddc744b4353e0cc2a55c306e59e59288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:25 +0000

    Add package uib-experiments/1.6.0/uib_experiments-1.6.0-py3-none-any.whl

---
+++ packages/uib-experiments/uib_experiments-1.6.0-py3-none-any.whl/uib_experiments/__init__.py
@@ -0,0 +1,4 @@
+from uib_experiments.decorator import experiment as decorator_experiment
+from uib_experiments.keras import telegramCallback
+from uib_experiments.data import dades
+from uib_experiments.experiment import experiment
