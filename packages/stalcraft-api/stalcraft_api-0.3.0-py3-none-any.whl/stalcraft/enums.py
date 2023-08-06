39aa1cc6ad6b4d9abd8991d4aab9349f55b3c5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:48 +0000

    Add package stalcraft-api/0.3.0/stalcraft_api-0.3.0-py3-none-any.whl

---
+++ packages/stalcraft-api/stalcraft_api-0.3.0-py3-none-any.whl/stalcraft/enums.py
@@ -0,0 +1,60 @@
+from enum import Enum
+
+
+class BaseUrl(Enum):
+    """
+    DEMO: Demo API
+    PRODUCTION: Production API
+    """
+
+    DEMO = "http://dapi.stalcraft.net"
+    PRODUCTION = "http://eapi.stalcraft.net"
+
+
+class StatusCode(Enum):
+    OK = 200
+    INVALID_PARAMETER = 400
+    UNAUTHORISED = 401
+    NOT_FOUND = 404
+    RATE_LIMIT = 429
+
+
+class Region(Enum):
+    """
+    RU: Russia
+    EU: Europe
+    NA: North America
+    SEA: South East Asia
+    """
+
+    RU = "ru"
+    EU = "eu"
+    NA = "na"
+    SEA = "sea"
+
+
+class Sort(Enum):
+    TIME_CREATED = "time_created"
+    TIME_LEFT = "time_left"
+    CURRENT_PRICE = "current_price"
+    BUYOUT_PRICE = "buyout_price"
+
+
+class Order(Enum):
+    ASCENDING = ASC = "asc"
+    DESCENDING = DESC = "desc"
+
+
+class Rank(Enum):
+    RECRUIT = "RECRUIT"
+    COMMONER = "COMMONER"
+    SOLDIER = "SOLDIER"
+    SERGEANT = "SERGEANT"
+    OFFICER = "OFFICER"
+    COLONEL = "COLONEL"
+    LEADER = "LEADER"
+
+
+class ItemFolder(Enum):
+    RU = "ru"
+    GLOBAL = "global"
