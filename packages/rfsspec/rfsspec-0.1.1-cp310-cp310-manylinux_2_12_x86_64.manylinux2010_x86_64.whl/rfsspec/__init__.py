4e8ba31a7c01fdb775b5124e714272a10cc71278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:09 +0000

    Add package rfsspec/0.1.1/rfsspec-0.1.1-cp310-cp310-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/rfsspec/rfsspec-0.1.1-cp310-cp310-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/rfsspec/__init__.py
@@ -0,0 +1,10 @@
+import importlib.metadata
+
+__version__ = importlib.metadata.version("rfsspec")
+
+from rfsspec.http import RustyHTTPFileSystem
+from rfsspec.s3 import RustyS3FileSystem
+from rfsspec.gcs import RustyGCSFileSystem
+from rfsspec.azure import RustyAzureFileSystem
+
+__all__ = ["RustyS3FileSystem", "RustyHTTPFileSystem", "RustyGCSFileSystem", "RustyAzureFileSystem"]
