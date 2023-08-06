4965700fe640e65f658c1fa34aa4dc4bbec68a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:44 +0000

    Add package data-disaggregation/0.9.0/data_disaggregation-0.9.0-py3-none-any.whl

---
+++ packages/data-disaggregation/data_disaggregation-0.9.0-py3-none-any.whl/data_disaggregation/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.9.0"
+
+# isort: skip_file -> keep order to prevent circular import
+from .ext import disagg
+from .classes import VT_Nominal, VT_Numeric, VT_NumericExt, VT_Ordinal
+
+__all__ = ["VT_Nominal", "VT_Ordinal", "VT_Numeric", "VT_NumericExt", "disagg"]
