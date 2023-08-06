faed5cabce5c03f9eff6bb0fec13cf308a1609ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:07 +0000

    Add package catnapmodule/0.21/catnapmodule-0.21.tar.gz

---
+++ packages/catnapmodule/catnapmodule-0.21.tar.gz/catnapmodule-0.21/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name="catnapmodule",
+    version="0.21",
+    packages=find_packages(),
+    author="Nap the Cat",
+    author_email="mohulhack@gmail.com"
+)
