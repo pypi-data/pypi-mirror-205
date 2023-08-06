54c11eebd2f3155f2f7a48d6b4d5a9dd197d2704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:24 +0000

    Add package aaaaa/1.0.1/aaaaa-1.0.1.tar.gz

---
+++ packages/aaaaa/aaaaa-1.0.1.tar.gz/aaaaa-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='aaaaa',
+      version='1.0.1',
+      description='test',
+      author='Sifer',
+      author_email='sifed@qq.com',
+      url='https://sife-shuo.github.io/',
+      py_modules = ['aaaaa'],
+     )
