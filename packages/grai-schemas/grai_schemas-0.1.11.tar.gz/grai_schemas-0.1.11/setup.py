9903b1e1d38678ff76413489bd7dffc5835a59f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:25 +0000

    Add package grai-schemas/0.1.11/grai_schemas-0.1.11.tar.gz

---
+++ packages/grai-schemas/grai_schemas-0.1.11.tar.gz/grai_schemas-0.1.11/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['grai_schemas', 'grai_schemas.v1', 'grai_schemas.v1.metadata']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['multimethod>=1.9.1,<2.0.0', 'pydantic>=1.10.4,<2.0.0']
+
+setup_kwargs = {
+    'name': 'grai-schemas',
+    'version': '0.1.11',
+    'description': '',
+    'long_description': '',
+    'author': 'Ian Eaves',
+    'author_email': 'ian@grai.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
