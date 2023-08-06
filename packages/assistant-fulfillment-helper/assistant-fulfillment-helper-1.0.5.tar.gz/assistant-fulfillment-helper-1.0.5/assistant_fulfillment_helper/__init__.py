ac4b63f81f531ded93dbcf027b8014c12e4ca150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:06 +0000

    Add package assistant-fulfillment-helper/1.0.5/assistant-fulfillment-helper-1.0.5.tar.gz

---
+++ packages/assistant-fulfillment-helper/assistant-fulfillment-helper-1.0.5.tar.gz/assistant-fulfillment-helper-1.0.5/assistant_fulfillment_helper/__init__.py
@@ -0,0 +1,3 @@
+from .fulfillment_helper import FulfillmentHelper
+from .app.responses.fulfillment_helper_response import FulfillmentHelperResponse
+from .app.server.fulfillment_server import application
