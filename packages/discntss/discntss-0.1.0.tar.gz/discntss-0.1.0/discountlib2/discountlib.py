5ae86acd964f81be6266f6c622c0b477d3c37f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:13 +0000

    Add package discntss/0.1.0/discntss-0.1.0.tar.gz

---
+++ packages/discntss/discntss-0.1.0.tar.gz/discntss-0.1.0/discountlib2/discountlib.py
@@ -0,0 +1,8 @@
+discount_codes = {
+        "LAX": "Los Angeles International Airport",
+        "JFK": "John F. Kennedy International Airport",
+        "ORD": "Chicago O'Hare International Airport",
+        # Add more airport codes and names as needed
+    }
+def discount_code():
+    return discount_codes
---
+++ packages/discntss/discntss-0.1.0.tar.gz/discntss-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='discntss',
+    version='0.1.0',
+    description='A library for applying discounts to orders in a cloud kitchen Django application',
+    author='Meerath Nida Aman',
+    author_email='nidaaman77@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'django>=3.0'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
