96d5044086937e3c53f0ca37ddccd02e93d8ac89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:21 +0000

    Add package feeluown/3.8.11/feeluown-3.8.11.tar.gz

---
+++ packages/feeluown/feeluown-3.8.11.tar.gz/feeluown-3.8.11/feeluown/excs.py
@@ -0,0 +1,80 @@
+"""
+HELP: I do not know how to design exception classes,
+as a result, these interfaces can be changed frequently.
+"""
+
+from requests.exceptions import RequestException
+
+
+class FuoException(Exception):
+    pass
+
+
+class LibraryException(FuoException):
+    pass
+
+
+class ProviderIOError(LibraryException, RequestException):
+    """Read/write data from/to provider failed
+
+    currently, all providers use requests to send http request,
+    and many Requestexception are not catched, so ProviderIOError
+    inherit RequestException.
+    """
+
+    def __init__(self, message='', provider=None):
+        super().__init__(message)
+
+        self.message = message
+        self.provider = provider
+
+    def __str__(self):
+        if self.provider is None:
+            return self.message
+        return '[{}] {}'.format(self.provider, self.message)
+
+
+class CreateReaderFailed(ProviderIOError):
+    """(DEPRECATED) use ProviderIOError instead"""
+
+
+class ReaderException(ProviderIOError):
+    """(DEPRECATED) use ProviderIOError instead"""
+
+
+class ReadFailed(ProviderIOError):
+    """(DEPRECATED) use ProviderIOError instead"""
+
+
+class ResourceNotFound(LibraryException):
+    pass
+
+
+class ProviderAlreadyRegistered(LibraryException):
+    pass
+
+
+class ProviderNotFound(ResourceNotFound):
+    pass
+
+
+class ModelNotFound(ResourceNotFound):
+    """Model is not found
+
+    For example, a model identifier is invalid.
+
+    .. versionadded:: 3.7.7
+    """
+
+
+class NotSupported(LibraryException):
+    """Provider does not support the operation
+    """
+
+
+class MediaNotFound(ResourceNotFound):
+    pass
+
+
+class NoUserLoggedIn(LibraryException):
+    """(DEPRECATED) return None when there is no user logged in"""
---
+++ packages/feeluown/feeluown-3.8.11.tar.gz/feeluown-3.8.11/feeluown/player/__init__.py
@@ -0,0 +1,34 @@
+from .metadata import MetadataFields, Metadata
+from .playlist import PlaybackMode
+from .base_player import State
+from .mpvplayer import MpvPlayer as Player
+from .playlist import PlaylistMode, Playlist
+from .fm import FM
+from .radio import SongRadio
+from .lyric import LiveLyric, parse_lyric_text, Line as LyricLine, Lyric
+from .recently_played import RecentlyPlayed
+from .delegate import PlayerPositionDelegate
+
+
+__all__ = (
+    'PlaybackMode',
+    'State',
+
+    'FM',
+    'PlaylistMode',
+    'SongRadio',
+
+    'Player',
+    'Playlist',
+    'PlayerPositionDelegate',
+
+    'Metadata',
+    'MetadataFields',
+
+    'LiveLyric',
+    'parse_lyric_text',
+    'LyricLine',
+    'Lyric',
+
+    'RecentlyPlayed',
+)
