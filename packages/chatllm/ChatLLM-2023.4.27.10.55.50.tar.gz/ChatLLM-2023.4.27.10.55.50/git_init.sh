30846b28114c27f4e32a62590fa311c2f43e00fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:51 +0000

    Add package chatllm/2023.4.27.10.55.50/ChatLLM-2023.4.27.10.55.50.tar.gz

---
+++ packages/chatllm/ChatLLM-2023.4.27.10.55.50.tar.gz/ChatLLM-2023.4.27.10.55.50/git_init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#git init
+# shellcheck disable=SC2035
+git add *
+git commit -m "[feat] add ChatWhoosh"
+
+#git remote add origin git@github.com:yuanjie-ai/llm4gpt.git
+#git branch -M master
+
+git pull
+git push -u origin master
+# git remote remove origin
