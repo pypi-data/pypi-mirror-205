ec2c353fc5cebf2207fbd572c672c137ace1cf30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:28 +0000

    Add package nikhil-candlesticks/0.0.1/nikhil_candlesticks-0.0.1.tar.gz

---
+++ packages/nikhil-candlesticks/nikhil_candlesticks-0.0.1.tar.gz/nikhil_candlesticks-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'candlestick patterns'
+LONG_DESCRIPTION = 'Identifying candlestick patterns in python'
+
+# Setting up
+setup(
+    name="nikhil_candlesticks",
+    version=VERSION,
+    author="Nikhil",
+    author_email="cnikhil247@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['candlesticks', 'patterns', 'reversals','nikhil','trading'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
