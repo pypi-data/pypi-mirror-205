d24fcf0e5ff18cb25e92bd6192714f16038727d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:26 +0000

    Add package Misago/0.34.0/Misago-0.34.0-py3-none-any.whl

---
+++ packages/Misago/Misago-0.34.0-py3-none-any.whl/misago/conf/admin/forms/__init__.py
@@ -0,0 +1,7 @@
+from .analytics import ChangeAnalyticsSettingsForm
+from .base import ChangeSettingsForm
+from .captcha import ChangeCaptchaSettingsForm
+from .general import ChangeGeneralSettingsForm
+from .oauth2 import ChangeOAuth2SettingsForm
+from .threads import ChangeThreadsSettingsForm
+from .users import ChangeUsersSettingsForm
