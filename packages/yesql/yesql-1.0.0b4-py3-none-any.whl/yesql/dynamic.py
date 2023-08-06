b1ebe61b2126aba0e7dd17371beae40969d99020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:43 +0000

    Add package yesql/1.0.0b4/yesql-1.0.0b4-py3-none-any.whl

---
+++ packages/yesql/yesql-1.0.0b4-py3-none-any.whl/yesql/dynamic.py
@@ -0,0 +1,300 @@
+from __future__ import annotations
+
+import inspect
+from typing import TYPE_CHECKING, Any, Awaitable, Generic, Iterable, TypeVar, Union
+
+import pypika
+
+from yesql.core import parse, types
+
+if TYPE_CHECKING:
+    from yesql import drivers
+
+__all__ = ("DynamicQueryService",)
+
+_MT = TypeVar("_MT")
+_RT = TypeVar("_RT")
+_ConnT = TypeVar("_ConnT")
+_CtxT = TypeVar("_CtxT")
+_ReturnT = Union[Iterable[_MT], Iterable[types.ScalarT]]
+
+
+class DynamicQueryService(Generic[_MT]):
+    """Query Library for building ad-hoc queries in-memory.
+
+    This service acts as glue between the `pypika` query-builder library and Norma's
+    `QueryRepository`.
+
+    It is intended as an escape-hatch for the small subset of situations where
+    dynamically-built queries are actually needed (think an admin tool, like
+    Flask-Admin, for instance).
+    """
+
+    __slots__ = (
+        "service",
+        "table",
+        "builder",
+        "cursor_proxy",
+    )
+
+    def __init__(
+        self,
+        service: types.RepositoryProtocolT[_MT],  # noqa: F811
+        *,
+        schema: str = None,
+    ):
+        self.service = service
+        self.table = self._get_table(service.metadata.__tablename__, schema=schema)
+        self.builder = self._get_query_builder(
+            self.table, dialect=service.metadata.__dialect__
+        )
+
+    def execute(
+        self,
+        query: Union[str, pypika.queries.QueryBuilder],
+        *args,
+        connection: types.ConnectionT = None,
+        timeout: float = 10,
+        transaction: bool = True,
+        rollback: bool = False,
+        coerce: bool = True,
+        deserializer: types.DeserializerT | None = None,
+        modifier: parse.ModifierT = parse.MANY,
+        **kwargs,
+    ) -> Union[_ReturnT, Awaitable[_ReturnT]]:
+        """Execute any arbitrary query and return the result.
+
+        Args:
+            query:
+               Either a SQL string or a pypika Query.
+            *args:
+               Any args which should be passed on to the query.
+            connection: optional
+               A DBAPI connectable to use during executions.
+               Whether to coerce the query result into the model bound to the service.
+            timeout: defaults 10
+                The number of seconds to wait for the query to complete.
+            transaction: defaults True
+                Whether to execute this query within a transaction block.
+            rollback: defaults False
+                Whether to rollback the transaction scope of this query execution.
+            coerce: defaults True
+                Whether to coerce the query result into the model bound to the service.
+                Will only be applied if the execution modifier returns a full row object.
+            deserializer: optional
+                A custom deserializer for the query result. If none provided, this method
+                will make use of the default deserializer on the associated repository.
+            modifier: The execution modifier for this query.
+                Fetch all rows, one row, the first value in the first row,
+                or just the affected rows.
+            **kwargs:
+               Any keyword args to pass on to the query.
+        Returns:
+            The query result.
+        """
+        query = self._resolve_query(sql=query, modifier=modifier)
+        executor = getattr(self.service.executor, modifier)
+        if modifier in {parse.MANY, parse.ONE, parse.MULTI} and coerce:
+            deserializer = deserializer or (
+                self.service.serdes.deserializer
+                if modifier == parse.ONE
+                else self.service.serdes.bulk_deserializer
+            )
+            kwargs.update(deserializer=deserializer)
+        return executor(
+            query,
+            *args,
+            connection=connection,
+            timeout=timeout,
+            transaction=transaction,
+            rollback=rollback,
+            **kwargs,
+        )
+
+    def execute_cursor(
+        self,
+        query: Union[str, pypika.queries.QueryBuilder],
+        *args,
+        connection: types.ConnectionT = None,
+        timeout: float = 10,
+        transaction: bool = True,
+        rollback: bool = False,
+        modifier: parse.ModifierT = parse.MANY,
+        **kwargs,
+    ):
+        """Execute any arbitrary query and enter a cursor context.
+
+        Args:
+            query:
+               Either a SQL string or a pypika Query.
+            *args:
+               Any args which should be passed on to the query.
+            connection: optional
+               A DBAPI connectable to use during executions.
+               Whether to coerce the query result into the model bound to the service.
+            timeout: defaults 10
+                The number of seconds to wait for the query to complete.
+            transaction: defaults True
+                Whether to execute this query within a transaction block.
+            rollback: defaults False
+                Whether to rollback the transaction scope of this query execution.
+            modifier: The execution modifier for this query.
+                Fetch all rows, one row, the first value in the first row,
+                or just the affected rows.
+            **kwargs:
+               Any keyword args to pass on to the query.
+        """
+        query = self._resolve_query(sql=query, modifier=modifier)
+        executor = getattr(self.service.executor, modifier + "_cursor")
+        return executor(
+            query,
+            *args,
+            connection=connection,
+            timeout=timeout,
+            transaction=transaction,
+            rollback=rollback,
+            **kwargs,
+        )
+
+    def select(
+        self,
+        *fields,
+        connection: types.ConnectionT = None,
+        timeout: float = 10,
+        transaction: bool = True,
+        rollback: bool = False,
+        coerce: bool = True,
+        deserializer: types.DeserializerT | None = None,
+        modifier: parse.ModifierT = parse.MANY,
+        **where: Any,
+    ) -> Union[_ReturnT, Awaitable[_ReturnT]]:
+        """A convenience method for executing an arbitrary SELECT query.
+
+        Notes:
+            This method assumes that the result is coerceable into the model bound to
+            the QueryService. If that is not the case, then make sure to pass in
+            `coerce=False` to the call.
+
+        Args:
+            *fields:
+                Optionally specify specific fields to return.
+            connection: optional
+               A DBAPI connectable to use during executions.
+               Whether to coerce the query result into the model bound to the service.
+            timeout: defaults 10
+                The number of seconds to wait for the query to complete.
+            transaction: defaults True
+                Whether to execute this query within a transaction block.
+            rollback: defaults False
+                Whether to rollback the transaction scope of this query execution.
+            coerce: defaults True
+                Whether to coerce the query result into the model bound to the service.
+                Will only be applied if the execution modifier returns a full row object.
+            deserializer: optional
+                A custom deserializer for the query result. If none provided, this method
+                will make use of the default deserializer on the associated repository.
+            modifier: The execution modifier for this query.
+                Fetch all rows, one row, the first value in the first row,
+                or just the affected rows.
+            **where:
+                Optionally specify direct equality comparisons for the WHERE clause.
+        """
+        query = self.build_select(*fields, **where)
+        return self.execute(
+            query,
+            connection=connection,
+            timeout=timeout,
+            transaction=transaction,
+            rollback=rollback,
+            coerce=coerce,
+            deserializer=deserializer,
+            modifier=modifier,
+        )
+
+    def select_cursor(
+        self,
+        *fields,
+        connection: types.ConnectionT = None,
+        timeout: float = 10,
+        transaction: bool = True,
+        rollback: bool = False,
+        modifier: parse.ModifierT = parse.MANY,
+        **where: Any,
+    ):
+        """A convenience method for executing an arbitrary SELECT query and entering a cursor context.
+
+        Notes:
+            This method assumes that the result is coerceable into the model bound to
+            the QueryService. If that is not the case, then make sure to pass in
+            `coerce=False` to the call.
+
+        Args:
+            *fields:
+                Optionally specify specific fields to return.
+            connection: optional
+               A DBAPI connectable to use during executions.
+               Whether to coerce the query result into the model bound to the service.
+            timeout: defaults 10
+                The number of seconds to wait for the query to complete.
+            transaction: defaults True
+                Whether to execute this query within a transaction block.
+            rollback: defaults False
+                Whether to rollback the transaction scope of this query execution.
+            modifier: The execution modifier for this query.
+                Fetch all rows, one row, the first value in the first row,
+                or just the affected rows.
+            **where:
+                Optionally specify direct equality comparisons for the WHERE clause.
+        """
+        query = self.build_select(*fields, **where)
+        return self.execute_cursor(
+            query,
+            connection=connection,
+            timeout=timeout,
+            transaction=transaction,
+            rollback=rollback,
+            modifier=modifier,
+        )
+
+    def build_select(self, *fields: str, **where: Any) -> pypika.queries.QueryBuilder:
+        """A convenience method for building a simple SELECT statement.
+
+        Args:
+            *fields:
+                Optionally specify specific fields to return.
+            **where:
+                Optionally specify direct equality comparisons for the WHERE clause.
+        """
+        fs: Iterable[str] = fields or [self.builder.star]
+        criterion: pypika.Criterion = pypika.Criterion.all(
+            [getattr(self.table, c) == v for c, v in where.items()]
+        )
+        return self.builder.select(*fs).where(criterion)
+
+    @staticmethod
+    def _get_table(name: str, *, schema: str = None) -> pypika.Table:
+        return pypika.Table(name, schema=schema)
+
+    @classmethod
+    def _get_query_builder(
+        cls,
+        table: pypika.Table,
+        *,
+        dialect: drivers.SupportedDialectsT = pypika.Dialects.POSTGRESQL,
+    ) -> pypika.queries.QueryBuilder:
+        return pypika.Query.from_(table, dialect=dialect)
+
+    @staticmethod
+    def _resolve_query(
+        sql: Union[str, pypika.queries.QueryBuilder], modifier: parse.ModifierT
+    ) -> parse.QueryDatum:
+        if isinstance(sql, pypika.queries.QueryBuilder):
+            sql = sql.get_sql()
+
+        return parse.QueryDatum(
+            name="execute",
+            doc="",
+            sql=sql,
+            signature=inspect.Signature(),
+            modifier=modifier,
+        )
---
+++ packages/yesql/yesql-1.0.0b4-py3-none-any.whl/yesql/stubgen.py
@@ -0,0 +1,222 @@
+from __future__ import annotations
+
+import importlib
+import inspect
+import pathlib
+import string
+import sys
+import textwrap
+from types import ModuleType
+from typing import TypedDict
+
+import black
+from yesql.core.parse import QueryDatum
+from yesql.repository import BaseQueryRepository
+from yesql.statement import Statement
+
+
+def stubgen(module: str | ModuleType) -> pathlib.Path | None:
+    if isinstance(module, str):
+        module = get_module(module)
+
+    stub = get_stub_module(module)
+    if stub is None:
+        return None
+
+    module_path = pathlib.Path(module.__file__).resolve()
+    stub_path = module_path.with_suffix(".pyi")
+    stub_path.write_text(stub)
+    return stub_path
+
+
+def get_module(string) -> ModuleType:
+    module = importlib.import_module(string)
+    sys.modules[module.__name__] = module
+    return module
+
+
+def is_repository(o):
+    return inspect.isclass(o) and issubclass(o, BaseQueryRepository)
+
+
+def get_stub_module(module: ModuleType) -> str | None:
+    try:
+        module_def = inspect.getsource(module)
+    except OSError:
+        return None
+    replacements: list[tuple[str, str]] = []
+    repos: list[tuple[str, type[BaseQueryRepository]]] = inspect.getmembers(
+        module, is_repository
+    )
+    if not repos:
+        return None
+
+    for name, repo in repos:
+        try:
+            class_def = inspect.getsource(repo)
+        except (OSError, TypeError):
+            # TODO
+            continue
+
+        methods: list[str] = get_stub_methods(repo)
+        method_body = "".join(methods)
+        class_def_stub = class_def + textwrap.indent(method_body, "    ")
+        replacements.append((class_def, class_def_stub))
+
+    module_def_stub = "import typing\n" + module_def
+    for src, replacement in replacements:
+        module_def_stub = module_def_stub.replace(src, replacement)
+
+    module_def_stub_blackened = blacken(module_def_stub)
+    return module_def_stub_blackened
+
+
+def get_stub_methods(repo: type[BaseQueryRepository]) -> list[str]:
+    methods: list[str] = []
+    for method_name, statement in repo.__statements__.items():
+        default_return, raw_return = get_return_types(
+            modelname=repo.model.__name__, query=statement.query, isaio=repo.isaio
+        )
+        instance_params = get_instance_params(statement, repo)
+        query_params = get_query_params(statement, instance_params)
+        instance_sigs = get_signatures(
+            *instance_params.values(),
+            default_returns=default_return,
+            raw_returns=raw_return,
+            name=statement.query.name,
+        )
+        query_sigs = get_signatures(
+            *query_params.values(),
+            default_returns=default_return,
+            raw_returns=raw_return,
+            name=statement.query.name,
+        )
+        istr = method_template.substitute(
+            method_name=method_name,
+            doc=statement.query.doc,
+            **instance_sigs,
+        )
+        qstr = method_template.substitute(
+            method_name=method_name, doc=statement.query.doc, **query_sigs
+        )
+        methods.extend((istr, qstr))
+
+    return methods
+
+
+def get_return_types(modelname: str, query: QueryDatum, isaio: bool) -> tuple[str, str]:
+    if query.name.endswith("cursor"):
+        wrapper = "typing.AsyncContextManager" if isaio else "typing.ContextManager"
+        rtype = f"{wrapper}[yesql.types.CursorT]"
+        return rtype, rtype
+    default = modelname
+    raw = "typing.Any"
+    if query.modifier in {"many", "multi"}:
+        default = f"list[{modelname}]"
+    elif query.modifier in {"scalar", "raw"}:
+        default = "typing.Any"
+    elif query.modifier == "affected":
+        raw = default = "int"
+    if isaio:
+        default, raw = f"typing.Awaitable[{default}]", f"typing.Awaitable[{raw}]"
+    return default, raw
+
+
+def get_signatures(
+    *params, default_returns: str, raw_returns: str, name: str
+) -> StubSignatures:
+    sig = inspect.Signature([self_param, *params], return_annotation=default_returns)
+    if name.endswith("cursor"):
+        return {
+            "method_sig": sig,
+            "method_sig_coerce": sig,
+            "method_sig_no_coerce": sig,
+        }
+
+    sig_no_coerce = inspect.Signature(
+        [self_param, *params, coerce_false],
+        return_annotation=raw_returns,
+    )
+    sig_coerce = inspect.Signature(
+        [self_param, *params, coerce_true],
+        return_annotation=default_returns,
+    )
+    return {
+        "method_sig": sig,
+        "method_sig_coerce": sig_coerce,
+        "method_sig_no_coerce": sig_no_coerce,
+    }
+
+
+def get_instance_params(
+    statement: Statement, repo: type[BaseQueryRepository]
+) -> dict[str, inspect.Parameter]:
+    unprocessed = inspect.signature(statement.execute).parameters
+    processed = {}
+    for name, param in unprocessed.items():
+        if name in ("coerce", "args", "kwargs", "_"):
+            continue
+        annotation = param.annotation
+        if name == "instance":
+            annotation = f"{repo.model.__name__} | None"
+        elif name == "instances":
+            annotation = f"typing.Sequence[{repo.model.__name__}]"
+        elif name == "params":
+            annotation = (
+                "typing.Iterable[typing.Sequence | typing.Mapping[str, typing.Any]]"
+            )
+        elif name in ("connection", "serializer", "deserializer"):
+            annotation = "yesql.types." + param.annotation.rsplit(".")[-1]
+        processed[name] = param.replace(annotation=annotation)
+    return processed
+
+
+def get_query_params(
+    statement: Statement, instance_params: dict[str, inspect.Parameter]
+):
+    query_params = {**statement.query.signature.parameters, **instance_params}
+    query_params.pop("instance", None)
+    query_params.pop("instances", None)
+    return query_params
+
+
+def blacken(code: str) -> str:
+    pyver = sys.version_info.minor
+    black_pyver = black.TargetVersion(pyver)
+    black_mode = black.Mode(is_pyi=True, target_versions={black_pyver})
+    blackened = black.format_str(code, mode=black_mode)
+    return blackened
+
+
+class StubSignatures(TypedDict):
+    method_sig: inspect.Signature
+    method_sig_coerce: inspect.Signature
+    method_sig_no_coerce: inspect.Signature
+
+
+coerce_false = inspect.Parameter(
+    "coerce", inspect.Parameter.KEYWORD_ONLY, annotation="typing.Literal[False]"
+)
+coerce_true = inspect.Parameter(
+    "coerce", inspect.Parameter.KEYWORD_ONLY, annotation="typing.Literal[True]"
+)
+self_param = inspect.Parameter("self", kind=inspect.Parameter.POSITIONAL_ONLY)
+
+
+method_template_str = '''
+@typing.overload
+def ${method_name}${method_sig}:
+    """${doc}
+    """
+
+@typing.overload
+def ${method_name}${method_sig_coerce}:
+    """${doc}
+    """
+
+@typing.overload
+def ${method_name}${method_sig_no_coerce}:
+    """${doc}
+    """
+'''
+method_template = string.Template(method_template_str)
