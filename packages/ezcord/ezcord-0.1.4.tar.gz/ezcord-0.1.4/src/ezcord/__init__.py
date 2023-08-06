6bf78b305f3202bc4cb4e4f1e1dca1b90d53b480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:14 +0000

    Add package ezcord/0.1.4/ezcord-0.1.4.tar.gz

---
+++ packages/ezcord/ezcord-0.1.4.tar.gz/ezcord-0.1.4/src/ezcord/__init__.py
@@ -0,0 +1,12 @@
+__title__ = "ezcord"
+__author__ = "tibue99"
+__license__ = "MIT"
+__version__ = "0.1.4"
+
+from . import emb, logs, times, utils
+from .bot import Bot
+from .emb import *
+from .enums import *
+from .logs import *
+from .times import *
+from .utils import *
