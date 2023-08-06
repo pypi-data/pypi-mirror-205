ba763adff43b15d1bee54a19674bf82408d3591f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:33 +0000

    Add package git-open/1.3.0/git_open-1.3.0-py2.py3-none-any.whl

---
+++ packages/git-open/git_open-1.3.0-py2.py3-none-any.whl/git_open/cli.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import click
+import sys
+from .git_open import GitOpen
+from . import __version__
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    """Open a git repo in the browser from the terminal.
+
+    Running the command without any sub-command will open the main page of the repository.
+    """
+    if ctx.invoked_subcommand is None:
+        git_repo = GitOpen()
+        git_repo.open_from_terminal()
+
+
+@cli.command()
+def commit():
+    """Open the current commit"""
+    git_repo = GitOpen()
+    click.echo("Opening commit...")
+    git_repo.add_commit_to_url()
+    git_repo.open_from_terminal()
+
+
+@cli.command()
+@click.option(
+    "-t", "--target", default="master", help="Target branch/commit to compare with"
+)
+def compare(target):
+    """Open a compare view"""
+    git_repo = GitOpen()
+    click.echo("Opening compare...")
+    git_repo.add_compare_to_url(target)
+    git_repo.open_from_terminal()
+
+
+@cli.command()
+def branch():
+    """Open the current branch"""
+    git_repo = GitOpen()
+    click.echo("Opening branch...")
+    git_repo.add_branch_to_url()
+    git_repo.open_from_terminal()
+
+
+@cli.command()
+def actions():
+    """Open GitHub actions"""
+    git_repo = GitOpen()
+    click.echo("Opening GitHub actions...")
+    git_repo.add_actions_to_url()
+    git_repo.open_from_terminal()
+
+
+@cli.command()
+def version():
+    """Show the current git-open version"""
+    click.echo("version: {}".format(__version__))
+    sys.exit()
+
+
+if __name__ == "__main__":
+    cli(obj={})
