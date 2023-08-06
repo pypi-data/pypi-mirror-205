91142339a4a85cd5b5e7508979d31f714baf948c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:41 +0000

    Add package tryexcept-decorator/0.1.6/tryexcept_decorator-0.1.6.tar.gz

---
+++ packages/tryexcept-decorator/tryexcept_decorator-0.1.6.tar.gz/tryexcept_decorator-0.1.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tryexcept_decorator',
+    packages=["tryexcept_decorator"],
+    version='0.1.6',
+    author='Davide Di Grande',
+    author_email='davidedigrande.dev@gmail.com',
+    description='See README',
+    url='https://github.com/davidedigrande/exception_handler_decorator',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
