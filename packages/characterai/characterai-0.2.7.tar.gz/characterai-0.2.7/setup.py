81caddcbaa8fd859ac33d5dfe5ab16441f9412e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:24 +0000

    Add package characterai/0.2.7/characterai-0.2.7.tar.gz

---
+++ packages/characterai/characterai-0.2.7.tar.gz/characterai-0.2.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name='characterai',
+    version='0.2.7',
+    author='kramcat',
+    description='An unofficial API for character.ai for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kramcat/CharacterAI',
+    packages=['characterai'],
+    install_requires=['playwright==1.32.1'],
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
