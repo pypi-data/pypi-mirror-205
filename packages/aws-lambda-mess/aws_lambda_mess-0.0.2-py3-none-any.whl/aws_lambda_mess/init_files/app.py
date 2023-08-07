0705ef29ff476a35dd667a7173a90eb6c67edfef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:06 +0000

    Add package aws-lambda-mess/0.0.2/aws_lambda_mess-0.0.2-py3-none-any.whl

---
+++ packages/aws-lambda-mess/aws_lambda_mess-0.0.2-py3-none-any.whl/aws_lambda_mess/init_files/app.py
@@ -0,0 +1,26 @@
+from aws_lambda_mess.framework.Route import Route
+from aws_lambda_mess.framework.success import success
+from aws_lambda_mess.framework.failures import bad_request
+from aws_lambda_mess.framework.server import run
+
+
+def index(params, body):
+    return success({"Hallo": "Index"})
+
+
+def greet(params, body):
+    return success({"Hallo": params["name"]})
+
+
+def default(params, body):
+    return bad_request()
+
+
+routes = [
+    Route(method_pattern="GET", path_pattern="/", handler=index),
+    Route(method_pattern="GET", path_pattern="/greet/<name>", handler=greet),
+    Route(method_pattern=".*", path_pattern=".*", handler=default)
+]
+
+if __name__ == "__main__":
+    run(9000, routes)
