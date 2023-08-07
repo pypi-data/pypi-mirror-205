913fb307d66ee8f780a53b4b5e2f2052da6fe2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:04:52 +0000

    Add package kfsd/0.0.7/kfsd-0.0.7.tar.gz

---
+++ packages/kfsd/kfsd-0.0.7.tar.gz/kfsd-0.0.7/kfsd/apps/core/auth/token.py
@@ -0,0 +1,9 @@
+from kfsd.apps.core.auth.base import BaseUser
+from kfsd.apps.core.auth.api.token import TokenAuth
+
+
+class TokenUser(BaseUser, TokenAuth):
+    def __init__(self, request):
+        BaseUser.__init__(self)
+        TokenAuth.__init__(self, request=request)
+        self.setUserInfo(self.getTokenUserInfo())
