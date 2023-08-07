ca622c43f4efcb8f34143951472870f9d990efdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:01 +0000

    Add package qcpython/1.0.3/QCpython-1.0.3.tar.gz

---
+++ packages/qcpython/QCpython-1.0.3.tar.gz/QCpython-1.0.3/test/quantumcircuit/test_qc_08.py
@@ -0,0 +1,45 @@
+from QCpy import QuantumCircuit
+import numpy as np
+
+
+def inc(x):
+    qc = QuantumCircuit(qubits=x, little_endian=True, prep='z')
+    qc.hadamard(0)
+    qc.hadamard(1)
+    qc.toffoli(0, 1, x - 1)
+    return qc.state()
+
+
+def test_08a():
+    assert (
+        inc(3) == np.array([
+            0.5 + 0j, 0.5 + 0j, 0.5 + 0j, 0 + 0j,
+            0 + 0j, 0 + 0j, 0 + 0j, 0.5 + 0j
+        ], 'F').reshape(8, 1)
+    ).all(), "test_08a Failed on hadamard -> hadamard -> toffoli"
+
+
+def test_08b():
+    assert (
+        inc(4) == np.array([
+            0.5 + 0j, 0.5 + 0j, 0.5 + 0j, 0 + 0j,
+            0 + 0j, 0 + 0j, 0 + 0j, 0 + 0j,
+            0 + 0j, 0 + 0j, 0 + 0j, 0.5 + 0j,
+            0 + 0j, 0 + 0j, 0 + 0j, 0 + 0j
+        ], 'F').reshape(16, 1)
+    ).all(), "test_08b Failed on hadamard -> hadamard -> toffoli"
+
+
+def test_08c():
+    assert (
+        inc(5) == np.array([
+            0.5 + 0j, 0.5 + 0j, 0.5 + 0j, 0 + 0j,
+            0 + 0j, 0 + 0j, 0 + 0j, 0 + 0j,
+            0 + 0j, 0 + 0j, 0 + 0j, 0 + 0j,
+            0 + 0j, 0 + 0j, 0 + 0j, 0 + 0j,
+            0 + 0j, 0 + 0j, 0 + 0j, 0.5 + 0j,
+            0 + 0j, 0 + 0j, 0 + 0j, 0 + 0j,
+            0 + 0j, 0 + 0j, 0 + 0j, 0 + 0j,
+            0 + 0j, 0 + 0j, 0 + 0j, 0 + 0j
+        ], 'F').reshape(32, 1)
+    ).all(), "test_08c Failed on hadamard -> hadamard -> toffoli"
