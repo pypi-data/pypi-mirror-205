5e6978d601c6f96f4411371b6c88c5e757671b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:46:55 +0000

    Add package jellyash/0.3.4/jellyash-0.3.4.tar.gz

---
+++ packages/jellyash/jellyash-0.3.4.tar.gz/jellyash-0.3.4/jellyash/duration.py
@@ -0,0 +1,25 @@
+import argparse
+from decimal import Decimal
+
+from .client import authed_client
+from .series import search_single_show
+
+
+def average_duration() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("show", nargs="+")
+    args = parser.parse_args()
+    client = authed_client()
+    try:
+        show = search_single_show(client, " ".join(args.show))
+    except ValueError as e:
+        print(str(e))
+        return
+    count = 0
+    duration = 0
+    for season in client.jellyfin.get_seasons(show.Id):
+        count += season.ChildCount
+        episodes = client.jellyfin.get_season(show.Id, season.Id)
+        duration += sum([e.RunTimeTicks for e in episodes])
+    average = (duration / count) / int(Decimal("6E+008"))
+    print(f"Average duration over {count} episodes: {average:.1f} minutes")
---
+++ packages/jellyash/jellyash-0.3.4.tar.gz/jellyash-0.3.4/jellyash/unwatched.py
@@ -0,0 +1,41 @@
+import argparse
+from operator import attrgetter
+
+from .client import authed_client
+from .series import search_single_show
+
+
+def unwatched() -> None:
+    client = authed_client()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("show", nargs="*")
+    args = parser.parse_args()
+    if not args.show:
+        all_unwatched(client)
+    else:
+        specific_unwatched(client, " ".join(args.show))
+
+
+def all_unwatched(client) -> None:
+    r = client.jellyfin.search_media_items(
+        term="", media="Series", limit=300
+    )
+    total = 0
+    for series in sorted(r, key=attrgetter("Name")):
+        if (count := series.UserData.UnplayedItemCount) > 0:
+            ending = "s" if count != 1 else ""
+            print(f"{series.Name}: {count} unwatched episode{ending}")
+            total += count
+    print(f"Total: {total} unwatched episodes")
+
+
+def specific_unwatched(client, term: str) -> None:
+    try:
+        show = search_single_show(client, term)
+    except ValueError as e:
+        print(str(e))
+        return
+    unwatched = show.UserData.UnplayedItemCount
+    total = sum(s.ChildCount for s in client.jellyfin.get_seasons(show.Id))
+    print(f"{show.Name}: {total - unwatched} watched episodes")
+    print(f"{show.Name}: {unwatched} unwatched episodes")
