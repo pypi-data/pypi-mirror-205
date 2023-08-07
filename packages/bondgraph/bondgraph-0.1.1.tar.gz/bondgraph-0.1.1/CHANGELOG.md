c6d406e1e9c61b93bd39b20458f6474091d5b8bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:46:47 +0000

    Add package bondgraph/0.1.1/bondgraph-0.1.1.tar.gz

---
+++ packages/bondgraph/bondgraph-0.1.1.tar.gz/bondgraph-0.1.1/CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.1.1] 2023-04-30
+### Changed
+- Project now only uses pyproject.toml for package information.
+
+## [0.1.0] 2023-04-29
+### Added
+- Basic functionality for creating simple bond graphs and generating differential equations.
+- Support for visualization of bond graphs using graphviz.
---
+++ packages/bondgraph/bondgraph-0.1.1.tar.gz/bondgraph-0.1.1/tests/test_core.py
@@ -0,0 +1,166 @@
+from bondgraph.common import AlgebraicLoopError
+from bondgraph.core import Bond, BondGraph
+from bondgraph.junctions import JunctionEqualEffort, JunctionEqualFlow
+from bondgraph.elements.basic import (
+    Element_R,
+    Element_I,
+    Element_C,
+    Transformer,
+    Source_effort,
+    Source_flow,
+)
+
+from sympy import Symbol as _
+import pytest
+
+
+def test_basic_i_element():
+    F = _("F")
+    r = _("r")
+    i = _("i")
+    p = _("p")
+
+    e_se = Source_effort("f", F)
+    e_r = Element_R("r", r)
+    e_i = Element_I("i", i, p)
+    j = JunctionEqualFlow("j")
+
+    g = BondGraph()
+    g.add(Bond(e_se, j))
+    g.add(Bond(j, e_r))
+    g.add(Bond(j, e_i))
+
+    eqs = g.get_state_equations()
+    assert eqs[p] == (F - r * p / i)
+
+
+def test_basic_c_element():
+    F = _("F")
+    r = _("r")
+    c = _("c")
+    q = _("q")
+
+    e_se = Source_effort("F", F)
+    e_r = Element_R("r", r)
+    e_c = Element_C("c", c, q)
+    j = JunctionEqualFlow("j")
+
+    g = BondGraph()
+    g.add(Bond(e_se, j))
+    g.add(Bond(j, e_r))
+    g.add(Bond(j, e_c))
+
+    eqs = g.get_state_equations()
+    assert eqs[q] == (F - q / c) / r
+
+
+def test_more_complex():
+    F = _("F")
+    v = _("v")
+    r = _("r")
+    i = _("i")
+    c = _("c")
+    q = _("q")
+    p = _("p")
+
+    e_se = Source_effort("F", F)
+    j1 = JunctionEqualFlow("j1")
+    e_i = Element_I("i", i, p)
+    j2 = JunctionEqualEffort("j2")
+    j3 = JunctionEqualFlow("j3")
+    e_c = Element_C("c", c, q)
+    e_r = Element_R("r", r)
+    e_sf = Source_flow("v", v)
+
+    g = BondGraph()
+    g.add(Bond(e_se, j1))
+    g.add(Bond(j1, e_i))
+    g.add(Bond(j1, j2))
+    g.add(Bond(j2, j3))
+    g.add(Bond(e_sf, j2))
+    g.add(Bond(j3, e_c))
+    g.add(Bond(j3, e_r))
+
+    eqs = g.get_state_equations()
+    assert eqs[p].equals(F - r * v - r * p / i - q / c)
+    assert eqs[q].equals(v + p / i)
+
+
+def test_basic_transformer_1():
+    F = _("F")
+    r = _("r")
+    i = _("i")
+    p = _("p")
+    d = _("d")
+
+    e_se = Source_effort("F", F)
+    j1 = JunctionEqualFlow("j1")
+    e_i = Element_I("i", i, p)
+    j2 = JunctionEqualFlow("j2")
+    e_r = Element_R("r", r)
+    tf = Transformer("TF", d)
+
+    g = BondGraph()
+    g.add(Bond(e_se, j1))
+    g.add(Bond(j1, e_i))
+    g.add(Bond(j1, tf))
+    g.add(Bond(tf, j2))
+    g.add(Bond(j2, e_r))
+
+    eqs = g.get_state_equations()
+    assert eqs[p] == (F - (r * d**2 * p) / i)
+
+
+def test_basic_transformer_2():
+    F = _("F")
+    r = _("r")
+    c = _("c")
+    q = _("q")
+    d = _("d")
+
+    e_se = Source_effort("F", F)
+    j1 = JunctionEqualFlow("j1")
+    e_c = Element_C("c", c, q)
+    j2 = JunctionEqualFlow("j2")
+    e_r = Element_R("r", r)
+    tf = Transformer("TF", d)
+
+    g = BondGraph()
+    g.add(Bond(e_se, j1))
+    g.add(Bond(j1, e_c))
+    g.add(Bond(j1, tf))
+    g.add(Bond(tf, j2))
+    g.add(Bond(j2, e_r))
+
+    eqs = g.get_state_equations()
+    assert eqs[q] == ((F - q / c) / (r * d**2))
+
+
+def test_algebraic_loops():
+    F = _("F")
+    r1 = _("r1")
+    r2 = _("r2")
+    r3 = _("r3")
+    c = _("c")
+    q = _("q")
+
+    e_se = Source_effort("F", F)
+    e_r1 = Element_R("r1", r1)
+    e_r2 = Element_R("r2", r2)
+    e_r3 = Element_R("r3", r3)
+    j1 = JunctionEqualFlow("j1")
+    j2 = JunctionEqualEffort("j2")
+    j3 = JunctionEqualFlow("j3")
+    e_c = Element_C("c", c, q)
+
+    g = BondGraph()
+    g.add(Bond(e_se, j1))
+    g.add(Bond(j1, e_r1))
+    g.add(Bond(j1, j2))
+    g.add(Bond(j2, e_r2))
+    g.add(Bond(j2, j3))
+    g.add(Bond(j3, e_r3))
+    g.add(Bond(j3, e_c))
+
+    with pytest.raises(AlgebraicLoopError):
+        g.get_state_equations()
