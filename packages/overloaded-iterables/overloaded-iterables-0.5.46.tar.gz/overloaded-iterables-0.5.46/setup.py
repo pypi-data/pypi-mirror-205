91174c761c1a1cb6750c13f901e38b9554b69771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:50:22 +0000

    Add package overloaded-iterables/0.5.46/overloaded-iterables-0.5.46.tar.gz

---
+++ packages/overloaded-iterables/overloaded-iterables-0.5.46.tar.gz/overloaded-iterables-0.5.46/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='overloaded-iterables',
+    version='0.5.46',
+    description="Overloaded version of the built-in python classes: list and set to include some extra functionalities.",
+    long_description="Overloaded version of the built-in python classes: list and set to include some extra functionalities such as sort(), rms(), etc",
+    license='MIT',
+    author="Prithoo Medhi",
+    author_email='prithoo11335@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Arkiralor/overloaded_iterables',
+    keywords= [
+        'python',
+        'sequence',
+        'overloading',
+        'median',
+        'rms',
+        'root-mean-square',
+        'mean',
+        'sort'
+    ],
+    install_requires=[],
+)
+
+if __name__=="__main__":
+    pass
