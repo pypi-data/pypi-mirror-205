846386a731fdec729b9a50282e96f0be8b87b7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:44:30 +0000

    Add package leap-model-rebuilder/0.1.6.dev2/leap_model_rebuilder-0.1.6.dev2.tar.gz

---
+++ packages/leap-model-rebuilder/leap_model_rebuilder-0.1.6.dev2.tar.gz/leap_model_rebuilder-0.1.6.dev2/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['leap_model_rebuilder']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['protobuf>=3.19.6,<4.0.0']
+
+extras_require = \
+{':platform_machine == "arm64"': ['tensorflow-macos==2.12.0'],
+ ':platform_machine == "x86_64"': ['tensorflow==2.12.0']}
+
+setup_kwargs = {
+    'name': 'leap-model-rebuilder',
+    'version': '0.1.6.dev2',
+    'description': '',
+    'long_description': '# leap-model-rebuilder\nKeras model rebuilder tool for model rebuilding with alterations\n',
+    'author': 'dorhar',
+    'author_email': 'doron.harnoy@tensorleap.ai',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/tensorleap/leap-model-rebuilder',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
