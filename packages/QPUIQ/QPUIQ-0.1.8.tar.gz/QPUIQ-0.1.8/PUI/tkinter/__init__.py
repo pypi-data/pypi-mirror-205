a3dfcb79c557f510017b6c76e7a7d83e75b291c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:56 +0000

    Add package QPUIQ/0.1.8/QPUIQ-0.1.8.tar.gz

---
+++ packages/QPUIQ/QPUIQ-0.1.8.tar.gz/QPUIQ-0.1.8/PUI/tkinter/__init__.py
@@ -0,0 +1,20 @@
+from .application import *
+from .button import *
+from .canvas import *
+from .label import *
+from .layout import *
+from .progressbar import *
+from .textfield import *
+from .window import *
+
+Application = TkApplication
+Window = TkWindow
+HBox = TkHBox
+VBox = TkVBox
+Label = TkLabel
+Button = TkButton
+Canvas = TkCanvas
+CanvasText = TkCanvasText
+CanvasLine = TkCanvasLine
+TextField = TkEntry
+ProgressBar = TkProgressBar
