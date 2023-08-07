d61a05c197fd495bd17b681d1c7a180111c97709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:59 +0000

    Add package ErCore/1.2/ErCore-1.2.tar.gz

---
+++ packages/ErCore/ErCore-1.2.tar.gz/ErCore-1.2/ErCore/std/Base.py
@@ -0,0 +1,34 @@
+colors = {
+    'Reset': '\033[0m',
+    'Black': '\033[30m',
+    'Red': '\033[31m',
+    'Green': '\033[32m',
+    'Yellow': '\033[33m',
+    'Blue': '\033[34m',
+    'Magenta': '\033[35m',
+    'Cyan': '\033[36m',
+    'White': '\033[37m',
+}
+styles = {
+    'BOLD': '\033[1m',
+    'ITALIC': '\033[3m',
+    'UNDERLINE': '\033[4m',
+    'STRIKETHROUGH': '\033[9m',
+}
+
+# Colors
+RESET = colors['Reset']
+black = colors['Black']
+red = colors['Red']
+green = colors['Green']
+yellow = colors['Yellow']
+blue = colors['Blue']
+magenta = colors['Magenta']
+cyan = colors['Cyan']
+white = colors['White']
+
+# Styles
+bold = styles['BOLD']
+italic = styles['ITALIC']
+underline = styles['UNDERLINE']
+strikethrough = styles['STRIKETHROUGH']
---
+++ packages/ErCore/ErCore-1.2.tar.gz/ErCore-1.2/README.md
@@ -0,0 +1,7 @@
+# Core for package/projects
+
+### author: Eragod
+### version: 1.1
+
+
+## [Github](https://github.com/Eragod/ErCore)
