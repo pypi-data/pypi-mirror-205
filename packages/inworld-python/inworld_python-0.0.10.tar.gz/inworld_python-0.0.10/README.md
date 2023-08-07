92a59b05f209c38ddd6b85d4dc424455b2c7be51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:46:42 +0000

    Add package inworld-python/0.0.10/inworld_python-0.0.10.tar.gz

---
+++ packages/inworld-python/inworld_python-0.0.10.tar.gz/inworld_python-0.0.10/README.md
@@ -0,0 +1,34 @@
+# inworld_python
+
+Under construction! Not ready for production use. Currently experimenting and planning!
+
+Developed by Edgelord and ChatGPT (c) 2023
+
+## Examples of How To Use (Buggy Alpha Version)
+
+Installation
+```python
+pip install inworld-python
+```
+Note: If you don't have node js installed also do the following
+```python
+pip install 'nodejs-bin[cmd]'
+```
+
+Usage for dummies even.
+
+```python
+from inworld_python import inworld_chat
+
+chat_app = inworld_chat.InWorldChat('inworld_key', 
+                       'inworld_secret', 
+                       'inworld_scene'
+                      )
+
+# installs the inworld command in the current directory as iw.js
+chat_app.setup()
+
+# enter query, user_name, channel_id, and user_id
+out = chat_app.chat("What did I just ask?", "blowhard", "3232323", "23232323")
+print(out)
+```
---
+++ packages/inworld-python/inworld_python-0.0.10.tar.gz/inworld_python-0.0.10/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.10'
+DESCRIPTION = 'Inworld Wrapper for Python'
+LONG_DESCRIPTION = 'A python package that allows a user to create chat apps using inworld.'
+
+# Setting up
+setup(
+    name="inworld_python",
+    version=VERSION,
+    author="Edgelord (Lucky Robinson)",
+    author_email="<luckstarsfilm@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['nodejs-bin'],
+    keywords=['python', 'Inworld', 'NPC', 'Game Characters'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
