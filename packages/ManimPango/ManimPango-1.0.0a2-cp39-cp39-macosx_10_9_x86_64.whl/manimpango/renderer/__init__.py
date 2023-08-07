b2ba8ecbfd5b397fbdf3a90b205105cb846e0687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:03:30 +0000

    Add package ManimPango/1.0.0a2/ManimPango-1.0.0a2-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/ManimPango/ManimPango-1.0.0a2-cp39-cp39-macosx_10_9_x86_64.whl/manimpango/renderer/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from .image_renderer import ImageRenderer  # noqa: F403,F401
+from .svg_renderer import SVGRenderer  # noqa: F403,F401
+
+__all__ = ["ImageRenderer", "SVGRenderer"]
