fc345a212a7c31e979bc73c48f475fe7502cada4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:01:56 +0000

    Add package culturalconformity/0.0.14/culturalconformity-0.0.14.tar.gz

---
+++ packages/culturalconformity/culturalconformity-0.0.14.tar.gz/culturalconformity-0.0.14/culturalconformity/__init__.py
@@ -0,0 +1,9 @@
+from .frq_over_time import frq_over_time
+from .old_interactive import old_interactive
+from .stochastic import stochastic
+from .interactive import interactive
+from .frqs import frqs
+
+
+
+
