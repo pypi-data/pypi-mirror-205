7652dbb97cd967c63b915ccd5ba4879df21da0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:46:57 +0000

    Add package terminalplay/1.0.1/terminalplay-1.0.1.tar.gz

---
+++ packages/terminalplay/terminalplay-1.0.1.tar.gz/terminalplay-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='terminalplay',
+    version='1.0.1',
+    license='MIT',
+    author="Merwin JD",
+    author_email='merwin@fbi.ac',
+    packages=find_packages('terminalplay/src'),
+    package_dir={'': 'terminalplay/src'},
+    url='https://github.com/merwin-asm/Terminal_Play',
+    keywords='terminalplay, play videos, terminal',
+    install_requires=[
+          'moviepy',
+          'playsound',
+          'rich',
+          'sty',
+          'opencv-python'
+      ],
+      description="Play videos on your terminal; Works on windows as well as linux",
+      long_description="""
+      The video played will also have color and sound :)
+      Videos be converted into easily readable frames which will be printed on the terminal (This 
+      conversion will only take place once).
+      """,
+      platforms= ["windows","linux"],
+      
+)
