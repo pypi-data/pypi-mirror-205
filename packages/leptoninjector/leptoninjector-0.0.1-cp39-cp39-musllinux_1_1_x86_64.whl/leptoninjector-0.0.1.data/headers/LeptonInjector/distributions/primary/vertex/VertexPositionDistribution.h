a0ce2517084423ca3c80a9bd16adfc3a32db7f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:23 +0000

    Add package leptoninjector/0.0.1/leptoninjector-0.0.1-cp39-cp39-musllinux_1_1_x86_64.whl

---
+++ packages/leptoninjector/leptoninjector-0.0.1-cp39-cp39-musllinux_1_1_x86_64.whl/leptoninjector-0.0.1.data/headers/LeptonInjector/distributions/primary/vertex/VertexPositionDistribution.h
@@ -0,0 +1,84 @@
+#pragma once
+#ifndef LI_VertexPositionDistribution_H
+#define LI_VertexPositionDistribution_H
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <cereal/access.hpp>
+#include <cereal/types/polymorphic.hpp>
+#include <cereal/types/base_class.hpp>
+#include <cereal/types/utility.hpp>
+
+#include "LeptonInjector/math/Vector3D.h"
+
+#include "LeptonInjector/distributions/Distributions.h"
+
+namespace LI {
+namespace utilities {
+class LI_random;
+} // namespace utilities
+
+namespace detector {
+class EarthModel;
+} // namespace detector
+
+namespace dataclasses {
+struct InteractionRecord;
+struct InteractionSignature;
+}
+namespace crosssections {
+class CrossSectionCollection;
+} // namespace crosssections
+} // namespace LeptonInjector
+
+namespace LI {
+namespace distributions {
+
+class VertexPositionDistribution : virtual public InjectionDistribution {
+friend cereal::access;
+public:
+    virtual ~VertexPositionDistribution() {};
+private:
+    virtual LI::math::Vector3D SamplePosition(std::shared_ptr<LI::utilities::LI_random> rand, std::shared_ptr<LI::detector::EarthModel const> earth_model, std::shared_ptr<LI::crosssections::CrossSectionCollection const> cross_sections, LI::dataclasses::InteractionRecord & record) const = 0;
+    virtual LI::math::Vector3D SamplePosition(std::shared_ptr<LI::utilities::LI_random> rand, std::shared_ptr<LI::detector::EarthModel const> earth_model, std::shared_ptr<LI::crosssections::CrossSectionCollection const> cross_sections, LI::dataclasses::InteractionTreeDatum & datum) const;
+public:
+    virtual void Sample(std::shared_ptr<LI::utilities::LI_random> rand, std::shared_ptr<LI::detector::EarthModel const> earth_model, std::shared_ptr<LI::crosssections::CrossSectionCollection const> cross_sections, LI::dataclasses::InteractionRecord & record) const override;
+    virtual bool IsPositionDistribution() const override {return true;}
+    virtual double GenerationProbability(std::shared_ptr<LI::detector::EarthModel const> earth_model, std::shared_ptr<LI::crosssections::CrossSectionCollection const> cross_sections, LI::dataclasses::InteractionRecord const & record) const override = 0;
+    virtual std::vector<std::string> DensityVariables() const override;
+    virtual std::string Name() const override = 0;
+    virtual std::shared_ptr<InjectionDistribution> clone() const override = 0;
+    virtual std::pair<LI::math::Vector3D, LI::math::Vector3D> InjectionBounds(std::shared_ptr<LI::detector::EarthModel const> earth_model, std::shared_ptr<LI::crosssections::CrossSectionCollection const> cross_sections, LI::dataclasses::InteractionRecord const & interaction) const = 0;
+    virtual std::pair<LI::math::Vector3D, LI::math::Vector3D> InjectionBounds(std::shared_ptr<LI::detector::EarthModel const> earth_model, std::shared_ptr<LI::crosssections::CrossSectionCollection const> cross_sections, LI::dataclasses::InteractionTreeDatum const & datum) const;
+    virtual bool AreEquivalent(std::shared_ptr<LI::detector::EarthModel const> earth_model, std::shared_ptr<LI::crosssections::CrossSectionCollection const> cross_sections, std::shared_ptr<WeightableDistribution const> distribution, std::shared_ptr<LI::detector::EarthModel const> second_earth_model, std::shared_ptr<LI::crosssections::CrossSectionCollection const> second_cross_sections) const override;
+    template<typename Archive>
+    void save(Archive & archive, std::uint32_t const version) const {
+        if(version == 0) {
+            archive(cereal::virtual_base_class<InjectionDistribution>(this));
+        } else {
+            throw std::runtime_error("VertexPositionDistribution only supports version <= 0!");
+        }
+    }
+    template<typename Archive>
+    void load(Archive & archive, std::uint32_t const version) {
+        if(version == 0) {
+            archive(cereal::virtual_base_class<InjectionDistribution>(this));
+        } else {
+            throw std::runtime_error("VertexPositionDistribution only supports version <= 0!");
+        }
+    }
+protected:
+    virtual bool equal(WeightableDistribution const & distribution) const override = 0;
+    virtual bool less(WeightableDistribution const & distribution) const override = 0;
+};
+
+} // namespace distributions
+} // namespace LI
+
+CEREAL_CLASS_VERSION(LI::distributions::VertexPositionDistribution, 0);
+CEREAL_REGISTER_TYPE(LI::distributions::VertexPositionDistribution);
+CEREAL_REGISTER_POLYMORPHIC_RELATION(LI::distributions::InjectionDistribution, LI::distributions::VertexPositionDistribution);
+
+#endif // LI_VertexPositionDistribution_H
---
+++ packages/leptoninjector/leptoninjector-0.0.1-cp39-cp39-musllinux_1_1_x86_64.whl/leptoninjector-0.0.1.data/headers/LeptonInjector/injection/Process.h
@@ -0,0 +1,87 @@
+#pragma once
+#ifndef LI_Process_H
+#define LI_Process_H
+
+#include <array>
+#include <vector>
+
+#include <cereal/cereal.hpp>
+#include <cereal/archives/json.hpp>
+#include <cereal/archives/binary.hpp>
+#include <cereal/types/vector.hpp>
+#include <cereal/types/array.hpp>
+#include <cereal/types/polymorphic.hpp>
+#include <cereal/types/base_class.hpp>
+#include <cereal/types/utility.hpp>
+
+#include "LeptonInjector/serialization/array.h"
+
+#include "LeptonInjector/dataclasses/InteractionSignature.h"
+#include "LeptonInjector/dataclasses/InteractionTree.h"
+#include "LeptonInjector/distributions/Distributions.h"
+#include "LeptonInjector/crosssections/CrossSection.h"
+
+namespace LI {
+namespace injection {
+
+struct Process {
+    LI::dataclasses::Particle::ParticleType primary_type;
+    std::shared_ptr<crosssections::CrossSectionCollection> cross_sections;
+    void SetCrossSections(std::shared_ptr<crosssections::CrossSectionCollection> _cross_sections) {cross_sections = _cross_sections;}
+    bool operator==(Process const & other) const;
+    bool MatchesHead(std::shared_ptr<Process> const & other) const; // required to compared instances of derived structs
+    template<class Archive>
+    void serialize(Archive & archive, std::uint32_t const version) {
+        if(version == 0) {
+            archive(::cereal::make_nvp("PrimaryType", primary_type));
+            archive(::cereal::make_nvp("CrossSections", cross_sections));
+        } else {
+            throw std::runtime_error("Process only supports version <= 0!");
+        }
+    };
+};
+
+struct InjectionProcess : Process {
+    std::vector<std::shared_ptr<distributions::InjectionDistribution>> injection_distributions;
+    void AddInjectionDistribution(std::shared_ptr<distributions::InjectionDistribution> dist) {
+      for(auto _dist: injection_distributions) {
+        if((*_dist) == (*dist)) return;
+      }
+      injection_distributions.push_back(dist);
+    }
+    template<class Archive>
+    void serialize(Archive & archive, std::uint32_t const version) {
+        if(version == 0) {
+            archive(::cereal::make_nvp("InjectionDistributions", injection_distributions));
+            archive(cereal::virtual_base_class<Process>(this));
+        } else {
+            throw std::runtime_error("InjectionProcess only supports version <= 0!");
+        }
+    };
+};
+
+struct PhysicalProcess : Process{
+    std::vector<std::shared_ptr<distributions::WeightableDistribution>> physical_distributions;
+    void AddPhysicalDistribution(std::shared_ptr<distributions::WeightableDistribution> dist) {
+      for(auto _dist: physical_distributions) {
+        if((*_dist) == (*dist)) return;
+      }
+      physical_distributions.push_back(dist);
+    }
+    template<class Archive>
+    void serialize(Archive & archive, std::uint32_t const version) {
+        if(version == 0) {
+            archive(::cereal::make_nvp("PhysicalDistributions", physical_distributions));
+            archive(cereal::virtual_base_class<Process>(this));
+        } else {
+            throw std::runtime_error("PhysicalProcess only supports version <= 0!");
+        }
+    };
+};
+
+} // namespace injection
+} // namespace LI
+
+CEREAL_CLASS_VERSION(LI::injection::Process, 0);
+
+#endif // LI_Process_H
