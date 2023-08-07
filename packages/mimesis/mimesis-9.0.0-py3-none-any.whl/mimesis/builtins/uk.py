62b18acaa626f218a34d5ccb712bf662daf7e37a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:54 +0000

    Add package mimesis/9.0.0/mimesis-9.0.0-py3-none-any.whl

---
+++ packages/mimesis/mimesis-9.0.0-py3-none-any.whl/mimesis/builtins/uk.py
@@ -0,0 +1,34 @@
+"""Specific data provider for Ukraine (uk)."""
+import typing as t
+
+from mimesis.builtins.base import BaseSpecProvider
+from mimesis.enums import Gender
+from mimesis.locales import Locale
+from mimesis.types import MissingSeed, Seed
+
+__all__ = ["UkraineSpecProvider"]
+
+
+class UkraineSpecProvider(BaseSpecProvider):
+    """Class that provides special data for Ukraine (uk)."""
+
+    def __init__(self, seed: Seed = MissingSeed) -> None:
+        """Initialize attributes."""
+        super().__init__(locale=Locale.UK, seed=seed)
+        self._load_datafile(self._datafile)
+
+    class Meta:
+        """The name of the provider."""
+
+        name: t.Final[str] = "ukraine_provider"
+
+    def patronymic(self, gender: t.Optional[Gender] = None) -> str:
+        """Generate random patronymic name.
+
+        :param gender: Gender of person.
+        :type gender: str or int
+        :return: Patronymic name.
+        """
+        gender = self.validate_enum(gender, Gender)
+        patronymics: t.List[str] = self._extract(["patronymic", str(gender)])
+        return self.random.choice(patronymics)
