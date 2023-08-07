b9d37bfab51c4a7b6ac21e7f2b9ed2758357fe95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:04:46 +0000

    Add package SwanGPT/0.0.0/SwanGPT-0.0.0.tar.gz

---
+++ packages/SwanGPT/SwanGPT-0.0.0.tar.gz/SwanGPT-0.0.0/tmp/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: cunyue
+@software: PyCharm
+@file: main.py.py
+@time: 2023/5/1 02:05
+@describe:
+hello world
+"""
+
+
+def hello_world():
+    print("hello world")
