8ae135fd2b2ecdf16d62dd70b3336ad2923f50ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:44:21 +0000

    Add package djfuesah/1.0/djfuesah-1.0.tar.gz

---
+++ packages/djfuesah/djfuesah-1.0.tar.gz/djfuesah-1.0/main/file.py
@@ -0,0 +1,4 @@
+def printwtf():
+    print(“what the fuck!”)
+def say():
+    print(“Hello! what the fuck!”)
---
+++ packages/djfuesah/djfuesah-1.0.tar.gz/djfuesah-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'djfuesah',
+    version = '1.0',
+    keywords='ds caa',
+    description = 'a library for DS CAA Developer',
+    license = 'MIT License',
+    author = 'linzhe',
+    author_email = 'geekluca@qq.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
