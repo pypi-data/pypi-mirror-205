d504ad8db02f774ed2cd5dc816a4d5aa28870943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:47:23 +0000

    Add package UzbekTagger/0.0.8/UzbekTagger-0.0.8.tar.gz

---
+++ packages/UzbekTagger/UzbekTagger-0.0.8.tar.gz/UzbekTagger-0.0.8/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="UzbekTagger",
+    version="0.0.8",
+    author="Maksud Sharipov, Ollabergan Yuldashov",
+    author_email="maqsbek72@gmail.com, ollaberganyuldashov@gmail.com",
+    description="Part of Speech Tagger for Uzbek Language.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/MaksudSharipov/UzbekTokenizer",
+    project_urls={
+        "Bug Tracker": "https://https://github.com/MaksudSharipov/UzbekTokenizer/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=['python', 'UzbekTagger', 'uzbek texts', 'Tagger','POS','Part of Speech'],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[],
+    python_requires=">=3.6",
+    include_package_data=True,
+    package_data={"": ["*.xml"]},
+    #package_data={"": ["cyr_exwords.csv", "lat_exwords.csv"],},
+)
