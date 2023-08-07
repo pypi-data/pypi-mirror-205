ad999374b04d950f5272c8ba35e4f5c7c2feae6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:01:35 +0000

    Add package feedwarrior/0.5.5/feedwarrior-0.5.5.tar.gz

---
+++ packages/feedwarrior/feedwarrior-0.5.5.tar.gz/feedwarrior-0.5.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+
+requirements = []
+f = open('requirements.txt', 'r')
+while True:
+    l = f.readline()
+    if l == '':
+        break
+    requirements.append(l.rstrip())
+f.close()
+
+
+setup(
+    packages=[
+        'feedwarrior',
+        'feedwarrior.cmd',
+        'feedwarrior.adapters',
+        'feedwarrior.runnable',
+        ],
+    install_requires=[
+        requirements,
+        ],
+    entry_points = {
+        'console_scripts': [
+            'feedwarrior = feedwarrior.runnable.main:main',
+            ],
+        },
+    data_files=[("man/man1", ["man/man1/feedwarrior.1"],)],
+)
