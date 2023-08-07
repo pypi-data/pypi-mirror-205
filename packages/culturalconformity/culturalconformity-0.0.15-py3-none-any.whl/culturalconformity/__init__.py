6b84fccd574963cf78593bcd15bd23e6d441986f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:01 +0000

    Add package culturalconformity/0.0.15/culturalconformity-0.0.15-py3-none-any.whl

---
+++ packages/culturalconformity/culturalconformity-0.0.15-py3-none-any.whl/culturalconformity/__init__.py
@@ -0,0 +1,9 @@
+from .frq_over_time import frq_over_time
+from .old_interactive import old_interactive
+from .stochastic import stochastic
+from .new_interactive import new_interactive
+from .frqs import frqs
+
+
+
+
