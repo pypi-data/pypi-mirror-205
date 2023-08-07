222f395e79273b5c2a47b9cda067379f89fd6f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:58 +0000

    Add package TIMING-SOLUTION/0.1/TIMING-SOLUTION-0.1.tar.gz

---
+++ packages/TIMING-SOLUTION/TIMING-SOLUTION-0.1.tar.gz/TIMING-SOLUTION-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='TIMING-SOLUTION',
+    version='0.1',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    packages=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
