98a18714d3788e2da5988abae061717c3f64ee24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:04:51 +0000

    Add package icepool/0.29.0/icepool-0.29.0.tar.gz

---
+++ packages/icepool/icepool-0.29.0.tar.gz/icepool-0.29.0/src/icepool/func.py
@@ -0,0 +1,488 @@
+"""Free functions."""
+
+__docformat__ = 'google'
+
+import icepool
+from icepool.typing import Outcome, T, U, guess_star
+
+from collections import defaultdict
+from functools import cache, partial, update_wrapper, wraps
+import itertools
+import math
+
+from typing import Any, Callable, Final, Hashable, Iterable, Iterator, Literal, Sequence, TypeAlias, cast, overload
+
+
+@cache
+def d(sides: int, /) -> 'icepool.Die[int]':
+    """A standard die.
+
+    Specifically, the outcomes are `int`s from `1` to `sides` inclusive,
+    with quantity 1 each.
+
+    Don't confuse this with `icepool.Die()`:
+
+    * `icepool.Die([6])`: A `Die` that always rolls the integer 6.
+    * `icepool.d(6)`: A d6.
+
+    You can also import individual standard dice from the `icepool` module, e.g.
+    `from icepool import d6`.
+    """
+    if not isinstance(sides, int):
+        raise TypeError('Argument to standard() must be an int.')
+    elif sides < 1:
+        raise ValueError('Standard die must have at least one side.')
+    return icepool.Die(range(1, sides + 1))
+
+
+def __getattr__(key: str) -> 'icepool.Die[int]':
+    """Implements the `dX` syntax for standard die with no parentheses.
+
+    For example, `icepool.d6`.
+
+    Note that this behavior can't be imported into the global scope, but the
+    function `d()` can be.
+    """
+    if key[0] == 'd':
+        try:
+            return d(int(key[1:]))
+        except ValueError:
+            pass
+    raise AttributeError(key)
+
+
+def coin(n: int, d: int, /) -> 'icepool.Die[bool]':
+    """A `Die` that rolls `True` with probability `n / d`, and `False` otherwise.
+
+    If `n == 0` or `n == d` the result will have only one outcome.
+    """
+    data = {}
+    if n != d:
+        data[False] = d - n
+    if n != 0:
+        data[True] = n
+    return icepool.Die(data)
+
+
+def one_hot(sides: int, /) -> 'icepool.Die[tuple[bool, ...]]':
+    """A `Die` with `Vector` outcomes with one element set to `True` uniformly at random and the rest `False`.
+
+    This is an easy (if somewhat expensive) way of representing how many dice
+    in a pool rolled each number. For example, the outcomes of `10 @ one_hot(6)`
+    are the `(ones, twos, threes, fours, fives, sixes)` rolled in 10d6.
+    """
+    data = []
+    for i in range(sides):
+        outcome = [False] * sides
+        outcome[i] = True
+        data.append(icepool.Vector(outcome))
+    return icepool.Die(data)
+
+
+def from_cumulative(outcomes: Sequence[T],
+                    cumulative: 'Sequence[int] | Sequence[icepool.Die[bool]]',
+                    *,
+                    reverse: bool = False) -> 'icepool.Die[T]':
+    """Constructs a `Die` from a sequence of cumulative values.
+
+    Args:
+        outcomes: The outcomes of the resulting die. Sorted order is recommended
+            but not necessary.
+        cumulative: The cumulative values (inclusive) of the outcomes in the
+            order they are given to this function. These may be:
+            * `int` cumulative quantities.
+            * Dice representing the cumulative distribution at that point.
+        reverse: Iff true, both of the arguments will be reversed. This allows
+            e.g. constructing using a survival distribution.
+    """
+    if len(outcomes) == 0:
+        return icepool.Die({})
+
+    if reverse:
+        outcomes = list(reversed(outcomes))
+        cumulative = list(reversed(cumulative))  # type: ignore
+
+    prev = 0
+    d = {}
+
+    if isinstance(cumulative[0], icepool.Die):
+        cumulative = commonize_denominator(*cumulative)
+        for outcome, die in zip(outcomes, cumulative):
+            d[outcome] = die.quantity_ne(False) - prev
+            prev = die.quantity_ne(False)
+    elif isinstance(cumulative[0], int):
+        cumulative = cast(Sequence[int], cumulative)
+        for outcome, quantity in zip(outcomes, cumulative):
+            d[outcome] = quantity - prev
+            prev = quantity
+    else:
+        raise TypeError(
+            f'Unsupported type {type(cumulative)} for cumulative values.')
+
+    return icepool.Die(d)
+
+
+@overload
+def from_rv(rv, outcomes: Sequence[int], denominator: int,
+            **kwargs) -> 'icepool.Die[int]':
+    ...
+
+
+@overload
+def from_rv(rv, outcomes: Sequence[float], denominator: int,
+            **kwargs) -> 'icepool.Die[float]':
+    ...
+
+
+def from_rv(rv, outcomes: Sequence[int] | Sequence[float], denominator: int,
+            **kwargs) -> 'icepool.Die[int] | icepool.Die[float]':
+    """Constructs a `Die` from a rv object (as `scipy.stats`).
+    Args:
+        rv: A rv object (as `scipy.stats`).
+        outcomes: An iterable of `int`s or `float`s that will be the outcomes
+            of the resulting `Die`.
+            If the distribution is discrete, outcomes must be `int`s.
+        denominator: The denominator of the resulting `Die` will be set to this.
+        **kwargs: These will be forwarded to `rv.cdf()`.
+    """
+    if hasattr(rv, 'pdf'):
+        # Continuous distributions use midpoints.
+        midpoints = [(a + b) / 2 for a, b in zip(outcomes[:-1], outcomes[1:])]
+        cdf = rv.cdf(midpoints, **kwargs)
+        quantities_le = tuple(
+            int(round(x * denominator)) for x in cdf) + (denominator,)
+    else:
+        cdf = rv.cdf(outcomes, **kwargs)
+        quantities_le = tuple(int(round(x * denominator)) for x in cdf)
+    return from_cumulative(outcomes, quantities_le)
+
+
+def min_outcome(*dice: 'T | icepool.Die[T]') -> T:
+    """The minimum outcome among the dice. """
+    converted_dice = [icepool.implicit_convert_to_die(die) for die in dice]
+    return min(die.outcomes()[0] for die in converted_dice)
+
+
+def max_outcome(*dice: 'T | icepool.Die[T]') -> T:
+    """The maximum outcome among the dice. """
+    converted_dice = [icepool.implicit_convert_to_die(die) for die in dice]
+    return max(die.outcomes()[-1] for die in converted_dice)
+
+
+def align(*dice: 'T | icepool.Die[T]') -> tuple['icepool.Die[T]', ...]:
+    """Pads dice with zero quantities so that all have the same set of outcomes.
+
+    Args:
+        *dice: Any number of dice or single outcomes convertible to dice.
+
+    Returns:
+        A tuple of aligned dice.
+    """
+    converted_dice = [icepool.implicit_convert_to_die(die) for die in dice]
+    outcomes = set(
+        itertools.chain.from_iterable(die.outcomes() for die in converted_dice))
+    return tuple(die.set_outcomes(outcomes) for die in converted_dice)
+
+
+def align_range(
+        *dice: 'int | icepool.Die[int]') -> tuple['icepool.Die[int]', ...]:
+    """Pads dice with zero quantities so that all have the same set of consecutive `int` outcomes.
+
+    Args:
+        *dice: Any number of dice or single outcomes convertible to dice.
+
+    Returns:
+        A tuple of aligned dice.
+    """
+    converted_dice = [icepool.implicit_convert_to_die(die) for die in dice]
+    outcomes = range(icepool.min_outcome(*converted_dice),
+                     icepool.max_outcome(*converted_dice) + 1)
+    return tuple(die.set_outcomes(outcomes) for die in converted_dice)
+
+
+def commonize_denominator(
+        *dice: 'T | icepool.Die[T]') -> tuple['icepool.Die[T]', ...]:
+    """Scale the weights of the dice so that all of them have the same denominator.
+
+    Args:
+        *dice: Any number of dice or single outcomes convertible to dice.
+
+    Returns:
+        A tuple of dice with the same denominator.
+    """
+    converted_dice = [
+        icepool.implicit_convert_to_die(die).simplify() for die in dice
+    ]
+    denominator_lcm = math.lcm(
+        *(die.denominator() for die in converted_dice if die.denominator() > 0))
+    return tuple(
+        die.scale_quantities(denominator_lcm //
+                             die.denominator() if die.denominator() > 0 else 1)
+        for die in converted_dice)
+
+
+def reduce(func: 'Callable[[T, T], T | icepool.Die[T] | icepool.RerollType]',
+           dice: 'Iterable[T | icepool.Die[T]]',
+           *,
+           initial: 'T | icepool.Die[T] | None' = None) -> 'icepool.Die[T]':
+    """Applies a function of two arguments cumulatively to a sequence of dice.
+
+    Analogous to
+    [`functools.reduce()`](https://docs.python.org/3/library/functools.html#functools.reduce).
+
+    Args:
+        func: The function to map. The function should take two arguments,
+            which are an outcome from each of two dice, and produce an outcome
+            of the same type. It may also return `Reroll`, in which case the
+            entire sequence is effectively rerolled.
+        dice: A sequence of dice to map the function to, from left to right.
+        initial: If provided, this will be placed at the front of the sequence
+            of dice.
+        again_depth: Forwarded to the final die constructor.
+        again_end: Forwarded to the final die constructor.
+    """
+    # Conversion to dice is not necessary since map() takes care of that.
+    iter_dice = iter(dice)
+    if initial is not None:
+        result: 'icepool.Die[T]' = icepool.implicit_convert_to_die(initial)
+    else:
+        result = icepool.implicit_convert_to_die(next(iter_dice))
+    for die in iter_dice:
+        result = map(func, result, die)
+    return result
+
+
+def accumulate(
+        func: 'Callable[[T, T], T | icepool.Die[T]]',
+        dice: 'Iterable[T | icepool.Die[T]]',
+        *,
+        initial: 'T | icepool.Die[T] | None' = None
+) -> Iterator['icepool.Die[T]']:
+    """Applies a function of two arguments cumulatively to a sequence of dice, yielding each result in turn.
+
+    Analogous to
+    [`itertools.accumulate()`](https://docs.python.org/3/library/itertools.html#itertools.accumulate)
+    , though with no default function and
+    the same parameter order as `reduce()`.
+
+    The number of results is equal to the number of elements of `dice`, with
+    one additional element if `initial` is provided.
+
+    Args:
+        func: The function to map. The function should take two arguments,
+            which are an outcome from each of two dice.
+        dice: A sequence of dice to map the function to, from left to right.
+        initial: If provided, this will be placed at the front of the sequence
+            of dice.
+    """
+    # Conversion to dice is not necessary since map() takes care of that.
+    iter_dice = iter(dice)
+    if initial is not None:
+        result: 'icepool.Die[T]' = icepool.implicit_convert_to_die(initial)
+    else:
+        try:
+            result = icepool.implicit_convert_to_die(next(iter_dice))
+        except StopIteration:
+            return
+    yield result
+    for die in iter_dice:
+        result = map(func, result, die)
+        yield result
+
+
+def iter_cartesian_product(
+    *args: 'Outcome | icepool.Population | icepool.MultisetExpression'
+) -> Iterator[tuple[tuple, int]]:
+    """Yields the independent joint distribution of the arguments.
+
+    Args:
+        *args: These may be dice, which will be expanded into their joint
+            outcomes. Non-dice are left as-is.
+
+    Yields:
+        Tuples containing one outcome per arg and the joint quantity.
+    """
+
+    def arg_items(arg) -> Sequence[tuple[Any, int]]:
+        if isinstance(arg, icepool.Population):
+            return arg.items()
+        elif isinstance(arg, icepool.MultisetExpression):
+            if arg._free_arity() > 0:
+                raise ValueError('Expression must be fully bound.')
+            # Expression evaluators are difficult to type.
+            return arg.expand().items()  # type: ignore
+        else:
+            return [(arg, 1)]
+
+    for t in itertools.product(*(arg_items(arg) for arg in args)):
+        outcomes, quantities = zip(*t)
+        final_quantity = math.prod(quantities)
+        yield outcomes, final_quantity
+
+
+def map(
+    func:
+    'Callable[..., T | icepool.Die[T] | icepool.RerollType | icepool.AgainExpression]', /,
+    *args: 'Outcome | icepool.Die | icepool.MultisetExpression',
+    star: bool | None = None,
+    again_depth: int = 1,
+    again_end: 'T | icepool.Die[T] | icepool.RerollType | None' = None
+) -> 'icepool.Die[T]':
+    """Applies `func(outcome_of_die_0, outcome_of_die_1, ...)` for all joint outcomes.
+
+    See `map_function` for a decorator version of this.
+
+    Example: `map(lambda a, b: a + b, d6, d6)` is the same as d6 + d6.
+
+    `map()` is flexible but not very efficient for more than a few dice.
+    If at all possible, use `reduce()`, `MultisetExpression` methods, and/or
+    `MultisetEvaluator`s. Even `Pool.expand()` (which sorts rolls) is more
+    efficient than using `map` on the dice in order.
+
+    Args:
+        func: A function that takes one argument per input `Die` and returns an
+            argument to `Die()`.
+        *args: `func` will be called with all joint outcomes of these.
+            Allowed arg types are:
+            * Single outcome.
+            * `Die`. All outcomes will be sent to `func`.
+            * `MultisetExpression`. All sorted tuples of outcomes will be sent
+                to `func`, as `MultisetExpression.expand()`. The expression must
+                be fully bound.
+        star: If `True` and exactly one argument is provided, outcomes will be
+            unpacked before giving them to `func`.
+            If not provided, this will be guessed based on the function
+            signature. Since `map` accepts more than one argument, this is
+            ambiguous in the case of a variadic `func`, so in this case `star`
+            must be specified explicitly.
+        again_depth: Forwarded to the final die constructor.
+        again_end: Forwarded to the final die constructor.
+    """
+    if not callable(func):
+        raise TypeError(
+            'The first argument must be callable. Did you forget to provide a function?'
+        )
+
+    if len(args) == 0:
+        return icepool.Die([func()],
+                           again_depth=again_depth,
+                           again_end=again_end)
+
+    if len(args) == 1:
+        if star is None:
+            star = guess_star(func, variadic='error')
+        if star:
+            func = lambda o: func(*o)
+
+    final_outcomes = []
+    final_quantities = []
+    for outcomes, final_quantity in iter_cartesian_product(*args):
+        final_outcome = func(*outcomes)
+        if final_outcome is not icepool.Reroll:
+            final_outcomes.append(final_outcome)
+            final_quantities.append(final_quantity)
+
+    return icepool.Die(final_outcomes,
+                       final_quantities,
+                       again_depth=again_depth,
+                       again_end=again_end)
+
+
+@overload
+def map_function(
+        func:
+    'Callable[..., T | icepool.Die[T] | icepool.RerollType | icepool.AgainExpression]',
+        /) -> 'Callable[..., icepool.Die[T]]':
+    ...
+
+
+@overload
+def map_function(
+    func: None,
+    /,
+    *,
+    star: bool | None = None,
+    again_depth: int = 1,
+    again_end: 'T | icepool.Die[T] | icepool.RerollType | None' = None
+) -> 'Callable[..., Callable[..., icepool.Die[T]]]':
+    ...
+
+
+@overload
+def map_function(
+    func:
+    'Callable[..., T | icepool.Die[T] | icepool.RerollType | icepool.AgainExpression] | None' = None,
+    /,
+    *,
+    star: bool | None = None,
+    again_depth: int = 1,
+    again_end: 'T | icepool.Die[T] | icepool.RerollType | None' = None
+) -> 'Callable[..., icepool.Die[T]] | Callable[..., Callable[..., icepool.Die[T]]]':
+    ...
+
+
+def map_function(
+    func:
+    'Callable[..., T | icepool.Die[T] | icepool.RerollType | icepool.AgainExpression] | None' = None,
+    /,
+    *,
+    star: bool | None = None,
+    again_depth: int = 1,
+    again_end: 'T | icepool.Die[T] | icepool.RerollType | None' = None
+) -> 'Callable[..., icepool.Die[T]] | Callable[..., Callable[..., icepool.Die[T]]]':
+    """Decorator that turns a function that takes outcomes into a function that takes dice.
+
+    The result must be a `Die`.
+
+    This is basically a decorator version of `map()` and produces behavior
+    similar to AnyDice functions, though Icepool has different typing rules
+    among other differences.
+
+    `map_function` can either be used with no arguments:
+
+    ```
+    @map_function
+    def explode_six(x):
+        if x == 6:
+            return 6 + Again
+        else:
+            return x
+
+    explode_six(d6, again_depth=2)
+    ```
+
+    Or with keyword arguments, in which case the extra arguments are bound:
+
+    ```
+    @map_function(again_depth=2)
+    def explode_six(x):
+        if x == 6:
+            return 6 + Again
+        else:
+            return x
+
+    explode_six(d6)
+    ```
+
+    Args:
+        again_depth: Forwarded to the final die constructor.
+        again_end: Forwarded to the final die constructor.
+    """
+
+    if func is not None:
+        return update_wrapper(partial(map, func), func)
+    else:
+
+        def decorator(
+            func:
+            'Callable[..., T | icepool.Die[T] | icepool.RerollType | icepool.AgainExpression]'
+        ) -> 'Callable[..., icepool.Die[T]]':
+
+            return update_wrapper(
+                partial(map,
+                        func,
+                        star = star,
+                        again_depth=again_depth,
+                        again_end=again_end), func)
+
+        return decorator
---
+++ packages/icepool/icepool-0.29.0.tar.gz/icepool-0.29.0/src/icepool/population/keep.py
@@ -0,0 +1,141 @@
+__docformat__ = 'google'
+
+import icepool
+
+import math
+
+from icepool.typing import Outcome, T
+from typing import Literal, cast
+
+
+def lowest(*dice: 'T | icepool.Die[T]',
+           keep: int = 1,
+           drop: int = 0) -> 'icepool.Die[T]':
+    """The lowest outcome among the rolls, or the sum of some of the lowest.
+
+    The outcomes should support addition and multiplication if `keep != 1`.
+
+    Args:
+        *dice: The dice to be considered. At least one `Die` must be provided.
+        keep: The number of lowest dice will be summed.
+        drop: This number of lowest dice will be dropped before keeping dice
+            to be summed.
+    """
+    if keep < 0:
+        raise ValueError(f'keep={keep} cannot be negative.')
+    if drop < 0:
+        raise ValueError(f'drop={drop} cannot be negative.')
+
+    start = min(drop, len(dice))
+    stop = min(keep + drop, len(dice))
+    return _sum_slice(*dice, start=start, stop=stop)
+
+
+def highest(*dice: 'T | icepool.Die[T]',
+            keep: int = 1,
+            drop: int = 0) -> 'icepool.Die[T]':
+    """The highest outcome among the rolls, or the sum of some of the highest.
+
+    The outcomes should support addition and multiplication if `keep != 1`.
+
+    Args:
+        *dice: The dice to be considered. At least one `Die` must be provided.
+        keep: The number of highest dice will be summed.
+        drop: This number of highest dice will be dropped before keeping dice
+            to be summed.
+    """
+    if keep < 0:
+        raise ValueError(f'keep={keep} cannot be negative.')
+    if drop < 0:
+        raise ValueError(f'drop={drop} cannot be negative.')
+
+    start = len(dice) - min(keep + drop, len(dice))
+    stop = len(dice) - min(drop, len(dice))
+    return _sum_slice(*dice, start=start, stop=stop)
+
+
+def middle(*dice: 'T | icepool.Die[T]',
+           keep: int = 1,
+           tie: Literal['error', 'high', 'low'] = 'error') -> 'icepool.Die[T]':
+    """The middle of the outcomes among the rolls, or the sum of some of the middle.
+
+    The outcomes should support addition and multiplication if `keep != 1`.
+
+    Args:
+        keep: The number of outcomes to sum. If this is greater than the
+            current keep_size, all are kept.
+        tie: What to do if `keep` is odd but the current keep_size
+            is even, or vice versa.
+            * 'error' (default): Raises `IndexError`.
+            * 'high': The higher outcome is taken.
+            * 'low': The lower outcome is taken.
+    """
+    # Expression evaluators are difficult to type.
+    return icepool.Pool(dice).middle(keep, tie=tie).sum()  # type: ignore
+
+
+def _sum_slice(*dice, start: int, stop: int) -> 'icepool.Die':
+    """Common code for `lowest` and `highest`.
+
+    Args:
+        *dice: The dice (not converted to `Die` yet).
+        start, stop: The slice `start:stop` will be kept. These will be between
+            0 and len(dice) inclusive.
+    """
+
+    dice = tuple(icepool.implicit_convert_to_die(die) for die in dice)
+
+    if len(dice) == 0:
+        raise ValueError('At least one die must be provided.')
+
+    if any(die.is_empty() for die in dice):
+        return icepool.Die([])
+
+    # The inputs should support addition.
+    if start == stop:
+        return sum(die.zero() for die in dice)  # type: ignore
+
+    if start == 0 and stop == len(dice):
+        return sum(dice)  # type: ignore
+
+    if stop == 1:
+        return _lowest_single(*dice)
+
+    if start == len(dice) - 1:
+        return _highest_single(*dice)
+
+    # Use pool.
+    # Expression evaluators are difficult to type.
+    return icepool.Pool(dice)[start:stop].sum()  # type: ignore
+
+
+def _lowest_single(*args: 'T | icepool.Die[T]') -> 'icepool.Die[T]':
+    """Roll all the dice and take the lowest single one.
+
+    The maximum outcome is equal to the least maximum outcome among all
+    input dice.
+    """
+    dice = tuple(icepool.implicit_convert_to_die(arg) for arg in args)
+    max_outcome = min(die.max_outcome() for die in dice)
+    dice = tuple(die.clip(max_outcome=max_outcome) for die in dice)
+    dice = icepool.align(*dice)
+    quantities_ge = tuple(
+        math.prod(t) for t in zip(*(die.quantities_ge() for die in dice)))
+    return icepool.from_cumulative(dice[0].outcomes(),
+                                   quantities_ge,
+                                   reverse=True)
+
+
+def _highest_single(*args: 'T | icepool.Die[T]') -> 'icepool.Die[T]':
+    """Roll all the dice and take the highest single one.
+
+    The minimum outcome is equal to the greatest minimum outcome among all
+    input dice.
+    """
+    dice = tuple(icepool.implicit_convert_to_die(arg) for arg in args)
+    min_outcome = max(die.min_outcome() for die in dice)
+    dice = tuple(die.clip(min_outcome=min_outcome) for die in dice)
+    dice = icepool.align(*dice)
+    quantities_le = tuple(
+        math.prod(t) for t in zip(*(die.quantities_le() for die in dice)))
+    return icepool.from_cumulative(dice[0].outcomes(), quantities_le)
