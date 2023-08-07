e18ca8722781a518ea5bfaa5648ef80e71650dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:57 +0000

    Add package moethread/1.1.3/moethread-1.1.3.tar.gz

---
+++ packages/moethread/moethread-1.1.3.tar.gz/moethread-1.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+from moethread.version import __version__
+DESCRIPTION = 'Python wrapper for ThreadPoolExecutor to easily multithread resource bound tasks'
+LONG_DESCRIPTION = open('README.md').read()
+# Setting up
+setup(
+    name="moethread",
+    version=__version__,
+    author="mhamdan91 (Hamdan, Muhammad)",
+    author_email="<mhamdan.dev@gmail.com>",
+    url='https://github.com/mhamdan91/moethread',
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'multithreading', 'wrappers', 'decorator', 'pool', 'multitasking',
+              'easy multithreading', 'thread', 'parallel', 'concurrent'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
