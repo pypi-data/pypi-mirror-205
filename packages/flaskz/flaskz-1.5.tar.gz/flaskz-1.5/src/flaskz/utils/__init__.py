6b3073f7f5a754ee879ccaa274bfba2a2a89defa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:06 +0000

    Add package flaskz/1.5/flaskz-1.5.tar.gz

---
+++ packages/flaskz/flaskz-1.5.tar.gz/flaskz-1.5/src/flaskz/utils/__init__.py
@@ -0,0 +1,14 @@
+from ._app import *
+from ._cache import *
+from ._cls import *
+from ._common import *
+from ._timer import *
+#
+from ._magic import *
+from ._func import *
+#
+from ._request_api import *
+#
+from ._request_args import *
+from ._response import *
+
