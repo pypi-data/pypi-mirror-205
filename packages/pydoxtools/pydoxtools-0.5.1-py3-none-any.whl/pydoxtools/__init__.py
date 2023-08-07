39e6271ff53b8a57c6af0ca817b61df784ebb152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:42 +0000

    Add package pydoxtools/0.5.1/pydoxtools-0.5.1-py3-none-any.whl

---
+++ packages/pydoxtools/pydoxtools-0.5.1-py3-none-any.whl/pydoxtools/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.5.1'
+
+import logging
+
+from .document import Document
+from .document_base import Pipeline
+#from .document import DocumentSet
+
+logger = logging.getLogger(__name__)
