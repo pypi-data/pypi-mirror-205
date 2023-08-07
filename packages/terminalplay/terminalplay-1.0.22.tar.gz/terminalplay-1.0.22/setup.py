a565556ad1d99a894b684058b5b4188a47fa7761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:46:19 +0000

    Add package terminalplay/1.0.22/terminalplay-1.0.22.tar.gz

---
+++ packages/terminalplay/terminalplay-1.0.22.tar.gz/terminalplay-1.0.22/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import setuptools
+
+with open("README.rst", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='terminalplay',
+    version='1.0.22',
+    license='MIT',
+    author="Merwin JD",
+    author_email='merwin@fbi.ac',
+    package_dir={'': 'terminalplay/src'},
+    url='https://github.com/merwin-asm/Terminal_Play',
+    keywords='terminalplay, play videos, terminal',
+    packages = setuptools.find_packages(where="terminalplay/src"),
+    install_requires=[
+          'moviepy',
+          'playsound',
+          'rich',
+          'sty',
+          'opencv-python'
+      ],
+      description="Play videos on your terminal; Works on windows as well as linux;The video played will also have color and sound :)",
+          long_description = long_description,
+    long_description_content_type = "text/markdown",
+      platforms= ["windows","linux"],
+    project_urls = {
+        "Bug Tracker": "https://github.com/merwin-asm/Terminal_Play/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+      
+)
