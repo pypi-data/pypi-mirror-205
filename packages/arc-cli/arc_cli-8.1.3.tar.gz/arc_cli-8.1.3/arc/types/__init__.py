df436fe1172c35fa4e70d9331cc693a95beb297c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:06:07 +0000

    Add package arc-cli/8.1.3/arc_cli-8.1.3.tar.gz

---
+++ packages/arc-cli/arc_cli-8.1.3.tar.gz/arc_cli-8.1.3/arc/types/__init__.py
@@ -0,0 +1,15 @@
+"""
+.. include:: ../../wiki/Data-Types.md
+"""
+
+from .aliases import Alias
+from .convert import convert
+from .file import File, Stdin, Stream
+from .network import *
+from .numbers import *
+from .path import *
+from .semver import SemVer
+from .state import State
+from .strings import *
+from .type_info import TypeInfo
+from .users import *
