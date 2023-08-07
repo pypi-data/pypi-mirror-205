7321796920d235fbb2c1159f95a7b63c8355c4d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:01:27 +0000

    Add package elliptic-sdk/0.0.4/elliptic_sdk-0.0.4.tar.gz

---
+++ packages/elliptic-sdk/elliptic_sdk-0.0.4.tar.gz/elliptic_sdk-0.0.4/elliptic_sdk/settings.py
@@ -0,0 +1,18 @@
+"""Elliptic SDK elliptic_sdk/settings.py."""
+from pydantic import BaseSettings, Field
+
+
+class Settings(BaseSettings):
+    """Settings."""
+
+    api_key: str = Field('', env='ELLIPTIC_API_KEY')
+    api_secret: str = Field('', env='ELLIPTIC_API_SECRET')
+
+    class Config:
+        """Config."""
+
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+
+settings = Settings()
