7e24df56b718e87cddfec6d442f718e21a1a342a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:59 +0000

    Add package geotoolkit/0.1.0/geotoolkit-0.1.0.tar.gz

---
+++ packages/geotoolkit/geotoolkit-0.1.0.tar.gz/geotoolkit-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+import setuptools
+
+setup(name='geotoolkit',
+    version='0.1.0',
+    description='Peforming calculations with geographic points',
+    author='Tim Hanewich',
+    url='https://github.com/TimHanewich/geotoolkit',
+    packages=setuptools.find_packages()
+    )
