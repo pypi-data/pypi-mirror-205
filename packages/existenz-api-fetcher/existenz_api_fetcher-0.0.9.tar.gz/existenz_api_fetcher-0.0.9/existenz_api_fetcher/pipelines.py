a05c34a8331044e852f720f9c30edb287f86199d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:01:36 +0000

    Add package existenz-api-fetcher/0.0.9/existenz_api_fetcher-0.0.9.tar.gz

---
+++ packages/existenz-api-fetcher/existenz_api_fetcher-0.0.9.tar.gz/existenz_api_fetcher-0.0.9/existenz_api_fetcher/pipelines.py
@@ -0,0 +1,54 @@
+from functools import reduce
+
+import pandas as pd
+
+
+def preprocess(df):
+    """
+    Batch processes a DataFrame to make it easier to use.
+    Args: DataFrame
+    Return: Processed Dataframe
+    """
+    if df.empty:
+        return None
+    else:
+        df.drop(['result', 'table', '_start', '_stop', '_field', '_measurement', 'loc'], axis=1, inplace=True)
+        df.set_index('_time', inplace=True)  # Set datetime as DataFrame index
+        df.index.names = ['DateTime']
+        df['_value'] = df['_value'].interpolate(method="time")  # Interpolate missing data
+        df.index = df.index.to_series().apply(lambda x: x.date()) # Removes unnecessary time
+        # Converting the index as date
+        df.index = pd.to_datetime(df.index)
+        return df
+
+
+def compute(df):
+    """
+    Generates a dataframe from an existing one.
+    Args: DataFrame
+    Return: New Dataframe
+    """
+    if df is None:
+        return None
+    else:
+        new_df = df.filter(['_value'], axis=1)
+        new_df.index = df.index
+        new_df['_field'] = 'pet'
+        new_df = new_df[["_value"]]
+        return new_df
+
+
+def merge(*args):
+    """
+    Function to merge DataFrames.
+    *Args: Dataframes
+    Returns: DataFrame with DateTime index and all parameters as columns
+    """
+    if len(args) == 1:
+        return args[0]
+    else:
+        df = reduce(lambda left, right: pd.merge(left, right, on=['DateTime'],
+                                                 how='outer'), [d for d in args if d is not None])
+        # Interpolate missing values
+        df = df.astype(float).interpolate(method="time", limit_direction="both")
+        return df
---
+++ packages/existenz-api-fetcher/existenz_api_fetcher-0.0.9.tar.gz/existenz_api_fetcher-0.0.9/existenz_api_fetcher/test.py
@@ -0,0 +1,4 @@
+from existenz_api_fetcher import hydro
+
+df = hydro.height('2119')
+print(df)
