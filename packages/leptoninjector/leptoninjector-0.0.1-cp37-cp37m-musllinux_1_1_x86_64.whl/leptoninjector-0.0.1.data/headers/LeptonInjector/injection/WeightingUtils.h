7b8bd82956b1d57fa0036bf4902db4b57ce64e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:15 +0000

    Add package leptoninjector/0.0.1/leptoninjector-0.0.1-cp37-cp37m-musllinux_1_1_x86_64.whl

---
+++ packages/leptoninjector/leptoninjector-0.0.1-cp37-cp37m-musllinux_1_1_x86_64.whl/leptoninjector-0.0.1.data/headers/LeptonInjector/injection/WeightingUtils.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef LI_WeightingUtils_H
+#define LI_WeightingUtils_H
+
+
+namespace LI {
+namespace injection {
+
+double CrossSectionProbability(std::shared_ptr<LI::detector::EarthModel const>, std::shared_ptr<LI::crosssections::CrossSectionCollection const>, LI::dataclasses::InteractionRecord const &);
+
+} // namespace injection
+} // namespace LI
+
+#endif // LI_WeightingUtils_H
