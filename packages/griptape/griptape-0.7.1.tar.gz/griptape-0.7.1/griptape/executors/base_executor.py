769dc0baacda9f627d59679d61795eaae15403b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:01:43 +0000

    Add package griptape/0.7.1/griptape-0.7.1.tar.gz

---
+++ packages/griptape/griptape-0.7.1.tar.gz/griptape-0.7.1/griptape/executors/base_executor.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+import json
+from typing import TYPE_CHECKING, Union
+import inspect
+import os
+from abc import ABC, abstractmethod
+from griptape.artifacts import BaseArtifact, TextArtifact
+
+if TYPE_CHECKING:
+    from griptape.core import BaseTool
+
+
+class BaseExecutor(ABC):
+    def execute(self, tool_activity: callable, value: BaseArtifact) -> BaseArtifact:
+        result = self.before_execute(tool_activity, value)
+        result = self.executor_result_to_artifact(
+            self.try_execute(tool_activity, result)
+        )
+        result = self.after_execute(tool_activity, result)
+
+        return result
+
+    def before_execute(self, tool_activity: callable, value: BaseArtifact) -> BaseArtifact:
+        for ramps in tool_activity.__self__.ramps.get(tool_activity.config["name"], []):
+            value = ramps.process_input(tool_activity, value)
+
+        return value
+
+    def after_execute(self, tool_activity: callable, value: BaseArtifact) -> BaseArtifact:
+        for ramps in tool_activity.__self__.ramps.get(tool_activity.config["name"], []):
+            value = ramps.process_output(tool_activity, value)
+
+        return value
+
+    def executor_result_to_artifact(self, result: Union[BaseArtifact, str]) -> BaseArtifact:
+        if isinstance(result, BaseArtifact):
+            return result
+        else:
+            try:
+                from griptape.schemas import TextArtifactSchema, ErrorArtifactSchema
+
+                result_dict = json.loads(result)
+
+                if result_dict["type"] == "TextArtifact":
+                    return TextArtifactSchema().load(result_dict)
+                elif result_dict["type"] == "ErrorArtifact":
+                    return ErrorArtifactSchema().load(result_dict)
+                else:
+                    return TextArtifact(result)
+            except Exception as e:
+                return TextArtifact(result)
+
+    @abstractmethod
+    def try_execute(self, tool_activity: callable, value: BaseArtifact) -> Union[BaseArtifact, str]:
+        ...
+
+    def tool_dir(self, tool: BaseTool):
+        class_file = inspect.getfile(tool.__class__)
+
+        return os.path.dirname(os.path.abspath(class_file))
---
+++ packages/griptape/griptape-0.7.1.tar.gz/griptape-0.7.1/griptape/summarizers/prompt_driver_summarizer.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+import logging
+from attr import define, field
+from griptape.utils.text import to_vector_index
+from griptape.utils import J2
+from griptape.drivers import BasePromptDriver
+from griptape.summarizers.base_summarizer import BaseSummarizer
+
+if TYPE_CHECKING:
+    from griptape.memory import Run
+
+
+@define
+class PromptDriverSummarizer(BaseSummarizer):
+    driver: BasePromptDriver = field(kw_only=True)
+
+    def summarize_runs(self, previous_summary: str, runs: list[Run]) -> str:
+        try:
+            if len(runs) > 0:
+                return self.driver.run(
+                    value=J2("prompts/summarize.j2").render(
+                        summary=previous_summary,
+                        runs=runs
+                    )
+                ).value
+            else:
+                return previous_summary
+        except Exception as e:
+            logging.error(f"Error summarizing memory: {type(e).__name__}({e})")
+
+            return previous_summary
+
+    def summarize_text(self, text: str) -> str:
+        return str(
+            to_vector_index(text).query(
+                "Generate a summary. Include URLs in the summary."
+            )
+        ).strip()
