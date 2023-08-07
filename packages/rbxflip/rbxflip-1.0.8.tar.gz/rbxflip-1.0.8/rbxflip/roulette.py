73c58a78c0df02240b683f4d5cb85f724b5c35e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:01:41 +0000

    Add package rbxflip/1.0.8/rbxflip-1.0.8.tar.gz

---
+++ packages/rbxflip/rbxflip-1.0.8.tar.gz/rbxflip-1.0.8/rbxflip/roulette.py
@@ -0,0 +1,16 @@
+import requests
+
+class Roulette:
+    def __init__(self, roulette_data: dict):
+        self.round = roulette_data['round']
+        self.id = roulette_data['id']
+        self.players = roulette_data['players']
+        self.client_seed = roulette_data['clientSeed']
+
+
+    def __str__(self):
+        return f"Round: {self.round}\nID: {self.id}\nPlayers: {self.players}\nClient Seed: {self.client_seed}"
+
+def get_roulette():
+    r = requests.get('https://api.rbxflip.com/games/roulette').json()
+    return Roulette(r)
